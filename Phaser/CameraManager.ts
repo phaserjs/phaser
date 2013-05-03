9da64e6bafaadd791d648705c0074d39c73190cc
Sean <pixelpicosean@gmail.com>
2013-05-03 19:22:04 +0800

    Finish document for AnimationManager, CameraManager, SoundManager and TweenManager class.

--- Phaser/CameraManager.ts
+++ Phaser/CameraManager.ts
@@ -15,6 +15,15 @@ module Phaser {
 
     export class CameraManager {
 
+        /**
+         * CameraManager constructor
+         * This will create a new <code>Camera</code> with position and size.
+         *
+         * @param x         X Position of the created camera.
+         * @param y         y Position of the created camera.
+         * @param width     Width of the created camera.
+         * @param height    Height of the created camera.
+         */
         constructor(game: Game, x: number, y: number, width: number, height: number) {
 
             this._game = game;
@@ -25,24 +34,56 @@ module Phaser {
 
         }
 
+        /**
+         * Local private reference to Game.
+         */
         private _game: Game;
+        /**
+         * Local container for storing camera.
+         */
         private _cameras: Camera[];
+        /**
+         * Local helper stores index of next created camera.
+         */
         private _cameraInstance: number = 0;
 
+        /**
+         * Currently used camera.
+         */
         public current: Camera;
 
+        /**
+         * Get all the cameras.
+         *
+         * @returns {array} An array contains all the cameras.
+         */
         public getAll(): Camera[] {
             return this._cameras;
         }
 
+        /**
+         * Update cameras.
+         */
         public update() {
             this._cameras.forEach((camera) => camera.update());
         }
 
+        /**
+         * Render cameras.
+         */
         public render() {
             this._cameras.forEach((camera) => camera.render());
         }
 
+        /**
+         * Create a new camera with specific position and size.
+         *
+         * @param   x       X position of the new camera.
+         * @param   y       Y position of the new camera.
+         * @param   width   Width of the new camera.
+         * @param   height  Height of the new camera.
+         * @returns {Camera=} The newly created camera object.
+         */
         public addCamera(x: number, y: number, width: number, height: number): Camera {
 
             var newCam: Camera = new Camera(this._game, this._cameraInstance, x, y, width, height);
@@ -55,6 +96,12 @@ module Phaser {
 
         }
 
+        /**
+         * Remove a new camera with its id.
+         *
+         * @param   id ID of the camera you want to remove.
+         * @returns {boolean} True if successfully removed the camera, otherwise return false.
+         */
         public removeCamera(id: number): bool {
 
             for (var c = 0; c < this._cameras.length; c++)
@@ -76,6 +123,9 @@ module Phaser {
 
         }
 
+        /**
+         * Clean up memory.
+         */
         public destroy() {
 
             this._cameras.length = 0;
