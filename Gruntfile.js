7c763c19c1b76d132e78dd4c8f74ee06538bfae9
Florent Cailhol <florent.cailhol@gmail.com>
2013-09-25 17:39:39 +0200

    Improve phaser.js generation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var files = require('./files');
+
 module.exports = function (grunt) {
 
   grunt.loadNpmTasks('assemble');
@@ -6,131 +8,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  var files = {
-    phaser: [
-      'src/Intro.js',
-      'src/pixi/Pixi.js',
-      'src/Phaser.js',
-      'src/utils/Utils.js',
-      'src/pixi/core/Matrix.js',
-      'src/pixi/core/Point.js',
-      'src/pixi/core/Rectangle.js',
-      'src/pixi/display/DisplayObject.js',
-      'src/pixi/display/DisplayObjectContainer.js',
-      'src/pixi/display/Sprite.js',
-      'src/pixi/display/Stage.js',
-      'src/pixi/extras/CustomRenderable.js',
-      'src/pixi/extras/Strip.js',
-      'src/pixi/extras/Rope.js',
-      'src/pixi/extras/TilingSprite.js',
-      'src/pixi/filters/FilterBlock.js',
-      'src/pixi/filters/MaskFilter.js',
-      'src/pixi/primitives/Graphics.js',
-      'src/pixi/renderers/canvas/CanvasGraphics.js',
-      'src/pixi/renderers/canvas/CanvasRenderer.js',
-      'src/pixi/renderers/webgl/WebGLBatch.js',
-      'src/pixi/renderers/webgl/WebGLGraphics.js',
-      'src/pixi/renderers/webgl/WebGLRenderer.js',
-      'src/pixi/renderers/webgl/WebGLRenderGroup.js',
-      'src/pixi/renderers/webgl/WebGLShaders.js',
-      'src/pixi/text/BitmapText.js',
-      'src/pixi/text/Text.js',
-      'src/pixi/textures/BaseTexture.js',
-      'src/pixi/textures/Texture.js',
-      'src/pixi/textures/RenderTexture.js',
-      'src/pixi/utils/EventTarget.js',
-      'src/pixi/utils/Polyk.js',
-      'src/core/Camera.js',
-      'src/core/State.js',
-      'src/core/StateManager.js',
-      'src/core/LinkedList.js',
-      'src/core/Signal.js',
-      'src/core/SignalBinding.js',
-      'src/core/Plugin.js',
-      'src/core/PluginManager.js',
-      'src/core/Stage.js',
-      'src/core/Group.js',
-      'src/core/World.js',
-      'src/core/Game.js',
-      'src/input/Input.js',
-      'src/input/Keyboard.js',
-      'src/input/Mouse.js',
-      'src/input/MSPointer.js',
-      'src/input/Pointer.js',
-      'src/input/Touch.js',
-      'src/input/InputHandler.js',
-      'src/gameobjects/Events.js',
-      'src/gameobjects/GameObjectFactory.js',
-      'src/gameobjects/Sprite.js',
-      'src/gameobjects/TileSprite.js',
-      'src/gameobjects/Text.js',
-      'src/gameobjects/BitmapText.js',
-      'src/gameobjects/Button.js',
-      'src/gameobjects/Graphics.js',
-      'src/gameobjects/RenderTexture.js',
-      'src/system/Canvas.js',
-      'src/system/StageScaleMode.js',
-      'src/system/Device.js',
-      'src/system/RequestAnimationFrame.js',
-      'src/math/RandomDataGenerator.js',
-      'src/math/Math.js',
-      'src/math/QuadTree.js',
-      'src/geom/Circle.js',
-      'src/geom/Point.js',
-      'src/geom/Rectangle.js',
-      'src/net/Net.js',
-      'src/tween/TweenManager.js',
-      'src/tween/Tween.js',
-      'src/tween/Easing.js',
-      'src/time/Time.js',
-      'src/animation/AnimationManager.js',
-      'src/animation/Animation.js',
-      'src/animation/Frame.js',
-      'src/animation/FrameData.js',
-      'src/animation/Parser.js',
-      'src/loader/Cache.js',
-      'src/loader/Loader.js',
-      'src/loader/Parser.js',
-      'src/sound/Sound.js',
-      'src/sound/SoundManager.js',
-      'src/utils/Debug.js',
-      'src/utils/Color.js',
-      'src/physics/arcade/ArcadePhysics.js',
-      'src/physics/arcade/Body.js',
-      'src/particles/Particles.js',
-      'src/particles/arcade/ArcadeParticles.js',
-      'src/particles/arcade/Emitter.js',
-      'src/tilemap/Tilemap.js',
-      'src/tilemap/TilemapLayer.js',
-      'src/tilemap/Tile.js',
-      'src/tilemap/TilemapRenderer.js',
-    ],
-    physics: [
-      'src/physics/advanced/Math.js',
-      'src/physics/advanced/Util.js',
-      'src/physics/advanced/Collision.js',
-      'src/physics/advanced/Body.js',
-      'src/physics/advanced/Joint.js',
-      'src/physics/advanced/Shape.js',
-      'src/physics/advanced/Contact.js',
-      'src/physics/advanced/ContactSolver.js',
-      'src/physics/advanced/Space.js',
-      'src/physics/advanced/joints/Angle.js',
-      'src/physics/advanced/joints/Revolute.js',
-      'src/physics/advanced/joints/Weld.js',
-      'src/physics/advanced/joints/Wheel.js',
-      'src/physics/advanced/joints/Prismatic.js',
-      'src/physics/advanced/joints/Distance.js',
-      'src/physics/advanced/joints/Rope.js',
-      'src/physics/advanced/joints/Mouse.js',
-      'src/physics/advanced/shapes/Circle.js',
-      'src/physics/advanced/shapes/Segment.js',
-      'src/physics/advanced/shapes/Poly.js',
-      'src/physics/advanced/shapes/Triangle.js',
-      'src/physics/advanced/shapes/Box.js',
-    ]
-  }
+  grunt.loadTasks('lib/grunt');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -144,19 +22,15 @@ module.exports = function (grunt) {
     },
     concat: {
       phaser: {
-        options: {
-          process: function(src, filepath) {
-            if (filepath == 'src/Intro.js') {
-              var tmp = grunt.file.read('src/Phaser.js');
-              var version = tmp.substr(tmp.indexOf('VERSION:') + 10, 5);
-              return src.replace('{buildDate}', (new Date()).toUTCString()).replace('{version}', version);
-            }
-
-            return src;
-          }
-        },
         files: {
           'build/phaser.js': files.phaser,
+        }
+      }
+    },
+    process: {
+      phaser: {
+        files: {
+          'build/phaser.js': 'build/phaser.js'
         }
       }
     },
@@ -219,7 +93,9 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('default', ['concat:phaser', 'uglify:phaser']);
-  grunt.registerTask('examples', ['clean:examples', 'assemble:examples', 'copy:examples'])
+  grunt.registerTask('build', ['concat:phaser', 'process:phaser', 'uglify:phaser']);
+  grunt.registerTask('examples', ['build', 'clean:examples', 'assemble:examples', 'copy:examples']);
+
+  grunt.registerTask('default', ['build']);
 
 };
