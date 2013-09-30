90b5f098830d2d9dde32656078b87524ce449d27
Iain Carsberg <horrain@gmail.com>
2013-09-30 09:27:12 +0100

    Added a UMD wrapper

--- build/build.php
+++ build/build.php
@@ -7,6 +7,7 @@ date_default_timezone_set('Europe/London');
 $vf = file_get_contents(dirname(__FILE__) . '/../src/Phaser.js');
 $version = substr($vf, strpos($vf, 'VERSION: ') + 10, 5);
 $buildLog = "Building version $version \n\n";
+$header = "";
 
 $js = file(dirname(__FILE__) . '/../examples/js.php');
 $output = "";
@@ -35,13 +36,33 @@ for ($i = 0; $i < count($js); $i++)
 
 			//	{version}
 			$source = str_replace('{version}', $version, $source);
-		}
 
-		$output .= $source . "\n";
+			// Set the header
+			$header = $source;
+			
+		} else {
+			$output .= $source . "\n";
+		}
 	}
 }
 
-file_put_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'phaser.js', $output);
+// Create a UMD wrapper, to allow Phaser to be exposed to AMD, Node and Browsers
+$template = <<<EOT
+%s(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.Phaser = factory();
+    }
+}(this, function (b) {
+%s
+    return Phaser;
+}));
+EOT;
+
+file_put_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'phaser.js', sprintf($template, $header, $output));
 
 if(php_sapi_name() == 'cli' || empty($_SERVER['REMOTE_ADDR'])) {
   echo "\ndone\n\n";
