9b212a4f196a3f43780a656e89a387c461b8744d
Iain Carsberg <horrain@gmail.com>
2013-09-30 08:32:46 +0100

    Minor changes so it can be built from the command line
    From the root directory entering 'php build/build.php' will concatinate
    all of the files into build/phaser.js

--- build/build.php
+++ build/build.php
@@ -1,50 +1,56 @@
+<?php
+
+date_default_timezone_set('Europe/London');
+
+//	Get the version number
+//	VERSION: '1.0.5', 
+$vf = file_get_contents(dirname(__FILE__) . '/../src/Phaser.js');
+$version = substr($vf, strpos($vf, 'VERSION: ') + 10, 5);
+$buildLog = "Building version $version \n\n";
+
+$js = file(dirname(__FILE__) . '/../examples/js.php');
+$output = "";
+
+for ($i = 0; $i < count($js); $i++)
+{
+	//	<script src="../src/Phaser.js"></script>		
+	$line = trim($js[$i]);
+
+	if (strpos($line, '<script') !== false)
+	{
+		$line = str_replace('<script src="', '', $line);
+		$line = dirname(__FILE__) . DIRECTORY_SEPARATOR . str_replace('"></script>', '', $line);
+		$filename = substr($line, strrpos($line, '/') + 1);
+
+		$buildLog .= $line . "\n";
+		// echo $filename . "\n";
+
+		//	Read the file in
+		$source = file_get_contents($line);
+
+		if ($filename == 'Intro.js')
+		{
+			//	Built at: {buildDate}
+			$source = str_replace('{buildDate}', date('r'), $source);
+
+			//	{version}
+			$source = str_replace('{version}', $version, $source);
+		}
+
+		$output .= $source . "\n";
+	}
+}
+
+file_put_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'phaser.js', $output);
+
+if(php_sapi_name() == 'cli' || empty($_SERVER['REMOTE_ADDR'])) {
+  echo "\ndone\n\n";
+  return;
+}
+
+?>
 <a href="http://yui.2clics.net/">Minify it</a><br />
 
 <textarea style="width: 800px; height: 800px">
-<?php
-	//	Get the version number
-	//	VERSION: '1.0.5', 
-	$vf = file_get_contents('../src/Phaser.js');
-	$version = substr($vf, strpos($vf, 'VERSION: ') + 10, 5);
-
-	echo "Building version $version \n\n";
-
-	$js = file('../examples/js.php');
-	$output = "";
-
-	for ($i = 0; $i < count($js); $i++)
-	{
-		//	<script src="../src/Phaser.js"></script>		
-		$line = trim($js[$i]);
-
-		if (strpos($line, '<script') !== false)
-		{
-			$line = str_replace('<script src="', '', $line);
-			$line = str_replace('"></script>', '', $line);
-			$filename = substr($line, strrpos($line, '/') + 1);
-
-			echo $line . "\n";
-			// echo $filename . "\n";
-
-			//	Read the file in
-			$source = file_get_contents($line);
-
-			if ($filename == 'Intro.js')
-			{
-				//	Built at: {buildDate}
-				$source = str_replace('{buildDate}', date('r'), $source);
-
-				//	{version}
-				$source = str_replace('{version}', $version, $source);
-			}
-
-			$output .= $source . "\n";
-		}
-	}
-
-	//echo $output;
-
-	file_put_contents('phaser.js', $output);
-
-?>
+<?php echo $buildLog; ?>
 </textarea>
