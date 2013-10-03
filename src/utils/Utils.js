c8760b43411fd5b48388fb1ebaba85a24369c8ba
Richard Davey <rdavey@gmail.com>
2013-10-03 04:17:44 +0100

    Docs update.

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -96,6 +96,8 @@ Phaser.Utils = {
     /**
     * This is a slightly modified version of http://api.jquery.com/jQuery.extend/
     * @method Phaser.Utils.extend
+    * @param {boolean} deep - Perform a deep copy?
+    * @param {object} target - The target object to copy to.
     * @return {object} The extended object.
     */
 	extend: function () {
