35dd98c72f3d46e86000f24f3b040cc86b1b705f
Román Jiménez <drrouman@gmail.com>
2013-11-05 20:31:33 +0100

    Added some docstring to the Polygon class

--- src/geom/Polygon.js
+++ src/geom/Polygon.js
@@ -1,3 +1,14 @@
+/**
+* Creates a new Polygon. You have to provide a list of points
+* @class Phaser.Polygon
+* @classdesc The polygon represents a list of orderded points in space
+* @constructor
+* @param points* {Array<Point>|Array<Number>|Point...|Number...} This can be an array of Points that form the polygon,
+*      a flat array of numbers that will be interpreted as [x,y, x,y, ...], or the arugments passed can be
+*      all the points of the polygon e.g. `new PIXI.Polygon(new PIXI.Point(), new PIXI.Point(), ...)`, or the
+*      arguments passed can be flat x,y values e.g. `new PIXI.Polygon(x,y, x,y, x,y, ...)` where `x` and `y` are
+*      Numbers.
+**/
 Phaser.Polygon = function (points) {
 
     PIXI.Polygon.call(this, points);
