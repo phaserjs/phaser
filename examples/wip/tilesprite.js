5bbb7e390b4b1d15389f79db5e4e437b325707fa
photonstorm <rdavey@gmail.com>
2014-02-13 15:04:01 +0000

    Tilesprite test.

---
+++ examples/wip/tilesprite.js
@@ -0,0 +1,43 @@
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+
+var tilesprite;
+var cursors;
+
+function preload() {
+
+    game.load.image('starfield', 'assets/misc/starfield.jpg');
+
+}
+
+function create() {
+
+    // tilesprite = game.add.tileSprite(0, 0, 800, 600, 'starfield');
+
+    tilesprite = game.add.tileSprite(100, 100, 400, 300, 'starfield');
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+}
+
+function update() {
+
+    if (cursors.left.isDown)
+    {
+        tilesprite.tilePosition.x += 8;
+    }
+    else if (cursors.right.isDown)
+    {
+        tilesprite.tilePosition.x -= 8;
+    }
+
+    if (cursors.up.isDown)
+    {
+        tilesprite.tilePosition.y += 8;
+    }
+    else if (cursors.down.isDown)
+    {
+        tilesprite.tilePosition.y -= 8;
+    }
+
+}
