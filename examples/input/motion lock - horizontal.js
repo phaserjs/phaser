b03c6231e1531d7ce4cf662c50a66a9f9f6f1351
alvinsight <alvinsight@gmail.com>
2014-02-21 17:58:52 +0000

    And some more examples updated :)

--- examples/input/motion lock - horizontal.js
+++ examples/input/motion lock - horizontal.js
@@ -18,7 +18,7 @@ function create() {
 
     //  Enable Input detection. Sprites have this disabled by default, 
     //  so you have to start it if you want to interact with them.
-    sprite.input.start(0,true);
+    sprite.inputEnabled = true;
 
     //  This allows you to drag the sprite. The parameter controls if you drag from the position you touched it (false)
     //  or if it will snap to the center (true)
