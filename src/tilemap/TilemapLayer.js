539d70d58139cb00520249ca2e6e28914215fc8f
photonstorm <rdavey@gmail.com>
2014-03-22 08:32:33 +0000

    Testing multi layer blank map updates.

--- src/tilemap/TilemapLayer.js
+++ src/tilemap/TilemapLayer.js
@@ -306,11 +306,11 @@ Phaser.TilemapLayer.prototype.postUpdate = function () {
     this.render();
 
     //  Fixed to Camera?
-    // if (this._cache[7] === 1)
-    // {
+    if (this._cache[7] === 1)
+    {
         this.position.x = (this.game.camera.view.x + this.cameraOffset.x) / this.game.camera.scale.x;
         this.position.y = (this.game.camera.view.y + this.cameraOffset.y) / this.game.camera.scale.y;
-    // }
+    }
 
     //  Update any Children
     // for (var i = 0, len = this.children.length; i < len; i++)
