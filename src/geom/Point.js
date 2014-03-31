14b16fb583e2dfd728a7b529014846a44306354c
Lewis Lane <septimus@rotates.org>
2014-03-31 16:01:35 +0100

    Update Point.js

--- src/geom/Point.js
+++ src/geom/Point.js
@@ -462,13 +462,6 @@ Phaser.Point.rotate = function (a, x, y, angle, asDegrees, distance) {
 * @param {Phaser.Point} [out] - Optional Point to store the value in, if not supplied a new Point object will be created.
 * @return {Phaser.Point} The new Point object.
 */
-/**
-* Calculates centroid (or midpoint) from an array of points. If only one point is provided, that point is returned.
-* @method Phaser.Point.centroid
-* @param {Phaser.Point[]} points - The array of one or more points.
-* @param {Phaser.Point} [out] - Optional Point to store the value in, if not supplied a new Point object will be created.
-* @return {Phaser.Point} The new Point object.
-*/
 Phaser.Point.centroid = function (points, out) {
 
     if (typeof out === "undefined") { out = new Phaser.Point(); }
