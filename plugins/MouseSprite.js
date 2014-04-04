8416dd920eacac845666af2fbfaefb9d69233af3
jdowell <jeremy@codevinsky.com>
2014-04-04 00:53:08 -0500

    Fixed jshint errors

--- plugins/MouseSprite.js
+++ plugins/MouseSprite.js
@@ -4,13 +4,13 @@
  * @param {parent} parent The parent object
  */
 Phaser.Plugin.MouseSprite = function (game, parent) {
-  Phaser.Plugin.call(this, game, parent);
-  // sets the game element's cursor style to none
-  document.getElementById(game.parent).style.cursor = 'none';
+    Phaser.Plugin.call(this, game, parent);
+    // sets the game element's cursor style to none
+    document.getElementById(game.parent).style.cursor = 'none';
 
-  // setup the mouse listener
-  this.game.input.mouse.mouseMoveCallback = this.updateLocation;
-  this.game.input.mouse.callbackContext = this;
+    // setup the mouse listener
+    this.game.input.mouse.mouseMoveCallback = this.updateLocation;
+    this.game.input.mouse.callbackContext = this;
 };
 
 //  Extends the Phaser.Plugin template, setting up values we need
@@ -22,14 +22,14 @@ Phaser.Plugin.MouseSprite.prototype.constructor = Phaser.Plugin.MouseSprite;
 * @type {Phaser.Sprite}
 */
 Phaser.Plugin.MouseSprite.prototype.setSprite = function(sprite) {
-  this.sprite = sprite;
-  this.sprite.anchor.setTo(0.5, 0.5);
+    this.sprite = sprite;
+    this.sprite.anchor.setTo(0.5, 0.5);
 };
 
 /**
 * This is run when a mouseMove event is detected
 */
 Phaser.Plugin.MouseSprite.prototype.updateLocation = function() {
-  this.sprite.x = this.game.input.mouse.x;
-  this.sprite.y = this.game.input.mouse.y;
+    this.sprite.x = this.game.input.mouse.x;
+    this.sprite.y = this.game.input.mouse.y;
 };
