185278e311531d4012b006facb1c1f89bb3a0347
adamholdenyall <adammholden@gmail.com>
2014-05-03 17:40:50 -0500

    Update phaser.d.ts

--- build/phaser.d.ts
+++ build/phaser.d.ts
@@ -1079,6 +1079,7 @@ declare module Phaser {
 
         constructor(sprite: Phaser.Sprite);
 
+        currentAnim : Phaser.Animation;
         currentFrame: Phaser.Frame;
         frame: number;
         frameData: Phaser.FrameData;
