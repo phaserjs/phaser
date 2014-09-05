22df587bd39d01ffc013d075a44b60690a608cce
photonstorm <rdavey@gmail.com>
2014-09-05 15:46:21 +0100

    Swapped to using FlexLayers.

--- src/core/FlexGrid.js
+++ src/core/FlexGrid.js
@@ -78,43 +78,70 @@ Phaser.FlexGrid.prototype = {
 
     //  Need ability to create your own layers with custom scaling, etc.
 
-    createFluidLayer: function () {
+    /**
+     * A fluid layer is centered on the game and maintains its aspect ratio as it scales up and down.
+     *
+     * @method createFluidLayer
+     * @return {Phaser.FlexLayer} The Layer object.
+     */
+    createFluidLayer: function (children) {
 
-        var layer = this.game.add.group();
+        var layer = new Phaser.FlexLayer(this, this.positionFluid, this.boundsFluid, this.scaleFluid);
 
-        //  Override the position and scale so they point to our objects instead, this will keep them matched
-        layer.position = this.positionFluid;
-        layer.scale = this.scaleFluid;
+        this.game.world.add(layer);
 
         this.layers.push(layer);
+
+        if (typeof children !== 'undefined')
+        {
+            layer.addMultiple(children);
+        }
 
         return layer;
 
     },
 
-    createFullLayer: function () {
+    /**
+     * A full layer is placed at 0,0 and extends to the full size of the game. Children are scaled according to the fluid ratios.
+     *
+     * @method createFullLayer
+     * @return {Phaser.FlexLayer} The Layer object.
+     */
+    createFullLayer: function (children) {
 
-        var layer = this.game.add.group();
+        var layer = new Phaser.FlexLayer(this, this.positionFull, this.boundsFull, this.scaleFluid);
 
-        //  Override the position and scale so they point to our objects instead, this will keep them matched
-        layer.position = this.positionFull;
-        layer.scale = this.scaleFull;
+        this.game.world.add(layer);
 
         this.layers.push(layer);
+
+        if (typeof children !== 'undefined')
+        {
+            layer.addMultiple(children);
+        }
 
         return layer;
 
     },
 
-    createFixedLayer: function () {
+    /**
+     * A fixed layer is centered on the game and is the size of the required dimensions and is never scaled.
+     *
+     * @method createFixedLayer
+     * @return {Phaser.FlexLayer} The Layer object.
+     */
+    createFixedLayer: function (children) {
 
-        var layer = this.game.add.group();
+        var layer = new Phaser.FlexLayer(this, this.positionNone, this.boundsNone, this.scaleNone);
 
-        //  Override the position and scale so they point to our objects instead, this will keep them matched
-        layer.position = this.positionNone;
-        layer.scale = this.scaleNone;
+        this.game.world.add(layer);
 
         this.layers.push(layer);
+
+        if (typeof children !== 'undefined')
+        {
+            layer.addMultiple(children);
+        }
 
         return layer;
 
@@ -141,13 +168,17 @@ Phaser.FlexGrid.prototype = {
 
     refresh: function () {
 
+        // console.log(this.manager.bounds);
+
+        this.boundsFull = this.manager.bounds;
+
         this.multiplier = Math.min((this.manager.height / this.height), (this.manager.width / this.width));
 
         this.boundsFluid.width = Math.round(this.width * this.multiplier);
         this.boundsFluid.height = Math.round(this.height * this.multiplier);
 
         this.scaleFluid.set(this.boundsFluid.width / this.width, this.boundsFluid.height / this.height);
-        this.scaleFull.set(this.boundsFull.width / this.width, this.boundsFull.height / this.height);
+        // this.scaleFull.set(this.boundsFull.width / this.width, this.boundsFull.height / this.height);
 
         this.boundsFluid.centerOn(this.manager.bounds.centerX, this.manager.bounds.centerY);
         this.boundsNone.centerOn(this.manager.bounds.centerX, this.manager.bounds.centerY);
@@ -158,6 +189,14 @@ Phaser.FlexGrid.prototype = {
     },
 
     debug: function () {
+
+        // for (var i = 0; i < this.layers.length; i++)
+        // {
+        //     this.layers[i].debug();
+        // }
+
+        this.game.debug.text(this.boundsFull.width + ' x ' + this.boundsFull.height, this.boundsFull.x + 4, this.boundsFull.y + 16);
+        this.game.debug.geom(this.boundsFull, 'rgba(0,0,255,0.9', false);
 
         this.game.debug.text(this.boundsFluid.width + ' x ' + this.boundsFluid.height, this.boundsFluid.x + 4, this.boundsFluid.y + 16);
         this.game.debug.geom(this.boundsFluid, 'rgba(255,0,0,0.9', false);
