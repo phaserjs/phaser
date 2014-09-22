c6231ddd382f1f506cbcd1b5c0aaafe562720ae7
Richard Davey <rdavey@gmail.com>
2014-09-22 11:32:26 +0100

    jsdoc fix

--- src/particles/arcade/ArcadeParticles.js
+++ src/particles/arcade/ArcadeParticles.js
@@ -1,1 +1,12 @@
+/**
+* @author       Richard Davey <rich@photonstorm.com>
+* @copyright    2014 Photon Storm Ltd.
+* @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+*/
+
+/**
+* Arcade Particles is a Particle System integrated with Arcade Physics.
+*
+* @class Phaser.Particles.Arcade
+*/
 Phaser.Particles.Arcade = {};
