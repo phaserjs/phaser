bfb39a286412f4cf7f56de4a4c1155c3416c6b09
Lewis Lane <septimus@rotates.org>
2014-10-31 11:52:05 +0000

    Added removeFrom method
    Allows the removal of all Tweens from a specific object, array of objects or group of objects.

--- src/tween/TweenManager.js
+++ src/tween/TweenManager.js
@@ -68,6 +68,43 @@ Phaser.TweenManager.prototype = {
         this._add = [];
 
     },
+    
+    /**
+    * Remove all tweens from a specific object, array of objects or group.
+    * @method Phaser.TweenManager#removeFrom
+    * @param {object|object[]|Phaser.Group} obj - The object you want to remove the tweens from.
+    * @param {boolean} children - If passing a group, setting this to true will remove the tweens from all of its children instead of the group itself.
+    */
+    removeFrom = function(obj, children) {
+        
+        var o, c, t, len;
+        
+        if (Array.isArray(obj) )
+        {
+            for (o = 0, len = obj.length; o < len; o++)
+            {
+                this.removeFrom(obj[o]);   
+            }
+        }
+        else if (obj.type === Phaser.GROUP && children)
+        {
+            for (c = 0, len = obj.children.length; c < len; c++)
+            {
+                this.removeFrom(obj.children[c]);   
+            }
+        }
+        else
+        {
+            for (t = 0, len = this._tweens.length; t < len; t++)
+            {
+                if (obj === this._tweens[t]._object)
+                {
+                    this.remove(this._tweens[t]);
+                }
+            }
+        }
+        
+    },
 
     /**
     * Add a new tween into the TweenManager.
