d37fdd4217fa3921df24aa62a6946df1e5aa16a8
photonstorm <rdavey@gmail.com>
2014-11-11 01:07:52 +0000

    Latest Pixi dev release.

--- src/pixi/primitives/Graphics.js
+++ src/pixi/primitives/Graphics.js
@@ -799,6 +799,9 @@ PIXI.Graphics.prototype._renderCanvas = function(renderSession)
  */
 PIXI.Graphics.prototype.getBounds = function( matrix )
 {
+    // return an empty object if the item is a mask!
+    if(this.isMask)return PIXI.EmptyRectangle;
+
     if(this.dirty)
     {
         this.updateBounds();
@@ -891,7 +894,7 @@ PIXI.Graphics.prototype.updateBounds = function()
             shape = data.shape;
            
 
-            if(type === PIXI.Graphics.RECT || type === PIXI.Graphics.RRECT)
+            if(type === PIXI.Graphics.RECT || type === PIXI.Graphics.RREC)
             {
                 x = shape.x - lineWidth/2;
                 y = shape.y - lineWidth/2;
