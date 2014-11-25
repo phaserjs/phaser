0d9678e512f9037b686ca480907c90f82d5b631e
photonstorm <rdavey@gmail.com>
2014-11-25 02:50:39 +0000

    Promoted Game time loop count to protected public var. Checked in Sprite.lifespan decrement to avoid over-decreasing the lifespan (#1358)

--- src/gameobjects/Sprite.js
+++ src/gameobjects/Sprite.js
@@ -246,7 +246,8 @@ Phaser.Sprite.prototype.preUpdate = function() {
         return false;
     }
 
-    if (this.lifespan > 0)
+    //  Only apply lifespan decrement in the first updateLogic pass.
+    if (this.lifespan > 0 && this.game.count === 0)
     {
         this.lifespan -= this.game.time.elapsedMS;
 
