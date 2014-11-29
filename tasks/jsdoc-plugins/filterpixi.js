4029d536b08ba3f34b2246e111de1a1baf64d5b4
Paul <pstickne@gmail.com>
2014-11-29 03:45:32 -0800

    Pixi documentation filtering
    Added the filterpixi.js jsdoc plugin which marks some PIXI
    interaction-related methods and properties as private. This ensures that
    they do not show up in the final Phaser documentation.

---
+++ tasks/jsdoc-plugins/filterpixi.js
@@ -0,0 +1,45 @@
+/**
+* Mark various PIXI properties/methods as internal if they are not relevant to Phaser. 
+*/
+
+var path = require('path');
+
+function docletParamsAcceptInteractionData (doclet) {
+
+    if (Array.isArray(doclet.params)) {
+        return doclet.params.some(function (p) {
+            return p.type && p.type.names.some(function (n) {
+                return n === 'PIXI.InteractionData';
+            });
+        });
+    }
+
+}
+
+var unwantedNames = {
+    'PIXI.DisplayObject#defaultCursor': 1,
+    'PIXI.DisplayObject#interactive' : 1
+};
+
+function hasUnwantedName (doclet) {
+
+    var longname = doclet.longname;
+    return unwantedNames[longname];
+
+}
+
+exports.handlers = {};
+exports.handlers.newDoclet = function (e) {
+
+    var doclet = e.doclet;
+
+    if (docletParamsAcceptInteractionData(doclet))
+    {
+        doclet.access = 'private';
+    }
+    else if (hasUnwantedName(doclet))
+    {
+        doclet.access = 'private';
+    }
+
+};
