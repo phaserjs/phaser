4ad6df9a29137ffce297b99aab4e80d314a270af
photonstorm <rdavey@gmail.com>
2014-12-09 23:38:23 +0000

    A test to try for #1424

--- src/gameobjects/Sprite.js
+++ src/gameobjects/Sprite.js
@@ -365,6 +365,8 @@ Phaser.Sprite.prototype.postUpdate = function() {
         this.position.y = (this.game.camera.view.y + this.cameraOffset.y) / this.game.camera.scale.y;
     }
 
+    this.updateTransform();
+
     //  Update any Children
     for (var i = 0, len = this.children.length; i < len; i++)
     {
