11889cd6b5cea768dc0458c4e62bbd88b2a34d0a
photonstorm <rdavey@gmail.com>
2015-02-17 16:39:40 +0000

    Added the Graphics consts.

--- src/PixiDefaults.js
+++ src/PixiDefaults.js
@@ -31,3 +31,12 @@ if (PIXI.RenderTexture.tempMatrix === undefined)
 {
     PIXI.RenderTexture.tempMatrix = new PIXI.Matrix();
 }
+
+if (PIXI.Graphics.POLY === undefined)
+{
+    PIXI.Graphics.POLY = Phaser.POLYGON;
+    PIXI.Graphics.RECT = Phaser.RECTANGLE;
+    PIXI.Graphics.CIRC = Phaser.CIRCLE;
+    PIXI.Graphics.ELIP = Phaser.ELLIPSE;
+    PIXI.Graphics.RREC = Phaser.ROUNDEDRECTANGLE;
+}
