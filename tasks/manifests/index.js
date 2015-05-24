de8dbe68ff7249c1bfab6f6a4034b3934f871f6b
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-05-24 16:23:01 -0300

    Adding the manifests `index.js` file.

---
+++ tasks/manifests/index.js
@@ -0,0 +1,46 @@
+/*
+ *
+ */
+
+'use strict';
+
+
+module.exports = {
+
+    'pixi'         : require('./pixi.json'),
+    'intro'        : require('./intro.json'),
+    'phaser'       : require('./phaser.json'),
+    'geom'         : require('./geom.json'),
+    'core'         : require('./core.json'),
+    'input'        : require('./input.json'),
+    'gamepad'      : require('./gamepad.json'),
+    'keyboard'     : require('./keyboard.json'),
+    'components'   : require('./components.json'),
+    'gameobjects'  : require('./gameobjects.json'),
+    'bitmapdata'   : require('./bitmapdata.json'),
+    'graphics'     : require('./graphics.json'),
+    'rendertexture': require('./rendertexture.json'),
+    'text'         : require('./text.json'),
+    'bitmaptext'   : require('./bitmaptext.json'),
+    'retrofont'    : require('./retrofont.json'),
+    'system'       : require('./system.json'),
+    'math'         : require('./math.json'),
+    'net'          : require('./net.json'),
+    'tweens'       : require('./tweens.json'),
+    'time'         : require('./time.json'),
+    'animation'    : require('./animation.json'),
+    'loader'       : require('./loader.json'),
+    'sound'        : require('./sound.json'),
+    'debug'        : require('./debug.json'),
+    'utils'        : require('./utils.json'),
+    'physics'      : require('./physics.json'),
+    'arcade'       : require('./physics.arcade.json'),
+    'ninja'        : require('./physics.ninja.json'),
+    'p2'           : require('./physics.p2.json'),
+    'tilemaps'     : require('./tilemaps.json'),
+    'particles'    : require('./particles.json'),
+    'creature'     : require('./creature.json'),
+    'video'        : require('./video.json'),
+    'outro'        : require('./outro.json')
+
+};
