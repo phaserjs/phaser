bdcb15fcde158ed7c3446867a1409b0d8ed81801
photonstorm <rdavey@gmail.com>
2015-05-26 20:01:13 +0100

    Tween.to and Tween.from can now accept `null` as the ease parameter value. If `null` it will use the default tween, as per the documentation (thanks @nkovacs #1817)

--- src/tween/Tween.js
+++ src/tween/Tween.js
@@ -189,7 +189,7 @@ Phaser.Tween.prototype = {
     to: function (properties, duration, ease, autoStart, delay, repeat, yoyo) {
 
         if (typeof duration === 'undefined' || duration <= 0) { duration = 1000; }
-        if (typeof ease === 'undefined') { ease = Phaser.Easing.Default; }
+        if (typeof ease === 'undefined' || ease === null) { ease = Phaser.Easing.Default; }
         if (typeof autoStart === 'undefined') { autoStart = false; }
         if (typeof delay === 'undefined') { delay = 0; }
         if (typeof repeat === 'undefined') { repeat = 0; }
@@ -236,7 +236,7 @@ Phaser.Tween.prototype = {
     from: function (properties, duration, ease, autoStart, delay, repeat, yoyo) {
 
         if (typeof duration === 'undefined') { duration = 1000; }
-        if (typeof ease === 'undefined') { ease = Phaser.Easing.Default; }
+        if (typeof ease === 'undefined' || ease === null) { ease = Phaser.Easing.Default; }
         if (typeof autoStart === 'undefined') { autoStart = false; }
         if (typeof delay === 'undefined') { delay = 0; }
         if (typeof repeat === 'undefined') { repeat = 0; }
