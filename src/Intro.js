478d005f0bfc8412b5ebea977f0a2cc615400ec5
Richard Davey <rdavey@gmail.com>
2015-07-24 09:43:44 +0100

    Testing new UMD wrapper.

--- src/Intro.js
+++ src/Intro.js
@@ -4,6 +4,18 @@
 * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
 */
 
-(function(){
-
-    var root = this;
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['exports', 'Phaser'], function (exports, Phaser) {
+            factory((root.commonJsStrictGlobal = exports), Phaser);
+        });
+    } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+        // CommonJS
+        factory(exports, require('Phaser'));
+    } else {
+        // Browser globals
+        factory((root.commonJsStrictGlobal = {}), root.Phaser);
+    }
+}(this, function (exports, Phaser) {
+    
--- src/Outro.js
+++ src/Outro.js
@@ -4,17 +4,10 @@
 * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
 */
 
-    if (typeof exports !== 'undefined') {
-        if (typeof module !== 'undefined' && module.exports) {
-            exports = module.exports = Phaser;
-        }
-        exports.Phaser = Phaser;
-    } else if (typeof define !== 'undefined' && define.amd) {
-        define('Phaser', (function() { return root.Phaser = Phaser; }) ());
-    } else {
-        root.Phaser = Phaser;
-    }
-}).call(this);
+    // attach properties to the exports object to define
+    // the exported module properties.
+    exports.action = function () {};
+}));
 
 /*
 * "What matters in this life is not what we do but what we do for others, the legacy we leave and the imprint we make." - Eric Meyer
