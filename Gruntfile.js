7b66a782a06954f9875ecc6e5b60c15f8fd744e5
spayton <stuart@azureweb.plus.com>
2015-11-05 13:28:32 +0000

    Enhanced the intro/outro usage.  The Pixidefaults inclusion have been decoupled from the Phaser Outro and put into a separate module, prior to this it would not be possible to exclude the UMD wrapper and keep the pixi defaults, which I'm not sure would have been much use to anyone, now you can optionaly keep the pixi defaults.
    Also the Pixi intro module has been split into intro/main modules, so it's intro/outro modules can be excluded cleanly without being tied to the body of the Pixi code. Finally the Pixi into/outro exclusion is tied to Phaser's excludes, ie if Phasers intro/outro is excluded so are Pixi's - this could be separated by giving pixi it's own module list but I can't see why anyone would not want to exclude the wrappers from both, they can always generate the Phaser/Pixi modules in separate passes if they want differing wrapping anyway.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-/// <binding />
+/// <binding BeforeBuild='default' />
 module.exports = function (grunt) {
 
     var loadConfig = require('load-grunt-config');
@@ -32,7 +32,6 @@ module.exports = function (grunt) {
     grunt.registerTask('custom', 'Build a custom version of Phaser', function(arg) {
 
         var modules = {
-
             'intro':            { 'description': 'Phaser UMD wrapper',                          'optional': true, 'stub': false },
             'phaser':           { 'description': 'Phaser Globals',                              'optional': false, 'stub': false },
             'geom':             { 'description': 'Geometry Classes',                            'optional': false, 'stub': false },
@@ -73,8 +72,8 @@ module.exports = function (grunt) {
             'particles':        { 'description': 'Arcade Physics Particle System',              'optional': true, 'stub': true },
             'creature':         { 'description': 'Creature Animation Tool Support',             'optional': true, 'stub': false },
             'video':            { 'description': 'Video Game Object',                           'optional': true, 'stub': false },
+            'pixidefs':         { 'description': 'Pixi defaults',                               'optional': true, 'stub': false },
             'outro':            { 'description': 'Phaser UMD closure',                          'optional': true, 'stub': false }
-
         };
 
         grunt.log.writeln("---------------------");
@@ -220,9 +219,16 @@ module.exports = function (grunt) {
                 //  3) PIXI
 
                 grunt.log.writeln("-> PIXI");
-                tasks.push('concat:pixiIntro');
-                pixiFilelist.push('<%= modules_dir %>/pixi-intro.js');
+                
+                if (!excludedKeys['intro'])
+                {
+                    tasks.push('concat:pixiIntro');
+                    pixiFilelist.push('<%= modules_dir %>/pixi-intro.js');
+                }
 
+                tasks.push('concat:pixiMain');
+                pixiFilelist.push('<%= modules_dir %>/pixi-main.js');
+                
                 //  Optional Rope
                 if (!excludedKeys['rope'])
                 {
@@ -240,8 +246,11 @@ module.exports = function (grunt) {
                 }
 
                 //  PIXI Outro
-                tasks.push('concat:pixiOutro');
-                pixiFilelist.push('<%= modules_dir %>/pixi-outro.js');
+                if (!excludedKeys['outro'])
+                {
+                    tasks.push('concat:pixiOutro');
+                    pixiFilelist.push('<%= modules_dir %>/pixi-outro.js');
+                }
 
                 grunt.config.set('pixiFilelist', pixiFilelist);
 
@@ -283,9 +292,16 @@ module.exports = function (grunt) {
                 //  3) PIXI
 
                 grunt.log.writeln("-> PIXI");
-                tasks.push('concat:pixiIntro');
-                filelist.push('<%= modules_dir %>/pixi-intro.js');
+                
+                if (!excludedKeys['intro'])
+                {
+                    tasks.push('concat:pixiIntro');
+                    filelist.push('<%= modules_dir %>/pixi-intro.js');
+                }
 
+                tasks.push('concat:pixiMain');
+                filelist.push('<%= modules_dir %>/pixi-main.js');
+                
                 //  Optional Rope
                 if (!excludedKeys['rope'])
                 {
@@ -303,8 +319,11 @@ module.exports = function (grunt) {
                 }
 
                 //  PIXI Outro
-                tasks.push('concat:pixiOutro');
-                filelist.push('<%= modules_dir %>/pixi-outro.js');
+                if (!excludedKeys['outro'])
+                {
+                    tasks.push('concat:pixiOutro');
+                    filelist.push('<%= modules_dir %>/pixi-outro.js');
+                }
             }
 
             //  And now for Phaser
