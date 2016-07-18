263238ba4243e21e64cfa7796a02de20ead15524
Pete Baron <simoleste@gmail.com>
2016-07-18 12:56:34 +1200

    Comments and clean-up.

--- src/pixi/renderers/webgl/shaders/TilemapShader.js
+++ src/pixi/renderers/webgl/shaders/TilemapShader.js
@@ -1,5 +1,19 @@
 /**
  * @author Mat Groves http://matgroves.com/ @Doormat23
+ *
+ * This shader is used to render a batch of tiles stored as a tri-strip with
+ * degenerate triangles at the end of each row or group of tiles (A group is a
+ * row of tiles with content followed by one or more empty tiles which are not
+ * drawn).
+ *
+ * Settings available are:
+ * 
+ * uAlpha - the alpha blending factor for a batch draw
+ * uCentreOffset - the offset to the centre of the drawing area, in WebGL units (-1...1)
+ * uScale - the scaling factor for a batch draw
+ * uImageSampler - the source texture containing the tile images
+ * aPosition - the attribute set by the batch data for drawing location
+ * 
  */
 
 /**
@@ -84,8 +98,8 @@ PIXI.TilemapShader.prototype.init = function()
     this.uAlpha = gl.getUniformLocation(program, 'uAlpha');
     this.uScale = gl.getUniformLocation(program, 'uScale');
 
-    this.attributes = [this.aPosition, this.uSampler];
-    this.uniforms = [this.uCentreOffset, this.uAlpha, this.uScale];
+    this.attributes = [this.aPosition];
+    this.uniforms = [this.uCentreOffset, this.uAlpha, this.uScale, this.uSampler];
 
     this.program = program;
 };
@@ -98,8 +112,8 @@ PIXI.TilemapShader.prototype.init = function()
 PIXI.TilemapShader.prototype.destroy = function()
 {
     this.gl.deleteProgram( this.program );
-    this.uniforms = null;
     this.gl = null;
 
+    this.uniforms = null;
     this.attributes = null;
 };
