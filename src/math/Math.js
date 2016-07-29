a532f7b2c4bcfa0d9c91d4c43c104076f6a6e01b
Richard Davey <rdavey@gmail.com>
2016-07-29 03:58:25 +0100

    Optimised Math.getShortestAngle.

--- src/math/Math.js
+++ src/math/Math.js
@@ -378,20 +378,26 @@ Phaser.Math = {
     * this method, and get the shortest angle back between the two of them.
     *
     * The angle returned will be in the same range. If the returned angle is
-    * less than 0 then it's a counter-clockwise rotation, if >= 0 then it's
+    * greater than 0 then it's a counter-clockwise rotation, if < 0 then it's
     * a clockwise rotation.
     * 
     * @method Phaser.Math#getShortestAngle
     * @param {number} angle1 - The first angle. In the range -180 to 180.
     * @param {number} angle2 - The second angle. In the range -180 to 180.
-    * @return {number} The shortest angle, in degrees. If less than zero it's counter-clockwise, otherwise clockwise.
+    * @return {number} The shortest angle, in degrees. If greater than zero it's a counter-clockwise rotation.
     */
     getShortestAngle: function (angle1, angle2) {
 
         var difference = angle2 - angle1;
+
+        if (difference === 0)
+        {
+            return 0;
+        }
+
         var times = Math.floor((difference - (-180)) / 360);
 
-        return (difference - (times * 360)) * -1;
+        return difference - (times * 360);
 
     },
 
