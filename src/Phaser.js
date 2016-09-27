00abd388e88f71ba1e300382dca6bc8313b52ccc
Richard Davey <rdavey@gmail.com>
2016-09-27 02:00:40 +0100

    Bumped version.

--- src/Phaser.js
+++ src/Phaser.js
@@ -14,7 +14,7 @@ var Phaser = Phaser || {    // jshint ignore:line
     * @constant
     * @type {string}
     */
-    VERSION: '3.0.0 b1',
+    VERSION: '3.0.0 b2',
 
     /**
     * An array of Phaser game instances.
