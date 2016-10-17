c39c97f9d8afef223f375a3cf0087f9797ef1640
Richard Davey <rdavey@gmail.com>
2016-10-17 21:22:55 +0100

    Starting to split up.

---
+++ src/core/MainLoop.js
@@ -0,0 +1,10 @@
+/**
+* @author       Richard Davey <rich@photonstorm.com>
+* @copyright    2016 Photon Storm Ltd.
+* @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+*/
+Phaser.MainLoop = function ()
+{
+
+
+};
