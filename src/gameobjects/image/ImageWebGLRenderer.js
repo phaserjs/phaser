8e2c5b7669d508950880ccfd4d650f45946fea34
Richard Davey <rdavey@gmail.com>
2016-10-31 00:03:57 +0000

    Removed dead code.

--- src/gameobjects/image/ImageWebGLRenderer.js
+++ src/gameobjects/image/ImageWebGLRenderer.js
@@ -16,61 +16,12 @@ Phaser.Renderer.WebGL.GameObjects.Image = {
             return;
         }
 
-        //  The below works
-
-        var uvs = frame.uvs;
         var verts = src.transform.glVertextData;
         var index = src.frame.source.glTextureIndex;
         var tint = src.color._glTint;
         var bg = src.color._glBg;
 
-        renderer.batch.add(frame.source, src.blendMode, verts, uvs, index, alpha, tint, bg);
-
-        /*
-
-        // var verts = src.transform.cloneVertexData();
-        var worldAlpha = 0.2;
-
-        //  0.2, 0.4, 0.6, 0.8, 1
-
-        //  xy0 Top Left Vert
-        //  xy1 Top Right Vert
-        //  xy2 Bottom Right Vert
-        //  xy3 Bottom Left Vert
-
-        //  Let's just try 5 clones below the current sprite?
-
-        verts.x0 += 8 * 5;
-        verts.x1 += 8 * 5;
-        verts.x2 += 8 * 5;
-        verts.x3 += 8 * 5;
-
-        verts.y0 += 8 * 5;
-        verts.y1 += 8 * 5;
-        verts.y2 += 8 * 5;
-        verts.y3 += 8 * 5;
-
-
-        for (var i = 0; i < 5; i++)
-        {
-            alpha = worldAlpha * 255 << 24;
-
-            renderer.spriteBatch.addToBatch(src, verts, uvs, index, alpha, tint, bg);
-
-            verts.x0 -= 8;
-            verts.x1 -= 8;
-            verts.x2 -= 8;
-            verts.x3 -= 8;
-
-            verts.y0 -= 8;
-            verts.y1 -= 8;
-            verts.y2 -= 8;
-            verts.y3 -= 8;
-
-            worldAlpha += 0.2;
-        }
-        */
-
+        renderer.batch.add(frame.source, src.blendMode, verts, frame.uvs, index, alpha, tint, bg);
     }
 
 };
