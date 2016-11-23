ed625c33d454a0523c46ffe9cf9f8872c9f4dce5
Stoneman1 <aleksi1578@msn.com>
2016-11-23 11:50:39 +0200

    Some browsers use cancelAnimationFrame with prefix like this: msCancelRequestAnimationFrame (IE11 for example). This will fix it

--- v2-community/src/utils/RequestAnimationFrame.js
+++ v2-community/src/utils/RequestAnimationFrame.js
@@ -42,7 +42,7 @@ Phaser.RequestAnimationFrame = function(game, forceSetTimeOut) {
     for (var x = 0; x < vendors.length && !window.requestAnimationFrame; x++)
     {
         window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
     }
 
     /**
