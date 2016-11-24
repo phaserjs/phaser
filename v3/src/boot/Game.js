247a8379616b0a79450195de48b811087ea13db3
photonstorm <rdavey@gmail.com>
2016-11-24 17:01:52 +0000

    Game update.

--- v3/src/boot/Game.js
+++ v3/src/boot/Game.js
@@ -11,6 +11,62 @@ var Game = function (config)
 {
     this.config = new Config(config);
 
+    //  Decide which of the following should be Game properties, or placed elsewhere ...
+
+    this.renderer = null;
+    this.canvas = null;
+    this.context = null;
+
+    /**
+    * @property {string|HTMLElement} parent - The Games DOM parent.
+    * @default
+    */
+    this.parent = parent;
+
+    this.isBooted = false;
+    this.isRunning = false;
+
+    /**
+    * @property {Phaser.RequestAnimationFrame} raf - Automatically handles the core game loop via requestAnimationFrame or setTimeout
+    * @protected
+    */
+    this.raf = null;
+
+    /**
+    * @property {Phaser.TextureManager} textures - Reference to the Phaser Texture Manager.
+    */
+    this.textures = null;
+
+    /**
+    * @property {Phaser.UpdateManager} updates - Reference to the Phaser Update Manager.
+    */
+    this.updates = null;
+
+    /**
+    * @property {Phaser.Cache} cache - Reference to the assets cache.
+    */
+    this.cache = null;
+
+    /**
+    * @property {Phaser.Input} input - Reference to the input manager
+    */
+    this.input = null;
+
+    /**
+    * @property {Phaser.StateManager} state - The StateManager.
+    */
+    // this.state = new Phaser.StateManager(this, stateConfig);
+
+    /**
+    * @property {Phaser.Device} device - Contains device information and capabilities.
+    */
+    // this.device = Phaser.Device;
+
+    // this.rnd = new Phaser.RandomDataGenerator([ (Date.now() * Math.random()).toString() ]);
+
+    // this.device.whenReady(this.boot, this);
+
+
     DebugHeader(this);
 };
 
