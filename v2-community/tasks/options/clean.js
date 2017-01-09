71cee10a55c5cd1de9fd0aae77fd40b497031219
photonstorm <rdavey@gmail.com>
2017-01-09 14:48:39 +0000

    Removed Phaser CE. It has now moved to its own repo at https://github.com/photonstorm/phaser-ce

--- v2-community/tasks/options/clean.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-    docs: ['<%= docs_dir %>/*'],
-    build: ['<%= compile_dir %>/*', '<%= modules_dir %>/*.js'],
-    release: ['<%= release_dir %>/*.js', '<%= release_dir %>/*.map', '<%= release_custom_dir %>/*'],
-    out: ['out']
-
-};
