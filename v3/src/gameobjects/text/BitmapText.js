2d10cff333fdd5ed661126ccd4f7ffaac9adbe63
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2017-02-23 17:52:52 -0300

    Bitmap text rendering on WebGL

--- v3/src/gameobjects/text/BitmapText.js
+++ v3/src/gameobjects/text/BitmapText.js
@@ -1,7 +1,53 @@
 var Class = require('../../utils/Class');
 var GameObject = require('../GameObject');
 var Components = require('../../components');
-var Renderer = require('./BitmapTextRenderer')
+var Renderer = require('./BitmapTextRenderer');
+
+var ParseXMLBitmapFont = function (xml, xSpacing, ySpacing, frame) 
+{
+    var data = {};
+    var info = xml.getElementsByTagName('info')[0];
+    var common = xml.getElementsByTagName('common')[0];
+
+    data.font = info.getAttribute('face');
+    data.size = parseInt(info.getAttribute('size'), 10);
+    data.lineHeight = parseInt(common.getAttribute('lineHeight'), 10) + ySpacing;
+    data.chars = {};
+
+    var letters = xml.getElementsByTagName('char');
+
+    var x = (frame) ? frame.x : 0;
+    var y = (frame) ? frame.y : 0;
+
+    for (var i = 0; i < letters.length; i++)
+    {
+        var charCode = parseInt(letters[i].getAttribute('id'), 10);
+
+        data.chars[charCode] = {
+            x: x + parseInt(letters[i].getAttribute('x'), 10),
+            y: y + parseInt(letters[i].getAttribute('y'), 10),
+            width: parseInt(letters[i].getAttribute('width'), 10),
+            height: parseInt(letters[i].getAttribute('height'), 10),
+            xOffset: parseInt(letters[i].getAttribute('xoffset'), 10),
+            yOffset: parseInt(letters[i].getAttribute('yoffset'), 10),
+            xAdvance: parseInt(letters[i].getAttribute('xadvance'), 10) + xSpacing,
+            kerning: {}
+        };
+    }
+
+    var kernings = xml.getElementsByTagName('kerning');
+
+    for (i = 0; i < kernings.length; i++)
+    {
+        var first = parseInt(kernings[i].getAttribute('first'), 10);
+        var second = parseInt(kernings[i].getAttribute('second'), 10);
+        var amount = parseInt(kernings[i].getAttribute('amount'), 10);
+
+        data.chars[second].kerning[first] = amount;
+    }
+
+    return data;
+}
 
 var BitmapText = new Class({
 
@@ -11,7 +57,6 @@ var BitmapText = new Class({
         Components.Size,
         Components.Alpha,
         Components.BlendMode,
-        Components.ScaleMode,
         Components.Visible,
         Renderer
     ],
@@ -23,7 +68,7 @@ var BitmapText = new Class({
         GameObject.call(this, state);
 
         this.text = typeof text === 'string' ? text : '';
-        this.fontData = this.state.sys.cache.xml.get(key);
+        this.fontData = ParseXMLBitmapFont(this.state.sys.cache.xml.get(key), 0, 0);
         
         this.setTexture(key, null);
         this.setPosition(x, y);
--- v3/src/gameobjects/text/BitmapTextWebGLRenderer.js
+++ v3/src/gameobjects/text/BitmapTextWebGLRenderer.js
@@ -15,6 +15,121 @@ var BitmapTextWebGLRenderer = function (renderer, src, interpolationPercentage, 
     var f = cameraMatrix[5];
     var cameraScrollX = camera.scrollX;
     var cameraScrollY = camera.scrollY;
+    var text = src.text;
+    var textLength = text.length;
+    var chars = src.fontData.chars;
+    var lineHeight = src.fontData.lineHeight;
+    var blitterBatch = renderer.blitterBatch;
+    var alpha = src.alpha;
+    var vertexDataBuffer = blitterBatch.vertexDataBuffer;
+    var vertexBuffer = vertexDataBuffer.floatView;
+    var vertexOffset = 0;
+    var srcX = src.x; 
+    var srcY = src.y;
+    var texture = src.texture.source[0].glTexture
+    var textureWidth = src.texture.source[0].width;
+    var textureHeight = src.texture.source[0].height
+    var xAdvance = 0;
+    var yAdvance = 0;
+    var indexCount = 0;
+    var charCode = 0;
+    var glyph = null;
+    var glyphX = 0;
+    var glyphY = 0;
+    var glyphW = 0;
+    var glyphH = 0;
+    var x = 0;
+    var y = 0;
+    var xw = 0;
+    var yh = 0;
+    var tx = 0;
+    var ty = 0;
+    var txw = 0;
+    var tyh = 0;
+    var umin = 0;
+    var umax = 0;
+    var vmin = 0;
+    var vmax = 0;
+    var lastGlyph = null;
+    var lastCharCode = 0;
+
+    for (var index = 0; index < textLength; ++index)
+    {
+        charCode = text.charCodeAt(index);
+        if (charCode === 10)
+        {
+            xAdvance = 0;
+            indexCount = 0;
+            yAdvance += lineHeight;
+            lastGlyph = null;
+            continue;
+        }
+
+        glyph = chars[charCode];
+        if (!glyph)
+        {
+            continue;
+        }
+
+        glyphX = glyph.x;
+        glyphY = glyph.y;
+        glyphW = glyph.width;
+        glyphH = glyph.height;
+        x = (srcX + indexCount + glyph.xOffset + xAdvance) - cameraScrollX;
+        y = (srcY + glyph.yOffset + yAdvance) - cameraScrollY;
+
+        if (lastGlyph !== null)
+        {
+            var kerningOffset = glyph.kerning[lastCharCode];
+            x += (kerningOffset !== undefined) ? kerningOffset : 0;
+        }
+
+        xw = x + glyphW;
+        yh = y + glyphH;
+        tx = x * a + y * c + e;
+        ty = x * b + y * d + f;
+        txw = xw * a + yh * c + e;
+        tyh = xw * b + yh * d + f;
+        umin = glyphX / textureWidth;
+        umax = (glyphX + glyphW) / textureWidth;
+        vmin = glyphY / textureHeight;
+        vmax = (glyphY + glyphH) / textureHeight;
+
+        if (blitterBatch.elementCount >= blitterBatch.maxParticles)
+        {
+            blitterBatch.flush();
+        }
+
+        renderer.setBatch(blitterBatch, texture);
+        vertexOffset = vertexDataBuffer.allocate(20);
+        blitterBatch.elementCount += 6;
+
+        vertexBuffer[vertexOffset++] = tx;
+        vertexBuffer[vertexOffset++] = ty;
+        vertexBuffer[vertexOffset++] = umin;
+        vertexBuffer[vertexOffset++] = vmin;
+        vertexBuffer[vertexOffset++] = alpha;
+        vertexBuffer[vertexOffset++] = tx;
+        vertexBuffer[vertexOffset++] = tyh;
+        vertexBuffer[vertexOffset++] = umin;
+        vertexBuffer[vertexOffset++] = vmax;
+        vertexBuffer[vertexOffset++] = alpha;
+        vertexBuffer[vertexOffset++] = txw;
+        vertexBuffer[vertexOffset++] = tyh;
+        vertexBuffer[vertexOffset++] = umax;
+        vertexBuffer[vertexOffset++] = vmax;
+        vertexBuffer[vertexOffset++] = alpha;
+        vertexBuffer[vertexOffset++] = txw;
+        vertexBuffer[vertexOffset++] = ty;
+        vertexBuffer[vertexOffset++] = umax;
+        vertexBuffer[vertexOffset++] = vmin;
+        vertexBuffer[vertexOffset++] = alpha;
+
+        xAdvance += glyph.xAdvance;
+        indexCount += 1;
+        lastGlyph = glyph;
+        lastCharCode = charCode;
+    }
 };
 
 module.exports = BitmapTextWebGLRenderer;
