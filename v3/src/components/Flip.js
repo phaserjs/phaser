fb9cc95229b9b04b2be6f46a69f32a323660486c
Richard Davey <rdavey@gmail.com>
2017-03-27 23:53:22 +0100

    Added setFlip methods.

--- v3/src/components/Flip.js
+++ v3/src/components/Flip.js
@@ -3,6 +3,28 @@ var Flip = {
     flipX: false,
     flipY: false,
 
+    setFlipX: function (value)
+    {
+        this.flipX = value;
+
+        return this;
+    },
+
+    setFlipY: function (value)
+    {
+        this.flipX = value;
+
+        return this;
+    },
+
+    setFlip: function (x, y)
+    {
+        this.flipX = x;
+        this.flipY = y;
+
+        return this;
+    },
+
     resetFlip: function ()
     {
         this.flipX = false;
