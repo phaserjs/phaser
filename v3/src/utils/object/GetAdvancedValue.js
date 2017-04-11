05920eae8eaeedc48aac97f20503c8b9a4e639d8
Richard Davey <rdavey@gmail.com>
2017-04-11 02:48:48 +0100

    An advanced object property extraction function. Allows you to defined property callbacks, arrays to pick from, random ints and random floats.

---
+++ v3/src/utils/object/GetAdvancedValue.js
@@ -0,0 +1,62 @@
+var MATH = require('../../math');
+var GetObjectValue = require('./GetObjectValue');
+
+//  Allowed types:
+
+//  Implicit
+//  {
+//      x: 4
+//  }
+//
+//  From function
+//  {
+//      x: function ()
+//  }
+//
+//  Randomly pick one element from the array
+//  {
+//      x: [a, b, c, d, e, f]
+//  }
+//
+//  Random integer between min and max:
+//  {
+//      x: { randInt: [min, max] }
+//  }
+//
+//  Random float between min and max:
+//  {
+//      x: { randFloat: [min, max] }
+//  }
+
+var GetAdvancedValue = function (source, key, defaultValue)
+{
+    var value = GetObjectValue(source, key, null);
+
+    if (value === null)
+    {
+        return defaultValue;
+    }
+    else if (Array.isArray(value))
+    {
+        return MATH.RND.pick(value);
+    }
+    else if (typeof value === 'object')
+    {
+        if (value.hasOwnProperty('randInt'))
+        {
+            return MATH.RND.integerInRange(value.randInt[0], value.randInt[1]);
+        }
+        else if (value.hasOwnProperty('randFloat'))
+        {
+            return MATH.RND.realInRange(value.randFloat[0], value.randFloat[1]);
+        }
+    }
+    else if (typeof value === 'function')
+    {
+        return value(key);
+    }
+
+    return value;
+};
+
+module.exports = GetAdvancedValue;
