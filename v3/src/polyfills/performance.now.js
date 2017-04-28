960acce62c3014c2442c1c3084e2b267e81d8170
photonstorm <rdavey@gmail.com>
2017-04-28 03:13:22 +0100

    eslint fixes

--- v3/src/polyfills/performance.now.js
+++ v3/src/polyfills/performance.now.js
@@ -6,27 +6,31 @@
 /**
  * performance.now
  */
-(function(){
+(function () {
 
-  if ("performance" in window == false) {
-      window.performance = {};
-  }
-  
-  Date.now = (Date.now || function () {  // thanks IE8
-      return new Date().getTime();
-  });
-
-  if ("now" in window.performance == false)
-  {
-    var nowOffset = Date.now();
-    
-    if (performance.timing && performance.timing.navigationStart){
-      nowOffset = performance.timing.navigationStart
+    if ('performance' in window === false)
+    {
+        window.performance = {};
     }
 
-    window.performance.now = function now(){
-      return Date.now() - nowOffset;
+    //  Thanks IE8
+    Date.now = (Date.now || function () {
+        return new Date().getTime();
+    });
+
+    if ('now' in window.performance === false)
+    {
+        var nowOffset = Date.now();
+
+        if (performance.timing && performance.timing.navigationStart)
+        {
+            nowOffset = performance.timing.navigationStart;
+        }
+
+        window.performance.now = function now ()
+        {
+            return Date.now() - nowOffset;
+        }
     }
-  }
 
 })();
