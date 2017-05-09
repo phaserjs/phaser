5950bc84a2f52479b53107de1bb0d95bc4e2e9be
Richard Davey <rdavey@gmail.com>
2017-05-09 15:39:59 +0100

    Added some comments.

--- v3/src/animation/frame/GetNextTick.js
+++ v3/src/animation/frame/GetNextTick.js
@@ -1,7 +1,11 @@
 var GetNextTick = function (component)
 {
+    // accumulator += delta * _timeScale
+    // after a large delta surge (perf issue for example) we need to adjust for it here
+
     //  When is the next update due?
     component.accumulator -= component.nextTick;
+
     component.nextTick = component.msPerFrame + component.currentFrame.duration;
 };
 
