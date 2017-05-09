1fd4eb13a1b325d6d327844841986bd707dccbbc
Richard Davey <rdavey@gmail.com>
2017-05-10 00:42:59 +0100

    Added a string-lookup EaseMap.

---
+++ v3/src/math/easing/EaseMap.js
@@ -0,0 +1,67 @@
+var Back = require('./Back');
+var Bounce = require('./Bounce');
+var Circular = require('./Circular');
+var Cubic = require('./Cubic');
+var Elastic = require('./Elastic');
+var Expo = require('./Expo');
+var Linear = require('./Linear');
+var Quadratic = require('./Quadratic');
+var Quartic = require('./Quartic');
+var Quintic = require('./Quintic');
+var Sine = require('./Sine');
+
+//  EaseMap
+module.exports = {
+
+    'Power0': Linear.None,
+    'Power1': Quadratic.Out,
+    'Power2': Cubic.Out,
+    'Power3': Quartic.Out,
+    'Power4': Quintic.Out,
+
+    'Linear': Linear.None,
+    'Quad': Quadratic.Out,
+    'Cubic': Cubic.Out,
+    'Quart': Quartic.Out,
+    'Quint': Quintic.Out,
+    'Sine': Sinusoidal.Out,
+    'Expo': Exponential.Out,
+    'Circ': Circular.Out,
+    'Elastic': Elastic.Out,
+    'Back': Back.Out,
+    'Bounce': Bounce.Out,
+
+    'Quad.easeIn': Quadratic.In,
+    'Cubic.easeIn': Cubic.In,
+    'Quart.easeIn': Quartic.In,
+    'Quint.easeIn': Quintic.In,
+    'Sine.easeIn': Sinusoidal.In,
+    'Expo.easeIn': Exponential.In,
+    'Circ.easeIn': Circular.In,
+    'Elastic.easeIn': Elastic.In,
+    'Back.easeIn': Back.In,
+    'Bounce.easeIn': Bounce.In,
+
+    'Quad.easeOut': Quadratic.Out,
+    'Cubic.easeOut': Cubic.Out,
+    'Quart.easeOut': Quartic.Out,
+    'Quint.easeOut': Quintic.Out,
+    'Sine.easeOut': Sinusoidal.Out,
+    'Expo.easeOut': Exponential.Out,
+    'Circ.easeOut': Circular.Out,
+    'Elastic.easeOut': Elastic.Out,
+    'Back.easeOut': Back.Out,
+    'Bounce.easeOut': Bounce.Out,
+
+    'Quad.easeInOut': Quadratic.InOut,
+    'Cubic.easeInOut': Cubic.InOut,
+    'Quart.easeInOut': Quartic.InOut,
+    'Quint.easeInOut': Quintic.InOut,
+    'Sine.easeInOut': Sinusoidal.InOut,
+    'Expo.easeInOut': Exponential.InOut,
+    'Circ.easeInOut': Circular.InOut,
+    'Elastic.easeInOut': Elastic.InOut,
+    'Back.easeInOut': Back.InOut,
+    'Bounce.easeInOut': Bounce.InOut
+
+};
