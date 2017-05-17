49770eff46d966d6fef35261ebfda02262ca1634
Richard Davey <rdavey@gmail.com>
2017-05-17 02:43:41 +0100

    Moved Tween reserved words to own file.

---
+++ v3/src/tween/ReservedProps.js
@@ -0,0 +1,14 @@
+//  RESERVED properties that a Tween config object uses
+
+module.exports = [
+    'duration',
+    'ease',
+    'loop',
+    'offset',
+    'paused',
+    'props',
+    'repeat',
+    'targets',
+    'useFrames',
+    'yoyo'
+];
