47d4dc4cf2dd97a8c99b6ed5f66637736df946ea
Richard Davey <rdavey@gmail.com>
2017-05-17 04:37:20 +0100

    Added new reserved words.

--- v3/src/tween/ReservedProps.js
+++ v3/src/tween/ReservedProps.js
@@ -1,13 +1,17 @@
 //  RESERVED properties that a Tween config object uses
 
 module.exports = [
+    'delay',
     'duration',
     'ease',
+    'elasticity',
     'loop',
     'offset',
+    'onCompleteDelay',
     'paused',
     'props',
     'repeat',
+    'repeatDelay',
     'targets',
     'useFrames',
     'yoyo'
