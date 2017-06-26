2aadd2ac79a6a4778cad33046bbaca26677fe744
photonstorm <rdavey@gmail.com>
2017-06-26 13:17:31 +0100

    Added setScrollFactor method.

--- v3/src/components/ScrollFactor.js
+++ v3/src/components/ScrollFactor.js
@@ -1,6 +1,18 @@
 var ScrollFactor = {
+
     scrollFactorX: 1.0,
     scrollFactorY: 1.0,
+
+    setScrollFactor: function (x, y)
+    {
+        if (x === undefined) { x = y; }
+
+        this.scrollFactorX = x;
+        this.scrollFactorY = y;
+
+        return this;
+    }
+
 };
 
 module.exports = ScrollFactor;
