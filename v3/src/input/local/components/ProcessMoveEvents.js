102fa3e5d1dd7100069884c8caa2503735fb8f26
photonstorm <rdavey@gmail.com>
2017-07-24 14:10:32 +0100

    Movement event now checks draggable.

--- v3/src/input/local/components/ProcessMoveEvents.js
+++ v3/src/input/local/components/ProcessMoveEvents.js
@@ -2,6 +2,9 @@ var InputEvent = require('../events');
 
 var ProcessMoveEvents = function (pointer, currentlyOver)
 {
+    var i;
+    var interactiveObject;
+
     if (currentlyOver.length === 0)
     {
         //  Dispatch MOVE event, even though pointer isn't over anything
@@ -10,9 +13,9 @@ var ProcessMoveEvents = function (pointer, currentlyOver)
     else
     {
         //  Go through all objects the pointer is over and dispatch them
-        for (var i = 0; i < currentlyOver.length; i++)
+        for (i = 0; i < currentlyOver.length; i++)
         {
-            var interactiveObject = currentlyOver[i];
+            interactiveObject = currentlyOver[i];
 
             this.events.dispatch(new InputEvent.MOVE(pointer, interactiveObject.gameObject, currentlyOver));
 
@@ -26,10 +29,9 @@ var ProcessMoveEvents = function (pointer, currentlyOver)
     }
 
     //  Check the list of Draggable Items
-    /*
-    for (var i = 0; i < this.children.draggable.length; i++)
+    for (i = 0; i < this.children.draggable.length; i++)
     {
-        var interactiveObject = this.children.draggable[i];
+        interactiveObject = this.children.draggable[i];
 
         if (!interactiveObject.enabled)
         {
@@ -39,16 +41,14 @@ var ProcessMoveEvents = function (pointer, currentlyOver)
         if (pointer.justUp && interactiveObject.isDragged)
         {
             //  Drag End
-            this.childOnDragEnd(i, pointer, interactiveObject);
+            this.childOnDragEnd(pointer, interactiveObject, currentlyOver);
         }
         else if (interactiveObject.isDragged)
         {
             //  Drag
-            this.childOnDrag(i, pointer, interactiveObject);
+            this.childOnDrag(pointer, interactiveObject, currentlyOver);
         }
     }
-    */
-
 };
 
 module.exports = ProcessMoveEvents;
