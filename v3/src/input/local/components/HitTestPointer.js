e9d7940e74e9cdee79eca8e880c5f18cb631185d
Richard Davey <rdavey@gmail.com>
2017-07-29 01:54:17 +0100

    HitTest only tests the camera below the pointer and there is no benefit polling every single camera

--- v3/src/input/local/components/HitTestPointer.js
+++ v3/src/input/local/components/HitTestPointer.js
@@ -1,20 +1,19 @@
 var HitTestPointer = function (pointer)
 {
-    var output = [];
+    var camera = this.cameras.getCameraBelowPointer(pointer);
 
-    //  Get a list of all objects that can be seen by all the cameras in the scene and store in 'output' array.
-    //  All objects in this array are input enabled, as checked by the hitTest function, so we don't need to check later on as well.
-    for (var i = 0; i < this.cameras.length; i++)
+    if (camera)
     {
-        var camera = this.cameras[i];
+        pointer.camera = camera;
 
-        if (camera.inputEnabled)
-        {
-            output = output.concat(this.manager.hitTest(this._list, pointer.x, pointer.y, camera));
-        }
+        //  Get a list of all objects that can be seen by the camera below the pointer in the scene and store in 'output' array.
+        //  All objects in this array are input enabled, as checked by the hitTest function, so we don't need to check later on as well.
+        return this.manager.hitTest(this._list, pointer.x, pointer.y, camera);
     }
-
-    return output;
+    else
+    {
+        return [];
+    }
 };
 
 module.exports = HitTestPointer;
