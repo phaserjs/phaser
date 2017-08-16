579489d47a5942c5dcdf7c359a4c0278ced9b91f
Richard Davey <rdavey@gmail.com>
2017-08-16 22:26:14 +0100

    Fixed Rectangle inflate. Now deflates with negative values too.

--- v3/src/geom/rectangle/Inflate.js
+++ v3/src/geom/rectangle/Inflate.js
@@ -1,21 +1,17 @@
+var CenterOn = require('./CenterOn');
+
 //  Increases the size of the Rectangle object by the specified amounts.
 //  The center point of the Rectangle object stays the same, and its size increases 
 //  to the left and right by the x value, and to the top and the bottom by the y value.
 
 var Inflate = function (rect, x, y)
 {
-    //  Get the current center
-    var cx = rect.x + (rect.width / 2);
-    var cy = rect.y + (rect.height / 2);
+    var cx = rect.centerX;
+    var cy = rect.centerY;
 
-    //  Inflate
-    rect.width = 2 * x;
-    rect.height = 2 * y;
+    rect.setSize(rect.width + (x * 2), rect.height + (y * 2));
 
-    rect.x = x - (rect.width / 2);
-    rect.y = y - (rect.height / 2);
-
-    return rect;
+    return CenterOn(rect, cx, cy);
 };
 
 module.exports = Inflate;
