6218c05f37638c29e186c01d28fdf66f18bd8b1e
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2017-08-22 17:44:40 -0300

    Fixed texfile loader

--- v3/src/loader/filetypes/TextFile.js
+++ v3/src/loader/filetypes/TextFile.js
@@ -38,4 +38,23 @@ var TextFile = new Class({
 
 });
 
+TextFile.create = function (loader, key, url, xhrSettings)
+{
+    if (Array.isArray(key))
+    {
+        for (var i = 0; i < key.length; i++)
+        {
+            //  If it's an array it has to be an array of Objects, so we get everything out of the 'key' object
+            loader.addFile(new TextFile(key[i], url, loader.path, xhrSettings));
+        }
+    }
+    else
+    {
+        loader.addFile(new TextFile(key, url, loader.path, xhrSettings));
+    }
+
+    //  For method chaining
+    return loader;
+};
+
 module.exports = TextFile;
