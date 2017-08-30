a4b357050d7e294318ce7756d22d9813ac1ef71e
Richard Davey <rdavey@gmail.com>
2017-08-30 23:56:18 +0100

    You can pass in the config object as the children argument to a Group

--- v3/src/gameobjects/group/GroupFactory.js
+++ v3/src/gameobjects/group/GroupFactory.js
@@ -2,6 +2,12 @@ var Group = require('./Group');
 
 var GroupFactory = function (scene, children, config)
 {
+    if (typeof children === 'object' && config === undefined)
+    {
+        config = children;
+        children = [];
+    }
+
     return new Group(scene, children, config);
 };
 
