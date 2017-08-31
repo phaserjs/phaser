69376bf90f52e6954113a4666e1ff1d50b402e0f
Richard Davey <rdavey@gmail.com>
2017-08-31 17:26:39 +0100

    Working through canvas flip origin bug.

--- v3/src/renderer/canvas/utils/DrawImage.js
+++ v3/src/renderer/canvas/utils/DrawImage.js
@@ -33,11 +33,17 @@ var DrawImage = function (src, camera)
     var dy = frame.y - src.displayOriginY;
 
     ctx.save();
-    ctx.translate(src.x - camera.scrollX, src.y - camera.scrollY);
+    ctx.translate(src.x - camera.scrollX * src.scrollFactorX, src.y - camera.scrollY * src.scrollFactorY);
     ctx.rotate(src.rotation);
     ctx.scale(src.scaleX, src.scaleY);
     ctx.scale(src.flipX ? -1 : 1, src.flipY ? -1 : 1);
-    ctx.translate(src.dWidth * (src.flipX ? 1 : 0), src.dHeight * (src.flipY ? 1 : 0));
+
+    //  There is no such property as src.dWidth or src.dHeight, so this call has no effect:
+    // ctx.translate(src.dWidth * (src.flipX ? 1 : 0), src.dHeight * (src.flipY ? 1 : 0));
+
+    // dx -= (src.flipX && src.originX !== 0.5) ? cd.dWidth - (src.originX * cd.dWidth) : 0;
+    // dy -= (src.flipY && src.originY !== 0.5) ? cd.dHeight - (src.originY * cd.dHeight) : 0;
+
     ctx.drawImage(frame.source.image, cd.sx, cd.sy, cd.sWidth, cd.sHeight, dx, dy, cd.dWidth, cd.dHeight);
     ctx.restore();
 };
