65deb4b75cbb4b540a84e00d3fc9247f64da6aae
Igor Petushkov <igor.petushkov@kroppli.com>
2017-09-16 22:40:23 +0300

    Fix animation component delay/repeat/repeatDelay/yoyo attributes when load

--- v3/src/animation/frame/inc/Load.js
+++ v3/src/animation/frame/inc/Load.js
@@ -14,10 +14,10 @@ var Load = function (component, startFrame)
         component.duration = this.duration;
         component.msPerFrame = this.msPerFrame;
         component.skipMissedFrames = this.skipMissedFrames;
-        component._delay = this.delay;
-        component._repeat = this.repeat;
-        component._repeatDelay = this.repeatDelay;
-        component._yoyo = this.yoyo;
+        component._delay = component._delay || this.delay;
+        component._repeat = component._repeat || this.repeat;
+        component._repeatDelay = component._repeatDelay || this.repeatDelay;
+        component._yoyo = component._yoyo || this.yoyo;
         component._callbackArgs[1] = this;
         component._updateParams = component._callbackArgs.concat(this.onUpdateParams);
     }
