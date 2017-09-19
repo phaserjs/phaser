aa10f15214b9a8604a0868cc460437eb5d968f5b
Richard Davey <rdavey@gmail.com>
2017-09-19 15:48:27 +0100

    Fixed typo

--- v3/src/actions/SetBlendMode.js
+++ v3/src/actions/SetBlendMode.js
@@ -2,7 +2,7 @@ var SetBlendMode = function (items, value)
 {
     for (var i = 0; i < items.length; i++)
     {
-        items[i].setBlendMode(value;
+        items[i].setBlendMode(value);
     }
 
     return items;
