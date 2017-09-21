3845461c0377fbcbe99e022d3a2991a96b257dce
Richard Davey <rdavey@gmail.com>
2017-09-21 02:30:35 +0100

    Swapped t position in CatmullRom

--- v3/src/math/interpolation/CatmullRomInterpolation.js
+++ v3/src/math/interpolation/CatmullRomInterpolation.js
@@ -13,21 +13,21 @@ var CatmullRomInterpolation = function (v, k)
             i = Math.floor(f = m * (1 + k));
         }
 
-        return CatmullRom(v[(i - 1 + m) % m], v[i], v[(i + 1) % m], v[(i + 2) % m], f - i);
+        return CatmullRom(f - i, v[(i - 1 + m) % m], v[i], v[(i + 1) % m], v[(i + 2) % m]);
     }
     else
     {
         if (k < 0)
         {
-            return v[0] - (CatmullRom(v[0], v[0], v[1], v[1], -f) - v[0]);
+            return v[0] - (CatmullRom(-f, v[0], v[0], v[1], v[1]) - v[0]);
         }
 
         if (k > 1)
         {
-            return v[m] - (CatmullRom(v[m], v[m], v[m - 1], v[m - 1], f - m) - v[m]);
+            return v[m] - (CatmullRom(f - m, v[m], v[m], v[m - 1], v[m - 1]) - v[m]);
         }
 
-        return CatmullRom(v[i ? i - 1 : 0], v[i], v[m < i + 1 ? m : i + 1], v[m < i + 2 ? m : i + 2], f - i);
+        return CatmullRom(f - i, v[i ? i - 1 : 0], v[i], v[m < i + 1 ? m : i + 1], v[m < i + 2 ? m : i + 2]);
     }
 };
 
