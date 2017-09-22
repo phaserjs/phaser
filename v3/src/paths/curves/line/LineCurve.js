c4f9c41c53171f5ad79a9741ecba68bbf319dceb
Richard Davey <rdavey@gmail.com>
2017-09-22 01:34:53 +0100

    LineCurve can take an array of points now

--- v3/src/paths/curves/line/LineCurve.js
+++ v3/src/paths/curves/line/LineCurve.js
@@ -17,6 +17,12 @@ var LineCurve = new Class({
     //  vec2s
     function LineCurve (v1, v2)
     {
+        if (Array.isArray(v1))
+        {
+            v2 = new Vector2(v1[2], v1[3]);
+            v1 = new Vector2(v1[0], v1[1]);
+        }
+
         Curve.call(this);
 
         this.v1 = v1;
