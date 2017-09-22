ba2f351544531e82f6817920f0f665f4e1eed117
Richard Davey <rdavey@gmail.com>
2017-09-22 01:34:30 +0100

    Added the consts back into Math

--- v3/src/math/index.js
+++ v3/src/math/index.js
@@ -1,4 +1,16 @@
+var RND = require('./random-data-generator/RandomDataGenerator');
+
 module.exports = {
+
+    //  Consts
+    PI2: Math.PI * 2,
+    TAU: Math.PI * 0.5,
+    EPSILON: 1.0e-6,
+    DEG_TO_RAD: Math.PI / 180,
+    RAD_TO_DEG: 180 / Math.PI,
+
+    //  Random Data Generator
+    RND: new RND(),
 
     //  Collections of functions
     Angle: require('./angle/'),
