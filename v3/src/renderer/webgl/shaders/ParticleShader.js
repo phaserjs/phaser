a7357096848a7d8a2436e71c65e92c4244aba5ab
Richard Davey <rdavey@gmail.com>
2017-09-22 12:07:07 +0100

    Removed backticks so it can build the dist file.

--- v3/src/renderer/webgl/shaders/ParticleShader.js
+++ v3/src/renderer/webgl/shaders/ParticleShader.js
@@ -1,35 +1,43 @@
-module.exports = {
-    vert: `
-        precision mediump float;
-        uniform mat4 u_view_matrix;
+var ParticleShader = function ()
+{
+    var vert = [
+        'precision mediump float;',
+        'uniform mat4 u_view_matrix;',
 
-        attribute vec2 a_position;
-        attribute vec2 a_tex_coord;
-        attribute vec4 a_color;
+        'attribute vec2 a_position;',
+        'attribute vec2 a_tex_coord;',
+        'attribute vec4 a_color;',
 
-        varying vec4 v_color;
-        varying vec2 v_tex_coord;
+        'varying vec4 v_color;',
+        'varying vec2 v_tex_coord;',
 
-        void main()
-        {
-            gl_Position = u_view_matrix * vec4(a_position, 1.0, 1.0);
+        'void main()',
+        '{',
+        '    gl_Position = u_view_matrix * vec4(a_position, 1.0, 1.0);',
 
-            v_color = a_color;
-            v_tex_coord = a_tex_coord;
-        }
-    `,
-    frag: `
-        precision mediump float;
+        '    v_color = a_color;',
+        '    v_tex_coord = a_tex_coord;',
+        '}'
+    ];
 
-        uniform sampler2D u_main_sampler;
+    var frag = [
+        'precision mediump float;',
 
-        varying vec4 v_color;
-        varying vec2 v_tex_coord;
+        'uniform sampler2D u_main_sampler;',
 
-        void main()
-        {
-            gl_FragColor = texture2D(u_main_sampler, v_tex_coord) * v_color;
-        }
+        'varying vec4 v_color;',
+        'varying vec2 v_tex_coord;',
 
-    `
+        'void main()',
+        '{',
+        '    gl_FragColor = texture2D(u_main_sampler, v_tex_coord) * v_color;',
+        '}'
+    ];
+
+    return {
+        vert: vert.join('\n'),
+        frag: frag.join('\n')
+    };
 };
+
+module.exports = ParticleShader();
