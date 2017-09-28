9e8d2394bd1f415afbfb7880f85d7785a5576ec5
Richard Davey <rdavey@gmail.com>
2017-09-28 01:00:42 +0100

    Sorted out the rotateToPath settings

--- v3/src/gameobjects/pathfollower/PathFollower.js
+++ v3/src/gameobjects/pathfollower/PathFollower.js
@@ -1,7 +1,9 @@
 var Class = require('../../utils/Class');
+var DegToRad = require('../../math/DegToRad');
 var GetBoolean = require('../../tween/builder/GetBoolean');
 var GetValue = require('../../utils/object/GetValue');
 var Sprite = require('../sprite/Sprite');
+var TWEEN_CONST = require('../../tween/tween/const');
 var Vector2 = require('../../math/Vector2');
 
 var PathFollower = new Class({
@@ -17,6 +19,9 @@ var PathFollower = new Class({
         this.path = path;
 
         this.rotateToPath = false;
+
+        this.pathRotationVerticalAdjust = false;
+
         this.pathRotationOffset = 0;
 
         this.pathOffset = new Vector2(x, y);
@@ -24,6 +29,8 @@ var PathFollower = new Class({
         this.pathVector = new Vector2();
 
         this.pathTween;
+
+        this._prevDirection = TWEEN_CONST.PLAYING_FORWARD;
     },
 
     setPath: function (path, config)
@@ -45,13 +52,16 @@ var PathFollower = new Class({
         return this;
     },
 
-    //  rotation offset in radians
-    setRotateToPath: function (value, offset)
+    //  rotation offset in degrees
+    setRotateToPath: function (value, offset, verticalAdjust)
     {
         if (offset === undefined) { offset = 0; }
+        if (verticalAdjust === undefined) { verticalAdjust = false; }
 
         this.rotateToPath = value;
+
         this.pathRotationOffset = offset;
+        this.pathRotationVerticalAdjust = verticalAdjust;
 
         return this;
     },
@@ -87,6 +97,7 @@ var PathFollower = new Class({
 
         this.rotateToPath = GetBoolean(config, 'rotateToPath', false);
         this.pathRotationOffset = GetValue(config, 'rotationOffset', 0);
+        this.pathRotationVerticalAdjust = GetBoolean(config, 'verticalAdjust', false);
 
         this.pathTween = this.scene.sys.tweens.addCounter(config);
 
@@ -95,6 +106,16 @@ var PathFollower = new Class({
 
         this.pathOffset.x = this.x - this.pathOffset.x;
         this.pathOffset.y = this.y - this.pathOffset.y;
+
+        this._prevDirection = TWEEN_CONST.PLAYING_FORWARD;
+
+        if (this.rotateToPath)
+        {
+            //  Set the rotation now (in case the tween has a delay on it, etc)
+            var nextPoint = this.path.getPoint(0.1);
+
+            this.rotation = Math.atan2(nextPoint.y - this.y, nextPoint.x - this.x) + DegToRad(this.pathRotationOffset);
+        }
 
         return this;
     },
@@ -141,8 +162,16 @@ var PathFollower = new Class({
 
         var tween = this.pathTween;
 
-        if (tween && tween.isPlaying())
+        if (tween)
         {
+            var tweenData = tween.data[0];
+
+            if (tweenData.state !== TWEEN_CONST.PLAYING_FORWARD && tweenData.state !== TWEEN_CONST.PLAYING_BACKWARD)
+            {
+                //  If delayed, etc then bail out
+                return;
+            }
+
             var pathVector = this.pathVector;
 
             this.path.getPoint(tween.getValue(), pathVector);
@@ -154,9 +183,31 @@ var PathFollower = new Class({
 
             this.setPosition(pathVector.x, pathVector.y);
 
+            var speedX = this.x - oldX;
+            var speedY = this.y - oldY;
+
+            if (speedX === 0 && speedY === 0)
+            {
+                //  Bail out early
+                return;
+            }
+
+            if (tweenData.state !== this._prevDirection)
+            {
+                //  We've changed direction, so don't do a rotate this frame
+                this._prevDirection = tweenData.state;
+
+                return;
+            }
+
             if (this.rotateToPath)
             {
-                this.rotation = Math.atan2(this.y - oldY, this.x - oldX);
+                this.rotation = Math.atan2(speedY, speedX) + DegToRad(this.pathRotationOffset);
+
+                if (this.pathRotationVerticalAdjust)
+                {
+                    this.flipY = (this.rotation !== 0 && tweenData.state === TWEEN_CONST.PLAYING_BACKWARD);
+                }
             }
         }
     }
