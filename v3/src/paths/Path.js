f005380d67969f89a43da9136f420e59b729da5d
Richard Davey <rdavey@gmail.com>
2017-09-29 17:27:53 +0100

    Added Path.getBounds.

--- v3/src/paths/Path.js
+++ v3/src/paths/Path.js
@@ -6,6 +6,7 @@ var EllipseCurve = require('./curves/ellipse/EllipseCurve');
 var GameObjectFactory = require('../scene/plugins/GameObjectFactory');
 var LineCurve = require('./curves/line/LineCurve');
 var MoveTo = require('./MoveTo');
+var Rectangle = require('../geom/rectangle/Rectangle');
 var SplineCurve = require('./curves/spline/SplineCurve');
 var Vector2 = require('../math/Vector2');
 
@@ -110,6 +111,42 @@ var Path = new Class({
         if (clockwise === undefined) { clockwise = false; }
 
         return this.ellipseTo(radius, radius, 0, 360, clockwise, rotation);
+    },
+
+    getBounds: function (out, accuracy)
+    {
+        if (out === undefined) { out = new Rectangle(); }
+        if (accuracy === undefined) { accuracy = 16; }
+
+        out.x = Number.MAX_SAFE_INTEGER;
+        out.y = Number.MAX_SAFE_INTEGER;
+
+        var bounds = new Rectangle();
+        var maxRight = Number.MIN_SAFE_INTEGER;
+        var maxBottom = Number.MIN_SAFE_INTEGER;
+
+        for (var i = 0; i < this.curves.length; i++)
+        {
+            var curve = this.curves[i];
+
+            if (!curve.active)
+            {
+                continue;
+            }
+
+            curve.getBounds(bounds, accuracy);
+
+            out.x = Math.min(out.x, bounds.x);
+            out.y = Math.min(out.y, bounds.y);
+
+            maxRight = Math.max(maxRight, bounds.right);
+            maxBottom = Math.max(maxBottom, bounds.bottom);
+        }
+
+        out.right = maxRight;
+        out.bottom = maxBottom;
+
+        return out;
     },
 
     toJSON: function ()
