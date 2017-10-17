f751d4066a7ecb6f896dd5ec873bc39a9fd32521
Richard Davey <rdavey@gmail.com>
2017-10-17 21:31:28 +0100

    Added Math.FromPercent function

---
+++ v3/src/math/FromPercent.js
@@ -0,0 +1,22 @@
+var Clamp = require('./Clamp');
+
+/**
+ * Return a value based on the range between `min` and `max` and the percentage given.
+ *
+ * @function Phaser.Math.FromPercent
+ * @since 3.0.0
+ *
+ * @param {float} percent - A value between 0 and 1 representing the percentage.
+ * @param {number} min - [description]
+ * @param {number} [max] - [description]
+ *
+ * @return {number} [description]
+ */
+var FromPercent = function (percent, min, max)
+{
+    percent = Clamp(percent, 0, 1);
+
+    return (max - min) * percent;
+};
+
+module.exports = FromPercent;
