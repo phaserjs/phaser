5cb8f98619a4b666f7858ddb3f5dbb055a4ebd25
Richard Davey <rdavey@gmail.com>
2017-10-21 05:05:29 +0100

    Made the index real-time

--- v3/src/math/MinMax2.js
+++ v3/src/math/MinMax2.js
@@ -11,10 +11,25 @@ var MinMax2 = new Class({
 
     initialize:
 
-    function MinMax2 (min, max)
+    function MinMax2 (min, max, steps)
+    {
+        this.min = 0;
+        this.max = 0;
+        this.steps = 0;
+
+        this._current = 0;
+
+        if (min !== undefined)
+        {
+            this.set(min, max, steps);
+        }
+    },
+
+    set: function (min, max, steps)
     {
         if (Array.isArray(min))
         {
+            steps = min[2];
             max = min[1];
             min = min[0];
         }
@@ -24,50 +39,32 @@ var MinMax2 = new Class({
 
             min = (obj.hasOwnProperty('x')) ? obj.x : obj.min;
             max = (obj.hasOwnProperty('y')) ? obj.y : obj.max;
+            steps = obj.steps;
         }
 
         if (min === undefined) { min = 0; }
         if (max === undefined) { max = min; }
+        if (steps === undefined) { steps = 0; }
 
         this.min = min;
         this.max = max;
+        this.steps = steps;
 
         this._current = min;
-    },
-
-    set: function (min, max)
-    {
-        if (Array.isArray(min))
-        {
-            max = min[1];
-            min = min[0];
-        }
-        else if (typeof min === 'object')
-        {
-            var obj = min;
-
-            min = obj.min;
-            max = obj.max;
-        }
-
-        if (min === undefined) { min = 0; }
-        if (max === undefined) { max = min; }
-
-        this.min = min;
-        this.max = max;
 
         return this;
     },
 
     clone: function ()
     {
-        return new MinMax2(this.min, this.max);
+        return new MinMax2(this.min, this.max, this.steps);
     },
 
     copy: function (dest)
     {
         dest.min = this.min;
         dest.max = this.max;
+        dest.steps = this.steps;
 
         return this;
     },
@@ -108,6 +105,46 @@ var MinMax2 = new Class({
     getRandomFloat: function ()
     {
         return FloatBetween(this.min, this.max);
+    },
+
+    getNext: function ()
+    {
+        var value;
+
+        if (this.steps > 0)
+        {
+            value = this._current;
+
+            var i = value + ((this.max - this.min) / this.steps);
+
+            this._current = Wrap(i, this.min, this.max);
+        }
+        else
+        {
+            value = this.getRandom();
+        }
+
+        return value;
+    },
+
+    getNextFloat: function ()
+    {
+        var value;
+
+        if (this.steps > 0)
+        {
+            value = this._current;
+
+            var i = value + ((this.max - this.min) / this.steps);
+
+            this._current = Wrap(i, this.min, this.max);
+        }
+        else
+        {
+            value = this.getRandomFloat();
+        }
+
+        return value;
     },
 
     current: {
