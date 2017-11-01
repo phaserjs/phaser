cf6b6eb418af77f4d02fd27f4c9cc56e6e37db03
Richard Davey <rdavey@gmail.com>
2017-11-01 13:09:48 +0000

    EdgeZone has new updateSource and changeSource methods and seamless argument

--- v3/src/gameobjects/particles/zones/EdgeZone.js
+++ v3/src/gameobjects/particles/zones/EdgeZone.js
@@ -5,22 +5,67 @@ var EdgeZone = new Class({
 
     initialize:
 
-    function EdgeZone (source, quantity, stepRate, yoyo)
+    function EdgeZone (source, quantity, stepRate, yoyo, seamless)
     {
         if (yoyo === undefined) { yoyo = false; }
+        if (seamless === undefined) { seamless = true; }
 
         this.source = source;
 
-        this.points = source.getPoints(quantity, stepRate);
+        this.points = [];
+
+        this.quantity = quantity;
+
+        this.stepRate = stepRate;
 
         this.yoyo = yoyo;
 
         this.counter = -1;
 
-        this._length = this.points.length;
+        this.seamless = seamless;
+
+        this._length = 0;
 
         //  0 = forwards, 1 = backwards
         this._direction = 0;
+
+        this.updateSource();
+    },
+
+    updateSource: function ()
+    {
+        this.points = this.source.getPoints(this.quantity, this.stepRate);
+
+        //  Remove ends?
+        if (this.seamless)
+        {
+            var a = this.points[0];
+            var b = this.points[this.points.length - 1];
+
+            if (a.x === b.x && a.y === b.y)
+            {
+                this.points.pop();
+            }
+        }
+
+        var oldLength = this._length;
+
+        this._length = this.points.length;
+
+        //  Adjust counter if we now have less points than before
+        if (this._length < oldLength && this.counter > this._length)
+        {
+            this.counter = this._length - 1;
+        }
+
+        return this;
+    },
+
+    changeSource: function (source)
+    {
+        this.source = source;
+
+        return this.updateSource();
     },
 
     getPoint: function (particle)
@@ -29,12 +74,12 @@ var EdgeZone = new Class({
         {
             this.counter++;
 
-            if (this.counter === this._length)
+            if (this.counter >= this._length)
             {
                 if (this.yoyo)
                 {
                     this._direction = 1;
-                    this.counter--;
+                    this.counter = this._length - 1;
                 }
                 else
                 {
@@ -62,8 +107,11 @@ var EdgeZone = new Class({
 
         var point = this.points[this.counter];
 
-        particle.x = point.x;
-        particle.y = point.y;
+        if (point)
+        {
+            particle.x = point.x;
+            particle.y = point.y;
+        }
     }
 
 });
