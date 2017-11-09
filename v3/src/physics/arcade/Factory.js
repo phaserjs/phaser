39a4525d0884f6cbc44af22e0cf27c9cae062a46
Richard Davey <rdavey@gmail.com>
2017-11-09 16:31:09 +0000

    Added a Static Physicss Group. A Group that contains only Static Bodies.

--- v3/src/physics/arcade/Factory.js
+++ v3/src/physics/arcade/Factory.js
@@ -1,8 +1,9 @@
 var ArcadeImage = require('./ArcadeImage');
 var ArcadeSprite = require('./ArcadeSprite');
 var Class = require('../../utils/Class');
-var PhysicsGroup = require('./PhysicsGroup');
 var CONST = require('./const');
+var PhysicsGroup = require('./PhysicsGroup');
+var StaticPhysicsGroup = require('./StaticPhysicsGroup');
 
 var Factory = new Class({
 
@@ -19,40 +20,53 @@ var Factory = new Class({
 
     staticImage: function (x, y, key, frame)
     {
-        var image = new ArcadeImage(this.scene, CONST.STATIC_BODY, x, y, key, frame);
+        var image = new ArcadeImage(this.scene, x, y, key, frame);
 
         this.sys.displayList.add(image);
+
+        this.world.enableBody(image, CONST.STATIC_BODY);
 
         return image;
     },
 
     image: function (x, y, key, frame)
     {
-        var image = new ArcadeImage(this.scene, CONST.DYNAMIC_BODY, x, y, key, frame);
+        var image = new ArcadeImage(this.scene, x, y, key, frame);
 
         this.sys.displayList.add(image);
+
+        this.world.enableBody(image, CONST.DYNAMIC_BODY);
 
         return image;
     },
 
     staticSprite: function (x, y, key, frame)
     {
-        var sprite = new ArcadeSprite(this.scene, CONST.STATIC_BODY, x, y, key, frame);
+        var sprite = new ArcadeSprite(this.scene, x, y, key, frame);
 
         this.sys.displayList.add(sprite);
         this.sys.updateList.add(sprite);
+
+        this.world.enableBody(sprite, CONST.STATIC_BODY);
 
         return sprite;
     },
 
     sprite: function (x, y, key, frame)
     {
-        var sprite = new ArcadeSprite(this.scene, CONST.DYNAMIC_BODY, x, y, key, frame);
+        var sprite = new ArcadeSprite(this.scene, x, y, key, frame);
 
         this.sys.displayList.add(sprite);
         this.sys.updateList.add(sprite);
 
+        this.world.enableBody(sprite, CONST.DYNAMIC_BODY);
+
         return sprite;
+    },
+
+    staticGroup: function (children, config)
+    {
+        return new StaticPhysicsGroup(this.world, this.world.scene, children, config);
     },
 
     group: function (children, config)
---
+++ v3/src/physics/arcade/StaticPhysicsGroup.js
@@ -0,0 +1,65 @@
+//  Phaser.Physics.Arcade.StaticPhysicsGroup
+
+var ArcadeSprite = require('./ArcadeSprite');
+var Class = require('../../utils/Class');
+var CONST = require('./const');
+var Group = require('../../gameobjects/group/Group');
+
+var StaticPhysicsGroup = new Class({
+
+    Extends: Group,
+
+    initialize:
+
+    function StaticPhysicsGroup (world, scene, children, config)
+    {
+        if (config === undefined && !Array.isArray(children) && typeof children === 'object')
+        {
+            config = children;
+            children = null;
+        }
+        else if (config === undefined)
+        {
+            config = {};
+        }
+
+        this.world = world;
+
+        config.createCallback = this.createCallback;
+        config.removeCallback = this.removeCallback;
+        config.classType = ArcadeSprite;
+
+        this.physicsType = CONST.STATIC_BODY;
+
+        Group.call(this, scene, children, config);
+    },
+
+    createCallback: function (child)
+    {
+        if (!child.body)
+        {
+            this.world.enableBody(child, CONST.STATIC_BODY);
+        }
+    },
+
+    removeCallback: function (child)
+    {
+        if (child.body)
+        {
+            this.world.disableBody(child);
+        }
+    },
+
+    refresh: function ()
+    {
+        var children = this.children.entries;
+
+        for (var i = 0; i < children.length; i++)
+        {
+            children[i].body.reset();
+        }
+    }
+
+});
+
+module.exports = StaticPhysicsGroup;
