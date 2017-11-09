7c657a916a09a9f837a89567165d1577a430127e
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2017-11-09 14:43:56 +0100

    added a TODO to implement more accurate Web Audio support check

--- v3/src/device/Audio.js
+++ v3/src/device/Audio.js
@@ -35,6 +35,10 @@ var Audio = {
 function init ()
 {
     Audio.audioData = !!(window['Audio']);
+
+    // TODO check if AudioContext is actually implemented
+    // sometimes browsers have a stub object for AudioContext
+    // which will falsely report Web Audio as available
     Audio.webAudio = !!(window['AudioContext'] || window['webkitAudioContext']);
 
     var audioElement = document.createElement('audio');
