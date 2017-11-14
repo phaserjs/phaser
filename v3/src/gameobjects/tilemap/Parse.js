bf1955313519e17e9f5e5eeb29e7ba8bc438fbf1
Michael Hadley <michaelwesthadley@gmail.com>
2017-11-14 15:02:19 -0600

    Tilemap parsers: add ability to insert null instead of blank tile

--- v3/src/gameobjects/tilemap/Parse.js
+++ v3/src/gameobjects/tilemap/Parse.js
@@ -1,20 +1,20 @@
 var Formats = require('./Formats');
 var Parsers = require('./parsers');
 
-var Parse = function (key, mapFormat, mapData, tileWidth, tileHeight, width, height)
+var Parse = function (key, mapFormat, mapData, tileWidth, tileHeight, width, height, insertNull)
 {
     var newMap;
 
     switch(mapFormat)
     {
         case (Formats.TILEMAP_2D_ARRAY):
-            newMap = Parsers.Parse2DArray(key, mapData, tileWidth, tileHeight, width, height);
+            newMap = Parsers.Parse2DArray(key, mapData, tileWidth, tileHeight, width, height, insertNull);
             break;
         case (Formats.TILEMAP_CSV):
-            newMap = Parsers.ParseCSV(key, mapData, tileWidth, tileHeight, width, height);
+            newMap = Parsers.ParseCSV(key, mapData, tileWidth, tileHeight, width, height, insertNull);
             break;
         case (Formats.TILEMAP_TILED_JSON):
-            newMap = Parsers.ParseTiledJSON(key, mapData);
+            newMap = Parsers.ParseTiledJSON(key, mapData, insertNull);
             break;
         default:
             console.warn('Unrecognized tilemap data format: ' + mapFormat);
