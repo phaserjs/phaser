52bb930cdf571ab48844b6651d33de5042f63cd8
Richard Davey <rdavey@gmail.com>
2017-11-17 18:28:44 +0000

    Abort if distance is zero.

--- v3/src/actions/RotateAroundDistance.js
+++ v3/src/actions/RotateAroundDistance.js
@@ -18,6 +18,12 @@ var RotateAroundDistance = function (items, point, angle, distance)
     var x = point.x;
     var y = point.y;
 
+    //  There's nothing to do
+    if (distance === 0)
+    {
+        return items;
+    }
+
     for (var i = 0; i < items.length; i++)
     {
         MathRotateAroundDistance(items[i], x, y, angle, distance);
