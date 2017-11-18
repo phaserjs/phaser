e2960486a9ea8ff24511713fe1f3abae4e5b8cc7
Michael Hadley <michaelwesthadley@gmail.com>
2017-11-18 15:38:46 -0600

    Filling out Tile with some methods & props from v2

--- v3/src/gameobjects/tilemap/Tile.js
+++ v3/src/gameobjects/tilemap/Tile.js
@@ -24,6 +24,18 @@ var Tile = new Class({
         this.width = width;
         this.height = height;
         this.properties = {};
+        this.rotation = 0;
+        this.collideLeft = false;
+        this.collideRight = false;
+        this.collideUp = false;
+        this.collideDown = false;
+        this.faceLeft = false;
+        this.faceRight = false;
+        this.faceTop = false;
+        this.faceBottom = false;
+        this.collisionCallback = null;
+        this.collisionCallbackContext = this;
+        this.scanned = false;
 
         // TODO: update renders to allow for using Components.Tint
         this.tint = 0xFFFFFF;
@@ -38,15 +50,98 @@ var Tile = new Class({
         this.visible = tile.visible;
         this.setFlip(tile.flipX, tile.flipY);
         this.tint = tile.tint;
-
-        // this.collideUp = tile.collideUp;
-        // this.collideDown = tile.collideDown;
-        // this.collideLeft = tile.collideLeft;
-        // this.collideRight = tile.collideRight;
-        // this.collisionCallback = tile.collisionCallback;
-        // this.collisionCallbackContext = tile.collisionCallbackContext;
+        this.rotation = tile.rotation;
+        this.collideUp = tile.collideUp;
+        this.collideDown = tile.collideDown;
+        this.collideLeft = tile.collideLeft;
+        this.collideRight = tile.collideRight;
+        this.collisionCallback = tile.collisionCallback;
+        this.collisionCallbackContext = tile.collisionCallbackContext;
 
         return this;
+    },
+
+    // Does not factor in scroll offset or tilemap layer position
+    containsPoint: function (x, y)
+    {
+        return !(x < this.worldX || y < this.worldY || x > this.right || y > this.bottom);
+    },
+
+    destroy: function ()
+    {
+        this.collisionCallback = undefined;
+        this.collisionCallbackContext = undefined;
+        this.properties = undefined;
+    },
+
+    // Does not factor in scroll offset or tilemap layer position
+    intersects: function (x, y, right, bottom)
+    {
+        return !(right <= this.worldX || bottom <= this.worldY || x >= this.right || y >= this.bottom);
+    },
+
+    isInteresting: function (collides, faces)
+    {
+        if (collides && faces) { return (this.canCollide || this.hasInterestingFace); }
+        else if (collides) { return this.collides; }
+        else if (faces) { return this.hasInterestingFace; }
+        return false;
+    },
+
+    resetCollision: function ()
+    {
+        this.collideLeft = false;
+        this.collideRight = false;
+        this.collideUp = false;
+        this.collideDown = false;
+
+        this.faceTop = false;
+        this.faceBottom = false;
+        this.faceLeft = false;
+        this.faceRight = false;
+    },
+
+    setCollision: function (left, right, up, down)
+    {
+        this.collideLeft = left;
+        this.collideRight = right;
+        this.collideUp = up;
+        this.collideDown = down;
+
+        this.faceLeft = left;
+        this.faceRight = right;
+        this.faceTop = up;
+        this.faceBottom = down;
+    },
+
+    setCollisionCallback: function (callback, context)
+    {
+        this.collisionCallback = callback;
+        this.collisionCallbackContext = context;
+    },
+
+    // True if this tile can collide on any of its faces or has a collision callback set.
+    canCollide: {
+        get: function ()
+        {
+            return (this.collideLeft || this.collideRight || this.collideUp || this.collideDown || this.collisionCallback);
+        }
+    },
+
+    // True if this tile can collide on any of its faces.
+    collides: {
+        get: function ()
+        {
+            return (this.collideLeft || this.collideRight || this.collideUp || this.collideDown);
+        }
+    },
+
+    // True if this tile has any interesting faces
+    hasInterestingFace: {
+        get: function ()
+        {
+            return (this.faceTop || this.faceBottom || this.faceLeft || this.faceRight);
+        }
     },
 
     left: {
