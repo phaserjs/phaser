32766ca509d6963f17d0528a4398b8def4f9451b
Richard Davey <rdavey@gmail.com>
2017-11-21 16:53:55 +0000

    New Matter system handler.

--- v3/src/physics/matter-js/Matter.js
+++ v3/src/physics/matter-js/Matter.js
@@ -1,18 +1,18 @@
 var Class = require('../../utils/Class');
 var Factory = require('./Factory');
-// var World = require('./World');
+var World = require('./World');
 
 var Matter = new Class({
 
     initialize:
 
+    //  Referenced from the Scene PhysicsManager as `system`
+
     function Matter (physicsManager, config)
     {
         this.config = config;
 
-
-
-        // physicsManager.world = new World(physicsManager.scene, config);
+        physicsManager.world = new World(physicsManager.scene, config);
 
         physicsManager.add = new Factory(physicsManager.world);
     }
