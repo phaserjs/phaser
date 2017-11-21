4702ed7400f8ac996cce90ac3727a92c48664e95
Richard Davey <rdavey@gmail.com>
2017-11-21 16:53:44 +0000

    New components index.

---
+++ v3/src/physics/matter-js/components/index.js
@@ -0,0 +1,14 @@
+//  Phaser.Physics.Matter.Body.Components
+
+module.exports = {
+
+    Bounce: require('./Bounce'),
+    Force: require('./Force'),
+    Friction: require('./Friction'),
+    Mass: require('./Mass'),
+    Static: require('./Static'),
+    Sensor: require('./Sensor'),
+    Transform: require('./Transform'),
+    Velocity: require('./Velocity')
+
+};
