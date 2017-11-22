f88732cbb7cef5052630b2dba78b51bd35009d5e
Richard Davey <rdavey@gmail.com>
2017-11-22 14:13:10 +0000

    Added Sleep Start and Sleep End events

--- v3/src/physics/matter-js/events/index.js
+++ v3/src/physics/matter-js/events/index.js
@@ -4,6 +4,8 @@ module.exports = {
 
     COLLISION_ACTIVE: require('./CollisionActiveEvent'),
     COLLISION_END: require('./CollisionEndEvent'),
-    COLLISION_START: require('./CollisionStartEvent')
+    COLLISION_START: require('./CollisionStartEvent'),
+    SLEEP_END: require('./SleepEndEvent'),
+    SLEEP_START: require('./SleepStartEvent')
 
 };
