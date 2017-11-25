432c1c0f6d83e414ca9194e57a6c4feaa84adc99
Michael Hadley <michaelwesthadley@gmail.com>
2017-11-25 08:40:07 -0600

    GetTilesWithin: clip rectangular area to map bounds

--- v3/src/gameobjects/tilemap/components/GetTilesWithin.js
+++ v3/src/gameobjects/tilemap/components/GetTilesWithin.js
@@ -1,13 +1,31 @@
 // TODO: add options for filtering by empty, collides, interestingFace
+// Get tiles within the rectangular area specified. Note: this clips the x, y, w & h to the map's
+// boundries.
 var GetTilesWithin = function (tileX, tileY, width, height, layer)
 {
-    if (tileX === undefined || tileX < 0) { tileX = 0; }
-    if (tileY === undefined || tileY < 0) { tileY = 0; }
-    if (width === undefined || tileX + width > layer.width)
+    if (tileX === undefined) { tileX = 0; }
+    if (tileY === undefined) { tileY = 0; }
+    if (width === undefined) { width = layer.width; }
+    if (height === undefined) { height = layer.height; }
+
+    // Clip x, y to top left of map, while shrinking width/height to match.
+    if (tileX < 0)
+    {
+        width += tileX;
+        tileX = 0;
+    }
+    if (tileY < 0)
+    {
+        height += tileY;
+        tileY = 0;
+    }
+
+    // Clip width and height to bottom right of map.
+    if (tileX + width > layer.width)
     {
         width = Math.max(layer.width - tileX, 0);
     }
-    if (height === undefined || tileY + height > layer.height)
+    if (tileY + height > layer.height)
     {
         height = Math.max(layer.height - tileY, 0);
     }
