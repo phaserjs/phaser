1fddd3de2a4fb085ec7cfbfe525f50e2fc2b768c
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2017-11-27 16:44:23 +0100

    Added setRate method to WebAudioSound class that calculates and sets playback rate based on sound's and manager's detune and rate values

--- v3/src/sound/webaudio/WebAudioSound.js
+++ v3/src/sound/webaudio/WebAudioSound.js
@@ -150,6 +150,16 @@ var WebAudioSound = new Class({
         }
     },
     destroy: function () {
+    },
+    /**
+     * @private
+     */
+    setRate: function () {
+        var cent = 1.0005777895065548; // Math.pow(2, 1/1200);
+        var totalDetune = Math.max(-1200, Math.min(this.currentConfig.detune + this.manager.detune, 1200));
+        var detuneRate = Math.pow(cent, totalDetune);
+        var totalRate = this.currentConfig.rate * this.manager.rate * detuneRate;
+        this.source.playbackRate.setValueAtTime(totalRate, 0);
     }
 });
 /**
