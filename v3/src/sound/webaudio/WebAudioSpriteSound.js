6412a3c3c2b602cc02bb6e2b5dc6b402d593f165
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2017-11-30 18:50:54 +0100

    Added WebAudioSpriteSound class - a Web Audio implementation of audio sprite sound

---
+++ v3/src/sound/webaudio/WebAudioSpriteSound.js
@@ -0,0 +1,6 @@
+var Class = require('../../utils/Class');
+var WebAudioSound = require('./WebAudioSound');
+var WebAudioSpriteSound = new Class({
+    Extends: WebAudioSound
+});
+module.exports = WebAudioSpriteSound;
