39edc8a9ac425c43ebd0bbd69be53f5029a8f3d7
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2017-11-30 18:53:12 +0100

    Defaulting config to empty object and calling super method in WebAudioSpriteSound constructor

--- v3/src/sound/webaudio/WebAudioSpriteSound.js
+++ v3/src/sound/webaudio/WebAudioSpriteSound.js
@@ -1,6 +1,10 @@
 var Class = require('../../utils/Class');
 var WebAudioSound = require('./WebAudioSound');
 var WebAudioSpriteSound = new Class({
-    Extends: WebAudioSound
+    Extends: WebAudioSound,
+    initialize: function WebAudioSpriteSound(manager, key, config) {
+        if (config === void 0) { config = {}; }
+        WebAudioSound.call(this, manager, key, config);
+    }
 });
 module.exports = WebAudioSpriteSound;
