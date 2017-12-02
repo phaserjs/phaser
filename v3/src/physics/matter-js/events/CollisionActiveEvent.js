44bb628381fea3a1028adb2e4becf85212c86eac
Richard Davey <rdavey@gmail.com>
2017-12-02 04:04:55 +0000

    The Pairs come out of objects

--- v3/src/physics/matter-js/events/CollisionActiveEvent.js
+++ v3/src/physics/matter-js/events/CollisionActiveEvent.js
@@ -13,10 +13,10 @@ var CollisionActiveEvent = new Class({
 
         this.pairs = pairs;
 
-        if (pairs.length > 0)
+        if (pairs.pairs.length > 0)
         {
-            this.bodyA = pairs[0].bodyA;
-            this.bodyB = pairs[0].bodyB;
+            this.bodyA = pairs.pairs[0].bodyA;
+            this.bodyB = pairs.pairs[0].bodyB;
         }
     }
 
