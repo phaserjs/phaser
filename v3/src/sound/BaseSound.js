344aec2b86000ef94984c5065a7b1e286cbe1735
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2017-12-05 19:40:25 +0100

    Added resetConfig method where seek config, and later delay config, are reset

--- v3/src/sound/BaseSound.js
+++ v3/src/sound/BaseSound.js
@@ -260,6 +260,13 @@ var BaseSound = new Class({
         this.detune = this.currentConfig.detune;
         // TODO assign other config values to buffer source
     },
+    /**
+     * @protected
+     */
+    resetConfig: function () {
+        this.currentConfig.seek = 0;
+        // TODO add delay
+    },
     fadeTo: function (volume, duration) {
         return null;
     },
