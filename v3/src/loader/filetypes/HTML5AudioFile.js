96cc4337456a6344f63165e6d39fa1429c860640
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2018-01-08 17:16:15 +0100

    Added load method to HTML5AudioFile class

--- v3/src/loader/filetypes/HTML5AudioFile.js
+++ v3/src/loader/filetypes/HTML5AudioFile.js
@@ -1,6 +1,7 @@
 var Class = require('../../utils/Class');
 var File = require('../File');
 var GetFastValue = require('../../utils/object/GetFastValue');
+var GetURL = require('../GetURL');
 
 //  Phaser.Loader.FileTypes.HTML5AudioFile
 
@@ -22,7 +23,29 @@ var HTML5AudioFile = new Class({
             };
 
             File.call(this, fileConfig);
+        },
+
+    //  Called by the Loader, starts the actual file downloading
+    load: function (callback, baseURL)
+    {
+        this.callback = callback;
+
+        this.data = [];
+
+        var instances = (this.config && this.config.instances) || 1;
+
+        for(var i = 0; i < instances; i++)
+        {
+            var audio = new Audio();
+            audio.name = this.key;
+            audio.preload = 'auto';
+            audio.src = GetURL(this, baseURL || '');
+            audio.load();
+
+            this.data.push(audio);
         }
+    }
+
 });
 
 module.exports = HTML5AudioFile;
