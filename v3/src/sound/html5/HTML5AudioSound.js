6909cbba49804db59b107be32e3228661ae30dfa
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2018-01-08 19:47:13 +0100

    Adding HTML5AudioSound class that represents HTML5 Audio implementation of BaseSound

---
+++ v3/src/sound/html5/HTML5AudioSound.js
@@ -0,0 +1,10 @@
+var Class = require('../../utils/Class');
+var BaseSound = require('../BaseSound');
+var HTML5AudioSound = new Class({
+    Extends: BaseSound,
+    initialize: function HTML5AudioSound(manager, key, config) {
+        if (config === void 0) { config = {}; }
+        BaseSound.call(this, manager, key, config);
+    }
+});
+module.exports = HTML5AudioSound;
