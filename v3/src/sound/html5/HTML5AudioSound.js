e9040240f8d480e52918bb5dc021ea678d7eacc6
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2018-01-08 19:54:40 +0100

    Added audio property docs

--- v3/src/sound/html5/HTML5AudioSound.js
+++ v3/src/sound/html5/HTML5AudioSound.js
@@ -4,6 +4,13 @@ var HTML5AudioSound = new Class({
     Extends: BaseSound,
     initialize: function HTML5AudioSound(manager, key, config) {
         if (config === void 0) { config = {}; }
+        /**
+         * Reference to HTML5 Audio tag used for playing sound.
+         *
+         * @private
+         * @property {HTMLAudioElement} audio
+         * @default null
+         */
         this.audio = null;
         BaseSound.call(this, manager, key, config);
     }
