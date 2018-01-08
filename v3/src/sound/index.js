df450a4e170e6cb806c1b24f5aa383728d376c02
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2018-01-08 19:25:26 +0100

    Adding HTML5AudioSoundManager class to index.js

--- v3/src/sound/index.js
+++ v3/src/sound/index.js
@@ -10,6 +10,8 @@ module.exports = {
     WebAudioSound: require('./webaudio/WebAudioSound'),
     WebAudioSoundManager: require('./webaudio/WebAudioSoundManager'),
 
+    HTML5AudioSoundManager: require('./html5/HTML5AudioSoundManager'),
+
     SoundEvent: require('./SoundEvent'),
     SoundValueEvent: require('./SoundValueEvent')
 };
