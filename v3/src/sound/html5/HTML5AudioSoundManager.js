197e370c7ba593a7b0236e37934c9134531a6ab3
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2018-01-08 19:26:32 +0100

    Implemented add method for HTML5AudioSoundManager class

--- v3/src/sound/html5/HTML5AudioSoundManager.js
+++ v3/src/sound/html5/HTML5AudioSoundManager.js
@@ -1,9 +1,15 @@
 var Class = require('../../utils/Class');
 var BaseSoundManager = require('../BaseSoundManager');
+var HTML5AudioSound = require('./HTML5AudioSound');
 var HTML5AudioSoundManager = new Class({
     Extends: BaseSoundManager,
     initialize: function HTML5AudioSoundManager(game) {
         BaseSoundManager.call(this, game);
+    },
+    add: function (key, config) {
+        var sound = new HTML5AudioSound(this, key, config);
+        this.sounds.push(sound);
+        return sound;
     }
 });
 module.exports = HTML5AudioSoundManager;
