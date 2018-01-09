5fe12421cd381e3445f900852ed137e720c62423
Richard Davey <rdavey@gmail.com>
2018-01-09 21:42:10 +0000

    Removed previous version folders.

--- v2/src/stubs/Create.js
+++
@@ -1,7 +0,0 @@
-/**
-* @author       Richard Davey <rich@photonstorm.com>
-* @copyright    2016 Photon Storm Ltd.
-* @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
-*/
-
-Phaser.Create = function () {};
