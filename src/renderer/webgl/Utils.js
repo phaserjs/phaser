15c45bda0e7c223c24fa9cb764666ce4b1658fc5
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2018-01-09 19:51:22 -0300

    BlitterBatch2 base rendering. Still missing Texture binding

--- src/renderer/webgl/Utils.js
+++ src/renderer/webgl/Utils.js
@@ -1,19 +1,23 @@
 module.exports = {
 
-	getTintFromFloats: function (r, g, b, a)
-	{
-		var ur = ((r * 255.0)|0) & 0xFF;
-		var ug = ((g * 255.0)|0) & 0xFF;
-		var ub = ((b * 255.0)|0) & 0xFF;
-		var ua = ((a * 255.0)|0) & 0xFF;
+    getTintFromFloats: function (r, g, b, a)
+    {
+        var ur = ((r * 255.0)|0) & 0xFF;
+        var ug = ((g * 255.0)|0) & 0xFF;
+        var ub = ((b * 255.0)|0) & 0xFF;
+        var ua = ((a * 255.0)|0) & 0xFF;
 
-		return (ua << 24) | (ub << 16) | (ug << 8) | ur;
-	},
+        return (ua << 24) | (ub << 16) | (ug << 8) | ur;
+    },
 
-	getTintAppendFloatAlpha: function (rgb, a)
-	{
-		//var ua = ((a * 255.0)|0) & 0xFF;
-		return 0;
-	}
+    getTintAppendFloatAlpha: function (rgb, a)
+    {
+        var ur = ((rgb >> 16)|0) & 0xff;
+        var ug = ((rgb >> 8)|0) & 0xff;
+        var ub = (rgb|0) & 0xff;
+        var ua = ((a * 255.0)|0) & 0xFF;
+
+        return (ua << 24) | (ub << 16) | (ug << 8) | ur;
+    }
 
 };
--- src/renderer/webgl/renderers/blitterbatch/BlitterBatch2.js
+++ src/renderer/webgl/renderers/blitterbatch/BlitterBatch2.js
@@ -68,12 +68,18 @@ var BlitterBatch = new Class({
             +0.0,
             +0.0
         ]);
+
+        this.vertexViewF32 = new Float32Array(this.vertexData);
+        this.vertexViewU32 = new Uint32Array(this.vertexData);
     },
 
     drawBlitter: function (blitter, camera)
     {
         this.beginDraw(blitter.shader, blitter.renderTarget);
         
+        var vertexViewF32 = this.vertexViewF32;
+        var vertexViewU32 = this.vertexViewU32;
+        var manager = this.manager;
         var list = blitter.getRenderList();
         var length = list.length;
         var cameraMatrix = camera.matrix.matrix;
@@ -98,7 +104,64 @@ var BlitterBatch = new Class({
                 var bob = list[batchOffset + index];
                 var frame = bob.frame;
                 var alpha = bob.alpha;
-                var tint =  Utils.getTintFromFloats(1, 1, 1, bob.alpha);
+                var tint =  Utils.getTintAppendFloatAlpha(0xffffff, bob.alpha);
+                var uvs = frame.uvs;
+                var flipX = bob.flipX;
+                var flipY = bob.flipY;
+                var x = bob.x + frame.x - cameraScrollX + (width * (flipX ? 1.0 : 0.0));
+                var y = bob.y + frame.y - cameraScrollY + (height * (flipY ? 1.0 : 0.0));
+                var width = frame.width * (flipX ? -1.0 : 1.0); 
+                var height = frame.height * (flipY ? -1.0 : 1.0);
+                var xw = x + width;                
+                var yh = y + height;
+                var tx = x * a + y * c + e;
+                var ty = x * b + y * d + f;
+                var txw = xw * a + yh * c + e;
+                var tyh = xw * b + yh * d + f;
+
+                // Bind Texture if texture wasn't bound.
+                // This needs to be here because of multiple
+                // texture atlas.
+
+                // manager.setTexture(frame.texture.source[frame.sourceIndex].glTexture, 0);
+            
+                vertexViewF32[vertexOffset + 0] = tx;
+                vertexViewF32[vertexOffset + 1] = ty;
+                vertexViewF32[vertexOffset + 2] = uvs.x0;
+                vertexViewF32[vertexOffset + 3] = uvs.y0;
+                vertexViewU32[vertexOffset + 4] = tint;
+
+                vertexViewF32[vertexOffset + 5] = tx;
+                vertexViewF32[vertexOffset + 6] = tyh;
+                vertexViewF32[vertexOffset + 7] = uvs.x1;
+                vertexViewF32[vertexOffset + 8] = uvs.y1;
+                vertexViewU32[vertexOffset + 9] = tint;
+
+                vertexViewF32[vertexOffset + 10] = txw;
+                vertexViewF32[vertexOffset + 11] = tyh;
+                vertexViewF32[vertexOffset + 12] = uvs.x2;
+                vertexViewF32[vertexOffset + 13] = uvs.y2;
+                vertexViewU32[vertexOffset + 14] = tint;
+
+                vertexViewF32[vertexOffset + 15] = tx;
+                vertexViewF32[vertexOffset + 16] = ty;
+                vertexViewF32[vertexOffset + 17] = uvs.x0;
+                vertexViewF32[vertexOffset + 18] = uvs.y0;
+                vertexViewU32[vertexOffset + 19] = tint;
+
+                vertexViewF32[vertexOffset + 20] = txw;
+                vertexViewF32[vertexOffset + 21] = tyh;
+                vertexViewF32[vertexOffset + 22] = uvs.x2;
+                vertexViewF32[vertexOffset + 23] = uvs.y2;
+                vertexViewU32[vertexOffset + 24] = tint;
+
+                vertexViewF32[vertexOffset + 25] = txw;
+                vertexViewF32[vertexOffset + 26] = ty;
+                vertexViewF32[vertexOffset + 27] = uvs.x3;
+                vertexViewF32[vertexOffset + 28] = uvs.y3;
+                vertexViewU32[vertexOffset + 29] = tint;
+
+                vertexOffset += 30;
             }
 
             batchOffset += batchSize;
