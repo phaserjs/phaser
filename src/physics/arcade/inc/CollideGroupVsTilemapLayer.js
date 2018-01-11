00cebf8eae26f69210e80f0b9b24e675544e0f03
Michael Hadley <michaelwesthadley@gmail.com>
2018-01-11 14:03:34 -0600

    CollideGroupVsTilemapLayer implementation

--- src/physics/arcade/inc/CollideGroupVsTilemapLayer.js
+++ src/physics/arcade/inc/CollideGroupVsTilemapLayer.js
@@ -1,8 +1,25 @@
 var CollideGroupVsTilemapLayer = function (group, tilemapLayer, collideCallback, processCallback, callbackContext, overlapOnly)
 {
-    // TODO
+    var children = group.getChildren();
 
-    return false;
+    if (children.length === 0)
+    {
+        return false;
+    }
+
+    var didCollide = false;
+
+    for (var i = 0; i < children.length; i++)
+    {
+        if (children[i].body)
+        {
+            if (this.collideSpriteVsTilemapLayer(children[i], tilemapLayer, collideCallback, processCallback, callbackContext, overlapOnly)) {
+                didCollide = true;
+            }
+        }
+    }
+
+    return didCollide;
 };
 
 module.exports = CollideGroupVsTilemapLayer;
