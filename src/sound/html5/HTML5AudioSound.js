cf83880fb05a70c445442de5c03a06e3edc576c8
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2018-01-11 17:35:25 +0100

    Defined tags property that holds reference to audio tags array

--- src/sound/html5/HTML5AudioSound.js
+++ src/sound/html5/HTML5AudioSound.js
@@ -5,6 +5,15 @@ var HTML5AudioSound = new Class({
     initialize: function HTML5AudioSound(manager, key, config) {
         if (config === void 0) { config = {}; }
         /**
+         * An array containing all HTML5 Audio tags that could be used for individual
+         * sound's playback. Number of instances depends on the config value passed
+         * to the Loader#audio method call, default is 1.
+         *
+         * @private
+         * @property {HTMLAudioElement[]} tags
+         */
+        this.tags = manager.game.cache.audio.get(key);
+        /**
          * Reference to HTML5 Audio tag used for playing sound.
          *
          * @private
