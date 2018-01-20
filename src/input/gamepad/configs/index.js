d9da905230198272247b9be56fbc28aaaca2d4a8
Richard Davey <rdavey@gmail.com>
2018-01-20 14:25:28 +0000

    Added XBox360 Controller config (closes #3175)

--- src/input/gamepad/configs/index.js
+++ src/input/gamepad/configs/index.js
@@ -3,6 +3,7 @@
 module.exports = {
 
     DUALSHOCK_4: require('./Sony_PlayStation_DualShock_4'),
-    SNES_USB: require('./SNES_USB_Controller')
+    SNES_USB: require('./SNES_USB_Controller'),
+    XBOX_360: require('./XBox360_Controller')
 
 };
