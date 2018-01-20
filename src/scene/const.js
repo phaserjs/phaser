2b4d6f17c859db7da1e3097bca807d6b1e578fe0
Richard Davey <rdavey@gmail.com>
2018-01-20 04:45:27 +0000

    Removed unused consts

--- src/scene/const.js
+++ src/scene/const.js
@@ -3,13 +3,6 @@ module.exports = {
 
     PENDING: 0,
     INSTALLED: 1,
-
-    BOOT: 0,
-    INIT: 1,
-    PRELOAD: 2,
-    CREATE: 3,
-    UPDATE: 4,
-    RENDER: 5,
-    SHUTDOWN: 6
+    SHUTDOWN: 2
 
 };
