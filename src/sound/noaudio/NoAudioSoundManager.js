fdcc4898cca507cf6fc61996c80c67d5113c3ec8
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2018-01-20 20:01:26 +0100

    Initializing pauseOnBlur property to true

--- src/sound/noaudio/NoAudioSoundManager.js
+++ src/sound/noaudio/NoAudioSoundManager.js
@@ -10,6 +10,7 @@ var NoAudioSoundManager = new Class({
         this.volume = 1;
         this.rate = 1;
         this.detune = 0;
+        this.pauseOnBlur = true;
     }
 });
 module.exports = NoAudioSoundManager;
