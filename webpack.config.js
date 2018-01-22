dfe2599e5549c608974dbd3464f5d2216dabba8b
Richard Davey <rdavey@gmail.com>
2018-01-22 22:51:49 +0000

    Added frag test

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     module: {
         rules: [
           {
-            test: /\.vert$/,
+            test: [ /\.vert$/, /\.frag$/ ],
             use: 'raw-loader'
           }
         ]
