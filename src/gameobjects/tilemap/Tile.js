c7a471e29a06141905d6a48bfae04f2c44e67fee
Michael Hadley <michaelwesthadley@gmail.com>
2018-01-23 07:52:53 -0600

    Convenience for getting Tileset collision group from Tile

--- src/gameobjects/tilemap/Tile.js
+++ src/gameobjects/tilemap/Tile.js
@@ -230,6 +230,17 @@ var Tile = new Class({
     },
 
     /**
+     * The collision group for this Tile, defined within the Tileset. This returns a reference to
+     * the collision group stored within the Tileset, so any modification of the returned object
+     * will impact all tiles that have the same index as this tile.
+     * @returns {object|null} tileset
+     */
+    getCollisionGroup: function ()
+    {
+        return this.tileset ? this.tileset.getTileCollisionGroup(this.index) : null;
+    },
+
+    /**
      * Clean up memory.
      */
     destroy: function ()
