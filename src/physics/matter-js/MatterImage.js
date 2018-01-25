2b20c8bbfb00de990f13eee7ee08ca4d8d513984
Michael Hadley <michaelwesthadley@gmail.com>
2018-01-25 16:05:49 -0600

    Add matter component for inertia - useful for platformers

--- src/physics/matter-js/MatterImage.js
+++ src/physics/matter-js/MatterImage.js
@@ -16,6 +16,7 @@ var MatterImage = new Class({
         Components.Force,
         Components.Friction,
         Components.Gravity,
+        Components.Inertia,
         Components.Mass,
         Components.Sensor,
         Components.SetBody,
