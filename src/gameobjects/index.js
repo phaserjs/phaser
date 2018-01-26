feee8e34e8e4f3ba06a13ee1b6d8b4dbbc39e62d
Richard Davey <rich@photonstorm.com>
2018-01-26 05:25:30 +0000

    Moved Container out of prod for now.

--- src/gameobjects/index.js
+++ src/gameobjects/index.js
@@ -13,7 +13,6 @@ var GameObjects = {
 
     BitmapText: require('./bitmaptext/static/BitmapText'),
     Blitter: require('./blitter/Blitter'),
-    Container: require('./container/Container'),
     DynamicBitmapText: require('./bitmaptext/dynamic/DynamicBitmapText'),
     DynamicTilemapLayer: require('./tilemap/dynamiclayer/DynamicTilemapLayer'),
     Graphics: require('./graphics/Graphics.js'),
@@ -35,7 +34,6 @@ var GameObjects = {
 
     Factories: {
         Blitter: require('./blitter/BlitterFactory'),
-        Container: require('./container/ContainerFactory'),
         DynamicBitmapText: require('./bitmaptext/dynamic/DynamicBitmapTextFactory'),
         Graphics: require('./graphics/GraphicsFactory'),
         Group: require('./group/GroupFactory'),
@@ -53,7 +51,6 @@ var GameObjects = {
 
     Creators: {
         Blitter: require('./blitter/BlitterCreator'),
-        Container: require('./container/ContainerCreator'),
         DynamicBitmapText: require('./bitmaptext/dynamic/DynamicBitmapTextCreator'),
         Graphics: require('./graphics/GraphicsCreator'),
         Group: require('./group/GroupCreator'),
