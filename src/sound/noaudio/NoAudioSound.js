1835217d7e8d00478bb25ca1f41e56af77dc3cb8
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2018-01-26 14:51:34 +0100

    Added NoAudioSound class constructor docs

--- src/sound/noaudio/NoAudioSound.js
+++ src/sound/noaudio/NoAudioSound.js
@@ -2,8 +2,25 @@ var Class = require('../../utils/Class');
 var EventEmitter = require('eventemitter3');
 var Extend = require('../../utils/object/Extend');
 var BaseSound = require('../BaseSound');
+/*!
+ * @author Pavle Goloskokovic <pgoloskokovic@gmail.com> (http://prunegames.com)
+ */
 var NoAudioSound = new Class({
     Extends: EventEmitter,
+    /**
+     * No audio implementation of the sound. It is used if audio has been
+     * disabled in the game config or the device doesn't support any audio.
+     *
+     * It represents a graceful degradation of sound logic that provides
+     * minimal functionality and prevents Phaser projects that use audio from
+     * breaking on devices that don't support any audio playback technologies.
+     *
+     * @class Phaser.Sound.NoAudioSound
+     * @constructor
+     * @param {Phaser.Sound.NoAudioSoundManager} manager - Reference to the current sound manager instance.
+     * @param {string} key - Asset key for the sound.
+     * @param {ISoundConfig} [config={}] - An optional config object containing default sound settings.
+     */
     initialize: function NoAudioSound(manager, key, config) {
         EventEmitter.call(this);
         this.manager = manager;
