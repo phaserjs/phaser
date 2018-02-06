0ebe23e396e845078317d7505d213cb757438e80
Richard Davey <rdavey@gmail.com>
2018-02-06 19:10:44 +0000

    Added jsdocs

--- src/gameobjects/pathfollower/PathFollowerFactory.js
+++ src/gameobjects/pathfollower/PathFollowerFactory.js
@@ -1,14 +1,22 @@
-var PathFollower = require('./PathFollower');
 var GameObjectFactory = require('../GameObjectFactory');
+var PathFollower = require('./PathFollower');
 
-//  When registering a factory function 'this' refers to the GameObjectFactory context.
-//  
-//  There are several properties available to use:
-//  
-//  this.scene - a reference to the Scene that owns the GameObjectFactory
-//  this.displayList - a reference to the Display List the Scene owns
-//  this.updateList - a reference to the Update List the Scene owns
-
+/**
+ * Creates a new PathFollower Game Object and adds it to the Scene.
+ *
+ * Note: This method will only be available if the PathFollower Game Object has been built into Phaser.
+ *
+ * @method Phaser.GameObjects.GameObjectFactory#follower
+ * @since 3.0.0
+ *
+ * @param {Phaser.Curves.Path} path - The Path this PathFollower is connected to.
+ * @param {number} x - The horizontal position of this Game Object in the world.
+ * @param {number} y - The vertical position of this Game Object in the world.
+ * @param {string} texture - The key of the Texture this Game Object will use to render with, as stored in the Texture Manager.
+ * @param {string|integer} [frame] - An optional frame from the Texture this Game Object is rendering with.
+ * 
+ * @return {Phaser.GameObjects.PathFollower} The Game Object that was created.
+ */
 GameObjectFactory.register('follower', function (path, x, y, key, frame)
 {
     var sprite = new PathFollower(this.scene, path, x, y, key, frame);
@@ -18,3 +26,11 @@ GameObjectFactory.register('follower', function (path, x, y, key, frame)
 
     return sprite;
 });
+
+//  When registering a factory function 'this' refers to the GameObjectFactory context.
+//  
+//  There are several properties available to use:
+//  
+//  this.scene - a reference to the Scene that owns the GameObjectFactory
+//  this.displayList - a reference to the Display List the Scene owns
+//  this.updateList - a reference to the Update List the Scene owns
