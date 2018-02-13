8afeafd66b3767e2ae7aad19a9766939be312f6d
Richard Davey <rich@photonstorm.com>
2018-02-13 08:09:13 +0000

    SetHitArea now calls setInteractive, fixing Group.createMultiple when a hitArea has been set.

--- src/actions/SetHitArea.js
+++ src/actions/SetHitArea.js
@@ -20,7 +20,7 @@ var SetHitArea = function (items, hitArea, hitAreaCallback)
 {
     for (var i = 0; i < items.length; i++)
     {
-        items[i].setHitArea(hitArea, hitAreaCallback);
+        items[i].setInteractive(hitArea, hitAreaCallback);
     }
 
     return items;
