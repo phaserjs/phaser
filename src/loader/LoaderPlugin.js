26403aa84fe7731fe30be2bf75f6d66978ed11d9
Richard Davey <rich@photonstorm.com>
2018-02-13 05:54:43 +0000

    Added setCORS method.

--- src/loader/LoaderPlugin.js
+++ src/loader/LoaderPlugin.js
@@ -278,6 +278,23 @@ var LoaderPlugin = new Class({
     /**
      * [description]
      *
+     * @method Phaser.Loader.LoaderPlugin#setCORS
+     * @since 3.0.0
+     *
+     * @param {string} crossOrigin - [description]
+     *
+     * @return {Phaser.Loader.LoaderPlugin} This Loader object.
+     */
+    setCORS: function (crossOrigin)
+    {
+        this.crossOrigin = crossOrigin;
+
+        return this;
+    },
+
+    /**
+     * [description]
+     *
      * @method Phaser.Loader.LoaderPlugin#addFile
      * @since 3.0.0
      *
