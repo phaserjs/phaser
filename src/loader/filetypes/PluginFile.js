6a77790aef16d2e6ab6b37e239d54699aca3e5cc
Niklas Berg <niklas.b3rg@gmail.com>
2018-02-14 23:35:05 +0100

    load.plugin now accepts class as an argument besides an url-string

--- src/loader/filetypes/PluginFile.js
+++ src/loader/filetypes/PluginFile.js
@@ -34,6 +34,13 @@ var PluginFile = new Class({
 
     function PluginFile (key, url, path, xhrSettings)
     {
+        // If the url variable refers to a class, add the plugin directly
+        if(typeof url === "function"){
+            this.key = key;
+            window[key] = url;
+            window[key].register(PluginManager);
+        }
+
         var fileKey = (typeof key === 'string') ? key : GetFastValue(key, 'key', '');
 
         var fileConfig = {
