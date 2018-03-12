be81ddfd5192271599467f9cc51220c606a69eb4
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2018-03-12 13:30:05 -0300

    Fixed issue on RenderTexture canvas clear. The clearRect was affected by previous transformations

--- src/gameobjects/rendertexture/RenderTextureCanvas.js
+++ src/gameobjects/rendertexture/RenderTextureCanvas.js
@@ -13,7 +13,10 @@ var RenderTextureCanvas = {
 
     clear: function ()
     {
+        this.context.save();
+        this.context.setTransform(1, 0, 0, 1, 0, 0);
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.restore();
         return this;
     },
 
