c7fd31d3d200057d5f06050ed19c5f111d1bf862
samme <samme@users.noreply.github.com>
2018-03-12 07:29:04 -0700

    Note samme

--- src/actions/WrapInRectangle.js
+++ src/actions/WrapInRectangle.js
@@ -1,5 +1,6 @@
 /**
  * @author       Richard Davey <rich@photonstorm.com>
+ * @author       samme <samme.npm@gmail.com>
  * @copyright    2018 Photon Storm Ltd.
  * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
  */
