c251eceaf6649d008aebb649bb4dc2d07b5e70fe
Richard Davey <rich@photonstorm.com>
2018-04-04 14:43:53 +0100

    jsdoc fixes

--- src/gameobjects/graphics/GraphicsFactory.js
+++ src/gameobjects/graphics/GraphicsFactory.js
@@ -15,7 +15,7 @@ var GameObjectFactory = require('../GameObjectFactory');
  * @method Phaser.GameObjects.GameObjectFactory#graphics
  * @since 3.0.0
  *
- * @param {object} config - [description]
+ * @param {object} [config] - [description]
  * 
  * @return {Phaser.GameObjects.Graphics} The Game Object that was created.
  */
