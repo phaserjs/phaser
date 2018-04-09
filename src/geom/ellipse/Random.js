5a2f4f81425c8ff8f22ed4bf7204d07aa2bb3a25
Mike Carter <mike@buddasworld.co.uk>
2018-04-09 22:15:16 +0200

    Fix for Ellipse area calculation when returning a random()
    random() didn't return a point anywhere in the Ellipse area.
    
    Full details and screenshots (before & after) can be found on https://github.com/photonstorm/phaser-ce/issues/522 assuming the issue is the same in Phaser 3.

--- src/geom/ellipse/Random.js
+++ src/geom/ellipse/Random.js
@@ -26,8 +26,8 @@ var Random = function (ellipse, out)
     var p = Math.random() * Math.PI * 2;
     var s = Math.sqrt(Math.random());
 
-    out.x = ellipse.x + ((s * Math.cos(p)) * ellipse.width / 2);
-    out.y = ellipse.y + ((s * Math.sin(p)) * ellipse.height / 2);
+    out.x = ellipse.x + ((s * Math.cos(p)) * ellipse.width);
+    out.y = ellipse.y + ((s * Math.sin(p)) * ellipse.height);
 
     return out;
 };
