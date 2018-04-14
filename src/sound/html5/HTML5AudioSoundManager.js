920e083a8a4f4d92a505eaf49e359d50e7f99745
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2018-04-14 18:54:01 +0200

    Updating sound duration only if it is not set yet (equal to 0)

--- src/sound/html5/HTML5AudioSoundManager.js
+++ src/sound/html5/HTML5AudioSoundManager.js
@@ -227,7 +227,11 @@ var HTML5AudioSoundManager = new Class({
         {
             this.forEachActiveSound(function (sound)
             {
-                sound.duration = sound.tags[0].duration;
+                if(sound.duration === 0)
+                {
+                    sound.duration = sound.tags[0].duration;
+                }
+
                 sound.totalDuration = sound.tags[0].duration;
             });
 
