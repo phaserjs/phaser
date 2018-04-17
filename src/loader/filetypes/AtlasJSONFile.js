2c3f13246d7d1e8c5a2b0ab62f064430faa48238
NemoStein <contato@nemostein.com.br>
2018-04-17 10:31:52 -0300

    Updated jsdocs (all XHR parameters are optional in Phaser.Loader.FileTypes)

--- src/loader/filetypes/AtlasJSONFile.js
+++ src/loader/filetypes/AtlasJSONFile.js
@@ -18,8 +18,8 @@ var JSONFile = require('./JSONFile.js');
  * @param {string} textureURL - The url to load the texture file from.
  * @param {string} atlasURL - The url to load the atlas file from.
  * @param {string} path - The path of the file.
- * @param {XHRSettingsObject} textureXhrSettings - Optional texture file specific XHR settings.
- * @param {XHRSettingsObject} atlasXhrSettings - Optional atlas file specific XHR settings.
+ * @param {XHRSettingsObject} [textureXhrSettings] - Optional texture file specific XHR settings.
+ * @param {XHRSettingsObject} [atlasXhrSettings] - Optional atlas file specific XHR settings.
  *
  * @return {object} An object containing two File objects to be added to the loader.
  */
--- src/loader/filetypes/ImageFile.js
+++ src/loader/filetypes/ImageFile.js
@@ -23,8 +23,8 @@ var GetFastValue = require('../../utils/object/GetFastValue');
  * @param {string} key - [description]
  * @param {string} url - [description]
  * @param {string} path - [description]
- * @param {XHRSettingsObject} xhrSettings - [description]
- * @param {object} config - [description]
+ * @param {XHRSettingsObject} [xhrSettings] - [description]
+ * @param {object} [config] - [description]
  */
 var ImageFile = new Class({
 
