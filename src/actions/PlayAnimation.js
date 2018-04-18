ead3537d45d154549223e2a4170559b13ae96f9a
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-04-19 08:31:21 +1000

    Added docs for PlayAnimation

--- src/actions/PlayAnimation.js
+++ src/actions/PlayAnimation.js
@@ -13,8 +13,8 @@
  * @generic {Phaser.GameObjects.GameObject[]} G - [items,$return]
  *
  * @param {(array|Phaser.GameObjects.GameObject[])} items - An array of Game Objects. The contents of this array are updated by this Action.
- * @param {string} key - [description]
- * @param {(string|integer)} [startFrame] - [description]
+ * @param {string} key - The name of the animation to play
+ * @param {(string|integer)} [startFrame] - The starting frame of the animation with the given key
  *
  * @return {(array|Phaser.GameObjects.GameObject[])} The array of Game Objects that was passed to this Action.
  */
