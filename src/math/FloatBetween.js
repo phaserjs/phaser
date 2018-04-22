9701d68af2edc00df55f9e6eb78bd0172b2da951
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-04-22 16:59:25 +1000

    Fill all description tags for Phaser.Math.FloatBetween

--- src/math/FloatBetween.js
+++ src/math/FloatBetween.js
@@ -5,15 +5,15 @@
  */
 
 /**
- * Generate a random floating point number between the given maximum and minimum bounds with the minimum bound inclusive, maximum bound exclusive.
+ * [description]
  *
  * @function Phaser.Math.FloatBetween
  * @since 3.0.0
  *
- * @param {float} min - The minimum bound of the generated float (inclusive).
- * @param {float} max - The maximum bound of the generated float (exclusive).
+ * @param {float} min - [description]
+ * @param {float} max - [description]
  *
- * @return {float} The randomly generated float.
+ * @return {float} [description]
  */
 var FloatBetween = function (min, max)
 {
