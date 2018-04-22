0a422fce0cfb31b52711d1511b6d37648f07f173
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-04-22 17:03:47 +1000

    Add full stops at the end of doc sentences

--- src/math/Difference.js
+++ src/math/Difference.js
@@ -13,7 +13,7 @@
  * @param {number} a - The first number in the calculation.
  * @param {number} b - The second number in the calculation.
  *
- * @return {number} The positive difference of the two given numbers
+ * @return {number} The positive difference of the two given numbers.
  */
 var Difference = function (a, b)
 {
