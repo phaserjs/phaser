544daa8d4ab72396edc9b64a2f96f80de21a820e
Chris Andrew <chris@hexus.io>
2018-06-01 11:31:37 +0100

    Documented the DisplayList and UpdateList plugins, and the BuildGameObject function.

--- src/gameobjects/rendertexture/RenderTextureCreator.js
+++ src/gameobjects/rendertexture/RenderTextureCreator.js
@@ -12,10 +12,10 @@ var RenderTexture = require('./RenderTexture');
 /**
  * @typedef {object} RenderTextureConfig
  *
- * @property {number} [x=0] - [description]
- * @property {number} [y=0] - [description]
- * @property {number} [width=32] - [description]
- * @property {number} [height=32] - [description]
+ * @property {number} [x=0] - The x coordinate of the RenderTexture's position.
+ * @property {number} [y=0] - The y coordinate of the RenderTexture's position.
+ * @property {number} [width=32] - The width of the RenderTexture.
+ * @property {number} [height=32] - The height of the RenderTexture.
  */
 
 /**
