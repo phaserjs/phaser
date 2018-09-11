640d90cb093be05fc26e2ca51becd6d04c6ef4fb
Richard Davey <rich@photonstorm.com>
2018-09-11 15:34:03 +0100

    Added jsdocs

--- src/gameobjects/shape/Shape.js
+++ src/gameobjects/shape/Shape.js
@@ -33,8 +33,8 @@ var Line = require('../../geom/line/Line');
  * @extends Phaser.GameObjects.Components.Visible
  *
  * @param {Phaser.Scene} scene - The Scene to which this Game Object belongs. A Game Object can only belong to one Scene at a time.
- * @param {number} x - The horizontal position of this Game Object in the world.
- * @param {number} y - The vertical position of this Game Object in the world.
+ * @param {string} [type] - The internal type of the Shape.
+ * @param {any} [data] - The data of the source shape geometry, if any.
  */
 var Shape = new Class({
 
@@ -63,29 +63,143 @@ var Shape = new Class({
 
         GameObject.call(this, scene, type);
 
+        /**
+         * The source Shape data. Typically a geometry object.
+         * You should not manipulate this directly.
+         *
+         * @name Phaser.GameObjects.Shape#data
+         * @type {any}
+         * @readonly
+         * @since 3.13.0
+         */
         this.data = data;
 
-        //  Holds earcut polygon fill data
+        /**
+         * Holds the polygon path data for filled rendering.
+         *
+         * @name Phaser.GameObjects.Shape#pathData
+         * @type {number[]}
+         * @readonly
+         * @since 3.13.0
+         */
         this.pathData = [];
+
+        /**
+         * Holds the earcut polygon path index data for filled rendering.
+         *
+         * @name Phaser.GameObjects.Shape#pathIndexes
+         * @type {integer[]}
+         * @readonly
+         * @since 3.13.0
+         */
         this.pathIndexes = [];
 
+        /**
+         * The fill color used by this Shape.
+         *
+         * @name Phaser.GameObjects.Shape#fillColor
+         * @type {number}
+         * @since 3.13.0
+         */
         this.fillColor = 0xffffff;
+
+        /**
+         * The fill alpha value used by this Shape.
+         *
+         * @name Phaser.GameObjects.Shape#fillAlpha
+         * @type {number}
+         * @since 3.13.0
+         */
         this.fillAlpha = 1;
 
-        this.lineWidth = 1;
+        /**
+         * The stroke color used by this Shape.
+         *
+         * @name Phaser.GameObjects.Shape#strokeColor
+         * @type {number}
+         * @since 3.13.0
+         */
         this.strokeColor = 0xffffff;
+
+        /**
+         * The stroke alpha value used by this Shape.
+         *
+         * @name Phaser.GameObjects.Shape#strokeAlpha
+         * @type {number}
+         * @since 3.13.0
+         */
         this.strokeAlpha = 1;
 
+        /**
+         * The stroke line width used by this Shape.
+         *
+         * @name Phaser.GameObjects.Shape#lineWidth
+         * @type {number}
+         * @since 3.13.0
+         */
+        this.lineWidth = 1;
+
+        /**
+         * Controls if this Shape is filled or not.
+         * Note that some Shapes do not support being filled (such as Line shapes)
+         *
+         * @name Phaser.GameObjects.Shape#isFilled
+         * @type {boolean}
+         * @since 3.13.0
+         */
         this.isFilled = false;
+
+        /**
+         * Controls if this Shape is stroked or not.
+         * Note that some Shapes do not support being stroked (such as Iso Box shapes)
+         *
+         * @name Phaser.GameObjects.Shape#isStroked
+         * @type {boolean}
+         * @since 3.13.0
+         */
         this.isStroked = false;
 
+        /**
+         * Controls if this Shape path is closed during rendering when stroked.
+         * Note that some Shapes are always closed when stroked (such as Ellipse shapes)
+         *
+         * @name Phaser.GameObjects.Shape#closePath
+         * @type {boolean}
+         * @since 3.13.0
+         */
         this.closePath = true;
 
+        /**
+         * Private internal value.
+         * A Line used when parsing internal path data to avoid constant object re-creation.
+         *
+         * @name Phaser.GameObjects.Curve#_tempLine
+         * @type {Phaser.Geom.Line}
+         * @private
+         * @since 3.13.0
+         */
         this._tempLine = new Line();
 
         this.initPipeline();
     },
 
+    /**
+     * Sets the fill color and alpha for this Shape.
+     * 
+     * If you wish for the Shape to not be filled then call this method with no arguments, or just set `isFilled` to `false`.
+     * 
+     * Note that some Shapes do not support fill colors, such as the Line shape.
+     * 
+     * This call can be chained.
+     *
+     * @method Phaser.GameObjects.Shape#setFillStyle
+     * @since 3.13.0
+     * 
+     * @param {number} [color] - The color used to fill this shape. If not provided the Shape will not be filled.
+     * @param {number} [alpha=1] - The alpha value used when filling this shape, if a fill color is given.
+     *
+     * @return {this} This Game Object instance.
+     */
     setFillStyle: function (color, alpha)
     {
         if (alpha === undefined) { alpha = 1; }
@@ -104,6 +218,23 @@ var Shape = new Class({
         return this;
     },
 
+    /**
+     * Sets the stroke color and alpha for this Shape.
+     * 
+     * If you wish for the Shape to not be stroked then call this method with no arguments, or just set `isStroked` to `false`.
+     * 
+     * Note that some Shapes do not support being stroked, such as the Iso Box shape.
+     * 
+     * This call can be chained.
+     *
+     * @method Phaser.GameObjects.Shape#setStrokeStyle
+     * @since 3.13.0
+     * 
+     * @param {number} [color] - The color used to stroke this shape. If not provided the Shape will not be stroked.
+     * @param {number} [alpha=1] - The alpha value used when stroking this shape, if a stroke color is given.
+     *
+     * @return {this} This Game Object instance.
+     */
     setStrokeStyle: function (lineWidth, color, alpha)
     {
         if (alpha === undefined) { alpha = 1; }
@@ -123,6 +254,19 @@ var Shape = new Class({
         return this;
     },
 
+    /**
+     * Sets if this Shape path is closed during rendering when stroked.
+     * Note that some Shapes are always closed when stroked (such as Ellipse shapes)
+     * 
+     * This call can be chained.
+     *
+     * @method Phaser.GameObjects.Shape#setClosePath
+     * @since 3.13.0
+     * 
+     * @param {boolean} value - Set to `true` if the Shape should be closed when stroked, otherwise `false`.
+     *
+     * @return {this} This Game Object instance.
+     */
     setClosePath: function (value)
     {
         this.closePath = value;
