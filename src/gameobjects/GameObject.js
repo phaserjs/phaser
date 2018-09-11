8c4e2ff09dc1dde4e541be7c3b6db358c1b2c914
James Simpson <james33@gmail.com>
2018-09-11 11:22:29 -0500

    Fix game object memory leak
    When calling `destroy` directly on a game object, the `shutdown` event for this game object on the scene never gets turned off, thus leading to a memory leak.

--- src/gameobjects/GameObject.js
+++ src/gameobjects/GameObject.js
@@ -560,6 +560,8 @@ var GameObject = new Class({
 
         //  Tell the Scene to re-sort the children
         sys.queueDepthSort();
+        
+        this.scene.sys.events.off('shutdown', this.destroy, this);
 
         this.active = false;
         this.visible = false;
