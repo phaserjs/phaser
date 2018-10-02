e4c38215cd982077e895a05781e3cb4687812d8a
Richard Davey <rich@photonstorm.com>
2018-10-02 11:33:05 +0100

    The `RequestAnimationFrame.step` and `stepTimeout` functions have been updated so that the new Frame is requested from raf before the main game step is called. This allows you to now stop the raf callback from within the game update or render loop. Fix #3952

--- src/dom/RequestAnimationFrame.js
+++ src/dom/RequestAnimationFrame.js
@@ -94,14 +94,14 @@ var RequestAnimationFrame = new Class({
          */
         this.step = function step (timestamp)
         {
-            // DOMHighResTimeStamp
+            //  DOMHighResTimeStamp
             _this.lastTime = _this.tick;
 
             _this.tick = timestamp;
 
-            _this.callback(timestamp);
-
             _this.timeOutID = window.requestAnimationFrame(step);
+
+            _this.callback(timestamp);
         };
 
         /**
@@ -122,9 +122,9 @@ var RequestAnimationFrame = new Class({
 
             _this.tick = d;
 
-            _this.callback(d);
-
             _this.timeOutID = window.setTimeout(stepTimeout, delay);
+
+            _this.callback(d);
         };
     },
 
