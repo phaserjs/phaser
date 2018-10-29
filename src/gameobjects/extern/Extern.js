d912189b91a96d4661b3c4e5138a40d62bd07fcc
Richard Davey <rich@photonstorm.com>
2018-10-29 23:07:30 +0000

    Added the Extern Game Object

---
+++ src/gameobjects/extern/Extern.js
@@ -0,0 +1,76 @@
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Class = require('../../utils/Class');
+var Components = require('../components');
+var GameObject = require('../GameObject');
+var ExternRender = require('./ExternRender');
+
+/**
+ * @classdesc
+ * An Extern Game Object.
+ *
+ * @class Extern
+ * @extends Phaser.GameObjects.GameObject
+ * @memberof Phaser.GameObjects
+ * @constructor
+ * @since 3.16.0
+ *
+ * @extends Phaser.GameObjects.Components.Alpha
+ * @extends Phaser.GameObjects.Components.BlendMode
+ * @extends Phaser.GameObjects.Components.Depth
+ * @extends Phaser.GameObjects.Components.Flip
+ * @extends Phaser.GameObjects.Components.Origin
+ * @extends Phaser.GameObjects.Components.ScaleMode
+ * @extends Phaser.GameObjects.Components.ScrollFactor
+ * @extends Phaser.GameObjects.Components.Size
+ * @extends Phaser.GameObjects.Components.Texture
+ * @extends Phaser.GameObjects.Components.Tint
+ * @extends Phaser.GameObjects.Components.Transform
+ * @extends Phaser.GameObjects.Components.Visible
+ *
+ * @param {Phaser.Scene} scene - The Scene to which this Game Object belongs. A Game Object can only belong to one Scene at a time.
+ */
+var Extern = new Class({
+
+    Extends: GameObject,
+
+    Mixins: [
+        Components.Alpha,
+        Components.BlendMode,
+        Components.Depth,
+        Components.Flip,
+        Components.Origin,
+        Components.ScaleMode,
+        Components.ScrollFactor,
+        Components.Size,
+        Components.Texture,
+        Components.Tint,
+        Components.Transform,
+        Components.Visible,
+        ExternRender
+    ],
+
+    initialize:
+
+    function Extern (scene)
+    {
+        GameObject.call(this, scene, 'Extern');
+    },
+
+    preUpdate: function (time, delta)
+    {
+        //  override this!
+    },
+
+    render: function (renderer, camera, calcMatrix)
+    {
+        //  override this!
+    }
+     
+});
+
+module.exports = Extern;
---
+++ src/gameobjects/extern/ExternFactory.js
@@ -0,0 +1,41 @@
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2018 Photon Storm Ltd.
+ * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
+ */
+
+var Extern = require('./Extern');
+var GameObjectFactory = require('../GameObjectFactory');
+
+/**
+ * Creates a new Extern Game Object and adds it to the Scene.
+ *
+ * Note: This method will only be available if the Extern Game Object has been built into Phaser.
+ *
+ * @method Phaser.GameObjects.GameObjectFactory#extern
+ * @since 3.16.0
+ *
+ * @param {number} x - The horizontal position of this Game Object in the world.
+ * @param {number} y - The vertical position of this Game Object in the world.
+ * @param {string} texture - The key of the Texture this Game Object will use to render with, as stored in the Texture Manager.
+ * @param {(string|integer)} [frame] - An optional frame from the Texture this Game Object is rendering with.
+ *
+ * @return {Phaser.GameObjects.Extern} The Game Object that was created.
+ */
+GameObjectFactory.register('extern', function ()
+{
+    var extern = new Extern(this.scene);
+
+    this.displayList.add(extern);
+    this.updateList.add(extern);
+
+    return extern;
+});
+
+//  When registering a factory function 'this' refers to the GameObjectFactory context.
+//
+//  There are several properties available to use:
+//
+//  this.scene - a reference to the Scene that owns the GameObjectFactory
+//  this.displayList - a reference to the Display List the Scene owns
+//  this.updateList - a reference to the Update List the Scene owns
