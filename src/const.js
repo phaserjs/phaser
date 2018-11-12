2f4f0d89addc009ceb6498bb475d2bc1a8f45aa7
Richard Davey <rich@photonstorm.com>
2018-11-12 22:22:26 +0000

    Bumped version.

--- src/const.js
+++ src/const.js
@@ -20,7 +20,7 @@ var CONST = {
      * @type {string}
      * @since 3.0.0
      */
-    VERSION: '3.16.0 Beta 1',
+    VERSION: '3.16.0 Beta 2',
 
     BlendModes: require('./renderer/BlendModes'),
 
