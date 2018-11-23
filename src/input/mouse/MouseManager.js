69a0dcc9cffd25d804f632b9d161e98dc736a5b3
Richard Davey <rich@photonstorm.com>
2018-11-23 16:23:47 +0000

    Remove debug data.

--- src/input/mouse/MouseManager.js
+++ src/input/mouse/MouseManager.js
@@ -8,9 +8,6 @@ var Class = require('../../utils/Class');
 var Features = require('../../device/Features');
 var NOOP = require('../../utils/Class');
 
-var Linear = require('../../math/Linear');
-var FuzzyEqual = require('../../math/fuzzy/Equal');
-
 //  https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent
 //  https://github.com/WICG/EventListenerOptions/blob/gh-pages/explainer.md
 
@@ -128,27 +125,6 @@ var MouseManager = new Class({
          * @since 3.0.0
          */
         this.pointerLockChange = NOOP;
-
-        this.prevTime = 0;
-        this.currentTime = 0;
-        this.deltaTime = 0;
-        this.deltaX = 0;
-        this.deltaY = 0;
-
-        this.mx = 0;
-        this.my = 0;
-        this.prevX = -1;
-        this.prevY = -1;
-        this.currentX = -1;
-        this.currentY = -1;
-        this.isMoving = false;
-
-        this.upX = 0;
-        this.upY = 0;
-
-        this.counter = 0;
-
-        this.moveFrame = 0;
 
         inputManager.events.once('boot', this.boot, this);
     },
@@ -271,49 +247,6 @@ var MouseManager = new Class({
                 return;
             }
     
-            _this.currentTime = event.timeStamp;
-
-            //  Apply smoothing
-
-            // var x = event.pageX - _this.manager.bounds.left;
-            // var y = event.pageY - _this.manager.bounds.top;
-
-            var x = event.pageX - _this.manager.bounds.left;
-            var y = event.pageY - _this.manager.bounds.top;
-            var a = 0.2;
-
-            if (_this.prevX === -1)
-            {
-                _this.currentX = x;
-                _this.currentY = y;
-
-                _this.prevX = x;
-                _this.prevY = y;
-            }
-            else
-            {
-                _this.prevX = _this.currentX;
-                _this.prevY = _this.currentY;
-    
-                // _this.currentX = x * a + _this.prevX * (1 - a);
-                // _this.currentY = y * a + _this.prevY * (1 - a);
-
-                _this.currentX = x;
-                _this.currentY = y;
-            }
-
-            _this.deltaX = _this.currentX - _this.prevX;
-            _this.deltaY = _this.currentY - _this.prevY;
-
-            // console.log('move', _this.moveFrame, _this.deltaX, _this.deltaY);
-
-            // _this.mx = Math.max(Math.abs(_this.deltaX), _this.mx);
-            // _this.my = Math.max(Math.abs(_this.deltaY), _this.my);
-
-            _this.moveFrame = _this.manager.game.frame;
-
-            _this.counter++;
-
             _this.manager.queueMouseMove(event);
     
             if (_this.capture)
@@ -330,19 +263,6 @@ var MouseManager = new Class({
                 return;
             }
     
-            _this.currentTime = event.timeStamp;
-
-            console.log('mdown', _this.moveFrame, _this.manager.game.frame, '=', _this.deltaX, _this.deltaY);
-
-            // _this.prevX = _this.currentX;
-            // _this.prevY = _this.currentY;
-
-            // _this.currentX = event.pageX - _this.manager.bounds.left;
-            // _this.currentY = event.pageY - _this.manager.bounds.top;
-
-            // _this.deltaX = _this.currentX - _this.prevX;
-            // _this.deltaY = _this.currentY - _this.prevY;
-
             _this.manager.queueMouseDown(event);
     
             if (_this.capture)
@@ -359,22 +279,6 @@ var MouseManager = new Class({
                 return;
             }
     
-            _this.currentTime = event.timeStamp;
-
-            _this.upX = _this.deltaX;
-            _this.upY = _this.deltaY;
-
-            console.log('mup', _this.moveFrame, _this.manager.game.frame, '=', _this.deltaX, _this.deltaY, 'mc', _this.counter);
-
-            // _this.prevX = _this.currentX;
-            // _this.prevY = _this.currentY;
-
-            // _this.currentX = event.pageX - _this.manager.bounds.left;
-            // _this.currentY = event.pageY - _this.manager.bounds.top;
-
-            // _this.deltaX = _this.currentX - _this.prevX;
-            // _this.deltaY = _this.currentY - _this.prevY;
-
             _this.manager.queueMouseUp(event);
     
             if (_this.capture)
@@ -423,46 +327,6 @@ var MouseManager = new Class({
         }
 
         this.enabled = true;
-    },
-
-    pre: function ()
-    {
-        //  calculate deltas
-        this.deltaTime = this.currentTime - this.prevTime;
-    },
-
-    post: function ()
-    {
-        return;
-
-        //  reset
-        this.prevTime = this.currentTime;
-
-        if (this.deltaX !== 0)
-        {
-            this.deltaX *= 0.95;
-
-            // this.deltaX = Linear(0, this.deltaX, 0.999);
-
-            if (FuzzyEqual(this.deltaX, 0, 0.1))
-            {
-                this.deltaX = 0;
-            }
-        }
-
-        if (this.deltaY !== 0)
-        {
-            this.deltaY *= 0.95;
-
-            // this.deltaY = Linear(0, this.deltaY, 0.999);
-
-            if (FuzzyEqual(this.deltaY, 0, 0.1))
-            {
-                this.deltaY = 0;
-            }
-        }
-
-        this.isMoving = (this.deltaX !== 0 || this.deltaY !== 0);
     },
 
     /**
