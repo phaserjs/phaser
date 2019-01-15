aa341854c769256b59ee6d46b42416f03017ed81
Richard Davey <rich@photonstorm.com>
2019-01-15 16:20:22 +0000

    Happy New Year

--- src/gameobjects/container/Container.js
+++ src/gameobjects/container/Container.js
@@ -1,7 +1,7 @@
 /**
  * @author       Richard Davey <rich@photonstorm.com>
  * @author       Felipe Alfonso <@bitnenfer>
- * @copyright    2018 Photon Storm Ltd.
+ * @copyright    2019 Photon Storm Ltd.
  * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
  */
 
--- src/sound/html5/HTML5AudioSoundManager.js
+++ src/sound/html5/HTML5AudioSoundManager.js
@@ -1,7 +1,7 @@
 /**
  * @author       Richard Davey <rich@photonstorm.com>
  * @author       Pavle Goloskokovic <pgoloskokovic@gmail.com> (http://prunegames.com)
- * @copyright    2018 Photon Storm Ltd.
+ * @copyright    2019 Photon Storm Ltd.
  * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
  */
 
