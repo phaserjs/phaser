cb0f3e079c067b98d2206d39396c7dab9f54fb03
J.C <jcyuan.space@gmail.com>
2019-01-25 11:02:38 +0800

    add doc for the static method. BTW: have to admit that Phaser is absolutely well considered, even for such case that user probably make all BMFont atlas combined into one.

--- src/gameobjects/bitmaptext/static/BitmapText.js
+++ src/gameobjects/bitmaptext/static/BitmapText.js
@@ -639,6 +639,24 @@ BitmapText.ALIGN_CENTER = 1;
  */
 BitmapText.ALIGN_RIGHT = 2;
 
+/**
+ * Parse an XML Bitmap Font from an Atlas.
+ *
+ * Adds the parsed Bitmap Font data to the cache with the `fontName` key.
+ *
+ * @name Phaser.GameObjects.BitmapText.ParseFromAtlas
+ * @since 3.0.0
+ *
+ * @param {Phaser.Scene} scene - The Scene to parse the Bitmap Font for.
+ * @param {string} fontName - The key of the font to add to the Bitmap Font cache.
+ * @param {string} textureKey - The key of the BitmapFont's texture.
+ * @param {string} frameKey - The key of the BitmapFont texture's frame.
+ * @param {string} xmlKey - The key of the XML data of the font to parse.
+ * @param {integer} [xSpacing] - The x-axis spacing to add between each letter.
+ * @param {integer} [ySpacing] - The y-axis spacing to add to the line height.
+ *
+ * @return {boolean} Whether the parsing was successful or not.
+ */
 BitmapText.ParseFromAtlas = ParseFromAtlas;
 
 module.exports = BitmapText;
