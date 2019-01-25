7c030285340a8015a6ae694020c35009886268e3
Richard Davey <rich@photonstorm.com>
2019-01-25 17:29:10 +0000

    Updated jsdocs

--- src/renderer/snapshot/index.js
+++ src/renderer/snapshot/index.js
@@ -7,7 +7,20 @@
 /**
  * @callback SnapshotCallback
  *
- * @param {HTMLImageElement} snapshot - [description]
+ * @param {(Phaser.Display.Color)|HTMLImageElement)} snapshot - Either a Color object if a single pixel is being grabbed, or a new Image which contains a snapshot of the canvas contents.
+ */
+
+/**
+ * @typedef {object} SnapshotState
+ *
+ * @property {SnapshotCallback} callback - The function to call after the snapshot is taken.
+ * @property {string} [type='image/png'] - The format of the image to create, usually `image/png` or `image/jpeg`.
+ * @property {number} [encoderOptions=0.92] - The image quality, between 0 and 1. Used for image formats with lossy compression, such as `image/jpeg`.
+ * @property {integer} [x=0] - The x coordinate to start the snapshot from.
+ * @property {integer} [y=0] - The y coordinate to start the snapshot from.
+ * @property {integer} [width] - The width of the snapshot.
+ * @property {integer} [height] - The height of the snapshot.
+ * @property {boolean} [getPixel=false] - Is this a snapshot to get a single pixel, or an area?
  */
 
 /**
