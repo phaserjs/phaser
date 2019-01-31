4d4f500629f89f295294eab032df72c063e654cc
Richard Davey <rich@photonstorm.com>
2019-01-31 11:12:00 +0000

    Improved typedefs

--- src/core/Config.js
+++ src/core/Config.js
@@ -112,22 +112,28 @@ var ValueToColor = require('../display/color/ValueToColor');
  */
 
 /**
+ * @typedef {object} WidthHeight
+ *
+ * @property {integer} [width=0] - The width.
+ * @property {integer} [height=0] - The height.
+ */
+
+/**
  * @typedef {object} ScaleConfig
  *
- * @property {(integer|string)} [width=1024] - The base width of your game. Can be an integer or a string: '100%'.
- * @property {(integer|string)} [height=768] - The base height of your game. Can be an integer or a string: '100%'.
- * @property {integer} [zoom=1] - The zoom value of the game canvas.
+ * @property {(integer|string)} [width=1024] - The base width of your game. Can be an integer or a string: '100%'. If a string it will only work if you have set a parent element that has a size.
+ * @property {(integer|string)} [height=768] - The base height of your game. Can be an integer or a string: '100%'. If a string it will only work if you have set a parent element that has a size.
+ * @property {(Phaser.Scale.Zoom|integer)} [zoom=1] - The zoom value of the game canvas.
  * @property {number} [resolution=1] - The rendering resolution of the canvas. This is reserved for future use and is currently ignored.
- * @property {(HTMLElement|string)} [parent] - The DOM element that will contain the game canvas, or its `id`. If undefined, or if the named element doesn't exist, the game canvas is inserted directly into the document body. If `null` no parent will be used and you are responsible for adding the canvas to your environment.
- * @property {boolean} [expandParent=true] - Is the Scale Manager allowed to adjust the CSS height property of the parent to be 100%?
- * @property {integer} [mode=0] - The scale mode.
- * @property {integer} [minWidth] - The minimum width the canvas can be scaled down to.
- * @property {integer} [minHeight] - The minimum height the canvas can be scaled down to.
- * @property {integer} [maxWidth] - The maximum width the canvas can be scaled up to.
- * @property {integer} [maxHeight] - The maximum height the canvas can be scaled up to.
+ * @property {?(HTMLElement|string)} [parent] - The DOM element that will contain the game canvas, or its `id`. If undefined, or if the named element doesn't exist, the game canvas is inserted directly into the document body. If `null` no parent will be used and you are responsible for adding the canvas to your environment.
+ * @property {boolean} [expandParent=true] - Is the Scale Manager allowed to adjust the CSS height property of the parent and/or document body to be 100%?
+ * @property {Phaser.Scale.ScaleModes} [mode=Phaser.Scale.ScaleModes.NONE] - The scale mode.
+ * @property {WidthHeight} [min] - The minimum width and height the canvas can be scaled down to.
+ * @property {WidthHeight} [max] - The maximum width the canvas can be scaled up to.
  * @property {boolean} [autoRound=false] - Automatically round the display and style sizes of the canvas. This can help with performance in lower-powered devices.
- * @property {integer} [autoCenter=0] - Automatically center the canvas within the parent? 0 = No centering. 1 = Center both horizontally and vertically. 2 = Center horizontally. 3 = Center vertically.
- * @property {integer} [resizeInterval=2000] - How many ms should elapse before checking if the browser size has changed?
+ * @property {Phaser.Scale.Center} [autoCenter=Phaser.Scale.Center.NO_CENTER] - Automatically center the canvas within the parent?
+ * @property {integer} [resizeInterval=500] - How many ms should elapse before checking if the browser size has changed?
+ * @property {?(HTMLElement|string)} [fullscreenTarget] - The DOM element that will be sent into full screen mode, or its `id`. If undefined Phaser will create its own div and insert the canvas into it when entering fullscreen mode.
  */
 
 /**
@@ -232,6 +238,7 @@ var ValueToColor = require('../display/color/ValueToColor');
  * @property {ImagesConfig} [images] - Images configuration.
  * @property {object} [physics] - Physics configuration.
  * @property {PluginObject|PluginObjectItem[]} [plugins] - Plugins to install.
+ * @property {ScaleConfig} [scale] - The Scale Manager configuration.
  */
 
 /**
@@ -276,12 +283,12 @@ var Config = new Class({
         this.height = GetValue(config, 'height', 768);
 
         /**
-         * @const {number} Phaser.Core.Config#zoom - The zoom factor, as used by the Scale Manager.
+         * @const {(Phaser.Scale.Zoom|integer)} Phaser.Core.Config#zoom - The zoom factor, as used by the Scale Manager.
          */
         this.zoom = GetValue(config, 'zoom', 1);
 
         /**
-         * @const {number} Phaser.Core.Config#resolution - The canvas device pixel resolution.
+         * @const {number} Phaser.Core.Config#resolution - The canvas device pixel resolution. Currently un-used.
          */
         this.resolution = GetValue(config, 'resolution', 1);
 
@@ -291,7 +298,7 @@ var Config = new Class({
         this.parent = GetValue(config, 'parent', undefined);
 
         /**
-         * @const {integer} Phaser.Core.Config#scaleMode - The scale mode as used by the Scale Manager. The default is zero, which is no scaling.
+         * @const {Phaser.Scale.ScaleModes} Phaser.Core.Config#scaleMode - The scale mode as used by the Scale Manager. The default is zero, which is no scaling.
          */
         this.scaleMode = GetValue(config, 'scaleMode', 0);
 
@@ -306,14 +313,19 @@ var Config = new Class({
         this.autoRound = GetValue(config, 'autoRound', false);
 
         /**
-         * @const {integer} Phaser.Core.Config#autoCenter - Automatically center the canvas within the parent? 0 = No centering. 1 = Center both horizontally and vertically. 2 = Center horizontally. 3 = Center vertically.
+         * @const {Phaser.Scale.Center} Phaser.Core.Config#autoCenter - Automatically center the canvas within the parent?
          */
         this.autoCenter = GetValue(config, 'autoCenter', 0);
 
         /**
          * @const {integer} Phaser.Core.Config#resizeInterval - How many ms should elapse before checking if the browser size has changed?
          */
-        this.resizeInterval = GetValue(config, 'autoRound', 2000);
+        this.resizeInterval = GetValue(config, 'resizeInterval', 500);
+
+        /**
+         * @const {?(HTMLElement|string)} Phaser.Core.Config#fullscreenTarget - The DOM element that will be sent into full screen mode, or its `id`. If undefined Phaser will create its own div and insert the canvas into it when entering fullscreen mode.
+         */
+        this.fullscreenTarget = GetValue(config, 'fullscreenTarget', null);
 
         /**
          * @const {integer} Phaser.Core.Config#minWidth - The minimum width, in pixels, the canvas will scale down to. A value of zero means no minimum.
@@ -351,6 +363,7 @@ var Config = new Class({
             this.autoRound = GetValue(scaleConfig, 'autoRound', this.autoRound);
             this.autoCenter = GetValue(scaleConfig, 'autoCenter', this.autoCenter);
             this.resizeInterval = GetValue(scaleConfig, 'resizeInterval', this.resizeInterval);
+            this.fullscreenTarget = GetValue(scaleConfig, 'fullscreenTarget', this.fullscreenTarget);
             this.minWidth = GetValue(scaleConfig, 'min.width', this.minWidth);
             this.maxWidth = GetValue(scaleConfig, 'max.width', this.maxWidth);
             this.minHeight = GetValue(scaleConfig, 'min.height', this.minHeight);
