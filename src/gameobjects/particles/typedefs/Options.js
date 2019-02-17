9a92defe022298523440d905a22c1c9db3685741
Gareth Jones <jones258@gmail.com>
2019-02-17 19:34:59 +1300

    Update jsdoc of `config` parameter of `ParticleManagerCreator`

---
+++ src/gameobjects/particles/typedefs/Options.js
@@ -0,0 +1,10 @@
+/**
+ * Options for the Particles game Object.
+ *
+ * @typedef {object} Phaser.GameObjects.Particles.Types.Options
+ * @since 3.0.0
+ *
+ * @property {string} key - The key of the Texture this Game Object will use to render with, as stored in the Texture Manager.
+ * @property {(string|integer|object)} [frame] - An optional frame from the Texture this Game Object is rendering with.
+ * @property {Phaser.GameObjects.Particles.Types.ParticleEmitterConfig|Phaser.GameObjects.Particles.Types.ParticleEmitterConfig[]} [emitters] - Configuration settings for one or more emitters to create.
+ */
