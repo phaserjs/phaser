be97c8a6de689646d9c4fcb8531966bd5935c6c4
Gareth Jones <jones258@gmail.com>
2019-02-17 19:36:49 +1300

    Update jsdoc of `config` parameter of `ZoneCreator`

---
+++ src/gameobjects/zone/typedefs/Options.js
@@ -0,0 +1,11 @@
+/**
+ * Options for the Zone game Object.
+ *
+ * @typedef {object} Phaser.GameObjects.Zone.Types.Options
+ * @since 3.0.0
+ *
+ * @property {number} [x] - The horizontal position of this Game Object in the world.
+ * @property {number} [y] - The vertical position of this Game Object in the world.
+ * @property {number} [width] - The width of the Game Object.
+ * @property {number} [height] - The height of the Game Object.
+ */
