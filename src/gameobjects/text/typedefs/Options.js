5ef6e27718f328058080c567e2338a30ae462166
Gareth Jones <jones258@gmail.com>
2019-02-17 19:52:17 +1300

    Update jsdoc of `config` parameter of `TextCreator`

---
+++ src/gameobjects/text/typedefs/Options.js
@@ -0,0 +1,13 @@
+/**
+ * Options for the Zone game Object.
+ *
+ * @typedef {object} Phaser.GameObjects.Text.Types.Options
+ * @extends {Phaser.GameObjects.Types.GameObjectConfig}
+ * @since 3.0.0
+ *
+ * @property {string} [text=''] - The text this Text object will display.
+ * @property {object} [style] - The text style configuration object.
+ * @property {number} [padding] - The padding value of the text. Overrides padding set by `style`.
+ * @property {boolean} [autoRound=true] - Whether to automatically round line positions.
+ * @property {number} [resolution=1] - The resolution for this Text object to use.
+ */
