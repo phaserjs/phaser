8deef696d828140453ff794ae7d9f9d96e580ee9
Gareth Jones <jones258@gmail.com>
2019-02-17 22:13:32 +1300

    Update jsdoc of `config` parameter of `QuadCreator`

--- src/gameobjects/quad/QuadCreator.js
+++ src/gameobjects/quad/QuadCreator.js
@@ -17,7 +17,7 @@ var Quad = require('./Quad');
  * @method Phaser.GameObjects.GameObjectCreator#quad
  * @since 3.0.0
  *
- * @param {object} config - The configuration object this Game Object will use to create itself.
+ * @param {Phaser.GameObjects.Quad.Types.QuadConfig} [config] - The configuration object this Game Object will use to create itself.
  * @param {boolean} [addToScene] - Add this Game Object to the Scene after creating it? If set this argument overrides the `add` property in the config object.
  *
  * @return {Phaser.GameObjects.Quad} The Game Object that was created.
---
+++ src/gameobjects/quad/typedefs/QuadConfig.js
@@ -0,0 +1,10 @@
+/**
+ * Options for the Quad game Object.
+ *
+ * @typedef {object} Phaser.GameObjects.Quad.Types.QuadConfig
+ * @extends {Phaser.GameObjects.Types.GameObjectConfig}
+ * @since 3.0.0
+ *
+ * @property {string} key - The key of the Texture this Game Object will use to render with, as stored in the Texture Manager.
+ * @property {(string|integer)} [frame] - An optional frame from the Texture this Game Object is rendering with.
+ */
