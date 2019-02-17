18070b6ab325004818d8434eff8e31b116d6bb07
Gareth Jones <jones258@gmail.com>
2019-02-17 19:53:46 +1300

    Mark `config` parameter as optional in `BitmapTextCreator`

--- src/gameobjects/bitmaptext/static/BitmapTextCreator.js
+++ src/gameobjects/bitmaptext/static/BitmapTextCreator.js
@@ -18,9 +18,9 @@ var GetValue = require('../../../utils/object/GetValue');
  * @method Phaser.GameObjects.GameObjectCreator#bitmapText
  * @since 3.0.0
  *
- * @param {Phaser.GameObjects.BitmapText.Types.BitmapTextConfig} config - The configuration object this Game Object will use to create itself.
+ * @param {Phaser.GameObjects.BitmapText.Types.BitmapTextConfig} [config] - The configuration object this Game Object will use to create itself.
  * @param {boolean} [addToScene] - Add this Game Object to the Scene after creating it? If set this argument overrides the `add` property in the config object.
- * 
+ *
  * @return {Phaser.GameObjects.BitmapText} The Game Object that was created.
  */
 GameObjectCreator.register('bitmapText', function (config, addToScene)
