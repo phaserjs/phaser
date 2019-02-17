6fa7aecdcdb52fec356537cd07e2fe8400b5a75f
Gareth Jones <jones258@gmail.com>
2019-02-17 19:33:11 +1300

    Update jsdoc of `config` parameter of `MeshCreator`

---
+++ src/gameobjects/mesh/typedefs/Options.js
@@ -0,0 +1,14 @@
+/**
+ * Options for the Mesh game Object.
+ *
+ * @typedef {object} Phaser.GameObjects.Mesh.Types.Options
+ * @since 3.0.0
+ *
+ * @property {string} key - The key of the Texture this Game Object will use to render with, as stored in the Texture Manager.
+ * @property {(string|integer)} [frame] - An optional frame from the Texture this Game Object is rendering with.
+ * @property {number[]} [vertices] - An array containing the vertices data for this Mesh.
+ * @property {number[]} [colors] - An array containing the color data for this Mesh.
+ * @property {number[]} [alphas] - An array containing the alpha data for this Mesh.
+ * @property {number[]} [uv] - An array containing the uv data for this Mesh.
+ *
+ */
