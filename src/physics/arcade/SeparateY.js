21df050e88031dd9b57943a1baff84774def4015
Richard Davey <rich@photonstorm.com>
2019-03-25 12:55:52 +0000

    Works using ColInfo object now

--- src/physics/arcade/SeparateY.js
+++ src/physics/arcade/SeparateY.js
@@ -26,24 +26,18 @@ var GetOverlapY = require('./GetOverlapY');
  *
  * @return {boolean} `true` if the two bodies overlap vertically, otherwise `false`.
  */
-var SeparateY = function (body1, body2, overlapOnly, bias)
+var SeparateY = function (collisionInfo)
 {
-    // console.log('');
-    // console.log('%c frame ' + body1.world._frame + '                                                                                     ', 'background-color: orange');
-    // console.log('body1:', body1.gameObject.name, 'vs body2:', body2.gameObject.name);
-    // console.log('pre-GetOverlap by = body1', body1.y, 'body2', body2.y);
-    // console.log('pre-GetOverlap gy = body1', body1.gameObject.y, 'body2', body2.gameObject.y);
-
-    var collisionInfo = GetOverlapY(body1, body2, overlapOnly, bias);
-
-    // console.log(collisionInfo);
-    // console.log('post-GetOverlap by = body1', body1.y, 'body2', body2.y);
-    // console.log('post-GetOverlap gy = body1', body1.gameObject.y, 'body2', body2.gameObject.y);
-
     var overlap = collisionInfo.overlapY;
-    var topFace = collisionInfo.face === CONST.FACING_UP;
+    var overlapOnly = collisionInfo.overlapOnly;
+
+    var topFace = collisionInfo.faceY === CONST.FACING_UP;
     var bottomFace = !topFace;
+
     var intersects = collisionInfo.intersects;
+
+    var body1 = collisionInfo.body1;
+    var body2 = collisionInfo.body2;
 
     var velocity1 = body1.velocity;
     var velocity2 = body2.velocity;
@@ -54,7 +48,18 @@ var SeparateY = function (body1, body2, overlapOnly, bias)
     var body1Immovable = (body1.physicsType === CONST.STATIC_BODY || body1.immovable);
     var body2Immovable = (body2.physicsType === CONST.STATIC_BODY || body2.immovable);
 
-    // console.log('body1 overlaps body2 across the', ((topFace) ? 'top' : 'bottom'), 'by', overlap, 'px');
+    console.log('');
+    console.log('%c Y frame ' + body1.world._frame + '                                                                                     ', 'background-color: pink');
+    console.log('body1:', body1.gameObject.name, 'vs body2:', body2.gameObject.name);
+
+    // console.log('pre-GetOverlap by = body1', body1.y, 'body2', body2.y);
+    // console.log('pre-GetOverlap gy = body1', body1.gameObject.y, 'body2', body2.gameObject.y);
+
+    // console.log(collisionInfo);
+    // console.log('post-GetOverlap by = body1', body1.y, 'body2', body2.y);
+    // console.log('post-GetOverlap gy = body1', body1.gameObject.y, 'body2', body2.gameObject.y);
+
+    console.log('body1 overlaps body2 across the', ((topFace) ? 'top' : 'bottom'), 'by', overlap, 'px');
 
     //  Can't separate two immovable bodies, or a body with its own custom separation logic
     if (!intersects || overlapOnly || (body1Immovable && body2Immovable) || body1.customSeparateY || body2.customSeparateY)
@@ -172,8 +177,8 @@ var SeparateY = function (body1, body2, overlapOnly, bias)
         ny2 = 0;
     }
 
-    var totalA = collisionInfo.share1;
-    var totalB = collisionInfo.share2;
+    var totalA = collisionInfo.shareY1;
+    var totalB = collisionInfo.shareY2;
     
     // console.log('split at', totalA, totalB, 'of', overlap);
 
