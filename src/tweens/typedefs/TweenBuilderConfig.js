d6b3141b8b55f792bc66d8566689ce717358d8e1
samme <samme@users.noreply.github.com>
2019-05-29 10:45:55 -0700

    Add the remaining tween config properties

--- src/tweens/typedefs/TweenBuilderConfig.js
+++ src/tweens/typedefs/TweenBuilderConfig.js
@@ -8,11 +8,37 @@
  * @property {string} [ease='Power0'] - The easing equation to use for the tween.
  * @property {array} [easeParams] - Optional easing parameters.
  * @property {number} [hold=0] - The number of milliseconds to hold the tween for before yoyo'ing.
- * @property {number} [repeat=0] - The number of times to repeat the tween.
- * @property {number} [repeatDelay=0] - The number of milliseconds to pause before a tween will repeat.
+ * @property {number} [repeat=0] - The number of times each property tween repeats after the first play.
+ * @property {number} [repeatDelay=0] - The number of milliseconds to pause before a repeat.
  * @property {boolean} [yoyo=false] - Should the tween complete, then reverse the values incrementally to get back to the starting tween values? The reverse tweening will also take `duration` milliseconds to complete.
  * @property {boolean} [flipX=false] - Horizontally flip the target of the Tween when it completes (before it yoyos, if set to do so). Only works for targets that support the `flipX` property.
  * @property {boolean} [flipY=false] - Vertically flip the target of the Tween when it completes (before it yoyos, if set to do so). Only works for targets that support the `flipY` property.
+ * @property {number|function|object|array} [offset=null] - Used when the Tween is part of a Timeline.
+ * @property {number|function|object|array} [completeDelay=0] - The time the tween will wait before the onComplete event is dispatched once it has completed, in ms.
+ * @property {number|function|object|array} [loop=0] - The number of times the tween repeats after the first play. (A value of 1 means the tween will play twice, as it repeated once.) The first loop starts after every property tween has completed once.
+ * @property {number|function|object|array} [loopDelay=0] - The time the tween will pause before starting either a yoyo or returning to the start for a repeat.
+ * @property {boolean} [paused=false] - Does the tween start in a paused state (true) or playing (false)?
+ * @property {object} [props] - The properties to tween.
+ * @property {boolean} [useFrames=false] - Use frames or milliseconds?
+ * @property {any} [callbackScope] - Scope (this) for the callbacks. The default scope is the new tween.
+ * @property {function} [onComplete] - A function to call when the tween completes.
+ * @property {array} [onCompleteParams] - Additional parameters to pass to `onComplete`.
+ * @property {any} [onCompleteScope] - Scope (this) for `onComplete`.
+ * @property {function} [onLoop] - A function to call each time the tween loops.
+ * @property {array} [onLoopParams] - Additional parameters to pass to `onLoop`.
+ * @property {any} [onLoopScope] - Scope (this) for `onLoop`.
+ * @property {function} [onRepeat] - A function to call each time each property repeats.
+ * @property {array} [onRepeatParams] - Additional parameters to pass to `onRepeat`.
+ * @property {any} [onRepeatScope] - Scope (this) for `onRepeat`.
+ * @property {function} [onStart] - A function to call when the tween starts.
+ * @property {array} [onStartParams] - Additional parameters to pass to `onStart`.
+ * @property {any} [onStartScope] - Scope (this) for `onStart`.
+ * @property {function} [onUpdate] - A function to call each time the tween steps.
+ * @property {array} [onUpdateParams] - Additional parameters to pass to `onUpdate`.
+ * @property {any} [onUpdateScope] - Scope (this) for `onUpdate`.
+ * @property {function} [onYoyo] - A function to call each time the tween yoyos.
+ * @property {array} [onYoyoParams] - Additional parameters to pass to `onYoyo`.
+ * @property {any} [onYoyoScope] - Scope (this) for `onYoyo`.
  *
  * @example
  * {
