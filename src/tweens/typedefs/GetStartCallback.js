8410f6b131d75fb4e7af90c506e0632fe2665495
samme <samme@users.noreply.github.com>
2019-06-01 10:24:15 -0700

    Add types for tween `props` configs

---
+++ src/tweens/typedefs/GetStartCallback.js
@@ -0,0 +1,10 @@
+/**
+ * @callback Phaser.Types.Tweens.GetStartCallback
+ * @since 3.18.0
+ *
+ * @param {any} target - The tween target.
+ * @param {string} key - The target property.
+ * @param {number} value - The current value of the target property.
+ *
+ * @return {number} - The new value.
+ */
