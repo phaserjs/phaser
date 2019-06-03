d9ecd70f59f05f292ad39f17513e87349cc14c1f
samme <samme@users.noreply.github.com>
2019-06-03 12:12:01 -0700

    Add TimelineBuilderConfig type and callbacks

--- src/tweens/builders/TimelineBuilder.js
+++ src/tweens/builders/TimelineBuilder.js
@@ -19,40 +19,11 @@ var TweenBuilder = require('./TweenBuilder');
 /**
  * Builds a Timeline of Tweens based on a configuration object.
  *
- * The configuration object (`config`) can have the following properties:
- *
- * `tweens` - an array of tween configuration objects to create and add into the new Timeline, as described by `TweenBuilder`. If this doesn't exist or is empty, the Timeline will start off paused and none of the other configuration settings will be read. If it's a function, it will be called and its return value will be used as the array.
- * `targets` - an array (or function which returns one) of default targets to which to apply the Timeline. Each individual Tween configuration can override this value.
- * `totalDuration` - if specified, each Tween in the Timeline will get an equal portion of this duration, usually in milliseconds, by default. Each individual Tween configuration can override the Tween's duration.
- * `duration` - if `totalDuration` is not specified, the default duration, usually in milliseconds, of each Tween which will be created. Each individual Tween configuration can override the Tween's duration.
- * `delay`, `easeParams`, `ease`, `hold`, `repeat`, `repeatDelay`, `yoyo`, `flipX`, `flipY` - the default settings for each Tween which will be created, as specified by `TweenBuilder`. Each individual Tween configuration can override any of these values.
- * `completeDelay` - if specified, the time to wait, usually in milliseconds, before the Timeline completes.
- * `loop` - how many times the Timeline should loop, or -1 to loop indefinitely.
- * `loopDelay` - the time, usually in milliseconds, between each loop
- * `paused` - if `true`, the Timeline will start paused
- * `useFrames` - if `true`, all duration in the Timeline will be in frames instead of milliseconds
- * `callbackScope` - the default scope (`this` value) to use for each callback registered by the Timeline Builder. If not specified, the Timeline itself will be used.
- * `onStart` - if specified, the `onStart` callback for the Timeline, called every time it starts playing
- * `onStartScope` - the scope (`this` value) to use for the `onStart` callback. If not specified, the `callbackScope` will be used.
- * `onStartParams` - additional arguments to pass to the `onStart` callback. The Timeline will always be the first argument.
- * `onUpdate` - if specified, the `onUpdate` callback for the Timeline, called every frame it's active, regardless of its Tweens
- * `onUpdateScope` - the scope (`this` value) to use for the `onUpdate` callback. If not specified, the `callbackScope` will be used.
- * `onUpdateParams` - additional arguments to pass to the `onUpdate` callback. The Timeline will always be the first argument.
- * `onLoop` - if specified, the `onLoop` callback for the Timeline, called every time it loops
- * `onLoopScope` - the scope (`this` value) to use for the `onLoop` callback. If not specified, the `callbackScope` will be used.
- * `onLoopParams` - additional arguments to pass to the `onLoop` callback. The Timeline will always be the first argument.
- * `onYoyo` - if specified, the `onYoyo` callback for the Timeline, called every time it yoyos
- * `onYoyoScope` - the scope (`this` value) to use for the `onYoyo` callback. If not specified, the `callbackScope` will be used.
- * `onYoyoParams` - additional arguments to pass to the `onYoyo` callback. The first argument will always be `null`, while the Timeline will always be the second argument.
- * `onComplete` - if specified, the `onComplete` callback for the Timeline, called after it completes
- * `onCompleteScope` - the scope (`this` value) to use for the `onComplete` callback. If not specified, the `callbackScope` will be used.
- * `onCompleteParams` - additional arguments to pass to the `onComplete` callback. The Timeline will always be the first argument.
- *
  * @function Phaser.Tweens.Builders.TimelineBuilder
  * @since 3.0.0
  *
  * @param {Phaser.Tweens.TweenManager} manager - The Tween Manager to which the Timeline will belong.
- * @param {object} config - The configuration object for the Timeline, as described above.
+ * @param {Phaser.Types.Tweens.TimelineBuilderConfig} config - The configuration object for the Timeline.
  *
  * @return {Phaser.Tweens.Timeline} The created Timeline.
  */
---
+++ src/tweens/typedefs/TimelineOnYoyoCallback.js
@@ -0,0 +1,7 @@
+/**
+ * @callback Phaser.Types.Tweens.TimelineOnYoyoCallback
+ * @since 3.18.0
+ *
+ * @param {Phaser.Tweens.Timeline} timeline - The timeline.
+ * @param {...any} param - Any value passed in `onYoyoParams`.
+ */
