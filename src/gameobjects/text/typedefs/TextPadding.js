2cdb672d365ddefba8ec73ae7e6c1922223beaac
Richard Davey <rich@photonstorm.com>
2019-06-05 23:40:27 +0100

    Added new Text Padding typedef

---
+++ src/gameobjects/text/typedefs/TextPadding.js
@@ -0,0 +1,13 @@
+/**
+ * A Text Padding configuration object as used by the Text Style.
+ *
+ * @typedef {object} Phaser.Types.GameObjects.Text.TextPadding
+ * @since 3.18.0
+ *
+ * @property {number} [x] - If set this value is used for both the left and right padding.
+ * @property {number} [y] - If set this value is used for both the top and bottom padding.
+ * @property {number} [left] - The amount of padding added to the left of the Text object.
+ * @property {number} [right] - The amount of padding added to the right of the Text object.
+ * @property {number} [top] - The amount of padding added to the top of the Text object.
+ * @property {number} [bottom] - The amount of padding added to the bottom of the Text object.
+ */
