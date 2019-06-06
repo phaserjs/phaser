0a761e104c44eb5e10b466f8c352f0e55a913200
Richard Davey <rich@photonstorm.com>
2019-06-06 15:56:26 +0100

    Added new arguments to RESIZE event

--- src/scale/events/RESIZE_EVENT.js
+++ src/scale/events/RESIZE_EVENT.js
@@ -18,5 +18,8 @@
  * @param {Phaser.Structs.Size} gameSize - A reference to the Game Size component. This is the un-scaled size of your game canvas.
  * @param {Phaser.Structs.Size} baseSize - A reference to the Base Size component. This is the game size multiplied by resolution.
  * @param {Phaser.Structs.Size} displaySize - A reference to the Display Size component. This is the scaled canvas size, after applying zoom and scale mode.
+ * @param {number} resolution - The current resolution. Defaults to 1 at the moment.
+ * @param {number} previousWidth - If the `gameSize` has changed, this value contains its previous width, otherwise it contains the current width.
+ * @param {number} previousHeight - If the `gameSize` has changed, this value contains its previous height, otherwise it contains the current height.
  */
 module.exports = 'resize';
