496ac0f979bedcb9df02edadea692fcd152f6ae0
Richard Davey <rich@photonstorm.com>
2019-06-28 17:42:34 +0100

    Supports ranged staggers

--- src/tweens/builders/StaggerBuilder.js
+++ src/tweens/builders/StaggerBuilder.js
@@ -32,23 +32,26 @@ var StaggerBuilder = function (value, options)
     //  delay: this.tweens.stagger(100) // increase delay by 100ms for each elements.
     //  delay: this.tweens.stagger(100, {start: 500}) // delay starts at 500ms then increase by 100ms for each elements.
     //  delay: this.tweens.stagger(100, {from: 'center'}) (first, default, last, center, index)
+    //  delay: this.tweens.stagger([ 500, 1000 ]) // distributes evenly values between two numbers.
 
     var fromFirst = (from === 'first');
     var fromCenter = (from === 'center');
     var fromLast = (from === 'last');
     var fromValue = (typeof(from) === 'number');
 
+    var isRange = (Array.isArray(value));
+    var value1 = (isRange) ? parseFloat(value[0]) : parseFloat(value);
+    var value2 = (isRange) ? parseFloat(value[1]) : 0;
+    var maxValue = Math.max(value1, value2);
+
     var easeFunction = (ease) ? GetEaseFunction(ease) : null;
 
     if (!grid)
     {
-        result = function (index, total)
+        result = function (target, key, value, index, total, tween)
         {
-            if (total > 0)
-            {
-                //  zero offset
-                total--;
-            }
+            //  zero offset
+            total--;
 
             var fromIndex;
 
@@ -70,17 +73,24 @@ var StaggerBuilder = function (value, options)
             }
 
             var output = start;
+            var max = total * maxValue;
+            var spacing = value1;
+
+            if (isRange)
+            {
+                spacing += ((value2 - value1) / total) * fromIndex;
+            }
 
             if (easeFunction)
             {
-                output += ((total * value) * easeFunction(fromIndex / total));
+                output += (max * easeFunction(fromIndex / total));
             }
             else
             {
-                output += (fromIndex * value);
+                output += (isRange) ? spacing : (fromIndex * value1);
             }
 
-            console.log('i', index, 'of', total, 'fromIndex:', fromIndex, 'result: ', output);
+            console.log('i', index, 'of', total, 'fromIndex:', fromIndex, 'spacing:', spacing, 'result: ', output);
 
             return output;
         };
@@ -88,50 +98,6 @@ var StaggerBuilder = function (value, options)
     else
     {
     }
-
-    /*
-    if (from === 'first')
-    {
-        return function (index)
-        {
-            return start + (index * value);
-        };
-    }
-    else if (from === 'last')
-    {
-        return function (index, total)
-        {
-            return start + ((total - index) * value);
-        };
-    }
-    else if (from === 'center')
-    {
-        return function (index, total)
-        {
-            return start + ((Math.abs(index - total / 2) * value));
-        };
-
-    }
-    else if (typeof(from) === 'number')
-    {
-        return function (index)
-        {
-            return start + ((Math.abs(from - index) * value));
-        };
-    }
-
-    if (ease)
-    {
-        var easeFunction = GetEaseFunction(ease);
-
-        return function (index, total)
-        {
-            var max = total * value;
-
-            return start + (max * easeFunction(index / total));
-        };
-    }
-    */
 
     return result;
 };
