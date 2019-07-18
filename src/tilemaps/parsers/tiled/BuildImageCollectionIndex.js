87c6c243c0d7f88dca1591d8a3d7d544d00324f5
Faelin Landy <flandy@zixcorp.com>
2019-07-18 10:42:06 -0400

    doc changes

--- src/tilemaps/parsers/tiled/BuildImageCollectionIndex.js
+++ src/tilemaps/parsers/tiled/BuildImageCollectionIndex.js
@@ -1,13 +1,13 @@
 /**
- * @author       Richard Davey <rich@photonstorm.com>
- * @copyright    2019 Photon Storm Ltd.
+ * @author       Faelin Landy <faelin.landy@gmail.com>
+ * @copyright    2019 Faelin Landy
  * @license      {@link https://opensource.org/licenses/MIT|MIT License}
  */
 
 /**
  * Master list of tiles -> x, y, index in tileset.
  *
- * @function Phaser.Tilemaps.Parsers.Tiled.BuildTilesetIndex
+ * @function Phaser.Tilemaps.Parsers.Tiled.BuildImageCollectionIndex
  * @since 3.0.0
  *
  * @param {Phaser.Tilemaps.MapData} mapData - [description]
