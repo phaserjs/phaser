7b96356cfc1fd3be6602a7b6a0c871fed28d8b94
Richard Davey <rich@photonstorm.com>
2019-07-31 17:33:43 +0100

    Fixed setSkin method and added other helper methods

--- plugins/spine/src/gameobject/SpineGameObject.js
+++ plugins/spine/src/gameobject/SpineGameObject.js
@@ -234,6 +234,37 @@ var SpineGameObject = new Class({
         return output;
     },
 
+    getSkinList: function ()
+    {
+        var output = [];
+
+        var skeletonData = this.skeletonData;
+
+        if (skeletonData)
+        {
+            for (var i = 0; i < skeletonData.skins.length; i++)
+            {
+                output.push(skeletonData.skins[i].name);
+            }
+        }
+
+        return output;
+    },
+
+    getSlotList: function ()
+    {
+        var output = [];
+
+        var skeleton = this.skeleton;
+
+        for (var i = 0; i < skeleton.slots.length; i++)
+        {
+            output.push(skeleton.slots[i].data.name);
+        }
+
+        return output;
+    },
+
     getAnimationList: function ()
     {
         var output = [];
@@ -299,7 +330,13 @@ var SpineGameObject = new Class({
 
     setSkinByName: function (skinName)
     {
-        this.skeleton.setSkinByName(skinName);
+        var skeleton = this.skeleton;
+
+        skeleton.setSkinByName(skinName);
+
+        skeleton.setSlotsToSetupPose();
+
+        this.state.apply(skeleton);
 
         return this;
     },
@@ -320,6 +357,42 @@ var SpineGameObject = new Class({
     setMix: function (fromName, toName, duration)
     {
         this.stateData.setMix(fromName, toName, duration);
+
+        return this;
+    },
+
+    getAttachment: function (slotIndex, attachmentName)
+    {
+        return this.skeleton.getAttachment(slotIndex, attachmentName);
+    },
+
+    getAttachmentByName: function (slotName, attachmentName)
+    {
+        return this.skeleton.getAttachmentByName(slotName, attachmentName);
+    },
+
+    setAttachment: function (slotName, attachmentName)
+    {
+        return this.skeleton.setAttachment(slotName, attachmentName);
+    },
+
+    setToSetupPose: function ()
+    {
+        this.skeleton.setToSetupPose();
+
+        return this;
+    },
+
+    setSlotsToSetupPose: function ()
+    {
+        this.skeleton.setSlotsToSetupPose();
+
+        return this;
+    },
+
+    setBonesToSetupPose: function ()
+    {
+        this.skeleton.setBonesToSetupPose();
 
         return this;
     },
