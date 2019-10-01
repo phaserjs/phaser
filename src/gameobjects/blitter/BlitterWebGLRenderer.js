8a9e74ffe997c93939f5bae43efc0699ccd5dec2
Hua <binhua.tein@gmail.com>
2019-10-01 10:17:14 +0800

    Add tint feature of bob

--- src/gameobjects/blitter/BlitterWebGLRenderer.js
+++ src/gameobjects/blitter/BlitterWebGLRenderer.js
@@ -94,7 +94,7 @@ var BlitterWebGLRenderer = function (renderer, src, interpolationPercentage, cam
         var tx1 = calcMatrix.getX(xw, yh);
         var ty1 = calcMatrix.getY(xw, yh);
 
-        var tint = Utils.getTintAppendFloatAlpha(0xffffff, bobAlpha);
+        var tint = Utils.getTintAppendFloatAlpha(bob.tint, bobAlpha);
 
         //  Bind texture only if the Texture Source is different from before
         if (frame.sourceIndex !== prevTextureSourceIndex)
