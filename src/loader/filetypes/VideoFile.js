29b9801c5d3cdf22358b941e1f61ad53ba3db88d
Richard Davey <rich@photonstorm.com>
2019-10-10 14:26:03 +0100

    Load as blob works and finished documentation.

--- src/loader/filetypes/VideoFile.js
+++ src/loader/filetypes/VideoFile.js
@@ -29,8 +29,10 @@ var IsPlainObject = require('../../utils/object/IsPlainObject');
  * @param {Phaser.Loader.LoaderPlugin} loader - A reference to the Loader that is responsible for this file.
  * @param {(string|Phaser.Types.Loader.FileTypes.VideoFileConfig)} key - The key to use for this file, or a file configuration object.
  * @param {any} [urlConfig] - The absolute or relative URL to load this file from in a config object.
+ * @param {string} [loadEvent] - The load event to listen for when _not_ loading as a blob. Either 'loadeddata', 'canplay' or 'canplaythrough'.
+ * @param {boolean} [asBlob] - Load the video as a data blob, or via the Video element?
+ * @param {boolean} [noAudio] - Does the video have an audio track? If not you can enable auto-playing on it.
  * @param {Phaser.Types.Loader.XHRSettingsObject} [xhrSettings] - Extra XHR Settings specifically for this file.
- * @param {AudioContext} [audioContext] - The AudioContext this file will use to process itself.
  */
 var VideoFile = new Class({
 
@@ -44,6 +46,11 @@ var VideoFile = new Class({
         if (loadEvent === undefined) { loadEvent = 'loadeddata'; }
         if (asBlob === undefined) { asBlob = false; }
         if (noAudio === undefined) { noAudio = false; }
+
+        if (loadEvent !== 'loadeddata' && loadEvent !== 'canplay' && loadEvent !== 'canplaythrough')
+        {
+            loadEvent = 'loadeddata';
+        }
 
         var fileConfig = {
             type: 'video',
@@ -60,7 +67,8 @@ var VideoFile = new Class({
             }
         };
 
-        // console.log(fileConfig);
+        this.onLoadCallback = this.onVideoLoadHandler.bind(this);
+        this.onErrorCallback = this.onVideoErrorHandler.bind(this);
 
         File.call(this, loader, fileConfig);
     },
@@ -69,13 +77,11 @@ var VideoFile = new Class({
      * Called automatically by Loader.nextFile.
      * This method controls what extra work this File does with its loaded data.
      *
-     * @method Phaser.Loader.FileTypes.BinaryFile#onProcess
-     * @since 3.7.0
+     * @method Phaser.Loader.FileTypes.VideoFile#onProcess
+     * @since 3.20.0
      */
     onProcess: function ()
     {
-        // console.log('Video.onProcess', this.config.asBlob);
-
         this.state = CONST.FILE_PROCESSING;
 
         if (!this.config.asBlob)
@@ -85,18 +91,9 @@ var VideoFile = new Class({
             return;
         }
 
-        var video = document.createElement('video');
+        //  Load Video as blob
 
-        video.controls = false;
-        video.canplay = true;
-
-        video.setAttribute('autoplay', 'autoplay');
-        video.setAttribute('playsinline', 'playsinline');
-
-        if (this.config.noAudio)
-        {
-            video.muted = true;
-        }
+        var video = this.createVideoElement();
 
         this.data = video;
 
@@ -104,8 +101,6 @@ var VideoFile = new Class({
 
         this.data.onloadeddata = function ()
         {
-            // console.log('data.onloadeddata');
-            
             File.revokeObjectURL(_this.data);
 
             _this.onProcessComplete();
@@ -113,35 +108,61 @@ var VideoFile = new Class({
 
         this.data.onerror = function ()
         {
-            // console.log('data.onerror');
-
             File.revokeObjectURL(_this.data);
 
             _this.onProcessError();
         };
 
-        // console.log('onProcess createURL');
+        File.createObjectURL(video, this.xhrLoader.response, '');
 
-        File.createObjectURL(this.data, this.xhrLoader.response, '');
+        video.load();
     },
 
     /**
-     * Called when the file finishes loading, is sent a DOM ProgressEvent.
+     * Creates a Video Element within the DOM.
      *
-     * @method Phaser.Loader.File#onLoad
-     * @since 3.0.0
-     *
-     * @param {XMLHttpRequest} xhr - The XMLHttpRequest that caused this onload event.
-     * @param {ProgressEvent} event - The DOM ProgressEvent that resulted from this load.
-    onLoadVideo: function (event)
+     * @method Phaser.Loader.FileTypes.VideoFile#createVideoElement
+     * @private
+     * @since 3.20.0
+     * 
+     * @return {HTMLVideoElement} The newly created Video element.
+     */
+    createVideoElement: function ()
     {
-        console.log('onLoadVideo');
-        console.log(event);
-        console.log(this);
+        var video = document.createElement('video');
+    
+        video.controls = false;
+        video.crossOrigin = this.loader.crossOrigin;
 
+        if (this.config.noAudio)
+        {
+            video.muted = true;
+            video.defaultMuted = true;
+
+            video.setAttribute('autoplay', 'autoplay');
+        }
+
+        video.setAttribute('playsinline', 'playsinline');
+        video.setAttribute('preload', 'auto');
+
+        return video;
+    },
+
+    /**
+     * Internal load event callback.
+     *
+     * @method Phaser.Loader.FileTypes.VideoFile#onVideoLoadHandler
+     * @private
+     * @since 3.20.0
+     *
+     * @param {ProgressEvent} event - The DOM ProgressEvent that resulted from this load.
+     */
+    onVideoLoadHandler: function (event)
+    {
         var video = event.target;
 
-        video.removeEventListener(this.config.loadEvent, this.onLoadVideo, true);
+        video.removeEventListener(this.config.loadEvent, this.onLoadCallback, true);
+        video.removeEventListener('error', this.onErrorCallback, true);
 
         this.data = video;
 
@@ -149,90 +170,55 @@ var VideoFile = new Class({
 
         this.loader.nextFile(this, true);
     },
+
+    /**
+     * Internal load error event callback.
+     *
+     * @method Phaser.Loader.FileTypes.VideoFile#onVideoErrorHandler
+     * @private
+     * @since 3.20.0
+     *
+     * @param {ProgressEvent} event - The DOM ProgressEvent that resulted from this load.
      */
+    onVideoErrorHandler: function (event)
+    {
+        var video = event.target;
+
+        if (video)
+        {
+            video.removeEventListener(this.config.loadEvent, this.onLoadCallback, true);
+            video.removeEventListener('error', this.onErrorCallback, true);
+        }
+
+        this.resetXHR();
+
+        this.loader.nextFile(this, false);
+    },
 
     /**
      * Called by the Loader, starts the actual file downloading.
      * During the load the methods onLoad, onError and onProgress are called, based on the XHR events.
      * You shouldn't normally call this method directly, it's meant to be invoked by the Loader.
      *
-     * @method Phaser.Loader.FileTypes.HTML5AudioFile#load
-     * @since 3.0.0
+     * @method Phaser.Loader.FileTypes.VideoFile#load
+     * @since 3.20.0
      */
     load: function ()
     {
         var loadEvent = this.config.loadEvent;
-        var asBlob = this.config.asBlob;
-        var noAudio = this.config.noAudio;
 
-        if (asBlob)
+        if (this.config.asBlob)
         {
-            // console.log('Passing load over to File.load');
-            
             File.prototype.load.call(this);
         }
         else
         {
-            console.log('Loading as Video tag');
-
             this.percentComplete = 0;
 
-            var video = document.createElement('video');
-    
-            video.controls = false;
-            video.crossOrigin = this.loader.crossOrigin;
+            var video = this.createVideoElement();
 
-            if (noAudio)
-            {
-                video.muted = true;
-                video.defaultMuted = true;
-
-                video.setAttribute('autoplay', 'autoplay');
-            }
-
-            video.setAttribute('playsinline', 'playsinline');
-            video.setAttribute('preload', 'auto');
-
-            var _this = this;
-    
-            this.onVideoLoadHandler = function (event)
-            {
-                console.log('onVideoLoadHandler');
-                console.log(event);
-        
-                var video = event.target;
-        
-                video.removeEventListener(_this.config.loadEvent, _this.onVideoLoadHandler, true);
-        
-                _this.data = video;
-        
-                _this.resetXHR();
-        
-                _this.loader.nextFile(_this, true);
-            };
-
-            video.addEventListener(loadEvent, this.onVideoLoadHandler, true);
-
-            video.addEventListener('error', function (e)
-            {
-                console.log('Load Error');
-                console.log(e);
-            }, true);
-
-            video.addEventListener('loadstart', function (e)
-            {
-                console.log('Load Start');
-            }, true);
-
-            video.addEventListener('loadedmetadata', function (e)
-            {
-                console.log('Loaded Meta Data');
-            }, true);
-
-            video.addEventListener('emptied', function (e)
-            {
-                console.log('Load Emptied');
-            }, true);
+            video.addEventListener(loadEvent, this.onLoadCallback, true);
+            video.addEventListener('error', this.onErrorCallback, true);
 
             video.src = GetURL(this, this.loader.baseURL);
 
@@ -257,8 +243,6 @@ VideoFile.create = function (loader, key, urls, loadEvent, asBlob, noAudio, xhrS
     }
 
     var urlConfig = VideoFile.getVideoURL(game, urls);
-
-    // console.log(urlConfig);
 
     if (urlConfig)
     {
@@ -299,14 +283,14 @@ VideoFile.getVideoURL = function (game, urls)
 };
 
 /**
- * Adds an Audio or HTML5Audio file, or array of audio files, to the current load queue.
+ * Adds a Video file, or array of video files, to the current load queue.
  *
  * You can call this method from within your Scene's `preload`, along with any other files you wish to load:
  * 
  * ```javascript
  * function preload ()
  * {
- *     this.load.audio('title', [ 'music/Title.ogg', 'music/Title.mp3', 'music/Title.m4a' ]);
+ *     this.load.video('intro', [ 'video/level1.mp4', 'video/level1.webm', 'video/level1.mov' ]);
  * }
  * ```
  *
@@ -318,17 +302,19 @@ VideoFile.getVideoURL = function (game, urls)
  * Scene's `create` method is called you are guaranteed that all of those assets are ready for use and have been
  * loaded.
  * 
- * The key must be a unique String. It is used to add the file to the global Audio Cache upon a successful load.
- * The key should be unique both in terms of files being loaded and files already present in the Audio Cache.
+ * The key must be a unique String. It is used to add the file to the global Video Cache upon a successful load.
+ * The key should be unique both in terms of files being loaded and files already present in the Video Cache.
  * Loading a file using a key that is already taken will result in a warning. If you wish to replace an existing file
- * then remove it from the Audio Cache first, before loading a new one.
- *
+ * then remove it from the Video Cache first, before loading a new one.
+  *
  * Instead of passing arguments you can pass a configuration object, such as:
  * 
  * ```javascript
- * this.load.audio({
- *     key: 'title',
- *     url: [ 'music/Title.ogg', 'music/Title.mp3', 'music/Title.m4a' ]
+ * this.load.video({
+ *     key: 'intro',
+ *     url: [ 'video/level1.mp4', 'video/level1.webm', 'video/level1.mov' ],
+ *     asBlob: false,
+ *     noAudio: true
  * });
  * ```
  *
@@ -336,13 +322,14 @@ VideoFile.getVideoURL = function (game, urls)
  *
  * The URLs can be relative or absolute. If the URLs are relative the `Loader.baseURL` and `Loader.path` values will be prepended to them.
  *
- * Due to different browsers supporting different audio file types you should usually provide your audio files in a variety of formats.
- * ogg, mp3 and m4a are the most common. If you provide an array of URLs then the Loader will determine which _one_ file to load based on
- * browser support.
+ * Due to different browsers supporting different video file types you should usually provide your video files in a variety of formats.
+ * mp4, mov and webm are the most common. If you provide an array of URLs then the Loader will determine which _one_ file to load based on
+ * browser support, starting with the first in the array and progressing to the end.
+ * 
+ * Unlike most asset-types, videos do not _need_ to be preloaded. You can create a Video Game Object and then call its `loadURL` method,
+ * to load a video at run-time, rather than in advance.
  *
- * If audio has been disabled in your game, either via the game config, or lack of support from the device, then no audio will be loaded.
- *
- * Note: The ability to load this type of file will only be available if the Audio File type has been built into Phaser.
+ * Note: The ability to load this type of file will only be available if the Video File type has been built into Phaser.
  * It is available in the default build but can be excluded from custom builds.
  *
  * @method Phaser.Loader.LoaderPlugin#video
@@ -350,8 +337,10 @@ VideoFile.getVideoURL = function (game, urls)
  * @since 3.20.0
  *
  * @param {(string|Phaser.Types.Loader.FileTypes.VideoFileConfig|Phaser.Types.Loader.FileTypes.VideoFileConfig[])} key - The key to use for this file, or a file configuration object, or array of them.
- * @param {(string|string[])} [urls] - The absolute or relative URL to load the audio files from.
- * @param {any} [config] - An object containing an `instances` property for HTML5Audio. Defaults to 1.
+ * @param {(string|string[])} [urls] - The absolute or relative URL to load the video files from.
+ * @param {string} [loadEvent='loadeddata'] - The load event to listen for when _not_ loading as a blob. Either `loadeddata`, `canplay` or `canplaythrough`.
+ * @param {boolean} [asBlob=false] - Load the video as a data blob, or stream it via the Video element?
+ * @param {boolean} [noAudio=false] - Does the video have an audio track? If not you can enable auto-playing on it.
  * @param {Phaser.Types.Loader.XHRSettingsObject} [xhrSettings] - An XHR Settings configuration object. Used in replacement of the Loaders default XHR Settings.
  *
  * @return {Phaser.Loader.LoaderPlugin} The Loader instance.
