148e907b715069b0090769c85c347bbce83395fc
samme <samme@users.noreply.github.com>
2019-11-19 15:24:16 -0800

    Add Math.Distance methods
    - BetweenPoints()
    - BetweenPointsSquared()
    - Chebyshev()
    - Snake()

--- src/math/distance/index.js
+++ src/math/distance/index.js
@@ -11,7 +11,11 @@
 module.exports = {
 
     Between: require('./DistanceBetween'),
+    BetweenPoints: require('./DistanceBetweenPoints'),
+    BetweenPointsSquared: require('./DistanceBetweenPointsSquared'),
+    Chebyshev: require('./DistanceChebyshev'),
     Power: require('./DistancePower'),
+    Snake: require('./DistanceSnake'),
     Squared: require('./DistanceSquared')
 
 };
