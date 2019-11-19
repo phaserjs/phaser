c37058ef4bdbfd32b3ab6c7e2ea36cfd53e4c06e
Richard Davey <rich@photonstorm.com>
2019-11-19 11:49:54 +0000

    When calling `TileSprite.setTexture` or `setFrame`, if the new frame size didn't match the old one, the new fill pattern would become distorted and the `potWidth` and `potHeight` values would be incorrect.

--- src/gameobjects/tilesprite/TileSprite.js
+++ src/gameobjects/tilesprite/TileSprite.js
@@ -322,9 +322,15 @@ var TileSprite = new Class({
      */
     setFrame: function (frame)
     {
-        this.displayFrame = this.displayTexture.get(frame);
+        var newFrame = this.displayTexture.get(frame);
 
-        if (!this.displayFrame.cutWidth || !this.displayFrame.cutHeight)
+        this.potWidth = GetPowerOfTwo(newFrame.width);
+        this.potHeight = GetPowerOfTwo(newFrame.height);
+
+        //  So updateCanvas is triggered
+        this.canvas.width = 0;
+
+        if (!newFrame.cutWidth || !newFrame.cutHeight)
         {
             this.renderFlags &= ~_FLAG;
         }
@@ -332,6 +338,8 @@ var TileSprite = new Class({
         {
             this.renderFlags |= _FLAG;
         }
+
+        this.displayFrame = newFrame;
 
         this.dirty = true;
 
