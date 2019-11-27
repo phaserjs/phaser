a2a6124ee5450735867032ae4dab1f87bac5a5a0
Hua <binhua.tein@gmail.com>
2019-11-27 10:05:54 +0800

    Mark arguments as optional

--- src/curves/Curve.js
+++ src/curves/Curve.js
@@ -353,8 +353,8 @@ var Curve = new Class({
      * @method Phaser.Curves.Curve#getSpacedPoints
      * @since 3.0.0
      *
-     * @param {integer} divisions - The number of evenly spaced points from the curve to return. If falsy, step param will be used to calculate the number of points.
-     * @param {number} step - Step between points. Used to calculate the number of points to return when divisions is falsy. Ignored if divisions is positive.     
+     * @param {integer} [divisions] - The number of evenly spaced points from the curve to return. If falsy, step param will be used to calculate the number of points.
+     * @param {number} [step] - Step between points. Used to calculate the number of points to return when divisions is falsy. Ignored if divisions is positive.     
      * @param {(array|Phaser.Math.Vector2[])} [out] - An optional array to store the points in.
      *
      * @return {Phaser.Math.Vector2[]} [description]
