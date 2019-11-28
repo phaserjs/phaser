2a4dadf3a7f92d66afc96584533cd13af6a176db
Richard Davey <rich@photonstorm.com>
2019-11-28 16:18:53 +0000

    Update MatterWorldConfig.js

--- src/physics/matter-js/typedefs/MatterWorldConfig.js
+++ src/physics/matter-js/typedefs/MatterWorldConfig.js
@@ -23,19 +23,17 @@
  * @property {number} [correction=1] - An optional Number that specifies the time correction factor to apply to the update.
  * @property {function} [getDelta] - This function is called every time the core game loop steps, which is bound to the Request Animation Frame frequency unless otherwise modified.
  * @property {boolean} [autoUpdate=true] - Automatically call Engine.update every time the game steps.
- * @property {boolean} [debug=false] - Sets if Matter will render to the debug Graphic overlay. Do not enable this in production.
- * @property {boolean} [debugShowBody=true] - Should dynamic bodies be drawn to the debug graphic?
- * @property {boolean} [debugShowStaticBody=true] - Should static bodies be drawn to the debug graphic?
- * @property {boolean} [debugShowVelocity=true] - Should the velocity vector be drawn to the debug graphic?
- * @property {number} [debugBodyColor=0xff00ff] - The color that dynamic body debug outlines are drawn in.
- * @property {number} [debugBodyFillColor=0xe3a7e3] - The color that dynamic body debug fills are drawn in.
- * @property {number} [debugStaticBodyColor=0x0000ff] - The color that static body debug outlines are drawn in.
- * @property {number} [debugVelocityColor=0x00ff00] - The color that the debug velocity vector lines are drawn in.
- * @property {boolean} [debugShowJoint=true] - Render joints to the debug graphic.
- * @property {number} [debugJointColor=0x000000] - The color that the debug joints are drawn in.
- * @property {boolean} [debugWireframes=true] - Render the debug output as wireframes.
- * @property {boolean} [debugShowInternalEdges=false] - Render internal edges to the debug.
- * @property {boolean} [debugShowConvexHulls=false] - Render convex hulls to the debug.
- * @property {number} [debugConvexHullColor=0xaaaaaa] - The color that the debug convex hulls are drawn in, if enabled.
- * @property {boolean} [debugShowSleeping=false] - Render sleeping bodies the debug.
+ * @property {(boolean|Phaser.Types.Physics.Matter.MatterDebugConfig)} [debug=false] - Sets if Matter will render to the debug Graphic overlay. As of Phaser 3.22 it should be a `MatterDebugConfig` object instead of a boolean.
+ * @property {boolean} [debugShowBody=true] - Should dynamic bodies be drawn to the debug graphic? Deprecated in Phaser 3.22: use a `MatterDebugConfig` object instead.
+ * @property {boolean} [debugShowStaticBody=true] - Should static bodies be drawn to the debug graphic? Deprecated in Phaser 3.22: use a `MatterDebugConfig` object instead.
+ * @property {number} [debugBodyColor=0xff00ff] - The color that dynamic body debug outlines are drawn in. Deprecated in Phaser 3.22: use a `MatterDebugConfig` object instead.
+ * @property {number} [debugBodyFillColor=0xe3a7e3] - The color that dynamic body debug fills are drawn in. Deprecated in Phaser 3.22: use a `MatterDebugConfig` object instead.
+ * @property {number} [debugStaticBodyColor=0x0000ff] - The color that static body debug outlines are drawn in. Deprecated in Phaser 3.22: use a `MatterDebugConfig` object instead.
+ * @property {boolean} [debugShowJoint=true] - Render joints to the debug graphic. Deprecated in Phaser 3.22: use a `MatterDebugConfig` object instead.
+ * @property {number} [debugJointColor=0x000000] - The color that the debug joints are drawn in. Deprecated in Phaser 3.22: use a `MatterDebugConfig` object instead.
+ * @property {boolean} [debugWireframes=true] - Render the debug output as wireframes. Deprecated in Phaser 3.22: use a `MatterDebugConfig` object instead.
+ * @property {boolean} [debugShowInternalEdges=false] - Render internal edges to the debug. Deprecated in Phaser 3.22: use a `MatterDebugConfig` object instead.
+ * @property {boolean} [debugShowConvexHulls=false] - Render convex hulls to the debug. Deprecated in Phaser 3.22: use a `MatterDebugConfig` object instead.
+ * @property {number} [debugConvexHullColor=0xaaaaaa] - The color that the debug convex hulls are drawn in, if enabled. Deprecated in Phaser 3.22: use a `MatterDebugConfig` object instead.
+ * @property {boolean} [debugShowSleeping=false] - Render sleeping bodies the debug. Deprecated in Phaser 3.22: use a `MatterDebugConfig` object instead.
  */
