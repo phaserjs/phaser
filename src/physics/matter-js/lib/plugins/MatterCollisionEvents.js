f050b148ec83d339944dcc05e9d35eee19315af8
Richard Davey <rich@photonstorm.com>
2019-12-03 10:43:35 +0000

    Rewrote plugin to tidy it up.

--- src/physics/matter-js/lib/plugins/MatterCollisionEvents.js
+++ src/physics/matter-js/lib/plugins/MatterCollisionEvents.js
@@ -1,65 +1,116 @@
-var Matter = require('../../CustomMain');
+/**
+ * @author       @dxu https://github.com/dxu/matter-collision-events
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @license      {@link https://opensource.org/licenses/MIT|MIT License}
+ */
 
 var MatterCollisionEvents = {
-  name: 'matter-collision-events',
-  version: '0.1.5',
-  for: 'matter-js@^0.14.2',
-  silent: true, // no console log please
 
-  install: function(matter) {
-    // add the onCollide, onCollideEnd, and onCollideActive callback handlers
-    // to the native Matter.Body created
-    var create = matter.Body.create;
-    matter.Body.create = function() {
-      var body = create.apply(null, arguments);
-      body.onCollide = function(cb) { body._mceOC = cb; }
-      body.onCollideEnd = function(cb) { body._mceOCE = cb; }
-      body.onCollideActive = function(cb) { body._mceOCA = cb; }
-      return body;
+    name: 'matter-collision-events',
+    version: '0.1.6',
+    for: 'matter-js@^0.14.2',
+    silent: true,
+
+    install: function (matter)
+    {
+        matter.after('Body.create', function (body)
+        {
+            body.onCollideCallback;
+            body.onCollideEndCallback;
+            body.onCollideActiveCallback;
+
+            body.setOnCollide = function (callback)
+            {
+                this.onCollideCallback = callback;
+
+                return this;
+            }
+
+            body.setOnCollideEnd = function (callback)
+            {
+                this.onCollideEndCallback = callback;
+
+                return this;
+            }
+
+            body.setOnCollideActive = function (callback)
+            {
+                this.onCollideActiveCallback = callback;
+
+                return this;
+            }
+        });
+      
+        matter.after('Engine.create', function ()
+        {
+            matter.Events.on(this, 'collisionStart', function (event)
+            {
+                event.pairs.map(function (pair)
+                {
+                    var bodyA = pair.bodyA;
+                    var bodyB = pair.bodyB;
+
+                    matter.Events.trigger(bodyA, 'onCollide', { pair: pair });
+                    matter.Events.trigger(bodyB, 'onCollide', { pair: pair });
+
+                    if (bodyA.onCollideCallback)
+                    {
+                        bodyA.onCollideCallback(pair);
+                    }
+
+                    if (bodyB.onCollideCallback)
+                    {
+                        bodyB.onCollideCallback(pair);
+                    }
+                });
+            });
+
+            matter.Events.on(this, 'collisionActive', function (event)
+            {
+                event.pairs.map(function (pair)
+                {
+                    var bodyA = pair.bodyA;
+                    var bodyB = pair.bodyB;
+
+                    matter.Events.trigger(bodyA, 'onCollideActive', { pair: pair });
+                    matter.Events.trigger(bodyB, 'onCollideActive', { pair: pair });
+
+
+                    if (bodyA.onCollideActiveCallback)
+                    {
+                        bodyA.onCollideActiveCallback(pair);
+                    }
+
+                    if (bodyB.onCollideActiveCallback)
+                    {
+                        bodyB.onCollideActiveCallback(pair);
+                    }
+                });
+            });
+
+            matter.Events.on(this, 'collisionEnd', function (event)
+            {
+                event.pairs.map(function (pair)
+                {
+                    var bodyA = pair.bodyA;
+                    var bodyB = pair.bodyB;
+
+                    matter.Events.trigger(bodyA, 'onCollideEnd', { pair: pair });
+                    matter.Events.trigger(bodyB, 'onCollideEnd', { pair: pair });
+
+                    if (bodyA.onCollideEndCallback)
+                    {
+                        bodyA.onCollideEndCallback(pair);
+                    }
+
+                    if (bodyB.onCollideEndCallback)
+                    {
+                        bodyB.onCollideEndCallback(pair);
+                    }
+                });
+            });
+        });
     }
-    matter.after('Engine.create', function() {
-      matter.Events.on(this, 'collisionStart', function(event) {
-        event.pairs.map(function(pair) {
-          matter.Events.trigger(pair.bodyA, 'onCollide', { pair : pair });
-          matter.Events.trigger(pair.bodyB, 'onCollide', { pair : pair });
-          pair.bodyA._mceOC &&
-            pair.bodyA._mceOC(pair)
-          pair.bodyB._mceOC &&
-            pair.bodyB._mceOC(pair)
-        });
-      });
-
-      matter.Events.on(this, 'collisionActive', function(event) {
-        event.pairs.map(function(pair) {
-          matter.Events.trigger(
-            pair.bodyA,
-            'onCollideActive',
-            { pair: pair }
-          );
-          matter.Events.trigger(
-            pair.bodyB,
-            'onCollideActive',
-            { pair: pair }
-          );
-          pair.bodyA._mceOCA &&
-            pair.bodyA._mceOCA(pair)
-          pair.bodyB._mceOCA &&
-            pair.bodyB._mceOCA(pair)
-        });
-      });
-
-      matter.Events.on(this, 'collisionEnd', function(event) {
-        event.pairs.map(function(pair) {
-          matter.Events.trigger(pair.bodyA, 'onCollideEnd', { pair : pair });
-          matter.Events.trigger(pair.bodyB, 'onCollideEnd', { pair : pair });
-          pair.bodyA._mceOCE &&
-            pair.bodyA._mceOCE(pair)
-          pair.bodyB._mceOCE &&
-            pair.bodyB._mceOCE(pair)
-        });
-      });
-    });
-  }
 };
 
 module.exports = MatterCollisionEvents;
