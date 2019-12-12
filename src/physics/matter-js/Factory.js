093557a6ea658c19ddeb03075c2c27db223afe0b
Richard Davey <rich@photonstorm.com>
2019-12-12 13:31:36 +0000

    Removed worldConstraint changes

--- src/physics/matter-js/Factory.js
+++ src/physics/matter-js/Factory.js
@@ -15,7 +15,6 @@ var MatterSprite = require('./MatterSprite');
 var MatterTileBody = require('./MatterTileBody');
 var PointerConstraint = require('./PointerConstraint');
 var Vertices = require('./lib/geometry/Vertices');
-var Vector = require('./lib/geometry/Vector');
 
 /**
  * @classdesc
@@ -518,8 +517,6 @@ var Factory = new Class({
      * @method Phaser.Physics.Matter.Factory#worldConstraint
      * @since 3.0.0
      *
-     * @param {number} x - The x position in the world to create the constraint at.
-     * @param {number} y - The y position in the world to create the constraint at.
      * @param {MatterJS.Body} body - The Matter `Body` that this constraint is attached to.
      * @param {number} [length] - A number that specifies the target resting length of the constraint. If not given it is calculated automatically in `Constraint.create` from initial positions of the `constraint.bodyA` and `constraint.bodyB`.
      * @param {number} [stiffness=1] - A Number that specifies the stiffness of the constraint, i.e. the rate at which it returns to its resting `constraint.length`. A value of `1` means the constraint should be very stiff. A value of `0.2` means the constraint acts as a soft spring.
@@ -527,12 +524,11 @@ var Factory = new Class({
      *
      * @return {MatterJS.Constraint} A Matter JS Constraint.
      */
-    worldConstraint: function (x, y, body, length, stiffness, options)
+    worldConstraint: function (body, length, stiffness, options)
     {
         if (stiffness === undefined) { stiffness = 1; }
         if (options === undefined) { options = {}; }
 
-        options.pointA = Vector.create(x, y);
         options.bodyB = (body.type === 'body') ? body : body.body;
 
         if (!isNaN(length))
