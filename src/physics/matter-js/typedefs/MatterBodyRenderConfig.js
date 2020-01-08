6a8bce925020eed28136303766b70e34fae96f65
Richard Davey <rich@photonstorm.com>
2020-01-08 11:14:26 +0000

    Added essential new type defs

---
+++ src/physics/matter-js/typedefs/MatterBodyRenderConfig.js
@@ -0,0 +1,15 @@
+/**
+ * @typedef {object} Phaser.Types.Physics.Matter.MatterBodyRenderConfig
+ * @since 3.22.0
+ *              
+ * @property {boolean} [visible=true] - Should this body be rendered by the Debug Renderer?
+ * @property {number} [opacity=1] - The opacity of the body and all parts within it.
+ * @property {number} [fillColor] - The color value of the fill when rendering this body.
+ * @property {number} [fillOpacity] - The opacity of the fill when rendering this body, a value between 0 and 1.
+ * @property {number} [lineColor] - The color value of the line stroke when rendering this body.
+ * @property {number} [lineOpacity] - The opacity of the line when rendering this body, a value between 0 and 1.
+ * @property {number} [lineThickness] - If rendering lines, the thickness of the line.
+ * @property {object} [sprite] - Controls the offset between the body and the parent Game Object, if it has one.
+ * @property {number} [sprite.xOffset=0] - The horizontal offset between the body and the parent Game Object texture, if it has one.
+ * @property {number} [sprite.yOffset=0] - The vertical offset between the body and the parent Game Object texture, if it has one.
+ */
