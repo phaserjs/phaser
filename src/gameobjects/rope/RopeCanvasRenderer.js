162b07bd6bc0c6476184d3c1a5169a54e493d9e6
Richard Davey <rich@photonstorm.com>
2020-01-17 17:38:06 +0000

    Added new Rope Game Object

---
+++ src/gameobjects/rope/RopeCanvasRenderer.js
@@ -0,0 +1,23 @@
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2020 Photon Storm Ltd.
+ * @license      {@link https://opensource.org/licenses/MIT|MIT License}
+ */
+
+/**
+ * This is a stub function for Rope.Render. There is no Canvas renderer for Rope objects.
+ *
+ * @method Phaser.GameObjects.Rope#renderCanvas
+ * @since 3.0.0
+ * @private
+ *
+ * @param {Phaser.Renderer.Canvas.CanvasRenderer} renderer - A reference to the current active Canvas renderer.
+ * @param {Phaser.GameObjects.Rope} src - The Game Object being rendered in this call.
+ * @param {number} interpolationPercentage - Reserved for future use and custom pipelines.
+ * @param {Phaser.Cameras.Scene2D.Camera} camera - The Camera that is rendering the Game Object.
+ */
+var RopeCanvasRenderer = function ()
+{
+};
+
+module.exports = RopeCanvasRenderer;
