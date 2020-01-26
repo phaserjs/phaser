12d135f22375fef7fa308b4effc0f5f023248c35
Jason Kwok <JasonHK@users.noreply.github.com>
2020-01-26 17:25:13 +0800

    Added definitions for `Phaser.Types.Scenes.SceneEventsMap`

--- src/loader/LoaderPlugin.js
+++ src/loader/LoaderPlugin.js
@@ -1067,6 +1067,256 @@ var LoaderPlugin = new Class({
 
 });
 
+/**
+ * Return the listeners registered for a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#listeners
+ * @since 3.0.0
+ * 
+ * @generic {keyof Phaser.Types.Loader.LoaderPluginEventsMap} K - [event]
+ *
+ * @param {(string|symbol)} event - The event name.
+ *
+ * @return {Function[]} The registered listeners.
+ */
+
+/**
+ * Return the listeners registered for a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#listeners
+ * @since 3.0.0
+ *
+ * @param {(string|symbol)} event - The event name.
+ *
+ * @return {Function[]} The registered listeners.
+ */
+
+/**
+ * Return the number of listeners listening to a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#listenerCount
+ * @since 3.0.0
+ * 
+ * @generic {keyof Phaser.Types.Loader.LoaderPluginEventsMap} K - [event]
+ *
+ * @param {(string|symbol)} event - The event name.
+ *
+ * @return {number} The number of listeners.
+ */
+
+/**
+ * Return the number of listeners listening to a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#listenerCount
+ * @since 3.0.0
+ * 
+ * @generic {keyof Phaser.Types.Loader.LoaderPluginEventsMap} K - [event]
+ *
+ * @param {(string|symbol)} event - The event name.
+ *
+ * @return {number} The number of listeners.
+ */
+
+/**
+ * Calls each of the listeners registered for a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#emit
+ * @since 3.0.0
+ * 
+ * @generic {keyof Phaser.Types.Loader.LoaderPluginEventsMap} K - [event]
+ * @genericUse {Phaser.Types.Events.EmitArguments<Phaser.Types.Loader.LoaderPluginEventsMap[K]>} - [args]
+ *
+ * @param {(string|symbol)} event - The event name.
+ * @param {...*} [args] - Additional arguments that will be passed to the event handler.
+ *
+ * @return {boolean} `true` if the event had listeners, else `false`.
+ */
+
+/**
+ * Calls each of the listeners registered for a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#emit
+ * @since 3.0.0
+ *
+ * @param {(string|symbol)} event - The event name.
+ * @param {...*} [args] - Additional arguments that will be passed to the event handler.
+ *
+ * @return {boolean} `true` if the event had listeners, else `false`.
+ */
+
+/**
+ * Add a listener for a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#on
+ * @since 3.0.0
+ * 
+ * @generic {keyof Phaser.Types.Loader.LoaderPluginEventsMap} K - [event]
+ * @genericUse {Phaser.Types.Loader.LoaderPluginEventsMap[K]} - [fn]
+ *
+ * @param {(string|symbol)} event - The event name.
+ * @param {function} fn - The listener function.
+ * @param {*} [context=this] - The context to invoke the listener with.
+ *
+ * @return {this} `this`.
+ */
+
+/**
+ * Add a listener for a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#on
+ * @since 3.0.0
+ *
+ * @param {(string|symbol)} event - The event name.
+ * @param {function} fn - The listener function.
+ * @param {*} [context=this] - The context to invoke the listener with.
+ *
+ * @return {this} `this`.
+ */
+
+/**
+ * Add a listener for a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#addListener
+ * @since 3.0.0
+ * 
+ * @generic {keyof Phaser.Types.Loader.LoaderPluginEventsMap} K - [event]
+ * @genericUse {Phaser.Types.Loader.LoaderPluginEventsMap[K]} - [fn]
+ *
+ * @param {(string|symbol)} event - The event name.
+ * @param {function} fn - The listener function.
+ * @param {*} [context=this] - The context to invoke the listener with.
+ *
+ * @return {this} `this`.
+ */
+
+/**
+ * Add a listener for a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#addListener
+ * @since 3.0.0
+ *
+ * @param {(string|symbol)} event - The event name.
+ * @param {function} fn - The listener function.
+ * @param {*} [context=this] - The context to invoke the listener with.
+ *
+ * @return {this} `this`.
+ */
+
+/**
+ * Add a one-time listener for a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#once
+ * @since 3.0.0
+ * 
+ * @generic {keyof Phaser.Types.Loader.LoaderPluginEventsMap} K - [event]
+ * @genericUse {Phaser.Types.Loader.LoaderPluginEventsMap[K]} - [fn]
+ *
+ * @param {(string|symbol)} event - The event name.
+ * @param {function} fn - The listener function.
+ * @param {*} [context=this] - The context to invoke the listener with.
+ *
+ * @return {this} `this`.
+ */
+
+/**
+ * Add a one-time listener for a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#once
+ * @since 3.0.0
+ *
+ * @param {(string|symbol)} event - The event name.
+ * @param {function} fn - The listener function.
+ * @param {*} [context=this] - The context to invoke the listener with.
+ *
+ * @return {this} `this`.
+ */
+
+/**
+ * Remove the listeners of a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#removeListener
+ * @since 3.0.0
+ * 
+ * @generic {keyof Phaser.Types.Loader.LoaderPluginEventsMap} K - [event]
+ * @genericUse {Phaser.Types.Loader.LoaderPluginEventsMap[K]} - [fn]
+ *
+ * @param {(string|symbol)} event - The event name.
+ * @param {function} [fn] - Only remove the listeners that match this function.
+ * @param {*} [context] - Only remove the listeners that have this context.
+ * @param {boolean} [once] - Only remove one-time listeners.
+ *
+ * @return {this} `this`.
+ */
+
+/**
+ * Remove the listeners of a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#removeListener
+ * @since 3.0.0
+ *
+ * @param {(string|symbol)} event - The event name.
+ * @param {function} [fn] - Only remove the listeners that match this function.
+ * @param {*} [context] - Only remove the listeners that have this context.
+ * @param {boolean} [once] - Only remove one-time listeners.
+ *
+ * @return {this} `this`.
+ */
+
+/**
+ * Remove the listeners of a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#off
+ * @since 3.0.0
+ * 
+ * @generic {keyof Phaser.Types.Loader.LoaderPluginEventsMap} K - [event]
+ * @genericUse {Phaser.Types.Loader.LoaderPluginEventsMap[K]} - [fn]
+ *
+ * @param {(string|symbol)} event - The event name.
+ * @param {function} [fn] - Only remove the listeners that match this function.
+ * @param {*} [context] - Only remove the listeners that have this context.
+ * @param {boolean} [once] - Only remove one-time listeners.
+ *
+ * @return {this} `this`.
+ */
+
+/**
+ * Remove the listeners of a given event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#off
+ * @since 3.0.0
+ *
+ * @param {(string|symbol)} event - The event name.
+ * @param {function} [fn] - Only remove the listeners that match this function.
+ * @param {*} [context] - Only remove the listeners that have this context.
+ * @param {boolean} [once] - Only remove one-time listeners.
+ *
+ * @return {this} `this`.
+ */
+
+/**
+ * Remove all listeners, or those of the specified event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#removeAllListeners
+ * @since 3.0.0
+ * 
+ * @generic {keyof Phaser.Types.Loader.LoaderPluginEventsMap} K - [event]
+ *
+ * @param {(string|symbol)} [event] - The event name.
+ *
+ * @return {this} `this`.
+ */
+
+/**
+ * Remove all listeners, or those of the specified event.
+ *
+ * @method Phaser.Loader.LoaderPlugin#removeAllListeners
+ * @since 3.0.0
+ *
+ * @param {(string|symbol)} [event] - The event name.
+ *
+ * @return {this} `this`.
+ */
+
 PluginCache.register('Loader', LoaderPlugin, 'load');
 
 module.exports = LoaderPlugin;
