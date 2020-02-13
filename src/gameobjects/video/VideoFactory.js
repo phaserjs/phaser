f5128a428a92a9abbb7189586b53097def7918d3
Richard Davey <rich@photonstorm.com>
2020-02-13 12:13:13 +0000

    Fixed Video return type. Fix #5003

--- src/gameobjects/video/VideoFactory.js
+++ src/gameobjects/video/VideoFactory.js
@@ -8,19 +8,18 @@ var Video = require('./Video');
 var GameObjectFactory = require('../GameObjectFactory');
 
 /**
- * Creates a new Image Game Object and adds it to the Scene.
+ * Creates a new Video Game Object and adds it to the Scene.
  *
- * Note: This method will only be available if the Image Game Object has been built into Phaser.
+ * Note: This method will only be available if the Video Game Object has been built into Phaser.
  *
  * @method Phaser.GameObjects.GameObjectFactory#video
  * @since 3.20.0
  *
  * @param {number} x - The horizontal position of this Game Object in the world.
  * @param {number} y - The vertical position of this Game Object in the world.
- * @param {string} texture - The key of the Texture this Game Object will use to render with, as stored in the Texture Manager.
- * @param {(string|integer)} [frame] - An optional frame from the Texture this Game Object is rendering with.
+ * @param {string} [key] - Optional key of the Video this Game Object will play, as stored in the Video Cache.
  *
- * @return {Phaser.GameObjects.Image} The Game Object that was created.
+ * @return {Phaser.GameObjects.Video} The Game Object that was created.
  */
 GameObjectFactory.register('video', function (x, y, key)
 {
