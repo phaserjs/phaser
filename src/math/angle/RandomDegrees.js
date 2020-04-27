873384784297c81947344957a748fe861a9d2897
Richard Davey <rich@photonstorm.com>
2020-04-27 10:23:55 +0100

    Should be between -180, 180 #5075

--- src/math/angle/RandomDegrees.js
+++ src/math/angle/RandomDegrees.js
@@ -1,7 +1,14 @@
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @author       @samme
+ * @copyright    2020 Photon Storm Ltd.
+ * @license      {@link https://opensource.org/licenses/MIT|MIT License}
+ */
+
 var FloatBetween = require('../FloatBetween');
 
 /**
- * Returns a random angle in the range [0, 360].
+ * Returns a random angle in the range [-180, 180].
  *
  * @function Phaser.Math.Angle.RandomDegrees
  * @since 3.23.0
@@ -10,7 +17,7 @@ var FloatBetween = require('../FloatBetween');
  */
 var RandomDegrees = function ()
 {
-    return FloatBetween(0, 360);
+    return FloatBetween(-180, 180);
 };
 
 module.exports = RandomDegrees;
