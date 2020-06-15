d60c9a33d836c9c0c4f50043414a166f90d663bd
MerganThePirate <dmgf2008@hotmail.com>
2020-06-15 08:25:26 -0700

    Fixed bug from issue #5191

--- src/dom/AddToDOM.js
+++ src/dom/AddToDOM.js
@@ -33,7 +33,7 @@ var AddToDOM = function (element, parent)
             target = parent;
         }
     }
-    else if (element.parentElement)
+    else if (element.parentElement || parent === null)
     {
         return element;
     }
