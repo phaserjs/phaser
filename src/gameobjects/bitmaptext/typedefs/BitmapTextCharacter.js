c166976fcc4395363ed61798055a23557a78ea1c
Richard Davey <rich@photonstorm.com>
2020-07-31 12:06:57 +0100

    Update BitmapTextCharacter.js

--- src/gameobjects/bitmaptext/typedefs/BitmapTextCharacter.js
+++ src/gameobjects/bitmaptext/typedefs/BitmapTextCharacter.js
@@ -1,7 +1,8 @@
 /**
  * A single entry from the `BitmapTextSize` characters array.
  *
- * The position and dimensions take the scale factor into account.
+ * The position and dimensions take the font size into account,
+ * but are not translated into the local space of the Game Object itself.
  *
  * @typedef {object} Phaser.Types.GameObjects.BitmapText.BitmapTextCharacter
  * @since 3.25.0
@@ -13,5 +14,15 @@
  * @property {string} char - The character.
  * @property {number} code - The character code of the character.
  * @property {number} line - The line number the character appears on.
+ * @property {boolean} isTinted - Does this character have its own unique tint? (WebGL only).
+ * @property {number} tintEffect - The tint effect this character uses (WebGL only).
+ * @property {number} tintTL - The top-left tint color for this character (WebGL only).
+ * @property {number} tintTR - The top-right tint color for this character (WebGL only).
+ * @property {number} tintBL - The bottom-left tint color for this character (WebGL only).
+ * @property {number} tintBR - The bottom-right tint color for this character (WebGL only).
+ * @property {number} alphaTL - The top-left alpha value for this character (WebGL only).
+ * @property {number} alphaTR - The top-right alpha value for this character (WebGL only).
+ * @property {number} alphaBL - The bottom-left alpha value for this character (WebGL only).
+ * @property {number} alphaBR - The bottom-right alpha value for this character (WebGL only).
  * @property {Phaser.Types.GameObjects.BitmapText.BitmapFontCharacterData} glyph - Reference to the glyph object this character is using.
  */
