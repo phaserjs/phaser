fc6e7ef7596d1f41faceed24d68e3636aec2bcd0
Richard Davey <rich@photonstorm.com>
2020-07-31 15:46:05 +0100

    Removed un-used properties

--- src/gameobjects/bitmaptext/typedefs/BitmapTextCharacter.js
+++ src/gameobjects/bitmaptext/typedefs/BitmapTextCharacter.js
@@ -7,6 +7,7 @@
  * @typedef {object} Phaser.Types.GameObjects.BitmapText.BitmapTextCharacter
  * @since 3.50.0
  *
+ * @property {number} i - The index of this character within the BitmapText text string.
  * @property {number} x - The x position of the character in the BitmapText.
  * @property {number} y - The y position of the character in the BitmapText.
  * @property {number} w - The width of the character.
@@ -14,15 +15,5 @@
  * @property {string} char - The character.
  * @property {number} code - The character code of the character.
  * @property {number} line - The line number the character appears on.
- * @property {boolean} isTinted - Does this character have its own unique tint? (WebGL only).
- * @property {number} tintEffect - The tint effect this character uses (WebGL only).
- * @property {number} tintTL - The top-left tint color for this character (WebGL only).
- * @property {number} tintTR - The top-right tint color for this character (WebGL only).
- * @property {number} tintBL - The bottom-left tint color for this character (WebGL only).
- * @property {number} tintBR - The bottom-right tint color for this character (WebGL only).
- * @property {number} alphaTL - The top-left alpha value for this character (WebGL only).
- * @property {number} alphaTR - The top-right alpha value for this character (WebGL only).
- * @property {number} alphaBL - The bottom-left alpha value for this character (WebGL only).
- * @property {number} alphaBR - The bottom-right alpha value for this character (WebGL only).
  * @property {Phaser.Types.GameObjects.BitmapText.BitmapFontCharacterData} glyph - Reference to the glyph object this character is using.
  */
