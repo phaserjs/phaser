00e5c5fe0f4c63a08e204eaaf6afb9ff2c7fbb5c
Richard Davey <rich@photonstorm.com>
2020-08-04 11:25:10 +0100

    Update BitmapTextSize.js

--- src/gameobjects/bitmaptext/typedefs/BitmapTextSize.js
+++ src/gameobjects/bitmaptext/typedefs/BitmapTextSize.js
@@ -7,6 +7,7 @@
  * @property {Phaser.Types.GameObjects.BitmapText.BitmapTextLines} lines - Data about the lines of text within the BitmapText.
  * @property {Phaser.Types.GameObjects.BitmapText.BitmapTextCharacter[]} characters - An array containing per-character data. Only populated if `includeChars` is `true` in the `getTextBounds` call.
  * @property {Phaser.Types.GameObjects.BitmapText.BitmapTextWord[]} words - An array containing the word data from the BitmapText.
+ * @property {number} scale - The scale of the BitmapText font being rendered vs. font size in the text data.
  * @property {number} scaleX - The scale X value of the BitmapText.
  * @property {number} scaleY - The scale Y value of the BitmapText.
  * @property {string} wrappedText - The wrapped text, if wrapping enabled and required.
