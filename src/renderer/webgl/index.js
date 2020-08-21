8f5ee5391f628dd26c0c3d6fd1fa445985841104
Richard Davey <rich@photonstorm.com>
2020-08-21 15:59:41 +0100

    Removed redundant consts

--- src/renderer/webgl/index.js
+++ src/renderer/webgl/index.js
@@ -13,13 +13,6 @@ module.exports = {
     Utils: require('./Utils'),
     WebGLPipeline: require('./WebGLPipeline'),
     WebGLRenderer: require('./WebGLRenderer'),
-    Pipelines: require('./pipelines'),
-
-    // Constants
-    BYTE: 0,
-    SHORT: 1,
-    UNSIGNED_BYTE: 2,
-    UNSIGNED_SHORT: 3,
-    FLOAT: 4
+    Pipelines: require('./pipelines')
 
 };
