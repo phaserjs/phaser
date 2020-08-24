2af65b2a1de93d51c0c303ab955011f8a71c6c03
Richard Davey <rich@photonstorm.com>
2020-08-24 15:04:00 +0100

    Improved JSDocs and types. Fix #5217

--- src/animations/typedefs/AnimationFrame.js
+++ src/animations/typedefs/AnimationFrame.js
@@ -2,8 +2,8 @@
  * @typedef {object} Phaser.Types.Animations.AnimationFrame
  * @since 3.0.0
  *
- * @property {string} key - The key that the animation will be associated with. i.e. sprite.animations.play(key)
- * @property {(string|number)} frame - The key, or index number, of the frame within the animation.
+ * @property {string} key - The key of the texture within the Texture Manager to use for this Animation Frame.
+ * @property {(string|number)} [frame] - The key, or index number, of the frame within the texture to use for this Animation Frame.
  * @property {number} [duration=0] - The duration, in ms, of this frame of the animation.
  * @property {boolean} [visible] - Should the parent Game Object be visible during this frame of the animation?
  */
