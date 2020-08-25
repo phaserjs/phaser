6c26e272862306eb9a124ff283266eea2db0d841
Richard Davey <rich@photonstorm.com>
2020-08-25 18:29:58 +0100

    Update copy-to-examples.js

--- plugins/spine/copy-to-examples.js
+++ plugins/spine/copy-to-examples.js
@@ -1,7 +1,7 @@
 var fs = require('fs-extra');
 
 var source = './plugins/spine/dist/';
-var dest = '../phaser3-examples/public/plugins/3.8/';
+var dest = '../phaser3-examples/public/plugins/3.8.95/';
 
 if (fs.existsSync(dest))
 {
