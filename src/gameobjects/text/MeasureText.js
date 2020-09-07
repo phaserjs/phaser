a9e6604eb23459268615b84dba3a33b858af134e
Rex <binhua.tein@gmail.com>
2020-09-07 21:36:27 +0800

    Support IE
    IE and Firefox for Android do not have actualBoundingBoxAscent and actualBoundingBoxDescent properties.
    Use origin solution to get ascent and descent.

--- src/gameobjects/text/MeasureText.js
+++ src/gameobjects/text/MeasureText.js
@@ -9,7 +9,7 @@ var CanvasPool = require('../../display/canvas/CanvasPool');
 /**
  * Calculates the ascent, descent and fontSize of a given font style.
  *
- * @function Phaser.GameObjects.Text.MeasureText
+ * @function Phaser.GameObjects.MeasureText
  * @since 3.0.0
  *
  * @param {Phaser.GameObjects.TextStyle} textStyle - The TextStyle object to measure.
@@ -27,14 +27,117 @@ var MeasureText = function (textStyle)
     textStyle.syncFont(canvas, context);
 
     var metrics = context.measureText(textStyle.testString);
-    var ascent = metrics.actualBoundingBoxAscent;
-    var descent = metrics.actualBoundingBoxDescent;
+    
+    if (metrics.hasOwnProperty('actualBoundingBoxAscent') && metrics.hasOwnProperty('actualBoundingBoxDescent')) 
+    {
+        var ascent = metrics.actualBoundingBoxAscent;
+        var descent = metrics.actualBoundingBoxDescent;
+    
+        var output = {
+            ascent: ascent,
+            descent: descent,
+            fontSize: (ascent + descent)
+        };
+    
+        CanvasPool.remove(canvas);
+    
+        return output;
+    }
+
+    var width = Math.ceil(metrics.width * textStyle.baselineX);
+    var baseline = width;
+    var height = 2 * baseline;
+
+    baseline = baseline * textStyle.baselineY | 0;
+
+    canvas.width = width;
+    canvas.height = height;
+
+    context.fillStyle = '#f00';
+    context.fillRect(0, 0, width, height);
+
+    context.font = textStyle._font;
+
+    context.textBaseline = 'alphabetic';
+    context.fillStyle = '#000';
+    context.fillText(textStyle.testString, 0, baseline);
 
     var output = {
-        ascent: ascent,
-        descent: descent,
-        fontSize: (ascent + descent)
+        ascent: 0,
+        descent: 0,
+        fontSize: 0
     };
+
+    if (!context.getImageData(0, 0, width, height))
+    {
+        output.ascent = baseline;
+        output.descent = baseline + 6;
+        output.fontSize = output.ascent + output.descent;
+
+        CanvasPool.remove(canvas);
+
+        return output;
+    }
+
+    var imagedata = context.getImageData(0, 0, width, height).data;
+    var pixels = imagedata.length;
+    var line = width * 4;
+    var i;
+    var j;
+    var idx = 0;
+    var stop = false;
+
+    // ascent. scan from top to bottom until we find a non red pixel
+    for (i = 0; i < baseline; i++)
+    {
+        for (j = 0; j < line; j += 4)
+        {
+            if (imagedata[idx + j] !== 255)
+            {
+                stop = true;
+                break;
+            }
+        }
+
+        if (!stop)
+        {
+            idx += line;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    output.ascent = baseline - i;
+
+    idx = pixels - line;
+    stop = false;
+
+    // descent. scan from bottom to top until we find a non red pixel
+    for (i = height; i > baseline; i--)
+    {
+        for (j = 0; j < line; j += 4)
+        {
+            if (imagedata[idx + j] !== 255)
+            {
+                stop = true;
+                break;
+            }
+        }
+
+        if (!stop)
+        {
+            idx -= line;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    output.descent = (i - baseline);
+    output.fontSize = output.ascent + output.descent;
 
     CanvasPool.remove(canvas);
 
