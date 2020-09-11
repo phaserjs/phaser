6c8191f7c5c87db622df166f161ec5b0ae7fc2ff
Richard Davey <rich@photonstorm.com>
2020-09-11 15:29:01 +0100

    Fixed Audio and Video type defs. Fix #5295

--- src/loader/filetypes/typedefs/AudioFileConfig.js
+++ src/loader/filetypes/typedefs/AudioFileConfig.js
@@ -2,7 +2,7 @@
  * @typedef {object} Phaser.Types.Loader.FileTypes.AudioFileConfig
  *
  * @property {string} key - The key of the file. Must be unique within the Loader and Audio Cache.
- * @property {string} [urlConfig] - The absolute or relative URL to load the file from.
+ * @property {(string|string[])} [url] - The absolute or relative URLs to load the audio files from.
  * @property {Phaser.Types.Loader.XHRSettingsObject} [xhrSettings] - Extra XHR Settings specifically for this file.
- * @property {AudioContext} [audioContext] - The AudioContext this file will use to process itself.
+ * @property {AudioContext} [context] - The optional AudioContext this file will use to process itself.
  */
