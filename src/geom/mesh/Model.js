fcd1cf96b8b11e733c10707ce8f8f823f149df39
Richard Davey <rich@photonstorm.com>
2020-09-16 16:38:53 +0100

    Added dirty handling, drawDebug and destroy

--- src/geom/mesh/Model.js
+++ src/geom/mesh/Model.js
@@ -4,13 +4,16 @@
  * @license      {@link https://opensource.org/licenses/MIT|MIT License}
  */
 
+var AnimationState = require('../../animations/AnimationState');
 var Class = require('../../utils/Class');
+var Components = require('../../gameobjects/components');
 var Face = require('./Face');
 var GetCalcMatrix = require('../../gameobjects/GetCalcMatrix');
 var Matrix4 = require('../../math/Matrix4');
 var StableSort = require('../../utils/array/StableSort');
 var Vector3 = require('../../math/Vector3');
 var Vertex = require('./Vertex');
+var WrapAngle = require('../../math/angle/Wrap');
 
 /**
  * @classdesc
@@ -23,11 +26,33 @@ var Vertex = require('./Vertex');
  */
 var Model = new Class({
 
+    Mixins: [
+        Components.AlphaSingle,
+        Components.Size,
+        Components.Texture,
+        Components.Visible
+    ],
+
     initialize:
 
-    function Model (mesh, x, y, z)
+    function Model (mesh, texture, frame, x, y, z)
     {
+        if (x === undefined) { x = 0; }
+        if (y === undefined) { y = 0; }
+        if (z === undefined) { z = 0; }
+
         this.mesh = mesh;
+
+        this.scene = mesh.scene;
+
+        /**
+         * The Animation State of this Mesh.
+         *
+         * @name Phaser.GameObjects.Mesh#anims
+         * @type {Phaser.Animation.AnimationState}
+         * @since 3.50.0
+         */
+        this.anims = new AnimationState(this);
 
         /**
          * An array containing the Face instances belonging to this Mesh.
@@ -78,14 +103,29 @@ var Model = new Class({
          */
         this.hideCCW = true;
 
+        this.drawDebug = true;
+
         this.position = new Vector3(x, y, z);
         this.rotation = new Vector3();
         this.scale = new Vector3(1, 1, 1);
 
-        this.visible = true;
-        this.alpha = 1;
+        this.dirtyCache = [ x, y, z, 0, 0, 0, 1, 1, 1, 0 ];
 
         this.transformMatrix = new Matrix4();
+
+        if (!texture)
+        {
+            texture = this.scene.sys.textures.get('__WHITE');
+        }
+
+        this.setTexture(texture, frame);
+
+        this.setSizeToFrame();
+    },
+
+    emit: function ()
+    {
+        this.mesh.emit.call(arguments);
     },
 
     /**
@@ -110,11 +150,77 @@ var Model = new Class({
         return this;
     },
 
-    update: function (viewMatrix, projectionMatrix, width, height)
+    isDirty: function ()
     {
+        var position = this.position;
+        var rotation = this.rotation;
+        var scale = this.scale;
+
+        var dirtyCache = this.dirtyCache;
+
+        var px = position.x;
+        var py = position.y;
+        var pz = position.z;
+
+        var rx = rotation.x;
+        var ry = rotation.y;
+        var rz = rotation.z;
+
+        var sx = scale.x;
+        var sy = scale.y;
+        var sz = scale.z;
+
+        var faces = this.getFaceCount();
+
+        var pxCached = dirtyCache[0];
+        var pyCached = dirtyCache[1];
+        var pzCached = dirtyCache[2];
+
+        var rxCached = dirtyCache[3];
+        var ryCached = dirtyCache[4];
+        var rzCached = dirtyCache[5];
+
+        var sxCached = dirtyCache[6];
+        var syCached = dirtyCache[7];
+        var szCached = dirtyCache[8];
+
+        var fCached = dirtyCache[9];
+
+        dirtyCache[0] = px;
+        dirtyCache[1] = py;
+        dirtyCache[2] = pz;
+
+        dirtyCache[3] = rx;
+        dirtyCache[4] = ry;
+        dirtyCache[5] = rz;
+
+        dirtyCache[6] = sx;
+        dirtyCache[7] = sy;
+        dirtyCache[8] = sz;
+
+        dirtyCache[9] = faces;
+
+        return (
+            pxCached !== px || pyCached !== py || pzCached !== pz ||
+            rxCached !== rx || ryCached !== ry || rzCached !== rz ||
+            sxCached !== sx || syCached !== sy || szCached !== sz ||
+            fCached !== faces
+        );
+    },
+
+    preUpdate: function (time, delta, camera, width, height)
+    {
+        this.anims.update(time, delta);
+
+        if (!camera.dirty && !this.isDirty())
+        {
+            //  If neither the camera or the model is dirty, we can bail out and save lots of math
+            return;
+        }
+
         var transformMatrix = this.transformMatrix;
 
-        transformMatrix.setWorldMatrix(this.rotation, this.position, this.scale, viewMatrix, projectionMatrix);
+        transformMatrix.setWorldMatrix(this.rotation, this.position, this.scale, camera.viewMatrix, camera.projectionMatrix);
 
         var faces = this.faces;
 
@@ -128,6 +234,8 @@ var Model = new Class({
         }
 
         this.depthSort();
+
+        console.log('model.update');
     },
 
     /**
@@ -185,15 +293,14 @@ var Model = new Class({
      *
      * @param {number} x - The x position to check against.
      * @param {number} y - The y position to check against.
-     * @param {Phaser.Cameras.Scene2D.Camera} [camera] - The camera to pass the coordinates through. If not give, the default Scene Camera is used.
+     * @param {Phaser.Cameras.Scene2D.Camera} [camera] - The camera to pass the coordinates through. If not given, the default Scene Camera is used.
      *
      * @return {Phaser.Geom.Mesh.Face[]} An array of Face objects that intersect with the given point, ordered by depth.
      */
-    getFaceAt: function (x, y, camera)
+    getFaceAt: function (x, y, camera, calcMatrix)
     {
         if (camera === undefined) { camera = this.scene.sys.cameras.main; }
-
-        var calcMatrix = GetCalcMatrix(this.mesh, camera).calc;
+        if (calcMatrix === undefined) { calcMatrix = GetCalcMatrix(this.mesh, camera).calc; }
 
         var faces = this.faces;
         var results = [];
@@ -297,18 +404,134 @@ var Model = new Class({
     },
 
     /**
+     * Adds new vertices to this Model by parsing the given arrays.
+     *
+     * The `vertices` parameter is a numeric array consisting of `x` and `y` pairs.
+     * The `uvs` parameter is a numeric array consisting of `u` and `v` pairs.
+     * The `indicies` parameter is an optional array that, if given, is an indexed list of vertices to be added.
+     *
+     * The following example will create a 256 x 256 sized quad using an index array:
+     *
+     * ```javascript
+     * const vertices = [
+     *   -128, 128,
+     *   128, 128,
+     *   -128, -128,
+     *   128, -128
+     * ];
+     *
+     * const uvs = [
+     *   0, 1,
+     *   1, 1,
+     *   0, 0,
+     *   1, 0
+     * ];
+     *
+     * const indices = [ 0, 2, 1, 2, 3, 1 ];
+     *
+     * Mesh.addVertices(vertices, uvs, indicies);
+     * ```
+     *
+     * Vertices must be provided as x/y pairs, there is no `z` component used in this call. For that, please see
+     * `addModel` instead.
+     *
+     * @method Phaser.GameObjects.Mesh#addVertices
+     * @since 3.50.0
+     *
+     * @param {number[]} vertices - The vertices array.
+     * @param {number[]} uvs - The UVs array.
+     * @param {number[]} [indicies] - Optional vertex indicies array.
+     * @param {number|number[]} [colors=0xffffff] - An array of colors, one per vertex, or a single color value applied to all vertices.
+     * @param {number|number[]} [alphas=1] - An array of alpha values, one per vertex, or a single alpha value applied to all vertices.
+     *
+     * @return {this} This Mesh Game Object.
+     */
+    addVertices: function (vertices, uvs, indicies, colors, alphas)
+    {
+        if (colors === undefined) { colors = 0xffffff; }
+        if (alphas === undefined) { alphas = 1; }
+
+        if (vertices.length !== uvs.length)
+        {
+            throw new Error('Mesh - vertices and uv count not equal');
+        }
+
+        var i;
+        var vert;
+        var verts = this.vertices;
+        var faces = this.faces;
+
+        var isColorArray = Array.isArray(colors);
+        var isAlphaArray = Array.isArray(alphas);
+
+        if (Array.isArray(indicies) && indicies.length > 0)
+        {
+            for (i = 0; i < indicies.length; i++)
+            {
+                var index = indicies[i] * 2;
+
+                vert = new Vertex(
+                    vertices[index],
+                    vertices[index + 1],
+                    0,
+                    uvs[index],
+                    uvs[index + 1],
+                    (isColorArray) ? colors[i] : colors,
+                    (isAlphaArray) ? alphas[i] : alphas
+                );
+
+                verts.push(vert);
+            }
+        }
+        else
+        {
+            var colorIndex = 0;
+
+            for (i = 0; i < vertices.length; i += 2)
+            {
+                vert = new Vertex(
+                    vertices[i],
+                    vertices[i + 1],
+                    0,
+                    uvs[i],
+                    uvs[i + 1],
+                    (isColorArray) ? colors[colorIndex] : colors,
+                    (isAlphaArray) ? alphas[colorIndex] : alphas
+                );
+
+                verts.push(vert);
+
+                colorIndex++;
+            }
+        }
+
+        for (i = 0; i < verts.length; i += 3)
+        {
+            var vert1 = verts[i];
+            var vert2 = verts[i + 1];
+            var vert3 = verts[i + 2];
+
+            var face = new Face(vert1, vert2, vert3);
+
+            faces.push(face);
+        }
+
+        return this;
+    },
+
+    /**
      * Rotates all vertices of this Mesh around the X axis by the amount given.
      *
      * It then runs a depth sort on the faces before returning.
      *
-     * @method Phaser.Geom.Mesh.Model#rotateX
+     * @method Phaser.Geom.Mesh.Model#rotateVerticesX
      * @since 3.50.0
      *
      * @param {number} theta - The amount to rotate by in radians.
      *
      * @return {this} This Mesh Game Object.
      */
-    rotateX: function (theta)
+    rotateVerticesX: function (theta)
     {
         var ts = Math.sin(theta);
         var tc = Math.cos(theta);
@@ -333,14 +556,14 @@ var Model = new Class({
      *
      * It then runs a depth sort on the faces before returning.
      *
-     * @method Phaser.Geom.Mesh.Model#rotateY
+     * @method Phaser.Geom.Mesh.Model#rotateVerticesY
      * @since 3.50.0
      *
      * @param {number} theta - The amount to rotate by in radians.
      *
      * @return {this} This Mesh Game Object.
      */
-    rotateY: function (theta)
+    rotateVerticesY: function (theta)
     {
         var ts = Math.sin(theta);
         var tc = Math.cos(theta);
@@ -365,14 +588,14 @@ var Model = new Class({
      *
      * It then runs a depth sort on the faces before returning.
      *
-     * @method Phaser.Geom.Mesh.Model#rotateZ
+     * @method Phaser.Geom.Mesh.Model#rotateVerticesZ
      * @since 3.50.0
      *
      * @param {number} theta - The amount to rotate by in radians.
      *
      * @return {this} This Mesh Game Object.
      */
-    rotateZ: function (theta)
+    rotateVerticesZ: function (theta)
     {
         var ts = Math.sin(theta);
         var tc = Math.cos(theta);
@@ -392,6 +615,132 @@ var Model = new Class({
         return this.depthSort();
     },
 
+    x: {
+
+        get: function ()
+        {
+            return this.position.x;
+        },
+
+        set: function (value)
+        {
+            this.position.x = value;
+        }
+
+    },
+
+    y: {
+
+        get: function ()
+        {
+            return this.position.y;
+        },
+
+        set: function (value)
+        {
+            this.position.y = value;
+        }
+
+    },
+
+    z: {
+
+        get: function ()
+        {
+            return this.position.z;
+        },
+
+        set: function (value)
+        {
+            this.position.z = value;
+        }
+
+    },
+
+    rotationX: {
+
+        get: function ()
+        {
+            return this.rotation.x;
+        },
+
+        set: function (value)
+        {
+            this.rotation.x = WrapAngle(value);
+        }
+
+    },
+
+    rotationY: {
+
+        get: function ()
+        {
+            return this.rotation.y;
+        },
+
+        set: function (value)
+        {
+            this.rotation.y = WrapAngle(value);
+        }
+
+    },
+
+    rotationZ: {
+
+        get: function ()
+        {
+            return this.rotation.z;
+        },
+
+        set: function (value)
+        {
+            this.rotation.z = WrapAngle(value);
+        }
+
+    },
+
+    scaleX: {
+
+        get: function ()
+        {
+            return this.scale.x;
+        },
+
+        set: function (value)
+        {
+            this.scale.x = value;
+        }
+
+    },
+
+    scaleY: {
+
+        get: function ()
+        {
+            return this.scale.y;
+        },
+
+        set: function (value)
+        {
+            this.scale.y = value;
+        }
+
+    },
+
+    scaleZ: {
+
+        get: function ()
+        {
+            return this.scale.z;
+        },
+
+        set: function (value)
+        {
+            this.scale.z = value;
+        }
+
+    },
+
     /**
      * Destroys this Model, all of its Faces, Vertices and references.
      *
@@ -400,7 +749,17 @@ var Model = new Class({
      */
     destroy: function ()
     {
-        //  TODO
+        this.clearVertices();
+
+        this.anims.destroy();
+
+        this.mesh = null;
+        this.scene = null;
+        this.anims = null;
+        this.position = null;
+        this.rotation = null;
+        this.scale = null;
+        this.transformMatrix = null;
     }
 
 });
