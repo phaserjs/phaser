371fb5e68a2c45786b16b7be59a4e7a82772db5c
Richard Davey <rich@photonstorm.com>
2020-09-18 17:50:47 +0100

    Added normals, removed Vec3

--- src/geom/mesh/Vertex.js
+++ src/geom/mesh/Vertex.js
@@ -5,8 +5,6 @@
  */
 
 var Class = require('../../utils/Class');
-var Vector3 = require('../../math/Vector3');
-var Utils = require('../../renderer/webgl/Utils');
 
 /**
  * @classdesc
@@ -17,7 +15,6 @@ var Utils = require('../../renderer/webgl/Utils');
  *
  * @class Vertex
  * @memberof Phaser.Geom.Mesh
- * @extends Phaser.Math.Vector3
  * @constructor
  * @since 3.50.0
  *
@@ -31,43 +28,69 @@ var Utils = require('../../renderer/webgl/Utils');
  */
 var Vertex = new Class({
 
-    Extends: Vector3,
-
     initialize:
 
-    function Vertex (x, y, z, u, v, color, alpha)
+    function Vertex (x, y, z, u, v, normalX, normalY, normalZ, color, alpha)
     {
+        if (normalX === undefined) { normalX = 0; }
+        if (normalY === undefined) { normalY = 0; }
+        if (normalZ === undefined) { normalZ = 0; }
         if (color === undefined) { color = 0xffffff; }
         if (alpha === undefined) { alpha = 1; }
 
-        Vector3.call(this, x, y, z);
-
         /**
-         * The projected x coordinate of this vertex.
+         * The x coordinate of this vertex.
          *
          * @name Phaser.Geom.Mesh.Vertex#vx
          * @type {number}
          * @since 3.50.0
          */
-        this.vx = 0;
+        this.x = x;
 
         /**
-         * The projected y coordinate of this vertex.
+         * The y coordinate of this vertex.
          *
          * @name Phaser.Geom.Mesh.Vertex#vy
          * @type {number}
          * @since 3.50.0
          */
-        this.vy = 0;
+        this.y = y;
 
         /**
-         * The projected z coordinate of this vertex.
+         * The z coordinate of this vertex.
          *
          * @name Phaser.Geom.Mesh.Vertex#vz
          * @type {number}
          * @since 3.50.0
          */
-        this.vz = 0;
+        this.z = z;
+
+        /**
+         * The x normal of this vertex.
+         *
+         * @name Phaser.Geom.Mesh.Vertex#nx
+         * @type {number}
+         * @since 3.50.0
+         */
+        this.nx = normalX;
+
+        /**
+         * The y normal of this vertex.
+         *
+         * @name Phaser.Geom.Mesh.Vertex#ny
+         * @type {number}
+         * @since 3.50.0
+         */
+        this.ny = normalY;
+
+        /**
+         * The z normal of this vertex.
+         *
+         * @name Phaser.Geom.Mesh.Vertex#nz
+         * @type {number}
+         * @since 3.50.0
+         */
+        this.nz = normalZ;
 
         /**
          * UV u coordinate of this vertex.
@@ -115,7 +138,6 @@ var Vertex = new Class({
      * @param {Phaser.Math.Matrix4} transformMatrix - The transform matrix to apply to this vertex.
      * @param {number} width - The width of the parent Mesh.
      * @param {number} height - The height of the parent Mesh.
-     */
     transformCoordinatesLocal: function (transformMatrix, width, height)
     {
         var x = this.x;
@@ -133,39 +155,7 @@ var Vertex = new Class({
         this.vy = -(ty / tw) * height;
         this.vz = (tz / tw);
     },
-
-    /**
-     * Loads this vertex into the given Typed Arrays.
-     *
-     * @method Phaser.Geom.Mesh.Model#load
-     * @since 3.50.0
-     *
-     * @param {Float32Array} F32 - The Float32 Array to put the position data in.
-     * @param {Uint32Array} U32 - The Uint32 Array to put the color data in.
-     * @param {number} offset - The vertex offset to place the data at.
-     * @param {number} textureUnit - The currently bound texture unit.
-     * @param {number} alpha - The alpha value.
-     * @param {number} a - The transform matrix a value.
-     * @param {number} b - The transform matrix b value.
-     * @param {number} c - The transform matrix c value.
-     * @param {number} d - The transform matrix d value.
-     * @param {number} e - The transform matrix e value.
-     * @param {number} f - The transform matrix f value.
-     *
-     * @return {number} The new vertex offset.
      */
-    load: function (F32, U32, offset, textureUnit, tintEffect, alpha, a, b, c, d, e, f)
-    {
-        F32[++offset] = this.vx * a + this.vy * c + e;
-        F32[++offset] = this.vx * b + this.vy * d + f;
-        F32[++offset] = this.u;
-        F32[++offset] = this.v;
-        F32[++offset] = textureUnit;
-        F32[++offset] = tintEffect;
-        U32[++offset] = Utils.getTintAppendFloatAlpha(this.color, alpha * this.alpha);
-
-        return offset;
-    }
 
 });
 
