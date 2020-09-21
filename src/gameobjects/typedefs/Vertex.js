27eafe3661db3644bdc7dcb4b8ebf48b3c216031
Richard Davey <rich@photonstorm.com>
2020-09-21 15:56:21 +0100

    New Face and Vertex typedefs

---
+++ src/gameobjects/typedefs/Vertex.js
@@ -0,0 +1,14 @@
+/**
+ * @typedef {object} Phaser.Types.GameObjects.Vertex
+ * @since 3.50.0
+ *
+ * @property {number} x - The x coordinate of the vertex.
+ * @property {number} y - The y coordinate of the vertex.
+ * @property {number} z - The z coordinate of the vertex.
+ * @property {number} normalX - The x normal of the vertex.
+ * @property {number} normalY - The y normal of the vertex.
+ * @property {number} normalZ - The z normal of the vertex.
+ * @property {number} u - UV u texture coordinate of the vertex.
+ * @property {number} v - UV v texture coordinate of the vertex.
+ * @property {number} alpha - The alpha value of the vertex.
+ */
