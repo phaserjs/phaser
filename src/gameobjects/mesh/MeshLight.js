989e3e6e072fae467fa082fd4d5c4a0459449708
Richard Davey <rich@photonstorm.com>
2020-09-21 15:57:11 +0100

    Swapped to use RGB objects

--- src/gameobjects/mesh/MeshLight.js
+++ src/gameobjects/mesh/MeshLight.js
@@ -5,6 +5,7 @@
  */
 
 var Class = require('../../utils/Class');
+var RGB = require('../../display/RGB');
 var Vector3 = require('../../math/Vector3');
 
 /**
@@ -23,9 +24,32 @@ var MeshLight = new Class({
     function MeshLight (x, y, z)
     {
         this.position = new Vector3(x, y, z);
-        this.ambient = new Vector3(1, 1, 1);
-        this.diffuse = new Vector3(1, 1, 1);
-        this.specular = new Vector3(1, 1, 1);
+        this.ambient = new RGB(1, 1, 1);
+        this.diffuse = new RGB(1, 1, 1);
+        this.specular = new RGB(1, 1, 1);
+
+        //  cache structure = position
+        this.dirtyCache = [ 0, 0, 0 ];
+    },
+
+    isDirty: function ()
+    {
+        var position = this.position;
+        var dirtyCache = this.dirtyCache;
+
+        var x = position.x;
+        var y = position.y;
+        var z = position.z;
+
+        var xCached = dirtyCache[0];
+        var yCached = dirtyCache[1];
+        var zCached = dirtyCache[2];
+
+        dirtyCache[0] = x;
+        dirtyCache[1] = y;
+        dirtyCache[2] = z;
+
+        return (xCached !== x || yCached !== y || zCached !== z);
     },
 
     setPosition: function (x, y, z)
