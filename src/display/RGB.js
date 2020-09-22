7d5adab50202f3234db0238440047dd959eea246
Richard Davey <rich@photonstorm.com>
2020-09-22 11:17:40 +0100

    Added JSDocs

--- src/display/RGB.js
+++ src/display/RGB.js
@@ -60,6 +60,19 @@ var RGB = new Class({
         this.set(red, green, blue);
     },
 
+    /**
+     * Sets the red, green and blue values of this RGB object, flags it as being
+     * dirty and then invokes the `onChangeCallback`, if set.
+     *
+     * @method Phaser.Display.RGB#set
+     * @since 3.50.0
+     *
+     * @param {number} [red=0] - The red color value. A number between 0 and 1.
+     * @param {number} [green=0] - The green color value. A number between 0 and 1.
+     * @param {number} [blue=0] - The blue color value. A number between 0 and 1.
+     *
+     * @return {this} This RGB instance.
+     */
     set: function (red, green, blue)
     {
         if (red === undefined) { red = 0; }
@@ -73,6 +86,19 @@ var RGB = new Class({
         return this;
     },
 
+    /**
+     * Compares the given rgb parameters with those in this object and returns
+     * a boolean `true` value if they are equal, otherwise it returns `false`.
+     *
+     * @method Phaser.Display.RGB#equals
+     * @since 3.50.0
+     *
+     * @param {number} red - The red value to compare with this object.
+     * @param {number} green - The green value to compare with this object.
+     * @param {number} blue - The blue value to compare with this object.
+     *
+     * @return {boolean} `true` if the given values match those in this object, otherwise `false`.
+     */
     equals: function (red, green, blue)
     {
         var rgb = this._rgb;
@@ -80,6 +106,14 @@ var RGB = new Class({
         return (rgb.r === red && rgb.g === green && rgb.b === blue);
     },
 
+    /**
+     * Internal on change handler. Sets this object as being dirty and
+     * then invokes the `onChangeCallback`, if set, passing in the
+     * new RGB values.
+     *
+     * @method Phaser.Display.RGB#onChange
+     * @since 3.50.0
+     */
     onChange: function ()
     {
         this.dirty = true;
@@ -89,6 +123,16 @@ var RGB = new Class({
         this.onChangeCallback.call(this, rgb[0], rgb[1], rgb[2]);
     },
 
+    /**
+     * The red color value. Between 0 and 1.
+     *
+     * Changing this property will flag this RGB object as being dirty
+     * and invoke the `onChangeCallback` , if set.
+     *
+     * @name Phaser.Display.RGB#r
+     * @type {number}
+     * @since 3.50.0
+     */
     r: {
 
         get: function ()
@@ -104,6 +148,16 @@ var RGB = new Class({
 
     },
 
+    /**
+     * The green color value. Between 0 and 1.
+     *
+     * Changing this property will flag this RGB object as being dirty
+     * and invoke the `onChangeCallback` , if set.
+     *
+     * @name Phaser.Display.RGB#g
+     * @type {number}
+     * @since 3.50.0
+     */
     g: {
 
         get: function ()
@@ -119,6 +173,16 @@ var RGB = new Class({
 
     },
 
+    /**
+     * The blue color value. Between 0 and 1.
+     *
+     * Changing this property will flag this RGB object as being dirty
+     * and invoke the `onChangeCallback` , if set.
+     *
+     * @name Phaser.Display.RGB#b
+     * @type {number}
+     * @since 3.50.0
+     */
     b: {
 
         get: function ()
