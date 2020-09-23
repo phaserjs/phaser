212e7aa3012b61ef269f01e58cc744343e96d627
Richard Davey <rich@photonstorm.com>
2020-09-23 12:46:55 +0100

    The `Shape` class now includes the `ComputedSize` component properties and methods directly in the class, rather than applying as a mixin. `setSize` is now flagged as being `private`, because it shouldn't be used on Shape classes, which was leading to confusion as it appeared in the public-facing API. Fix #4811

--- src/gameobjects/shape/Shape.js
+++ src/gameobjects/shape/Shape.js
@@ -22,8 +22,7 @@ var Line = require('../../geom/line/Line');
  *
  * @extends Phaser.GameObjects.Components.AlphaSingle
  * @extends Phaser.GameObjects.Components.BlendMode
- * @extends Phaser.GameObjects.Components.ComputedSize
- * @extends Phaser.GameObjects.Components.Depth
+  * @extends Phaser.GameObjects.Components.Depth
  * @extends Phaser.GameObjects.Components.GetBounds
  * @extends Phaser.GameObjects.Components.Mask
  * @extends Phaser.GameObjects.Components.Origin
@@ -43,7 +42,6 @@ var Shape = new Class({
     Mixins: [
         Components.AlphaSingle,
         Components.BlendMode,
-        Components.ComputedSize,
         Components.Depth,
         Components.GetBounds,
         Components.Mask,
@@ -172,28 +170,54 @@ var Shape = new Class({
          * Private internal value.
          * A Line used when parsing internal path data to avoid constant object re-creation.
          *
-         * @name Phaser.GameObjects.Curve#_tempLine
+         * @name Phaser.GameObjects.Shape#_tempLine
          * @type {Phaser.Geom.Line}
          * @private
          * @since 3.13.0
          */
         this._tempLine = new Line();
 
+        /**
+         * The native (un-scaled) width of this Game Object.
+         *
+         * Changing this value will not change the size that the Game Object is rendered in-game.
+         * For that you need to either set the scale of the Game Object (`setScale`) or use
+         * the `displayWidth` property.
+         *
+         * @name Phaser.GameObjects.Shape#width
+         * @type {number}
+         * @since 3.13.0
+         */
+        this.width = 0;
+
+        /**
+         * The native (un-scaled) height of this Game Object.
+         *
+         * Changing this value will not change the size that the Game Object is rendered in-game.
+         * For that you need to either set the scale of the Game Object (`setScale`) or use
+         * the `displayHeight` property.
+         *
+         * @name Phaser.GameObjects.Shape#height
+         * @type {number}
+         * @since 3.0.0
+         */
+        this.height = 0;
+
         this.initPipeline();
     },
 
     /**
      * Sets the fill color and alpha for this Shape.
-     * 
+     *
      * If you wish for the Shape to not be filled then call this method with no arguments, or just set `isFilled` to `false`.
-     * 
+     *
      * Note that some Shapes do not support fill colors, such as the Line shape.
-     * 
+     *
      * This call can be chained.
      *
      * @method Phaser.GameObjects.Shape#setFillStyle
      * @since 3.13.0
-     * 
+     *
      * @param {number} [color] - The color used to fill this shape. If not provided the Shape will not be filled.
      * @param {number} [alpha=1] - The alpha value used when filling this shape, if a fill color is given.
      *
@@ -219,16 +243,16 @@ var Shape = new Class({
 
     /**
      * Sets the stroke color and alpha for this Shape.
-     * 
+     *
      * If you wish for the Shape to not be stroked then call this method with no arguments, or just set `isStroked` to `false`.
-     * 
+     *
      * Note that some Shapes do not support being stroked, such as the Iso Box shape.
-     * 
+     *
      * This call can be chained.
      *
      * @method Phaser.GameObjects.Shape#setStrokeStyle
      * @since 3.13.0
-     * 
+     *
      * @param {number} [lineWidth] - The width of line to stroke with. If not provided or undefined the Shape will not be stroked.
      * @param {number} [color] - The color used to stroke this shape. If not provided the Shape will not be stroked.
      * @param {number} [alpha=1] - The alpha value used when stroking this shape, if a stroke color is given.
@@ -257,12 +281,12 @@ var Shape = new Class({
     /**
      * Sets if this Shape path is closed during rendering when stroked.
      * Note that some Shapes are always closed when stroked (such as Ellipse shapes)
-     * 
+     *
      * This call can be chained.
      *
      * @method Phaser.GameObjects.Shape#setClosePath
      * @since 3.13.0
-     * 
+     *
      * @param {boolean} value - Set to `true` if the Shape should be closed when stroked, otherwise `false`.
      *
      * @return {this} This Game Object instance.
@@ -270,6 +294,34 @@ var Shape = new Class({
     setClosePath: function (value)
     {
         this.closePath = value;
+
+        return this;
+    },
+
+    /**
+     * Sets the internal size of this Game Object, as used for frame or physics body creation.
+     *
+     * This will not change the size that the Game Object is rendered in-game.
+     * For that you need to either set the scale of the Game Object (`setScale`) or call the
+     * `setDisplaySize` method, which is the same thing as changing the scale but allows you
+     * to do so by giving pixel values.
+     *
+     * If you have enabled this Game Object for input, changing the size will _not_ change the
+     * size of the hit area. To do this you should adjust the `input.hitArea` object directly.
+     *
+     * @method Phaser.GameObjects.Shape#setSize
+     * @private
+     * @since 3.13.0
+     *
+     * @param {number} width - The width of this Game Object.
+     * @param {number} height - The height of this Game Object.
+     *
+     * @return {this} This Game Object instance.
+     */
+    setSize: function (width, height)
+    {
+        this.width = width;
+        this.height = height;
 
         return this;
     },
@@ -287,6 +339,56 @@ var Shape = new Class({
         this._tempLine = null;
         this.pathData = [];
         this.pathIndexes = [];
+    },
+
+    /**
+     * The displayed width of this Game Object.
+     *
+     * This value takes into account the scale factor.
+     *
+     * Setting this value will adjust the Game Object's scale property.
+     *
+     * @name Phaser.GameObjects.Shape#displayWidth
+     * @type {number}
+     * @since 3.13.0
+     */
+    displayWidth: {
+
+        get: function ()
+        {
+            return this.scaleX * this.width;
+        },
+
+        set: function (value)
+        {
+            this.scaleX = value / this.width;
+        }
+
+    },
+
+    /**
+     * The displayed height of this Game Object.
+     *
+     * This value takes into account the scale factor.
+     *
+     * Setting this value will adjust the Game Object's scale property.
+     *
+     * @name Phaser.GameObjects.Shape#displayHeight
+     * @type {number}
+     * @since 3.13.0
+     */
+    displayHeight: {
+
+        get: function ()
+        {
+            return this.scaleY * this.height;
+        },
+
+        set: function (value)
+        {
+            this.scaleY = value / this.height;
+        }
+
     }
 
 });
