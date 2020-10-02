a51e0b8a68c35f4b29c8d39e234646c30454d997
Richard Davey <rich@photonstorm.com>
2020-10-02 11:22:18 +0100

    `Tilemaps.Components.GetWorldToTileXYFunction` is a new function that returns the correct conversion function to use.

--- src/tilemaps/components/GetWorldToTileXYFunction.js
+++ src/tilemaps/components/GetWorldToTileXYFunction.js
@@ -5,39 +5,39 @@
  */
 
 var CONST = require('../const');
-var HexagonalWorldToTileXY = require('./HexagonalWorldToTileXY');
-var IsometricWorldToTileXY = require('./IsometricWorldToTileXY');
+var HexagonalTileToWorldXY = require('./HexagonalTileToWorldXY');
+var IsometricTileToWorldXY = require('./IsometricTileToWorldXY');
 var NOOP = require('../../utils/NOOP');
-var StaggeredWorldToTileXY = require('./StaggeredWorldToTileXY');
-var WorldToTileXY = require('./WorldToTileXY');
+var StaggeredTileToWorldXY = require('./StaggeredTileToWorldXY');
+var TileToWorldXY = require('./TileToWorldXY');
 
 /**
  * Gets the correct function to use to translate tiles, based on the map orientation.
  *
- * @function Phaser.Tilemaps.Components.GetWorldToTileXYFunction
+ * @function Phaser.Tilemaps.Components.GetTileToWorldXYFunction
  * @since 3.50.0
  *
  * @param {number} orientation - The Tilemap orientation constant.
  *
- * @return {(Phaser.Tilemaps.Components.WorldToTileXY|Phaser.Tilemaps.Components.IsometricWorldToTileXY|Phaser.Tilemaps.Components.HexagonalWorldToTileXY|Phaser.Tilemaps.Components.StaggeredWorldToTileXY)} The function to use to translate tiles for the given map type.
+ * @return {(Phaser.Tilemaps.Components.TileToWorldXY|Phaser.Tilemaps.Components.IsometricTileToWorldXY|Phaser.Tilemaps.Components.HexagonalTileToWorldXY|Phaser.Tilemaps.Components.StaggeredTileToWorldXY)} The function to use to translate tiles for the given map type.
  */
-var GetWorldToTileXYFunction = function (orientation)
+var GetTileToWorldXYFunction = function (orientation)
 {
     if (orientation === CONST.ORTHOGONAL)
     {
-        return WorldToTileXY;
+        return TileToWorldXY;
     }
     else if (orientation === CONST.ISOMETRIC)
     {
-        return IsometricWorldToTileXY;
+        return IsometricTileToWorldXY;
     }
     else if (orientation === CONST.HEXAGONAL)
     {
-        return HexagonalWorldToTileXY;
+        return HexagonalTileToWorldXY;
     }
     else if (orientation === CONST.STAGGERED)
     {
-        return StaggeredWorldToTileXY;
+        return StaggeredTileToWorldXY;
     }
     else
     {
@@ -45,4 +45,4 @@ var GetWorldToTileXYFunction = function (orientation)
     }
 };
 
-module.exports = GetWorldToTileXYFunction;
+module.exports = GetTileToWorldXYFunction;
