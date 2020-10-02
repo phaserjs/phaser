ba4be029be0337f530a6dc6a842eaa69fe84bae3
Richard Davey <rich@photonstorm.com>
2020-10-02 10:18:57 +0100

    Create TilemapOrientationType.js

---
+++ src/tilemaps/typedefs/TilemapOrientationType.js
@@ -0,0 +1,6 @@
+/**
+ * A Tilemap Orientation Type.
+ *
+ * @typedef {(Phaser.Tilemaps.ORTHOGONAL|Phaser.Tilemaps.ISOMETRIC|Phaser.Tilemaps.STAGGERED|Phaser.Tilemaps.HEXAGONAL)} Phaser.Types.Tilemaps.TilemapOrientationType
+ * @since 3.50.0
+ */
