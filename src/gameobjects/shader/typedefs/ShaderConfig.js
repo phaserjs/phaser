c1d63d32dd589abec90992210a6a384ba3e25366
Patrick Sletvold <patricksletvold@hotmail.com>
2020-10-03 10:40:24 +0200

    Add Config typedefs for a bunch of GO Creators

---
+++ src/gameobjects/shader/typedefs/ShaderConfig.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {object} Phaser.Types.GameObjects.Shader.ShaderConfig
+ * @extends Phaser.Types.GameObjects.GameObjectConfig
+ * @since 3.50.0
+ *
+ * @property {(string|Phaser.Display.BaseShader)} key - The key of the shader to use from the shader cache, or a BaseShader instance.
+ * @property {number} [width=128] - The width of the Game Object.
+ * @property {number} [height=128] - The height of the Game Object.
+ */
