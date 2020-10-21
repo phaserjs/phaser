c730272ba7ba8fd749c4b29cbd752163e17ca61d
Richard Davey <rich@photonstorm.com>
2020-10-21 18:12:40 +0100

    Updated the configs to handle multiple shaders

--- src/renderer/webgl/typedefs/WebGLPipelineConfig.js
+++ src/renderer/webgl/typedefs/WebGLPipelineConfig.js
@@ -5,11 +5,12 @@
  * @property {Phaser.Game} game - The Phaser.Game instance that owns this pipeline.
  * @property {string} [name] - The name of the pipeline.
  * @property {GLenum} [topology=gl.TRIANGLES] - How the primitives are rendered. The default value is GL_TRIANGLES. Here is the full list of rendering primitives: (https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Constants).
- * @property {string} [vertShader] - The source code, as a string, for the vertex shader.
- * @property {string} [fragShader] - The source code, as a string, for the fragment shader. Can include `%count%` and `%forloop%` declarations for multi-texture support.
+ * @property {string} [vertShader] - The source code, as a string, for the vertex shader. If you need to assign multiple shaders, see the `shaders` property.
+ * @property {string} [fragShader] - The source code, as a string, for the fragment shader. Can include `%count%` and `%forloop%` declarations for multi-texture support. If you need to assign multiple shaders, see the `shaders` property.
  * @property {number} [vertexCapacity] - The number of vertices to hold in the batch. Defaults to `RenderConfig.batchSize` * 6.
  * @property {number} [vertexSize] - The size, in bytes, of a single entry in the vertex buffer. Defaults to Float32Array.BYTES_PER_ELEMENT * 6 + Uint8Array.BYTES_PER_ELEMENT * 4.
  * @property {ArrayBuffer} [vertices] - An optional Array Buffer full of pre-calculated vertices data.
- * @property {Phaser.Types.Renderer.WebGL.WebGLPipelineAttributesConfig} [attributes] - An array of shader attribute data.
- * @property {string[]} [uniforms] - An array of shader uniform names that will be looked-up to get the locations for.
+ * @property {Phaser.Types.Renderer.WebGL.WebGLPipelineAttributesConfig} [attributes] - An array of shader attribute data. All shaders bound to this pipeline must use the same attributes.
+ * @property {string[]} [uniforms] - An array of shader uniform names that will be looked-up to get the locations for. If you need to assign multiple shaders, see the `shaders` property.
+ * @property {Phaser.Types.Renderer.WebGL.WebGLPipelineShaderConfig[]} [shaders] - An array of shaders, all of which are created for this one pipeline. Uses the `vertShader`, `fragShader`, `attributes` and `uniforms` properties of this object as defaults.
  */
---
+++ src/renderer/webgl/typedefs/WebGLPipelineShaderConfig.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {object} Phaser.Types.Renderer.WebGL.WebGLPipelineShaderConfig
+ * @since 3.50.0
+ *
+ * @property {string} [name] - The name of the shader. Doesn't have to be unique, but makes shader look-up easier if it is.
+ * @property {string} [vertShader] - The source code, as a string, for the vertex shader. If not given, uses the `Phaser.Types.Renderer.WebGL.WebGLPipelineConfig.vertShader` property instead.
+ * @property {string} [fragShader] - The source code, as a string, for the fragment shader. Can include `%count%` and `%forloop%` declarations for multi-texture support. If not given, uses the `Phaser.Types.Renderer.WebGL.WebGLPipelineConfig.fragShader` property instead.
+ * @property {string[]} [uniforms] - An array of shader uniform names that will be looked-up to get the locations for. If not given, uses the `Phaser.Types.Renderer.WebGL.WebGLPipelineConfig.uniforms` property instead.
+ */
