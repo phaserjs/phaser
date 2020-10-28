9aeb76569683eb20a6ec1c4fc04083be448de93e
Richard Davey <rich@photonstorm.com>
2020-10-28 16:13:46 +0000

    Updated type defs

---
+++ src/renderer/webgl/typedefs/WebGLConst.js
@@ -0,0 +1,7 @@
+/**
+ * @typedef {object} Phaser.Types.Renderer.WebGL.WebGLConst
+ * @since 3.50.0
+ *
+ * @property {GLenum} enum - The data type of the attribute, i.e. `gl.BYTE`, `gl.SHORT`, `gl.UNSIGNED_BYTE`, `gl.FLOAT`, etc.
+ * @property {number} size - The size, in bytes, of the data type.
+ */
