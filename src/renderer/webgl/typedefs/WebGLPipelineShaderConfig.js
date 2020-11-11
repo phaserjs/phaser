8ef9f363709f8da99f528be2577bf01f7ba7e593
Richard Davey <rich@photonstorm.com>
2020-11-11 17:46:58 +0000

    Fixed missing property

--- src/renderer/webgl/typedefs/WebGLPipelineShaderConfig.js
+++ src/renderer/webgl/typedefs/WebGLPipelineShaderConfig.js
@@ -6,4 +6,5 @@
  * @property {string} [vertShader] - The source code, as a string, for the vertex shader. If not given, uses the `Phaser.Types.Renderer.WebGL.WebGLPipelineConfig.vertShader` property instead.
  * @property {string} [fragShader] - The source code, as a string, for the fragment shader. Can include `%count%` and `%forloop%` declarations for multi-texture support. If not given, uses the `Phaser.Types.Renderer.WebGL.WebGLPipelineConfig.fragShader` property instead.
  * @property {string[]} [uniforms] - An array of shader uniform names that will be looked-up to get the locations for. If not given, uses the `Phaser.Types.Renderer.WebGL.WebGLPipelineConfig.uniforms` property instead.
+ * @property {Phaser.Types.Renderer.WebGL.WebGLPipelineAttributesConfig} [attributes] - An array of shader attribute data. All shaders bound to this pipeline must use the same attributes.
  */
