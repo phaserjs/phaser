52acf601257b5f664c850746456eea34c432ed9a
Richard Davey <rich@photonstorm.com>
2020-11-16 11:32:34 +0000

    Multiply now optional for lots of combination effects

--- src/display/ColorMatrix.js
+++ src/display/ColorMatrix.js
@@ -172,12 +172,14 @@ var ColorMatrix = new Class({
      * @since 3.50.0
      *
      * @param {number} [value=0] - The amount of brightness to apply to this ColorMatrix. Between 0 (black) and 1.
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
      *
      * @return {this} This ColorMatrix instance.
      */
-    brightness: function (value)
+    brightness: function (value, multiply)
     {
         if (value === undefined) { value = 0; }
+        if (multiply === undefined) { multiply = false; }
 
         var b = value;
 
@@ -186,7 +188,7 @@ var ColorMatrix = new Class({
             0, b, 0, 0, 0,
             0, 0, b, 0, 0,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -196,12 +198,14 @@ var ColorMatrix = new Class({
      * @since 3.50.0
      *
      * @param {number} [value=0] - The amount of saturation to apply to this ColorMatrix.
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
      *
      * @return {this} This ColorMatrix instance.
      */
-    saturate: function (value)
+    saturate: function (value, multiply)
     {
         if (value === undefined) { value = 0; }
+        if (multiply === undefined) { multiply = false; }
 
         var x = (value * 2 / 3) + 1;
         var y = ((x - 1) * -0.5);
@@ -211,7 +215,7 @@ var ColorMatrix = new Class({
             y, x, y, 0, 0,
             y, y, x, 0, 0,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -220,11 +224,15 @@ var ColorMatrix = new Class({
      * @method Phaser.Display.ColorMatrix#saturation
      * @since 3.50.0
      *
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
+     *
      * @return {this} This ColorMatrix instance.
      */
-    desaturate: function ()
+    desaturate: function (multiply)
     {
-        return this.saturate(-1);
+        if (multiply === undefined) { multiply = false; }
+
+        return this.saturate(-1, multiply);
     },
 
     /**
@@ -234,12 +242,14 @@ var ColorMatrix = new Class({
      * @since 3.50.0
      *
      * @param {number} [rotation=0] - The amount of hue rotation to apply to this ColorMatrix, in degrees.
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
      *
      * @return {this} This ColorMatrix instance.
      */
-    hue: function (rotation)
+    hue: function (rotation, multiply)
     {
         if (rotation === undefined) { rotation = 0; }
+        if (multiply === undefined) { multiply = false; }
 
         rotation = rotation / 180 * Math.PI;
 
@@ -254,7 +264,7 @@ var ColorMatrix = new Class({
             lumR + cos * (-lumR) + sin * (0.143),lumG + cos * (1 - lumG) + sin * (0.140),lumB + cos * (-lumB) + sin * (-0.283), 0, 0,
             lumR + cos * (-lumR) + sin * (-(1 - lumR)),lumG + cos * (-lumG) + sin * (lumG),lumB + cos * (1 - lumB) + sin * (lumB), 0, 0,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -264,14 +274,16 @@ var ColorMatrix = new Class({
      * @since 3.50.0
      *
      * @param {number} [value=1] - The grayscale scale (0 is black).
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
      *
      * @return {this} This ColorMatrix instance.
      */
-    grayscale: function (value)
+    grayscale: function (value, multiply)
     {
         if (value === undefined) { value = 1; }
+        if (multiply === undefined) { multiply = false; }
 
-        return this.saturation(-value);
+        return this.saturate(-value, multiply);
     },
 
     /**
@@ -280,16 +292,20 @@ var ColorMatrix = new Class({
      * @method Phaser.Display.ColorMatrix#blackWhite
      * @since 3.50.0
      *
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
+     *
      * @return {this} This ColorMatrix instance.
      */
-    blackWhite: function ()
+    blackWhite: function (multiply)
     {
+        if (multiply === undefined) { multiply = false; }
+
         return this.multiply([
             0.3, 0.6, 0.1, 0, 0,
             0.3, 0.6, 0.1, 0, 0,
             0.3, 0.6, 0.1, 0, 0,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -299,12 +315,14 @@ var ColorMatrix = new Class({
      * @since 3.50.0
      *
      * @param {number} [value=0] - The amount of contrast to apply to this ColorMatrix.
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
      *
      * @return {this} This ColorMatrix instance.
      */
-    contrast: function (value)
+    contrast: function (value, multiply)
     {
         if (value === undefined) { value = 0; }
+        if (multiply === undefined) { multiply = false; }
 
         var v = value + 1;
         var o = -0.5 * (v - 1);
@@ -314,7 +332,7 @@ var ColorMatrix = new Class({
             0, v, 0, 0, o,
             0, 0, v, 0, o,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -323,16 +341,20 @@ var ColorMatrix = new Class({
      * @method Phaser.Display.ColorMatrix#negative
      * @since 3.50.0
      *
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
+     *
      * @return {this} This ColorMatrix instance.
      */
-    negative: function ()
+    negative: function (multiply)
     {
+        if (multiply === undefined) { multiply = false; }
+
         return this.multiply([
             -1, 0, 0, 1, 0,
             0, -1, 0, 1, 0,
             0, 0, -1, 1, 0,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -341,16 +363,20 @@ var ColorMatrix = new Class({
      * @method Phaser.Display.ColorMatrix#desaturateLuminance
      * @since 3.50.0
      *
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
+     *
      * @return {this} This ColorMatrix instance.
      */
-    desaturateLuminance: function ()
+    desaturateLuminance: function (multiply)
     {
+        if (multiply === undefined) { multiply = false; }
+
         return this.multiply([
             0.2764723, 0.9297080, 0.0938197, 0, -37.1,
             0.2764723, 0.9297080, 0.0938197, 0, -37.1,
             0.2764723, 0.9297080, 0.0938197, 0, -37.1,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -359,16 +385,20 @@ var ColorMatrix = new Class({
      * @method Phaser.Display.ColorMatrix#sepia
      * @since 3.50.0
      *
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
+     *
      * @return {this} This ColorMatrix instance.
      */
-    sepia: function ()
+    sepia: function (multiply)
     {
+        if (multiply === undefined) { multiply = false; }
+
         return this.multiply([
             0.393, 0.7689999, 0.18899999, 0, 0,
             0.349, 0.6859999, 0.16799999, 0, 0,
             0.272, 0.5339999, 0.13099999, 0, 0,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -378,19 +408,21 @@ var ColorMatrix = new Class({
      * @since 3.50.0
      *
      * @param {number} [intensity=0.1] - The intensity of this effect.
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
      *
      * @return {this} This ColorMatrix instance.
      */
-    night: function (intensity)
+    night: function (intensity, multiply)
     {
         if (intensity === undefined) { intensity = 0.1; }
+        if (multiply === undefined) { multiply = false; }
 
         return this.multiply([
             intensity * (-2.0), -intensity, 0, 0, 0,
             -intensity, 0, intensity, 0, 0,
             0, intensity, intensity * 2.0, 0, 0,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -399,16 +431,20 @@ var ColorMatrix = new Class({
      * @method Phaser.Display.ColorMatrix#lsd
      * @since 3.50.0
      *
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
+     *
      * @return {this} This ColorMatrix instance.
      */
-    lsd: function ()
+    lsd: function (multiply)
     {
+        if (multiply === undefined) { multiply = false; }
+
         return this.multiply([
             2, -0.4, 0.5, 0, 0,
             -0.5, 2, -0.4, 0, 0,
             -0.4, -0.5, 3, 0, 0,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -417,16 +453,20 @@ var ColorMatrix = new Class({
      * @method Phaser.Display.ColorMatrix#brown
      * @since 3.50.0
      *
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
+     *
      * @return {this} This ColorMatrix instance.
      */
-    brown: function ()
+    brown: function (multiply)
     {
+        if (multiply === undefined) { multiply = false; }
+
         return this.multiply([
             0.5997023498159715, 0.34553243048391263, -0.2708298674538042, 0, 47.43192855600873,
             -0.037703249837783157, 0.8609577587992641, 0.15059552388459913, 0, -36.96841498319127,
             0.24113635128153335, -0.07441037908422492, 0.44972182064877153, 0, -7.562075277591283,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -435,16 +475,20 @@ var ColorMatrix = new Class({
      * @method Phaser.Display.ColorMatrix#vintagePinhole
      * @since 3.50.0
      *
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
+     *
      * @return {this} This ColorMatrix instance.
      */
-    vintagePinhole: function ()
+    vintagePinhole: function (multiply)
     {
+        if (multiply === undefined) { multiply = false; }
+
         return this.multiply([
             0.6279345635605994, 0.3202183420819367, -0.03965408211312453, 0, 9.651285835294123,
             0.02578397704808868, 0.6441188644374771, 0.03259127616149294, 0, 7.462829176470591,
             0.0466055556782719, -0.0851232987247891, 0.5241648018700465, 0, 5.159190588235296,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -453,16 +497,20 @@ var ColorMatrix = new Class({
      * @method Phaser.Display.ColorMatrix#kodachrome
      * @since 3.50.0
      *
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
+     *
      * @return {this} This ColorMatrix instance.
      */
-    kodachrome: function ()
+    kodachrome: function (multiply)
     {
+        if (multiply === undefined) { multiply = false; }
+
         return this.multiply([
             1.1285582396593525, -0.3967382283601348, -0.03992559172921793, 0, 63.72958762196502,
             -0.16404339962244616, 1.0835251566291304, -0.05498805115633132, 0, 24.732407896706203,
             -0.16786010706155763, -0.5603416277695248, 1.6014850761964943, 0, 35.62982807460946,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -471,16 +519,20 @@ var ColorMatrix = new Class({
      * @method Phaser.Display.ColorMatrix#technicolor
      * @since 3.50.0
      *
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
+     *
      * @return {this} This ColorMatrix instance.
      */
-    technicolor: function ()
+    technicolor: function (multiply)
     {
+        if (multiply === undefined) { multiply = false; }
+
         return this.multiply([
             1.9125277891456083, -0.8545344976951645, -0.09155508482755585, 0, 11.793603434377337,
             -0.3087833385928097, 1.7658908555458428, -0.10601743074722245, 0, -70.35205161461398,
             -0.231103377548616, -0.7501899197440212, 1.847597816108189, 0, 30.950940869491138,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -489,16 +541,20 @@ var ColorMatrix = new Class({
      * @method Phaser.Display.ColorMatrix#polaroid
      * @since 3.50.0
      *
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
+     *
      * @return {this} This ColorMatrix instance.
      */
-    polaroid: function ()
+    polaroid: function (multiply)
     {
+        if (multiply === undefined) { multiply = false; }
+
         return this.multiply([
             1.438, -0.062, -0.062, 0, 0,
             -0.122, 1.378, -0.122, 0, 0,
             -0.016, -0.016, 1.483, 0, 0,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -507,16 +563,20 @@ var ColorMatrix = new Class({
      * @method Phaser.Display.ColorMatrix#shiftToBGR
      * @since 3.50.0
      *
+     * @param {boolean} [multiply=false] - Multiply the resulting ColorMatrix (`true`), or set it (`false`) ?
+     *
      * @return {this} This ColorMatrix instance.
      */
-    shiftToBGR: function ()
+    shiftToBGR: function (multiply)
     {
+        if (multiply === undefined) { multiply = false; }
+
         return this.multiply([
             0, 0, 1, 0, 0,
             0, 1, 0, 0, 0,
             1, 0, 0, 0, 0,
             0, 0, 0, 1, 0
-        ]);
+        ], multiply);
     },
 
     /**
@@ -529,9 +589,14 @@ var ColorMatrix = new Class({
      *
      * @return {this} This ColorMatrix instance.
      */
-    multiply: function (a)
+    multiply: function (a, multiply)
     {
         //  Duplicate _matrix into c
+
+        if (!multiply)
+        {
+            this.reset();
+        }
 
         var m = this._matrix;
         var c = [];
