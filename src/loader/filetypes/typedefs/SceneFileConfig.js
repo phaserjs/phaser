8f688cd9a5b98526b0425e34783d2dd79d82b24b
Richard Davey <rich@photonstorm.com>
2020-11-17 17:15:36 +0000

    Update SceneFileConfig.js

--- src/loader/filetypes/typedefs/SceneFileConfig.js
+++ src/loader/filetypes/typedefs/SceneFileConfig.js
@@ -3,6 +3,6 @@
  *
  * @property {string} key - The key of the file. Must be unique within both the Loader and the Text Cache.
  * @property {string} [url] - The absolute or relative URL to load the file from.
- * @property {string} [extension='txt'] - The default file extension to use if no url is provided.
+ * @property {string} [extension='js'] - The default file extension to use if no url is provided.
  * @property {Phaser.Types.Loader.XHRSettingsObject} [xhrSettings] - Extra XHR Settings specifically for this file.
  */
