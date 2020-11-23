da96d2b26faafdbe60b0d2ae5a0afc26c3e9b53d
Richard Davey <rich@photonstorm.com>
2020-11-23 11:31:23 +0000

    Fixed namespace

--- src/plugins/DefaultPlugins.js
+++ src/plugins/DefaultPlugins.js
@@ -5,11 +5,11 @@
  */
 
 /**
- * @typedef {object} Phaser.Plugins.DefaultPlugins
- *
- * @property {array} Global - These are the Global Managers that are created by the Phaser.Game instance.
- * @property {array} CoreScene - These are the core plugins that are installed into every Scene.Systems instance, no matter what.
- * @property {array} DefaultScene - These plugins are created in Scene.Systems in addition to the CoreScenePlugins.
+ * The Default Plugins.
+ * 
+ * @namespace Phaser.Plugins.DefaultPlugins
+ * @memberof Phaser.Plugins
+ * @since 3.0.0
  */
 
 var DefaultPlugins = {
