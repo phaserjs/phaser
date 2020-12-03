38b6d834620db38abb6f66790fa3b7cc72b8f8fc
Richard Davey <rich@photonstorm.com>
2020-12-03 14:48:21 +0000

    Fixed Canvas Alpha RenderTexture issue. Fix #5426

--- src/gameobjects/rendertexture/RenderTexture.js
+++ src/gameobjects/rendertexture/RenderTexture.js
@@ -1286,7 +1286,7 @@ var RenderTexture = new Class({
 
             var matrix = this.camera.matrix;
 
-            ctx.globalAlpha = this.globalAlpha;
+            ctx.globalAlpha = alpha;
 
             ctx.setTransform(matrix[0], matrix[1], matrix[2], matrix[3], matrix[4], matrix[5]);
 
