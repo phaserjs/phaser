27da1dfa4bab8e7c098e5d8c13540fb59e41143a
Richard Davey <rich@photonstorm.com>
2020-12-11 10:24:05 +0000

    Clear just the color buffer

--- src/renderer/webgl/RenderTarget.js
+++ src/renderer/webgl/RenderTarget.js
@@ -249,7 +249,7 @@ var RenderTarget = new Class({
 
             gl.clearColor(0, 0, 0, 0);
 
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
+            gl.clear(gl.COLOR_BUFFER_BIT);
         }
 
         if (adjustViewport)
