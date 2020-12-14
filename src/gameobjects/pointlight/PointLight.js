3e9350ed05a54170e66501a5083c96763fcaffb8
Richard Davey <rich@photonstorm.com>
2020-12-14 14:07:57 +0000

    Added JSDocs

--- src/gameobjects/pointlight/PointLight.js
+++ src/gameobjects/pointlight/PointLight.js
@@ -13,7 +13,29 @@ var Render = require('./PointLightRender');
 
 /**
  * @classdesc
- * TODO
+ * The Point Light Game Object provides a way to add a point light effect into your game,
+ * without the expensive shader processing requirements of the traditional Light Game Object.
+ *
+ * The difference is that the Point Light renders using a custom shader, designed to give the
+ * impression of a point light source, of variable radius, intensity and color, in your game.
+ * However, unlike the Light Game Object, it does not impact any other Game Objects, or use their
+ * normal maps for calcuations. This makes them extremely fast to render compared to Lights
+ * and perfect for special effects, such as flickering torches or muzzle flashes.
+ *
+ * For maximum performance you should batch Point Light Game Objects together. This means
+ * ensuring they follow each other consecutively on the display list. Ideally, use a Layer
+ * Game Object and then add just Point Lights to it, so that it can batch together the rendering
+ * of the lights. You don't _have_ to do this, and if you've only a handful of Point Lights in
+ * your game then it's perfectly safe to mix them into the dislay list as normal. However, if
+ * you're using a large number of them, please consider how they are mixed into the display list.
+ *
+ * The renderer will automatically cull Point Lights. Those with a radius that does not intersect
+ * with the Camera will be skipped in the rendering list. This happens automatically and the
+ * culled state is refreshed every frame, for every camera.
+ *
+ * The origin of a Point Light is always 0.5 and it cannot be changed.
+ *
+ * Point Lights are a WebGL only feature and do not have a Canvas counterpart.
  *
  * @class PointLight
  * @extends Phaser.GameObjects.GameObject
@@ -31,9 +53,13 @@ var Render = require('./PointLightRender');
  * @extends Phaser.GameObjects.Components.Transform
  * @extends Phaser.GameObjects.Components.Visible
  *
- * @param {Phaser.Scene} scene - The Scene to which this Game Object belongs. A Game Object can only belong to one Scene at a time.
- * @param {number} x - The horizontal position of this Game Object in the world.
- * @param {number} y - The vertical position of this Game Object in the world.
+ * @param {Phaser.Scene} scene - The Scene to which this Point Light belongs. A Point Light can only belong to one Scene at a time.
+ * @param {number} x - The horizontal position of this Point Light in the world.
+ * @param {number} y - The vertical position of this Point Light in the world.
+ * @param {number} [color=0xffffff] - The color of the Point Light, given as a hex value.
+ * @param {number} [radius=128] - The radius of the Point Light.
+ * @param {number} [intensity=1] - The intensity, or colr blend, of the Point Light.
+ * @param {number} [attenuation=0.1] - The attenuation  of the Point Light. This is the reduction of light from the center point.
  */
 var PointLight = new Class({
 
@@ -67,9 +93,39 @@ var PointLight = new Class({
 
         this.setPosition(x, y);
 
+        /**
+         * The color of this Point Light. This property is an instance of a
+         * Color object, so you can use the methods within it, such as `setTo(r, g, b)`
+         * to change the color value.
+         *
+         * @name Phaser.GameObjects.PointLight#color
+         * @type {Phaser.Display.Color}
+         * @since 3.50.0
+         */
         this.color = IntegerToColor(color);
 
+        /**
+         * The intensity of the Point Light.
+         *
+         * The colors of the light are multiplied by this value during rendering.
+         *
+         * @name Phaser.GameObjects.PointLight#intensity
+         * @type {number}
+         * @since 3.50.0
+         */
         this.intensity = intensity;
+
+        /**
+         * The attenuation of the Point Light.
+         *
+         * This value controls the force with which the light falls-off from the center of the light.
+         *
+         * Use small float-based values, i.e. 0.1.
+         *
+         * @name Phaser.GameObjects.PointLight#attenuation
+         * @type {number}
+         * @since 3.50.0
+         */
         this.attenuation = attenuation;
 
         //  read only:
@@ -79,6 +135,13 @@ var PointLight = new Class({
         this._radius = radius;
     },
 
+    /**
+     * The radius of the Point Light.
+     *
+     * @name Phaser.GameObjects.PointLight#radius
+     * @type {number}
+     * @since 3.50.0
+     */
     radius: {
 
         get: function ()
--- src/gameobjects/pointlight/PointLightFactory.js
+++ src/gameobjects/pointlight/PointLightFactory.js
@@ -12,9 +12,39 @@ var PointLight = require('./PointLight');
  *
  * Note: This method will only be available if the Point Light Game Object has been built into Phaser.
  *
+ * The Point Light Game Object provides a way to add a point light effect into your game,
+ * without the expensive shader processing requirements of the traditional Light Game Object.
+ *
+ * The difference is that the Point Light renders using a custom shader, designed to give the
+ * impression of a point light source, of variable radius, intensity and color, in your game.
+ * However, unlike the Light Game Object, it does not impact any other Game Objects, or use their
+ * normal maps for calcuations. This makes them extremely fast to render compared to Lights
+ * and perfect for special effects, such as flickering torches or muzzle flashes.
+ *
+ * For maximum performance you should batch Point Light Game Objects together. This means
+ * ensuring they follow each other consecutively on the display list. Ideally, use a Layer
+ * Game Object and then add just Point Lights to it, so that it can batch together the rendering
+ * of the lights. You don't _have_ to do this, and if you've only a handful of Point Lights in
+ * your game then it's perfectly safe to mix them into the dislay list as normal. However, if
+ * you're using a large number of them, please consider how they are mixed into the display list.
+ *
+ * The renderer will automatically cull Point Lights. Those with a radius that does not intersect
+ * with the Camera will be skipped in the rendering list. This happens automatically and the
+ * culled state is refreshed every frame, for every camera.
+ *
+ * The origin of a Point Light is always 0.5 and it cannot be changed.
+ *
+ * Point Lights are a WebGL only feature and do not have a Canvas counterpart.
+ *
  * @method Phaser.GameObjects.GameObjectFactory#pointlight
  * @since 3.50.0
  *
+ * @param {number} x - The horizontal position of this Point Light in the world.
+ * @param {number} y - The vertical position of this Point Light in the world.
+ * @param {number} [color=0xffffff] - The color of the Point Light, given as a hex value.
+ * @param {number} [radius=128] - The radius of the Point Light.
+ * @param {number} [intensity=1] - The intensity, or colr blend, of the Point Light.
+ * @param {number} [attenuation=0.1] - The attenuation  of the Point Light. This is the reduction of light from the center point.
  *
  * @return {Phaser.GameObjects.PointLight} The Game Object that was created.
  */
