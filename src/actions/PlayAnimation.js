b082ef09673a8a6d5c6de16d514aa5ee08a79904
Richard Davey <rich@photonstorm.com>
2021-02-16 11:53:12 +0000

    The `Actions.PlayAnimation` arguments have been updated to match the new animation system introduced in Phaser 3.50. It will now take either a string-key, or a play animation configuration object, and the `startFrame` parameter has been replaced with `ignoreIfPlaying`. The function will also only call `play` if the Game Object has an animation component, meaning you can now supply this action with a mixed-content array without errors. Fix #5555

--- src/actions/PlayAnimation.js
+++ src/actions/PlayAnimation.js
@@ -5,7 +5,9 @@
  */
 
 /**
- * Play an animation with the given key, starting at the given startFrame on all Game Objects in items.
+ * Play an animation on all Game Objects in the array that have an Animation component.
+ *
+ * You can pass either an animation key, or an animation configuration object for more control over the playback.
  *
  * @function Phaser.Actions.PlayAnimation
  * @since 3.0.0
@@ -13,16 +15,21 @@
  * @generic {Phaser.GameObjects.GameObject[]} G - [items,$return]
  *
  * @param {(array|Phaser.GameObjects.GameObject[])} items - An array of Game Objects. The contents of this array are updated by this Action.
- * @param {string} key - The name of the animation to play.
- * @param {(string|number)} [startFrame] - The starting frame of the animation with the given key.
+ * @param {(string|Phaser.Animations.Animation|Phaser.Types.Animations.PlayAnimationConfig)} key - The string-based key of the animation to play, or an Animation instance, or a `PlayAnimationConfig` object.
+ * @param {boolean} [ignoreIfPlaying=false] - If this animation is already playing then ignore this call.
  *
  * @return {(array|Phaser.GameObjects.GameObject[])} The array of Game Objects that was passed to this Action.
  */
-var PlayAnimation = function (items, key, startFrame)
+var PlayAnimation = function (items, key, ignoreIfPlaying)
 {
     for (var i = 0; i < items.length; i++)
     {
-        items[i].anims.play(key, startFrame);
+        var gameObject = items[i];
+
+        if (gameObject.anims)
+        {
+            gameObject.anims.play(key, ignoreIfPlaying);
+        }
     }
 
     return items;
