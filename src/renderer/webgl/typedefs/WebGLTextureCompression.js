670a66c697992623eadd14fbca7650ef4a0b8df4
Vladislav Forsh <vlad@robowhale.com>
2021-04-13 13:02:51 +0300

    Improve types for WebGL texture compression support
    (cherry picked from commit 5626acffec17c6b4d66c78b8e0aa52e5cfcd8478)

---
+++ src/renderer/webgl/typedefs/WebGLTextureCompression.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {object} Phaser.Types.Renderer.WebGL.WebGLTextureCompression
+ * @since 3.55.0
+ *
+ * @property {object|null} ETC1 - Indicates if ETC1 compression is supported on current device (mostly Android).
+ * @property {object|null} PVRTC - Indicates if PVRTC compression is supported on current device (mostly iOS).
+ * @property {object|null} S3TC - Indicates if S3TC compression is supported on current device.
+ */
