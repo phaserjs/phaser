78b9215e2f524bed25892b7c5f6cbb41c892fcef
Richard Davey <rich@photonstorm.com>
2021-05-26 15:38:29 +0100

    Added the missing `WebGLPipelineUniformsConfig` type def. Fix #5718 (thanks @PhaserEditor2D)

---
+++ src/renderer/webgl/typedefs/WebGLPipelineUniformsConfig.js
@@ -0,0 +1,11 @@
+/**
+ * @typedef {object} Phaser.Types.Renderer.WebGL.WebGLPipelineUniformsConfig
+ * @since 3.55.1
+ *
+ * @property {string} name - The name of the uniform as defined in the shader.
+ * @property {number} location - The location of the uniform.
+ * @property {number} [value1] - The first cached value of the uniform.
+ * @property {number} [value2] - The first cached value of the uniform.
+ * @property {number} [value3] - The first cached value of the uniform.
+ * @property {number} [value4] - The first cached value of the uniform.
+ */
