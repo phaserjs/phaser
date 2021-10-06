bf02aaa7ee6f7f16a930a575d59be96ecbf24ae9
Richard Davey <rich@photonstorm.com>
2021-10-06 18:48:04 +0100

    Update WebGLTextureCompression.js

--- src/renderer/webgl/typedefs/WebGLTextureCompression.js
+++ src/renderer/webgl/typedefs/WebGLTextureCompression.js
@@ -7,7 +7,7 @@
  * @property {object|undefined} BPTC - Indicates if BPTC compression is supported.
  * @property {object|undefined} ETC - Indicates if ETC compression is supported (mostly Android).
  * @property {object|undefined} ETC1 - Indicates if ETC1 compression is supported (mostly Android).
- * @property {object|undefined} IMG - Indicates the browser supports true color images.
+ * @property {object|undefined} IMG - Indicates the browser supports true color images (all browsers).
  * @property {object|undefined} PVRTC - Indicates if PVRTC compression is supported (mostly iOS).
  * @property {object|undefined} RGTC - Indicates if RGTC compression is supported (mostly iOS).
  * @property {object|undefined} S3TC - Indicates if S3TC compression is supported on current device (mostly Windows).
