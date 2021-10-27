dbeefcc40590803b8c49666de96a65064eaf9ba9
Richard Davey <rich@photonstorm.com>
2021-10-27 17:06:38 +0100

    Update CompressedTextureFileConfig.js

--- src/loader/filetypes/typedefs/CompressedTextureFileConfig.js
+++ src/loader/filetypes/typedefs/CompressedTextureFileConfig.js
@@ -5,6 +5,9 @@
  * @property {string} [type] - The container format, either PVR or KTX. If not given it will try to extract it from the textureURL extension.
  * @property {string} [textureURL] - The URL of the compressed texture file to load.
  * @property {string} [atlasURL] - Optional URL of a texture atlas JSON data file. If not given, the texture will be loaded as a single image.
+ * @property {string} [multiAtlasURL] - Optional URL of a multi-texture atlas JSON data file as created by Texture Packer Pro.
+ * @property {string} [multiPath] - Optional path to use when loading the textures defined in the multi atlas data.
+ * @property {string} [multiBaseURL] - Optional Base URL to use when loading the textures defined in the multi atlas data.
  */
 
 /**
