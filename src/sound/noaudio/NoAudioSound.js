1deb04bceee4b65bca18ab0fed8ad398b03181a5
Richard Davey <rich@photonstorm.com>
2021-11-08 15:51:56 +0000

    Documented all of the NoAudio properties and methods #5578

--- src/sound/noaudio/NoAudioSound.js
+++ src/sound/noaudio/NoAudioSound.js
@@ -55,14 +55,91 @@ var NoAudioSound = new Class({
 
         EventEmitter.call(this);
 
+        /**
+         * Local reference to the sound manager.
+         *
+         * @name Phaser.Sound.NoAudioSound#manager
+         * @type {Phaser.Sound.BaseSoundManager}
+         * @private
+         * @since 3.0.0
+         */
         this.manager = manager;
+
+        /**
+         * Asset key for the sound.
+         *
+         * @name Phaser.Sound.NoAudioSound#key
+         * @type {string}
+         * @readonly
+         * @since 3.0.0
+         */
         this.key = key;
+
+        /**
+         * Flag indicating if sound is currently playing.
+         *
+         * @name Phaser.Sound.NoAudioSound#isPlaying
+         * @type {boolean}
+         * @default false
+         * @readonly
+         * @since 3.0.0
+         */
         this.isPlaying = false;
+
+        /**
+         * Flag indicating if sound is currently paused.
+         *
+         * @name Phaser.Sound.NoAudioSound#isPaused
+         * @type {boolean}
+         * @default false
+         * @readonly
+         * @since 3.0.0
+         */
         this.isPaused = false;
+
+        /**
+         * A property that holds the value of sound's actual playback rate,
+         * after its rate and detune values has been combined with global
+         * rate and detune values.
+         *
+         * @name Phaser.Sound.NoAudioSound#totalRate
+         * @type {number}
+         * @default 1
+         * @readonly
+         * @since 3.0.0
+         */
         this.totalRate = 1;
+
+        /**
+         * A value representing the duration, in seconds.
+         * It could be total sound duration or a marker duration.
+         *
+         * @name Phaser.Sound.NoAudioSound#duration
+         * @type {number}
+         * @readonly
+         * @since 3.0.0
+         */
         this.duration = 0;
+
+        /**
+         * The total duration of the sound in seconds.
+         *
+         * @name Phaser.Sound.NoAudioSound#totalDuration
+         * @type {number}
+         * @readonly
+         * @since 3.0.0
+         */
         this.totalDuration = 0;
 
+        /**
+         * A config object used to store default sound settings' values.
+         * Default values will be set by properties' setters.
+         *
+         * @name Phaser.Sound.NoAudioSound#config
+         * @type {Phaser.Types.Sound.SoundConfig}
+         * @private
+         * @since 3.0.0
+         */
         this.config = Extend({
             mute: false,
             volume: 1,
@@ -74,61 +151,163 @@ var NoAudioSound = new Class({
             pan: 0
         }, config);
 
+        /**
+         * Reference to the currently used config.
+         * It could be default config or marker config.
+         *
+         * @name Phaser.Sound.BaseSound#currentConfig
+         * @type {Phaser.Types.Sound.SoundConfig}
+         * @private
+         * @since 3.0.0
+         */
         this.currentConfig = this.config;
+
+        /**
+         * This property is un-used in a NoAudioSound object.
+         *
+         * @name Phaser.Sound.NoAudioSound#mute
+         * @type {boolean}
+         * @readonly
+         * @since 3.0.0
+         */
         this.mute = false;
+
+        /**
+         * This property is un-used in a NoAudioSound object.
+         *
+         * @name Phaser.Sound.NoAudioSound#volume
+         * @type {number}
+         * @readonly
+         * @since 3.0.0
+         */
         this.volume = 1;
+
+        /**
+         * This property is un-used in a NoAudioSound object.
+         *
+         * @name Phaser.Sound.NoAudioSound#rate
+         * @type {number}
+         * @readonly
+         * @since 3.0.0
+         */
         this.rate = 1;
+
+        /**
+         * This property is un-used in a NoAudioSound object.
+         *
+         * @name Phaser.Sound.NoAudioSound#detune
+         * @type {number}
+         * @readonly
+         * @since 3.0.0
+         */
         this.detune = 0;
+
+        /**
+         * This property is un-used in a NoAudioSound object.
+         *
+         * @name Phaser.Sound.NoAudioSound#seek
+         * @type {number}
+         * @readonly
+         * @since 3.0.0
+         */
         this.seek = 0;
+
+        /**
+         * This property is un-used in a NoAudioSound object.
+         *
+         * @name Phaser.Sound.NoAudioSound#loop
+         * @type {boolean}
+         * @readonly
+         * @since 3.0.0
+         */
         this.loop = false;
+
+        /**
+         * This property is un-used in a NoAudioSound object.
+         *
+         * @name Phaser.Sound.NoAudioSound#pan
+         * @type {number}
+         * @readonly
+         * @since 3.0.0
+         */
         this.pan = 0;
+
+        /**
+         * This property is un-used in a NoAudioSound object.
+         *
+         * @name Phaser.Sound.NoAudioSound#markers
+         * @type {Object.<string, Phaser.Types.Sound.SoundMarker>}
+         * @default {}
+         * @readonly
+         * @since 3.0.0
+         */
         this.markers = {};
+
+        /**
+         * This property is un-used in a NoAudioSound object.
+         *
+         * @name Phaser.Sound.NoAudioSound#currentMarker
+         * @type {Phaser.Types.Sound.SoundMarker}
+         * @default null
+         * @readonly
+         * @since 3.0.0
+         */
         this.currentMarker = null;
+
+        /**
+         * This property is un-used in a NoAudioSound object.
+         *
+         * @name Phaser.Sound.NoAudioSound#pendingRemove
+         * @type {boolean}
+         * @readonly
+         * @since 3.0.0
+         */
         this.pendingRemove = false;
     },
 
     /**
+     * This method is empty in a NoAudioSound object.
+     *
      * @method Phaser.Sound.NoAudioSound#addMarker
      * @since 3.0.0
-     *
-     * @param {Phaser.Types.Sound.SoundMarker} marker - Marker object.
      *
      * @return {boolean} false
      */
     addMarker: returnFalse,
 
     /**
+     * This method is empty in a NoAudioSound object.
+     *
      * @method Phaser.Sound.NoAudioSound#updateMarker
      * @since 3.0.0
-     *
-     * @param {Phaser.Types.Sound.SoundMarker} marker - Marker object with updated values.
      *
      * @return {boolean} false
      */
     updateMarker: returnFalse,
 
     /**
+     * This method is empty in a NoAudioSound object.
+     *
      * @method Phaser.Sound.NoAudioSound#removeMarker
      * @since 3.0.0
-     *
-     * @param {string} markerName - The name of the marker to remove.
      *
      * @return {null} null
      */
     removeMarker: returnNull,
 
     /**
+     * This method is empty in a NoAudioSound object.
+     *
      * @method Phaser.Sound.NoAudioSound#play
      * @since 3.0.0
-     *
-     * @param {(string|Phaser.Types.Sound.SoundConfig)} [markerName=''] - If you want to play a marker then provide the marker name here. Alternatively, this parameter can be a SoundConfig object.
-     * @param {Phaser.Types.Sound.SoundConfig} [config] - Optional sound config object to be applied to this marker or entire sound if no marker name is provided. It gets memorized for future plays of current section of the sound.
      *
      * @return {boolean} false
      */
     play: returnFalse,
 
     /**
+     * This method is empty in a NoAudioSound object.
+     *
      * @method Phaser.Sound.NoAudioSound#pause
      * @since 3.0.0
      *
@@ -137,7 +316,7 @@ var NoAudioSound = new Class({
     pause: returnFalse,
 
     /**
-     * Resumes the sound.
+     * This method is empty in a NoAudioSound object.
      *
      * @method Phaser.Sound.NoAudioSound#resume
      * @since 3.0.0
@@ -147,7 +326,7 @@ var NoAudioSound = new Class({
     resume: returnFalse,
 
     /**
-     * Stop playing this sound.
+     * This method is empty in a NoAudioSound object.
      *
      * @method Phaser.Sound.NoAudioSound#stop
      * @since 3.0.0
@@ -168,18 +347,74 @@ var NoAudioSound = new Class({
         BaseSound.prototype.destroy.call(this);
     },
 
+    /**
+     * This method is empty in a NoAudioSound object.
+     *
+     * @method Phaser.Sound.NoAudioSound#setMute
+     * @since 3.0.0
+     *
+     * @return {this} This Sound instance.
+     */
     setMute: returnThis,
 
+    /**
+     * This method is empty in a NoAudioSound object.
+     *
+     * @method Phaser.Sound.NoAudioSound#setVolume
+     * @since 3.0.0
+     *
+     * @return {this} This Sound instance.
+     */
     setVolume: returnThis,
 
+    /**
+     * This method is empty in a NoAudioSound object.
+     *
+     * @method Phaser.Sound.NoAudioSound#setRate
+     * @since 3.0.0
+     *
+     * @return {this} This Sound instance.
+     */
     setRate: returnThis,
 
+    /**
+     * This method is empty in a NoAudioSound object.
+     *
+     * @method Phaser.Sound.NoAudioSound#setDetune
+     * @since 3.0.0
+     *
+     * @return {this} This Sound instance.
+     */
     setDetune: returnThis,
 
+    /**
+     * This method is empty in a NoAudioSound object.
+     *
+     * @method Phaser.Sound.NoAudioSound#setSeek
+     * @since 3.0.0
+     *
+     * @return {this} This Sound instance.
+     */
     setSeek: returnThis,
 
+    /**
+     * This method is empty in a NoAudioSound object.
+     *
+     * @method Phaser.Sound.NoAudioSound#setLoop
+     * @since 3.0.0
+     *
+     * @return {this} This Sound instance.
+     */
     setLoop: returnThis,
 
+    /**
+     * This method is empty in a NoAudioSound object.
+     *
+     * @method Phaser.Sound.NoAudioSound#setPan
+     * @since 3.0.0
+     *
+     * @return {this} This Sound instance.
+     */
     setPan: returnThis
 
 });
