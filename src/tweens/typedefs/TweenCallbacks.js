e39970eefff7f9a4ef788b52916586f8ee78a1af
Richard Davey <rich@photonstorm.com>
2022-08-18 15:18:16 +0100

    Create TweenCallbacks.js

---
+++ src/tweens/typedefs/TweenCallbacks.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {object} Phaser.Types.Tweens.TweenCallbacks
+ * @since 3.60.0
+ *
+ * @property {Phaser.Types.Tweens.TweenOnActiveCallback} [onActive] - A function to call when the tween becomes active within the Tween Manager.
+ * @property {Phaser.Types.Tweens.TweenOnStartCallback} [onStart] - A function to call when the tween starts playback, after any delays have expired.
+ * @property {Phaser.Types.Tweens.TweenOnCompleteCallback} [onComplete] - A function to call when the tween completes.
+ * @property {Phaser.Types.Tweens.TweenOnLoopCallback} [onLoop] - A function to call each time the tween loops.
+ * @property {Phaser.Types.Tweens.TweenOnRepeatCallback} [onRepeat] - A function to call each time the tween repeats. Called once per property per target.
+ * @property {Phaser.Types.Tweens.TweenOnStopCallback} [onStop] - A function to call when the tween is stopped.
+ * @property {Phaser.Types.Tweens.TweenOnUpdateCallback} [onUpdate] - A function to call each time the tween steps. Called once per property per target.
+ * @property {Phaser.Types.Tweens.TweenOnYoyoCallback} [onYoyo] - A function to call each time the tween yoyos. Called once per property per target.
+ */
