87ba0cdbb045bbc3bdd3bdb2bdbce141d5d21873
Richard Davey <rich@photonstorm.com>
2022-08-19 16:50:59 +0100

    Use substring instead

--- src/tweens/builders/GetProps.js
+++ src/tweens/builders/GetProps.js
@@ -31,7 +31,7 @@ var GetProps = function (config)
         for (key in config.props)
         {
             //  Skip any property that starts with an underscore
-            if (key.substr(0, 1) !== '_')
+            if (key.substring(0, 1) !== '_')
             {
                 keys.push({ key: key, value: config.props[key] });
             }
@@ -42,7 +42,7 @@ var GetProps = function (config)
         for (key in config)
         {
             //  Skip any property that is in the ReservedProps list or that starts with an underscore
-            if (RESERVED.indexOf(key) === -1 && key.substr(0, 1) !== '_')
+            if (RESERVED.indexOf(key) === -1 && key.substring(0, 1) !== '_')
             {
                 keys.push({ key: key, value: config[key] });
             }
