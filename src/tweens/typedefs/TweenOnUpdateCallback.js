ec870f6a86490054328f2dda6f499c74a560257f
Richard Davey <rich@photonstorm.com>
2022-08-19 14:58:30 +0100

    Corrected target param

--- src/tweens/typedefs/TweenOnUpdateCallback.js
+++ src/tweens/typedefs/TweenOnUpdateCallback.js
@@ -3,7 +3,7 @@
  * @since 3.18.0
  *
  * @param {Phaser.Tweens.Tween} tween - A reference to the Tween.
- * @param {(any|any[])} targets - The targets of the Tween. If this Tween has multiple targets this will be an array of the targets.
+ * @param {any} target - The current target of the Tween. If this Tween has multiple targets, this will be a reference to just the single one being updated prior to this callback.
  * @param {string} key - The property that is being updated on the target.
  * @param {number} current - The current value of the property being set on the target.
  * @param {number} previous - The previous value of the property being set on the target.
