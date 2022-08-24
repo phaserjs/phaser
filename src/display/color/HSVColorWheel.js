c7d66ded23317fc203673d15c90a5189384e38b3
Richard Davey <rich@photonstorm.com>
2022-08-24 13:55:53 +0100

    Corrected docs

--- src/display/color/HSVColorWheel.js
+++ src/display/color/HSVColorWheel.js
@@ -7,7 +7,7 @@
 var HSVToRGB = require('./HSVToRGB');
 
 /**
- * Get HSV color wheel values in an array which will be 360 elements in size.
+ * Generates an HSV color wheel which is an array of 360 Color objects, for each step of the wheel.
  *
  * @function Phaser.Display.Color.HSVColorWheel
  * @since 3.0.0
@@ -15,7 +15,7 @@ var HSVToRGB = require('./HSVToRGB');
  * @param {number} [s=1] - The saturation, in the range 0 - 1.
  * @param {number} [v=1] - The value, in the range 0 - 1.
  *
- * @return {Phaser.Types.Display.ColorObject[]} An array containing 360 elements, where each contains a single numeric value corresponding to the color at that point in the HSV color wheel.
+ * @return {Phaser.Types.Display.ColorObject[]} An array containing 360 ColorObject elements, where each element contains a Color object corresponding to the color at that point in the HSV color wheel.
  */
 var HSVColorWheel = function (s, v)
 {
