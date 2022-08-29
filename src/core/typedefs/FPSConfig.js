9017d29d527c8d022582a9b8b1844a5fc8ec694d
Richard Davey <rich@photonstorm.com>
2022-08-29 16:31:58 +0100

    Update FPSConfig.js

--- src/core/typedefs/FPSConfig.js
+++ src/core/typedefs/FPSConfig.js
@@ -4,6 +4,7 @@
  *
  * @property {number} [min=5] - The minimum acceptable rendering rate, in frames per second.
  * @property {number} [target=60] - The optimum rendering rate, in frames per second. This does not enforce the fps rate, it merely tells Phaser what rate is considered optimal for this game.
+ * @property {number} [limit=0] - Enforces an fps rate limit that the game step will run at, regardless of browser frequency. 0 means 'no limit'. Never set this higher than RAF can handle.
  * @property {boolean} [forceSetTimeOut=false] - Use setTimeout instead of requestAnimationFrame to run the game loop.
  * @property {number} [deltaHistory=10] - Calculate the average frame delta from this many consecutive frame intervals.
  * @property {number} [panicMax=120] - The amount of frames the time step counts before we trust the delta values again.
