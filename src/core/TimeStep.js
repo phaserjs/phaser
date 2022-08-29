f776037f8ecc6f0ea50740cb738974b696dd4d1a
Richard Davey <rich@photonstorm.com>
2022-08-29 16:29:37 +0100

    Lots of jsdoc updates. Fixed time reset. Split limit fps up.

--- src/core/TimeStep.js
+++ src/core/TimeStep.js
@@ -88,6 +88,9 @@ var TimeStep = new Class({
         /**
          * The minimum fps rate you want the Time Step to run at.
          *
+         * Setting this cannot guarantee the browser runs at this rate, it merely influences
+         * the internal timing values to help the Timestep know when it has gone out of sync.
+         *
          * @name Phaser.Core.TimeStep#minFps
          * @type {number}
          * @default 5
@@ -112,22 +115,55 @@ var TimeStep = new Class({
         /**
          * Enforce a frame rate limit. This forces how often the Game step will run. By default it is zero,
          * which means it will run at whatever limit the browser (via RequestAnimationFrame) can handle, which
-         * is the optimum rate for fast-action games. However, if you are building a non-game like a graphics
-         * generator, or low-intensity game, you can lower the step rate via this value (and its related config
-         * value). Setting this _beyond_ the rate of RequestAnimationFrame will make no difference at all.
-         * Use it purely to _restrict_ updates in low-intensity situations.
+         * is the optimum rate for fast-action or responsive games.
          *
-         * @name Phaser.Core.TimeStep#limitFps
+         * However, if you are building a non-game app, like a graphics generator, or low-intensity game that doesn't
+         * require 60fps, then you can lower the step rate via this Game Config value:
+         *
+         * ```js
+         * fps: {
+         *   limit: 30
+         * }
+         * ```
+         *
+         * Setting this _beyond_ the rate of RequestAnimationFrame will make no difference at all.
+         *
+         * Use it purely to _restrict_ updates in low-intensity situations only.
+         *
+         * @name Phaser.Core.TimeStep#fpsLimit
          * @type {number}
          * @default 0
          * @since 3.60.0
          */
-        this.limitFps = GetValue(config, 'limit', 0);
-
-        this.limitFpsTriggered = false;
+        this.fpsLimit = GetValue(config, 'limit', 0);
 
         /**
-         * The minFps value in ms.
+         * Internal value tracking if the fps has been rated limited this step.
+         *
+         * @name Phaser.Core.TimeStep#fpsLimitTriggered
+         * @type {boolean}
+         * @default false
+         * @since 3.60.0
+         */
+        this.fpsLimitTriggered = false;
+
+        /**
+         * Is the FPS rate limited?
+         *
+         * This is set by setting the Game Config `limit` value to a value above zero.
+         *
+         * Consider this property as read-only.
+         *
+         * @name Phaser.Core.TimeStep#hasFpsLimit
+         * @type {boolean}
+         * @default false
+         * @since 3.60.0
+         */
+        this.hasFpsLimit = (this.fpsLimit > 0);
+
+        /**
+         * The minimum fps value in ms.
+         *
          * Defaults to 200ms between frames (i.e. super slow!)
          *
          * @name Phaser.Core.TimeStep#_min
@@ -138,7 +174,8 @@ var TimeStep = new Class({
         this._min = 1000 / this.minFps;
 
         /**
-         * The targetFps value in ms.
+         * The target fps value in ms.
+         *
          * Defaults to 16.66ms between frames (i.e. normal)
          *
          * @name Phaser.Core.TimeStep#_target
@@ -161,6 +198,7 @@ var TimeStep = new Class({
 
         /**
          * The time at which the next fps rate update will take place.
+         *
          * When an fps update happens, the `framesThisSecond` value is reset.
          *
          * @name Phaser.Core.TimeStep#nextFpsUpdate
@@ -183,7 +221,7 @@ var TimeStep = new Class({
         this.framesThisSecond = 0;
 
         /**
-         * A callback to be invoked each time the Time Step steps.
+         * A callback to be invoked each time the TimeStep steps.
          *
          * @name Phaser.Core.TimeStep#callback
          * @type {Phaser.Types.Core.TimeStepCallback}
@@ -193,7 +231,7 @@ var TimeStep = new Class({
         this.callback = NOOP;
 
         /**
-         * You can force the Time Step to use Set Timeout instead of Request Animation Frame by setting
+         * You can force the TimeStep to use SetTimeOut instead of Request Animation Frame by setting
          * the `forceSetTimeOut` property to `true` in the Game Configuration object. It cannot be changed at run-time.
          *
          * @name Phaser.Core.TimeStep#forceSetTimeOut
@@ -205,7 +243,10 @@ var TimeStep = new Class({
         this.forceSetTimeOut = GetValue(config, 'forceSetTimeOut', false);
 
         /**
-         * The time, calculated at the start of the current step, as smoothed by the delta value.
+         * The time, updated each step by adding the elapsed delta time to the previous value.
+         *
+         * This differs from the `TimeStep.now` value, which is the high resolution time value
+         * as provided by Request Animation Frame.
          *
          * @name Phaser.Core.TimeStep#time
          * @type {number}
@@ -215,8 +256,9 @@ var TimeStep = new Class({
         this.time = 0;
 
         /**
-         * The time at which the game started running. This value is adjusted if the game is then
-         * paused and resumes.
+         * The time at which the game started running.
+         *
+         * This value is adjusted if the game is then paused and resumes.
          *
          * @name Phaser.Core.TimeStep#startTime
          * @type {number}
@@ -226,7 +268,9 @@ var TimeStep = new Class({
         this.startTime = 0;
 
         /**
-         * The time, as returned by `performance.now` of the previous step.
+         * The time of the previous step.
+         *
+         * This is typically a high resolution timer value, as provided by Request Animation Frame.
          *
          * @name Phaser.Core.TimeStep#lastTime
          * @type {number}
@@ -249,6 +293,7 @@ var TimeStep = new Class({
 
         /**
          * Is the browser currently considered in focus by the Page Visibility API?
+         *
          * This value is set in the `blur` method, which is called automatically by the Game instance.
          *
          * @name Phaser.Core.TimeStep#inFocus
@@ -349,7 +394,10 @@ var TimeStep = new Class({
         this.rawDelta = 0;
 
         /**
-         * The time, as returned by `performance.now` at the very start of the current step.
+         * The time, set at the start of the current step.
+         *
+         * This is typically a high resolution timer value, as provided by Request Animation Frame.
+         *
          * This can differ from the `time` value in that it isn't calculated based on the delta value.
          *
          * @name Phaser.Core.TimeStep#now
@@ -483,7 +531,160 @@ var TimeStep = new Class({
 
         this.callback = callback;
 
-        this.raf.start(this.step.bind(this), this.forceSetTimeOut, this._target);
+        var step = (this.hasFpsLimit) ? this.stepLimitFPS.bind(this) : this.step.bind(this);
+
+        this.raf.start(step, this.forceSetTimeOut, this._target);
+    },
+
+    /**
+     * Takes the delta value and smooths it based on the previous frames.
+     *
+     * Called automatically as part of the step.
+     *
+     * @method Phaser.Core.TimeStep#smoothDelta
+     * @since 3.60.0
+     *
+     * @param {number} delta - The delta value for this step.
+     *
+     * @return {number} The smoothed delta value.
+     */
+    smoothDelta: function (delta)
+    {
+        var idx = this.deltaIndex;
+        var history = this.deltaHistory;
+        var max = this.deltaSmoothingMax;
+
+        if (this._coolDown > 0 || !this.inFocus)
+        {
+            this._coolDown--;
+
+            delta = Math.min(delta, this._target);
+        }
+
+        if (delta > this._min)
+        {
+            //  Probably super bad start time or browser tab context loss,
+            //  so use the last 'sane' delta value
+
+            delta = history[idx];
+
+            //  Clamp delta to min (in case history has become corrupted somehow)
+            delta = Math.min(delta, this._min);
+        }
+
+        //  Smooth out the delta over the previous X frames
+
+        //  add the delta to the smoothing array
+        history[idx] = delta;
+
+        //  adjusts the delta history array index based on the smoothing count
+        //  this stops the array growing beyond the size of deltaSmoothingMax
+        this.deltaIndex++;
+
+        if (this.deltaIndex > max)
+        {
+            this.deltaIndex = 0;
+        }
+
+        //  Loop the history array, adding the delta values together
+        var avg = 0;
+
+        for (var i = 0; i < max; i++)
+        {
+            avg += history[i];
+        }
+
+        //  Then divide by the array length to get the average delta
+        avg /= max;
+
+        return avg;
+    },
+
+    /**
+     * Update the estimate of the frame rate, `fps`. Every second, the number
+     * of frames that occurred in that second are included in an exponential
+     * moving average of all frames per second, with an alpha of 0.25. This
+     * means that more recent seconds affect the estimated frame rate more than
+     * older seconds.
+     *
+     * When a browser window is NOT minimized, but is covered up (i.e. you're using
+     * another app which has spawned a window over the top of the browser), then it
+     * will start to throttle the raf callback time. It waits for a while, and then
+     * starts to drop the frame rate at 1 frame per second until it's down to just over 1fps.
+     * So if the game was running at 60fps, and the player opens a new window, then
+     * after 60 seconds (+ the 'buffer time') it'll be down to 1fps, so rafin'g at 1Hz.
+     *
+     * When they make the game visible again, the frame rate is increased at a rate of
+     * approx. 8fps, back up to 60fps (or the max it can obtain)
+     *
+     * There is no easy way to determine if this drop in frame rate is because the
+     * browser is throttling raf, or because the game is struggling with performance
+     * because you're asking it to do too much on the device.
+     *
+     * Compute the new exponential moving average with an alpha of 0.25.
+     *
+     * @method Phaser.Core.TimeStep#updateFPS
+     * @since 3.60.0
+     *
+     * @param {number} time - The timestamp passed in from RequestAnimationFrame or setTimeout.
+     */
+    updateFPS: function (time)
+    {
+        this.actualFps = 0.25 * this.framesThisSecond + 0.75 * this.actualFps;
+        this.nextFpsUpdate = time + 1000;
+        this.framesThisSecond = 0;
+        this.fpsLimitTriggered = false;
+    },
+
+    /**
+     * The main step method with an fps limiter. This is called each time the browser updates, either by Request Animation Frame,
+     * or by Set Timeout. It is responsible for calculating the delta values, frame totals, cool down history and more.
+     * You generally should never call this method directly.
+     *
+     * @method Phaser.Core.TimeStep#stepLimitFPS
+     * @since 3.60.0
+     *
+     * @param {number} time - The timestamp passed in from RequestAnimationFrame or setTimeout.
+     */
+    stepLimitFPS: function (time)
+    {
+        this.now = time;
+
+        //  delta time (time is in ms)
+        //  Math.max because Chrome will sometimes give negative deltas
+        var delta = Math.max(0, time - this.lastTime);
+
+        this.rawDelta = delta;
+
+        //  Real-world timer advance
+        this.time += this.rawDelta;
+
+        if (this.smoothStep)
+        {
+            delta = this.smoothDelta(delta);
+        }
+
+        //  Set as the world delta value (after smoothing, if applied)
+        this.delta = delta;
+
+        if (time >= this.nextFpsUpdate)
+        {
+            this.updateFPS(time);
+        }
+
+        this.framesThisSecond++;
+
+        if (!this.fpsLimitTriggered && this.framesThisSecond >= this.fpsLimit)
+        {
+            this.callback(time, delta);
+
+            this.fpsLimitTriggered = true;
+        }
+
+        //  Shift time value over
+        this.lastTime = time;
+
+        this.frame++;
     },
 
     /**
@@ -493,130 +694,38 @@ var TimeStep = new Class({
      *
      * @method Phaser.Core.TimeStep#step
      * @since 3.0.0
+     *
+     * @param {number} time - The timestamp passed in from RequestAnimationFrame or setTimeout.
      */
-    step: function ()
+    step: function (time)
     {
-        //  Because the timestamp passed in from raf represents the beginning of the main thread frame that we’re currently in,
-        //  not the actual time now, and as we want to compare this time value against Event timeStamps and the like, we need a
-        //  more accurate one:
-
-        var time = window.performance.now();
-
         this.now = time;
 
-        var before = time - this.lastTime;
-
-        if (before < 0)
-        {
-            //  Because, Chrome.
-            before = 0;
-        }
-
-        this.rawDelta = before;
-
-        var idx = this.deltaIndex;
-        var history = this.deltaHistory;
-        var max = this.deltaSmoothingMax;
-
         //  delta time (time is in ms)
-        var dt = before;
+        //  Math.max because Chrome will sometimes give negative deltas
+        var delta = Math.max(0, time - this.lastTime);
 
-        //  Delta Average
-        var avg = before;
-
-        //  When a browser switches tab, then comes back again, it takes around 10 frames before
-        //  the delta time settles down so we employ a 'cooling down' period before we start
-        //  trusting the delta values again, to avoid spikes flooding through our delta average
-
-        if (this.smoothStep)
-        {
-            if (this._coolDown > 0 || !this.inFocus)
-            {
-                this._coolDown--;
-
-                dt = Math.min(dt, this._target);
-            }
-
-            if (dt > this._min)
-            {
-                //  Probably super bad start time or browser tab context loss,
-                //  so use the last 'sane' dt value
-
-                dt = history[idx];
-
-                //  Clamp delta to min (in case history has become corrupted somehow)
-                dt = Math.min(dt, this._min);
-            }
-
-            //  Smooth out the delta over the previous X frames
-
-            //  add the delta to the smoothing array
-            history[idx] = dt;
-
-            //  adjusts the delta history array index based on the smoothing count
-            //  this stops the array growing beyond the size of deltaSmoothingMax
-            this.deltaIndex++;
-
-            if (this.deltaIndex > max)
-            {
-                this.deltaIndex = 0;
-            }
-
-            //  Loop the history array, adding the delta values together
-            avg = 0;
-
-            for (var i = 0; i < max; i++)
-            {
-                avg += history[i];
-            }
-
-            //  Then divide by the array length to get the average delta
-            avg /= max;
-        }
-
-        //  Set as the world delta value
-        this.delta = avg;
+        this.rawDelta = delta;
 
         //  Real-world timer advance
         this.time += this.rawDelta;
 
-        // Update the estimate of the frame rate, `fps`. Every second, the number
-        // of frames that occurred in that second are included in an exponential
-        // moving average of all frames per second, with an alpha of 0.25. This
-        // means that more recent seconds affect the estimated frame rate more than
-        // older seconds.
-        //
-        // When a browser window is NOT minimized, but is covered up (i.e. you're using
-        // another app which has spawned a window over the top of the browser), then it
-        // will start to throttle the raf callback time. It waits for a while, and then
-        // starts to drop the frame rate at 1 frame per second until it's down to just over 1fps.
-        // So if the game was running at 60fps, and the player opens a new window, then
-        // after 60 seconds (+ the 'buffer time') it'll be down to 1fps, so rafin'g at 1Hz.
-        //
-        // When they make the game visible again, the frame rate is increased at a rate of
-        // approx. 8fps, back up to 60fps (or the max it can obtain)
-        //
-        // There is no easy way to determine if this drop in frame rate is because the
-        // browser is throttling raf, or because the game is struggling with performance
-        // because you're asking it to do too much on the device.
-
-        if (time > this.nextFpsUpdate)
+        if (this.smoothStep)
         {
-            //  Compute the new exponential moving average with an alpha of 0.25.
-            this.actualFps = 0.25 * this.framesThisSecond + 0.75 * this.actualFps;
-            this.nextFpsUpdate = time + 1000;
-            this.framesThisSecond = 0;
-            this.limitFpsTriggered = false;
+            delta = this.smoothDelta(delta);
+        }
+
+        //  Set as the world delta value (after smoothing, if applied)
+        this.delta = delta;
+
+        if (time >= this.nextFpsUpdate)
+        {
+            this.updateFPS(time);
         }
 
         this.framesThisSecond++;
 
-        if (this.limitFps === 0 || (!this.limitFpsTriggered && this.framesThisSecond >= this.limitFps))
-        {
-            this.callback(time, avg);
-
-            this.limitFpsTriggered = true;
-        }
+        this.callback(time, delta);
 
         //  Shift time value over
         this.lastTime = time;
@@ -632,7 +741,16 @@ var TimeStep = new Class({
      */
     tick: function ()
     {
-        this.step();
+        var now = window.performance.now();
+
+        if (this.hasFpsLimit)
+        {
+            this.stepLimitFPS(now);
+        }
+        else
+        {
+            this.step(now);
+        }
     },
 
     /**
@@ -662,20 +780,30 @@ var TimeStep = new Class({
      */
     wake: function (seamless)
     {
+        if (seamless === undefined) { seamless = false; }
+
+        var now = window.performance.now();
+
         if (this.running)
         {
             return;
         }
         else if (seamless)
         {
-            this.startTime += -this.lastTime + (this.lastTime + window.performance.now());
+            this.startTime += -this.lastTime + (this.lastTime + now);
         }
 
-        this.raf.start(this.step.bind(this), this.forceSetTimeOut, this._target);
+        var step = (this.hasFpsLimit) ? this.stepLimitFPS.bind(this) : this.step.bind(this);
+
+        this.raf.start(step, this.forceSetTimeOut, this._target);
 
         this.running = true;
 
-        this.step();
+        this.nextFpsUpdate = now + 1000;
+        this.framesThisSecond = 0;
+        this.fpsLimitTriggered = false;
+
+        this.tick();
     },
 
     /**
@@ -733,10 +861,11 @@ var TimeStep = new Class({
     {
         this.stop();
 
-        this.callback = NOOP;
+        this.raf.destroy();
 
         this.raf = null;
         this.game = null;
+        this.callback = null;
     }
 
 });
