4e63ed31194416adabf7df842a7ebb786a3ec46f
Jernej Habjan <jhabjan@marinexchange.com>
2022-09-11 10:37:45 +0200

    Revert "Improve stable sort performance"
    This reverts commit 8ba85962e4e80636d4dda5df77a27f5d408f4de3.

--- src/utils/array/StableSort.js
+++ src/utils/array/StableSort.js
@@ -21,6 +21,123 @@ function Compare (a, b)
 }
 
 /**
+ * Process the array contents.
+ *
+ * @ignore
+ *
+ * @param {array} array - The array to process.
+ * @param {function} compare - The comparison function.
+ *
+ * @return {array} - The processed array.
+ */
+function Process (array, compare)
+{
+    // Short-circuit when there's nothing to sort.
+    var len = array.length;
+
+    if (len <= 1)
+    {
+        return array;
+    }
+
+    // Rather than dividing input, simply iterate chunks of 1, 2, 4, 8, etc.
+    // Chunks are the size of the left or right hand in merge sort.
+    // Stop when the left-hand covers all of the array.
+    var buffer = new Array(len);
+
+    for (var chk = 1; chk < len; chk *= 2)
+    {
+        RunPass(array, compare, chk, buffer);
+
+        var tmp = array;
+
+        array = buffer;
+
+        buffer = tmp;
+    }
+
+    return array;
+}
+
+/**
+ * Run a single pass with the given chunk size.
+ *
+ * @ignore
+ *
+ * @param {array} arr - The array to run the pass on.
+ * @param {function} comp - The comparison function.
+ * @param {number} chk - The number of iterations.
+ * @param {array} result - The array to store the result in.
+ */
+function RunPass (arr, comp, chk, result)
+{
+    var len = arr.length;
+    var i = 0;
+
+    // Step size / double chunk size.
+    var dbl = chk * 2;
+
+    // Bounds of the left and right chunks.
+    var l, r, e;
+
+    // Iterators over the left and right chunk.
+    var li, ri;
+
+    // Iterate over pairs of chunks.
+    for (l = 0; l < len; l += dbl)
+    {
+        r = l + chk;
+        e = r + chk;
+
+        if (r > len)
+        {
+            r = len;
+        }
+
+        if (e > len)
+        {
+            e = len;
+        }
+
+        // Iterate both chunks in parallel.
+        li = l;
+        ri = r;
+
+        while (true)
+        {
+            // Compare the chunks.
+            if (li < r && ri < e)
+            {
+                // This works for a regular `sort()` compatible comparator,
+                // but also for a simple comparator like: `a > b`
+                if (comp(arr[li], arr[ri]) <= 0)
+                {
+                    result[i++] = arr[li++];
+                }
+                else
+                {
+                    result[i++] = arr[ri++];
+                }
+            }
+            else if (li < r)
+            {
+                // Nothing to compare, just flush what's left.
+                result[i++] = arr[li++];
+            }
+            else if (ri < e)
+            {
+                result[i++] = arr[ri++];
+            }
+            else
+            {
+                // Both iterators are at the chunk ends.
+                break;
+            }
+        }
+    }
+}
+
+/**
  * An in-place stable array sort, because `Array#sort()` is not guaranteed stable.
  *
  * This is an implementation of merge sort, without recursion.
@@ -39,7 +156,15 @@ var StableSort = function (array, compare)
 {
     if (compare === undefined) { compare = Compare; }
 
-    return array.sort(compare);
+    var result = Process(array, compare);
+
+    // This simply copies back if the result isn't in the original array, which happens on an odd number of passes.
+    if (result !== array)
+    {
+        RunPass(result, null, array.length, array);
+    }
+
+    return array;
 };
 
 module.exports = StableSort;
