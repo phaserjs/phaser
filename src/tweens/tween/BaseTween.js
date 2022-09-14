0b14cc48981e014f4db201662ef9bd6a5a30267a
Richard Davey <rich@photonstorm.com>
2022-09-14 23:22:34 +0100

    Update BaseTween.js

--- src/tweens/tween/BaseTween.js
+++ src/tweens/tween/BaseTween.js
@@ -11,7 +11,11 @@ var TWEEN_CONST = require('./const');
 
 /**
  * @classdesc
- * TODO
+ * As the name implies, this is the base Tween class that both the Tween and TweenChain
+ * inherit from. It contains shared properties and methods common to both types of Tween.
+ *
+ * Typically you would never instantiate this class directly, although you could certainly
+ * use it to create your own variation of Tweens from.
  *
  * @class BaseTween
  * @memberof Phaser.Tweens
