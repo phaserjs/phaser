4b14d3d6c6a3a6c70f39a340db2f599ecdca3588
Richard Davey <rich@photonstorm.com>
2022-09-29 17:42:40 +0100

    Create StampConfig.js

---
+++ src/textures/typedefs/StampConfig.js
@@ -0,0 +1,15 @@
+/**
+ * An object containing the position and color data for a single pixel in a CanvasTexture.
+ *
+ * @typedef {object} Phaser.Types.Textures.StampConfig
+ * @since 3.60.0
+ *
+ * @property {number} [alpha=1] -  The alpha value used by the stamp.
+ * @property {number} [tint=0xffffff] -  The tint color value used by the stamp. WebGL only.
+ * @property {number} [angle=0] - The angle of the stamp in degrees. Rotation takes place around its origin.
+ * @property {number} [rotation=0] - The rotation of the stamp in radians. Rotation takes place around its origin.
+ * @property {number} [scaleX=1] - The horizontal scale of the stamp.
+ * @property {number} [scaleY=1] - The vertical scale of the stamp.
+ * @property {number} [originX=0.5] - The horizontal origin of the stamp. 0 is the left, 0.5 is the center and 1 is the right.
+ * @property {number} [originY=0.5] - The vertical origin of the stamp. 0 is the top, 0.5 is the center and 1 is the bottom.
+ */
