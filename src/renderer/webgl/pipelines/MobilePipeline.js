f0d3588f7b1bd9a54a75ea820b3890d27a090592
Richard Davey <rich@photonstorm.com>
2022-10-05 00:59:04 +0100

    Use batch system to avoid bufferSubData spamming - massively improves performance!

--- src/renderer/webgl/pipelines/MobilePipeline.js
+++ src/renderer/webgl/pipelines/MobilePipeline.js
@@ -5,6 +5,7 @@
  */
 
 var Class = require('../../../utils/Class');
+var Events = require('./events');
 var GetFastValue = require('../../../utils/object/GetFastValue');
 var MultiPipeline = require('./MultiPipeline');
 var ShaderSourceFS = require('../shaders/Mobile-frag.js');
@@ -86,9 +87,14 @@ var MobilePipeline = new Class({
                 normalized: true
             }
         ]);
-        config.forceZero = true;
 
         MultiPipeline.call(this, config);
+
+        //  vertexCapacity / 6 = batchSize
+
+        this.batch = [];
+
+        this.currentTexture = null;
     },
 
     /**
@@ -105,6 +111,110 @@ var MobilePipeline = new Class({
         WebGLPipeline.prototype.boot.call(this);
 
         this.set1i('uMainSampler', 0);
+    },
+
+    setGameObject: function (gameObject, frame)
+    {
+        if (frame === undefined) { frame = gameObject.frame; }
+
+        this.pushBatch(frame.source.glTexture);
+
+        return 0;
+    },
+
+    setTexture2D: function (texture)
+    {
+        if (texture === undefined) { texture = this.renderer.whiteTexture.glTexture; }
+
+        this.pushBatch(texture);
+
+        return 0;
+    },
+
+    pushBatch: function (texture)
+    {
+        if (texture !== this.currentTexture)
+        {
+            this.currentTexture = texture;
+
+            this.currentBatch = {
+                start: this.vertexCount,
+                count: 0,
+                texture: texture
+            };
+
+            this.batch.push(this.currentBatch);
+        }
+    },
+
+    onPreRender: function ()
+    {
+        this.batch.length = 0;
+        this.currentTexture = null;
+        this.currentBatch = null;
+    },
+
+    onBatch: function ()
+    {
+        this.currentBatch.count = (this.vertexCount - this.currentBatch.start);
+    },
+
+    flush: function (isPostFlush)
+    {
+        if (isPostFlush === undefined) { isPostFlush = false; }
+
+        if (this.vertexCount > 0)
+        {
+            this.emit(Events.BEFORE_FLUSH, this, isPostFlush);
+
+            this.onBeforeFlush(isPostFlush);
+
+            var gl = this.gl;
+            var vertexCount = this.vertexCount;
+            var vertexSize = this.currentShader.vertexSize;
+            var topology = this.topology;
+
+            if (this.active)
+            {
+                this.setVertexBuffer();
+
+                if (vertexCount === this.vertexCapacity)
+                {
+                    gl.bufferData(gl.ARRAY_BUFFER, this.vertexData, gl.DYNAMIC_DRAW);
+                }
+                else
+                {
+                    gl.bufferSubData(gl.ARRAY_BUFFER, 0, this.bytes.subarray(0, vertexCount * vertexSize));
+                }
+
+                gl.activeTexture(gl.TEXTURE0);
+
+                var batch = this.batch;
+
+                for (var i = 0; i < batch.length; i++)
+                {
+                    var texture = batch[i].texture;
+                    var start = batch[i].start;
+                    var count = batch[i].count;
+
+                    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+                    gl.drawArrays(topology, start, count);
+                }
+            }
+
+            this.vertexCount = 0;
+
+            this.batch.length = 0;
+            this.currentBatch = null;
+            this.currentTexture = null;
+
+            this.emit(Events.AFTER_FLUSH, this, isPostFlush);
+
+            this.onAfterFlush(isPostFlush);
+        }
+
+        return this;
     }
 
 });
