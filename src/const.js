2e2c6528b664410c98ab9ad932b4e5fe6d994442
Richard Davey <rich@photonstorm.com>
2022-10-09 18:25:34 +0100

    Preparing for beta 12

--- src/const.js
+++ src/const.js
@@ -20,7 +20,7 @@ var CONST = {
      * @type {string}
      * @since 3.0.0
      */
-    VERSION: '3.60.0-beta.11',
+    VERSION: '3.60.0-beta.12',
 
     BlendModes: require('./renderer/BlendModes'),
 
