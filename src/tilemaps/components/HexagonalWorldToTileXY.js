949852d8d5f88a372cf985d9f98d32ab09822f28
Richard Davey <rich@photonstorm.com>
2022-11-24 23:31:49 +0000

    Recoded the point conversion math in the `HexagonalWorldToTileXY` function as it was incorrect. Now detects any dimension hexagon correctly. Fix #5608

--- src/tilemaps/components/HexagonalWorldToTileXY.js
+++ src/tilemaps/components/HexagonalWorldToTileXY.js
@@ -6,43 +6,6 @@
 
 var Vector2 = require('../../math/Vector2');
 
-var PointyTop = {
-    b0: Math.sqrt(3) / 3,
-    b1: -1 / 3,
-    b2: 0,
-    b3: 2 / 3
-};
-
-function Hex (q, r, s)
-{
-    return { q: q, r: r, s: s };
-}
-
-function hexRound (h)
-{
-    var qi = Math.round(h.q);
-    var ri = Math.round(h.r);
-    var si = Math.round(h.s);
-    var qDiff = Math.abs(qi - h.q);
-    var rDiff = Math.abs(ri - h.r);
-    var sDiff = Math.abs(si - h.s);
-
-    if (qDiff > rDiff && qDiff > sDiff)
-    {
-        qi = -ri - si;
-    }
-    else if (rDiff > sDiff)
-    {
-        ri = -qi - si;
-    }
-    else
-    {
-        si = -qi - ri;
-    }
-
-    return Hex(qi, ri, si);
-}
-
 /**
  * Converts from world XY coordinates (pixels) to hexagonal tile XY coordinates (tile units), factoring in the
  * layer's position, scale and scroll. This will return a new Vector2 object or update the given
@@ -82,18 +45,43 @@ var HexagonalWorldToTileXY = function (worldX, worldY, snapToFloor, point, camer
         tileHeight *= tilemapLayer.scaleY;
     }
 
-    var size = { x: PointyTop.b0 * tileWidth, y: tileHeight / 2 };
-    var origin = { x: tileWidth / 2, y: tileHeight / 2 };
+    //  Hard-coded values for Pointy-Top Hexagons only
+    var b0 = 0.5773502691896257; // Math.sqrt(3) / 3
+    var b1 = -0.3333333333333333; // -1 / 3
+    var b2 = 0;
+    var b3 = 0.6666666666666666; // 2 / 3
 
-    var pt = new Vector2((worldX - origin.x) / size.x, (worldY - origin.y) / size.y);
+    var tileWidthHalf = tileWidth / 2;
+    var tileHeightHalf = tileHeight / 2;
 
-    var q = PointyTop.b0 * pt.x + PointyTop.b1 * pt.y;
-    var r = PointyTop.b2 * pt.x + PointyTop.b3 * pt.y;
-    var h = Hex(q, r, -q - r);
-    var hr = hexRound(h);
+    var px = (worldX - tileWidthHalf) / (b0 * tileWidth);
+    var py = (worldY - tileHeightHalf) / tileHeightHalf;
 
-    var y = hr.r;
-    var x = (y % 2 === 0) ? (hr.r / 2) + hr.q : (hr.r / 2) + hr.q - 0.5;
+    var q = b0 * px + b1 * py;
+    var r = b2 * px + b3 * py;
+    var s = -q - r;
+
+    var qi = Math.round(q);
+    var ri = Math.round(r);
+    var si = Math.round(s);
+
+    var qDiff = Math.abs(qi - q);
+    var rDiff = Math.abs(ri - r);
+    var sDiff = Math.abs(si - s);
+
+    if (qDiff > rDiff && qDiff > sDiff)
+    {
+        qi = -ri - si;
+    }
+    else if (rDiff > sDiff)
+    {
+        ri = -qi - si;
+    }
+
+    var y = ri;
+
+    //  odd-r implementation:
+    var x = (y % 2 === 0) ? (ri / 2) + qi : (ri / 2) + qi - 0.5;
 
     return point.set(x, y);
 };
