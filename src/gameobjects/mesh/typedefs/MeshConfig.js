0d9bd568e51ef4f1fea1ab371658ff0cc4211bf7
Richard Davey <rich@photonstorm.com>
2022-12-08 22:43:28 +0000

    Correct type

--- src/gameobjects/mesh/typedefs/MeshConfig.js
+++ src/gameobjects/mesh/typedefs/MeshConfig.js
@@ -3,7 +3,7 @@
  * @extends Phaser.Types.GameObjects.GameObjectConfig
  * @since 3.0.0
  *
- * @property {string} [key] - The key, or instance of the Texture this Game Object will use to render with, as stored in the Texture Manager.
+ * @property {string|Phaser.Textures.Texture} [key] - The key, or instance of the Texture this Game Object will use to render with, as stored in the Texture Manager.
  * @property {string|number} [frame] - An optional frame from the Texture this Game Object is rendering with.
  * @property {number[]} [vertices] - The vertices array. Either `xy` pairs, or `xyz` if the `containsZ` parameter is `true`.
  * @property {number[]} [uvs] - The UVs pairs array.
