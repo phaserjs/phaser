9140bedd34bb2b08f9e6277fc5d77543f3aa4462
Richard Davey <rich@photonstorm.com>
2022-12-09 16:01:02 +0000

    Added missing lineSpacing property

--- src/gameobjects/bitmaptext/typedefs/JSONBitmapText.js
+++ src/gameobjects/bitmaptext/typedefs/JSONBitmapText.js
@@ -7,5 +7,6 @@
  * @property {string} text - The text that this Bitmap Text displays.
  * @property {number} fontSize - The size of the font.
  * @property {number} letterSpacing - Adds / Removes spacing between characters.
+ * @property {number} lineSpacing - Adds / Removes spacing between lines in multi-line text.
  * @property {number} align - The alignment of the text in a multi-line BitmapText object.
  */
