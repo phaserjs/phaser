fc046b4d5b0a1eaa55c1b47876827ce0e513c195
Richard Davey <rich@photonstorm.com>
2022-12-30 18:58:30 +0000

    Create DeathZoneObject.js

---
+++ src/gameobjects/particles/typedefs/DeathZoneObject.js
@@ -0,0 +1,4 @@
+/**
+ * @typedef {Phaser.GameObjects.Particles.Zones.DeathZone|Phaser.Types.GameObjects.Particles.ParticleEmitterDeathZoneConfig|Phaser.Geom.Circle|Phaser.Geom.Ellipse|Phaser.Geom.Polygon|Phaser.Geom.Triangle} Phaser.Types.GameObjects.Particles.DeathZoneObject
+ * @since 3.60.0
+ */
