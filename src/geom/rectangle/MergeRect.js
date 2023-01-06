e4a4f30c27ed90d314a8aa71f6c38ba1a327f6f1
Richard Davey <rich@photonstorm.com>
2023-01-06 13:54:52 +0000

    Restored previous version

--- src/geom/rectangle/MergeRect.js
+++ src/geom/rectangle/MergeRect.js
@@ -5,7 +5,8 @@
  */
 
 /**
- * Merges the source rectangle into the target rectangle and returns the target rectangle.
+ * Merges the source rectangle into the target rectangle and returns the target.
+ * Neither rectangle should have a negative width or height.
  *
  * @function Phaser.Geom.Rectangle.MergeRect
  * @since 3.0.0
@@ -19,33 +20,17 @@
  */
 var MergeRect = function (target, source)
 {
-    // var minX = Math.min(target.x, source.x);
-    // var minY = Math.min(target.y, source.y);
+    var minX = Math.min(target.x, source.x);
+    var maxX = Math.max(target.right, source.right);
 
-    // target.x = minX;
-    // target.y = minY;
+    target.x = minX;
+    target.width = maxX - minX;
 
-    var x = Math.min(target.x, source.x);
-    var y = Math.min(target.y, source.y);
+    var minY = Math.min(target.y, source.y);
+    var maxY = Math.max(target.bottom, source.bottom);
 
-    var r = Math.max(target.right, source.right);
-    var b = Math.max(target.bottom, source.bottom);
-
-    var w = Math.abs(r - x);
-    var h = Math.abs(b - y);
-
-    target.setTo(x, y, w, h);
-
-    // var maxX = Math.max(target.right, source.right);
-
-    // target.x = minX;
-    // target.width = Math.abs(maxX - minX);
-
-    // var minY = Math.min(target.y, source.y);
-    // var maxY = Math.max(target.bottom, source.bottom);
-
-    // target.y = minY;
-    // target.height = Math.abs(maxY - minY);
+    target.y = minY;
+    target.height = maxY - minY;
 
     return target;
 };
