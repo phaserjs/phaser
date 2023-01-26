9ff5dc5e7759948baed11822b2fbfe8727afdc30
Alex West <alxwest@gmail.com>
2023-01-26 16:22:53 +0000

    Set defaults to web audio's standards.

--- src/sound/typedefs/SpatialSoundConfig.js
+++ src/sound/typedefs/SpatialSoundConfig.js
@@ -7,15 +7,15 @@
  *
  * @property {number} [x=0] - X coordinate for the source of the sound.
  * @property {number} [y=0] - Y coordinate for the source of the sound.
- * @property {number} [z=300] - Z coordinate for the source of the sound.
+ * @property {number} [z=0] - Z coordinate for the source of the sound.
  * @property {('equalpower'|'HRTF')} [panningModel='equalpower'] - The spacialization algorithm that's used to position the audio in 3D space.
- * @property {('linear'|'inverse'|'exponential')} [distanceModel='linear'] - Different algorithms which are used to reduce the volume of the audio source as it moves away from the listener.
+ * @property {('linear'|'inverse'|'exponential')} [distanceModel='inverse'] - Different algorithms which are used to reduce the volume of the audio source as it moves away from the listener.
  * @property {number} [orientationX=0] - X orientation of the sound.
  * @property {number} [orientationY=0] - Y orientation of the sound.
  * @property {number} [orientationZ=-1.0] - Z orientation of the sound.
  * @property {number} [refDistance=1] - Used by the distance models.
  * @property {number} [maxDistance =10000] - Maximum distance between the source and the listener.
- * @property {number} [rolloffFactor=1] - How quickly does the volume reduce as the panner moves away from the listener.
+ * @property {number} [rolloffFactor=1] - How quickly the volume is reduced as the panner moves away from the listener.
  * @property {number} [coneInnerAngle=360] - Specifies where the sound emanates from.
  * @property {number} [coneOuterAngle=360] - Specifies where the volume starts to drop away.
  * @property {number} [coneOuterGain=0] - Specifies the amount volume is reduced by.
