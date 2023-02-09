f21834c50b49daa0da6af71accde17ed7736a2ca
Emil Schnedler Vad <emil.vad@ahoot.dk>
2023-02-09 14:33:28 +0100

    container#getBounds now also support getTextBounds for bounds calculations

--- src/gameobjects/container/Container.js
+++ src/gameobjects/container/Container.js
@@ -412,16 +412,25 @@ var Container = new Class({
                 if (entry.getBounds)
                 {
                     entry.getBounds(tempRect);
+                }
+                else if (entry.getTextBounds)
+                {
+                    var textBounds = entry.getTextBounds().global;
+                    tempRect.setTo(textBounds.x, textBounds.y, textBounds.width, textBounds.height);
+                }
+                else
+                {
+                    continue;
+                }
 
-                    if (!hasSetFirst)
-                    {
-                        output.setTo(tempRect.x, tempRect.y, tempRect.width, tempRect.height);
-                        hasSetFirst = true;
-                    }
-                    else
-                    {
-                        Union(tempRect, output, output);
-                    }
+                if (!hasSetFirst)
+                {
+                    output.setTo(tempRect.x, tempRect.y, tempRect.width, tempRect.height);
+                    hasSetFirst = true;
+                }
+                else
+                {
+                    Union(tempRect, output, output);
                 }
             }
         }
