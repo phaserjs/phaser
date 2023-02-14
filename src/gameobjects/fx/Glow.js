4f5da3b3b819fe2d38dda5428c08dba783f1d7c6
Richard Davey <rich@photonstorm.com>
2023-02-14 01:30:23 +0000

    Added jsdocs

--- src/gameobjects/fx/Glow.js
+++ src/gameobjects/fx/Glow.js
@@ -29,15 +29,59 @@ var Glow = new Class({
     {
         BaseFX.call(this, FX_CONST.GLOW, gameObject);
 
+        /**
+         * The distance of the glow.
+         *
+         * This must be an integer and can be between 0 and 128 inclusive.
+         *
+         * The higher the value, the more iterations the shader takes
+         * when drawing the glow.
+         *
+         * @name Phaser.GameObjects.FX.Glow#distance
+         * @type {number}
+         * @since 3.60.0
+         */
         this.distance = 16;
+
+        /**
+         * The strength of the glow outward from the edge of the Sprite.
+         *
+         * @name Phaser.GameObjects.FX.Glow#outerStrength
+         * @type {number}
+         * @since 3.60.0
+         */
         this.outerStrength = 4;
+
+        /**
+         * The strength of the glow inward from the edge of the Sprite.
+         *
+         * @name Phaser.GameObjects.FX.Glow#innerStrength
+         * @type {number}
+         * @since 3.60.0
+         */
         this.innerStrength = 0;
+
+        /**
+         * If `true` on the glow is drawn, not the texture itself.
+         *
+         * @name Phaser.GameObjects.FX.Glow#knockout
+         * @type {number}
+         * @since 3.60.0
+         */
         this.knockout = false;
-        this._color = [ 1, 1, 1, 1 ];
+
+        /**
+         * A 4 element array of gl color values.
+         *
+         * @name Phaser.GameObjects.FX.Glow#glcolor
+         * @type {number[]}
+         * @since 3.60.0
+         */
+        this.glcolor = [ 1, 1, 1, 1 ];
     },
 
     /**
-     * The color of the glow.
+     * The color of the glow as a number value.
      *
      * @name Phaser.GameObjects.FX.Glow#color
      * @type {number}
@@ -47,14 +91,14 @@ var Glow = new Class({
 
         get: function ()
         {
-            var color = this._color;
+            var color = this.glcolor;
 
             return (((color[0] * 255) << 16) + ((color[1] * 255) << 8) + (color[2] * 255 | 0));
         },
 
         set: function (value)
         {
-            var color = this._color;
+            var color = this.glcolor;
 
             color[0] = ((value >> 16) & 0xFF) / 255;
             color[1] = ((value >> 8) & 0xFF) / 255;
