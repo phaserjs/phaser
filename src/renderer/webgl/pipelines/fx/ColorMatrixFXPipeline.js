3fb415b926c08957e4239c992c4a34603cf79a64
Richard Davey <rich@photonstorm.com>
2023-02-15 18:50:07 +0000

    Create ColorMatrixFXPipeline.js

---
+++ src/renderer/webgl/pipelines/fx/ColorMatrixFXPipeline.js
@@ -0,0 +1,34 @@
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2013-2023 Photon Storm Ltd.
+ * @license      {@link https://opensource.org/licenses/MIT|MIT License}
+ */
+
+var Class = require('../../../../utils/Class');
+var PostFXPipeline = require('../PostFXPipeline');
+
+var ColorMatrixFXPipeline = new Class({
+
+    Extends: PostFXPipeline,
+
+    initialize:
+
+    function ColorMatrixFXPipeline (game)
+    {
+        PostFXPipeline.call(this, {
+            game: game
+        });
+    },
+
+    onDraw: function (source)
+    {
+        var target = this.fullFrame1;
+
+        this.drawFrame(source, target);
+
+        this.copyToGame(target);
+    }
+
+});
+
+module.exports = ColorMatrixFXPipeline;
