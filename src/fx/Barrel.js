1b31ec12c1d6cf6576a03e11dc629148c0dcf03b
Richard Davey <rich@photonstorm.com>
2023-02-27 13:47:19 +0000

    Update Barrel.js

--- src/fx/Barrel.js
+++ src/fx/Barrel.js
@@ -18,7 +18,7 @@ var FX_CONST = require('./const');
  * @since 3.60.0
  *
  * @param {Phaser.GameObjects.GameObject} gameObject - A reference to the Game Object that has this fx.
- * @param {number} [amount=1] - The amount of distortion applied to the barrel effect. Typically keep this within the range 0 (no distortion) to 1.
+ * @param {number} [amount=1] - The amount of distortion applied to the barrel effect. A value of 1 is no distortion. Typically keep this within +- 1.
  */
 var Barrel = new Class({
 
@@ -35,7 +35,7 @@ var Barrel = new Class({
         /**
          * The amount of distortion applied to the barrel effect.
          *
-         * Typically keep this within the range 0 (no distortion) to 1.
+         * Typically keep this within the range 1 (no distortion) to +- 1.
          *
          * @name Phaser.FX.Barrel#amount
          * @type {number}
