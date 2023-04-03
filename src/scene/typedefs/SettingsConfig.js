7864024cbf63fd6e6a9bbc488cce87ad8953c6f9
samme <samme@users.noreply.github.com>
2023-04-03 16:03:51 -0700

    Docs: correct `cameras` types
    Phaser.Types.Scenes.SettingsConfig, Phaser.Types.Scenes.SettingsObject

--- src/scene/typedefs/SettingsConfig.js
+++ src/scene/typedefs/SettingsConfig.js
@@ -6,7 +6,7 @@
  * @property {boolean} [active=false] - Does the Scene start as active or not? An active Scene updates each step.
  * @property {boolean} [visible=true] - Does the Scene start as visible or not? A visible Scene renders each step.
  * @property {(false|Phaser.Types.Loader.FileTypes.PackFileSection)} [pack=false] - Files to be loaded before the Scene begins.
- * @property {?(Phaser.Types.Cameras.Scene2D.JSONCamera|Phaser.Types.Cameras.Scene2D.JSONCamera[])} [cameras=null] - An optional Camera configuration object.
+ * @property {?(Phaser.Types.Cameras.Scene2D.CameraConfig|Phaser.Types.Cameras.Scene2D.CameraConfig[])} [cameras=null] - An optional Camera configuration object.
  * @property {Object.<string, string>} [map] - Overwrites the default injection map for a scene.
  * @property {Object.<string, string>} [mapAdd] - Extends the injection map for a scene.
  * @property {Phaser.Types.Core.PhysicsConfig} [physics={}] - The physics configuration object for the Scene.
