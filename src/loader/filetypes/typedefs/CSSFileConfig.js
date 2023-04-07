49511838f439d5e20b88bf6cc6248451059d5d74
Richard Davey <rich@photonstorm.com>
2023-04-07 13:45:14 +0100

    Update CSSFileConfig.js

--- src/loader/filetypes/typedefs/CSSFileConfig.js
+++ src/loader/filetypes/typedefs/CSSFileConfig.js
@@ -3,6 +3,6 @@
  *
  * @property {string} key - The key of the file. Must be unique within the Loader.
  * @property {string} [url] - The absolute or relative URL to load the file from.
- * @property {string} [extension='js'] - The default file extension to use if no url is provided.
+ * @property {string} [extension='css'] - The default file extension to use if no url is provided.
  * @property {Phaser.Types.Loader.XHRSettingsObject} [xhrSettings] - Extra XHR Settings specifically for this file.
  */
