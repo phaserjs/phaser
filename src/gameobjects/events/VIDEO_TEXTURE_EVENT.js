950fabe3061e9c6fb618cc3ad88c329fbb298631
Richard Davey <rich@photonstorm.com>
2023-04-12 00:00:58 +0100

    Added texture ready event

---
+++ src/gameobjects/events/VIDEO_TEXTURE_EVENT.js
@@ -0,0 +1,26 @@
+/**
+ * @author       Richard Davey <rich@photonstorm.com>
+ * @copyright    2013-2023 Photon Storm Ltd.
+ * @license      {@link https://opensource.org/licenses/MIT|MIT License}
+ */
+
+/**
+ * The Video Game Object Texture Ready Event.
+ *
+ * This event is dispatched by a Video Game Object when it has finished creating its texture.
+ *
+ * This happens when the video has finished loading enough data for its first frame.
+ *
+ * If you wish to use the Video texture elsewhere in your game, such as as a Sprite texture,
+ * then you should listen for this event first, before creating the Sprites that use it.
+ *
+ * Listen for it from a Video Game Object instance using `Video.on('textureready', listener)`.
+ *
+ * @event Phaser.GameObjects.Events#VIDEO_TEXTURE
+ * @type {string}
+ * @since 3.60.0
+ *
+ * @param {Phaser.GameObjects.Video} video - The Video Game Object that emitted the event.
+ * @param {Phaser.Textures.Texture} texture - The Texture that was created.
+ */
+module.exports = 'textureready';
