cd020af107d1714200ace47af9fe89ab62dd7e69
Richard Davey <rich@photonstorm.com>
2023-07-21 17:13:26 +0100

    Guard against duplicate removal. Fix #6539

--- src/tweens/tween/BaseTween.js
+++ src/tweens/tween/BaseTween.js
@@ -479,7 +479,10 @@ var BaseTween = new Class({
      */
     remove: function ()
     {
-        this.parent.remove(this);
+        if (this.parent)
+        {
+            this.parent.remove(this);
+        }
 
         return this;
     },
@@ -505,7 +508,7 @@ var BaseTween = new Class({
      */
     stop: function ()
     {
-        if (!this.isRemoved() && !this.isPendingRemove() && !this.isDestroyed())
+        if (this.parent && !this.isRemoved() && !this.isPendingRemove() && !this.isDestroyed())
         {
             this.dispatchEvent(Events.TWEEN_STOP, 'onStop');
 
