4cfa8c42870b75641b092435149752390640ca23
Alvaro Estrada <aestrada@greentube.com>
2023-08-24 13:57:52 -0600

    Update ParticleEmitterFrameConfig.js
    Changed the configuration to better reflect what is accepted by the function

--- src/gameobjects/particles/typedefs/ParticleEmitterFrameConfig.js
+++ src/gameobjects/particles/typedefs/ParticleEmitterFrameConfig.js
@@ -2,7 +2,7 @@
  * @typedef {object} Phaser.Types.GameObjects.Particles.ParticleEmitterFrameConfig
  * @since 3.0.0
  *
- * @property {number|number[]|string|string[]|Phaser.Textures.Frame|Phaser.Textures.Frame[]} [frames] - One or more texture frames.
+ * @property {number[]|string[]|Phaser.Textures.Frame[]} [frames] - Array of texture frames.
  * @property {boolean} [cycle] - Whether texture frames will be assigned consecutively (true) or at random (false).
  * @property {number} [quantity] - The number of consecutive particles receiving each texture frame, when `cycle` is true.
  */
