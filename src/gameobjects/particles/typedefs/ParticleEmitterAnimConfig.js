6d22019f2658b8569361f51f693620d3f5f4cbf9
Richard Davey <rich@photonstorm.com>
2023-09-07 13:21:51 +0100

    Update ParticleEmitterAnimConfig.js

--- src/gameobjects/particles/typedefs/ParticleEmitterAnimConfig.js
+++ src/gameobjects/particles/typedefs/ParticleEmitterAnimConfig.js
@@ -3,6 +3,6 @@
  * @since 3.60.0
  *
  * @property {string|string[]} [anims] - One or more animations names.
- * @property {boolean} [cycle] - Whether animations will be assigned consecutively (true) or at random (false).
- * @property {number} [quantity] - The number of consecutive particles receiving each animation, when `cycle` is true.
+ * @property {boolean} [cycle=false] - Whether animations will be assigned consecutively (true) or at random (false).
+ * @property {number} [quantity=1] - The number of consecutive particles receiving each animation, when `cycle` is true.
  */
