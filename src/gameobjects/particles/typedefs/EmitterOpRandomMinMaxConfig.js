9d33475c5ddcddf1098d90e7e1a7f4f549830fa6
Alvaro Estrada <aestrada@greentube.com>
2023-09-07 20:53:17 -0600

    Changed the obligatory definition for an optional definition

--- src/gameobjects/particles/typedefs/EmitterOpRandomMinMaxConfig.js
+++ src/gameobjects/particles/typedefs/EmitterOpRandomMinMaxConfig.js
@@ -6,5 +6,5 @@
  *
  * @property {number} min - The minimum value.
  * @property {number} max - The maximum value.
- * @property {boolean|undefined} int - If true, this works like {@link EmitterOpRandomConfig}.
+ * @property {boolean} [int] - If true, this works like {@link EmitterOpRandomConfig}.
  */
