76a6786fbae82d63790a41fe4229b93df58d72b8
samme <samme@users.noreply.github.com>
2023-09-23 08:12:22 -0700

    Docs: change `persists` to `persist`

--- src/tweens/TweenManager.js
+++ src/tweens/TweenManager.js
@@ -413,7 +413,7 @@ var TweenManager = new Class({
      *
      * The tweens are played in order, from start to finish. You can optionally set the chain
      * to repeat as many times as you like. Once the chain has finished playing, or repeating if set,
-     * all tweens in the chain will be destroyed automatically. To override this, set the 'persists'
+     * all tweens in the chain will be destroyed automatically. To override this, set the `persist`
      * argument to 'true'.
      *
      * Playback will start immediately unless the _first_ Tween has been configured to be paused.
