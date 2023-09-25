1ce053c7a7c37ccee2711505ce6018cf1866240f
Alvaro Estrada <aestrada@greentube.com>
2023-09-24 23:51:44 -0600

    Removed interface EmitterOpRandomStartEndConfig
    The interface EmitterOpRandomStartEndConfig, even though it is another path to configure a random emit value, it does not fully describe that all the options for the EmitterOpEaseConfig are available to use. Instead it is better to add to the description of the EmitterOpEaseConfig that there is a random start value option available.

--- src/gameobjects/particles/typedefs/EmitterOpEaseConfig.js
+++ src/gameobjects/particles/typedefs/EmitterOpEaseConfig.js
@@ -6,6 +6,7 @@
  *
  * @property {number} start - The starting value.
  * @property {number} end - The ending value.
+ * @property {boolean} [random] - If true, the particle starts with a minimum random value between the start and end values.
  * @property {(string|function)} [ease='Linear'] - The ease to find. This can be either a string from the EaseMap, or a custom function.
  * @property {number[]} [easeParams] - An optional array of ease parameters to go with the ease.
  */
