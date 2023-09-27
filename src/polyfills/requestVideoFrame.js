1be82975f7284950550a039e8e4085fe13595a46
Richard Davey <rich@photonstorm.com>
2023-09-27 17:18:10 +0100

    The Request Video Frame polyfill will now check first to see if the browser supports `HTMLVideoElement` before trying to inspect its prototype. This should help in non-browser environments.

--- src/polyfills/requestVideoFrame.js
+++ src/polyfills/requestVideoFrame.js
@@ -1,6 +1,6 @@
 //  From https://github.com/ThaUnknown/rvfc-polyfill
 
-if (!('requestVideoFrameCallback' in HTMLVideoElement.prototype) && 'getVideoPlaybackQuality' in HTMLVideoElement.prototype)
+if (HTMLVideoElement && !('requestVideoFrameCallback' in HTMLVideoElement.prototype) && 'getVideoPlaybackQuality' in HTMLVideoElement.prototype)
 {
     HTMLVideoElement.prototype._rvfcpolyfillmap = {}
     HTMLVideoElement.prototype.requestVideoFrameCallback = function (callback) {
