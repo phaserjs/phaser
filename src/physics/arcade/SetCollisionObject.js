e25e2489c42618db149342ad1958de371c4c7db2
Richard Davey <rich@photonstorm.com>
2023-10-04 12:49:27 +0100

    Update SetCollisionObject.js

--- src/physics/arcade/SetCollisionObject.js
+++ src/physics/arcade/SetCollisionObject.js
@@ -1,6 +1,5 @@
 /**
  * @author       Richard Davey <rich@photonstorm.com>
- * @author       Benjamin D. Richards <benjamindrichards@gmail.com>
  * @copyright    2013-2023 Photon Storm Ltd.
  * @license      {@link https://opensource.org/licenses/MIT|MIT License}
  */
