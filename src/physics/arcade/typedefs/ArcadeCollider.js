4e61b9fd13604480ecf3789573808c22da80ec1e
Richard Davey <rich@photonstorm.com>
2023-10-04 16:23:17 +0100

    Create ArcadeCollider.js

---
+++ src/physics/arcade/typedefs/ArcadeCollider.js
@@ -0,0 +1,6 @@
+/**
+ * An Arcade Physics Collider Type.
+ *
+ * @typedef {(Phaser.Physics.Arcade.Sprite|Phaser.Physics.Arcade.Image|Phaser.Physics.Arcade.StaticGroup|Phaser.Physics.Arcade.Group|Phaser.Tilemaps.TilemapLayer)} Phaser.Types.Physics.Arcade.ArcadeCollider
+ * @since 3.61.0
+ */
