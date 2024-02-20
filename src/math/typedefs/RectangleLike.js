4df43d7bed035ec21dfc7832c2be458fe01e2855
Richard Davey <rich@photonstorm.com>
2024-02-20 23:52:54 +0000

    `Math.RectangleLike` is a new typedef that defines a rectangle-like object with public `x`, `y`, `width` and `height` properties.

---
+++ src/math/typedefs/RectangleLike.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {object} Phaser.Types.Math.RectangleLike
+ * @since 3.80.0
+ *
+ * @property {number} x - The x component.
+ * @property {number} y - The y component.
+ * @property {number} width - The width component.
+ * @property {number} height - The height component.
+ */
