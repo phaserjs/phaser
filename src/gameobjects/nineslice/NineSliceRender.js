ff945e15953a2048b33997680569c03413086eaa
Richard Davey <rich@photonstorm.com>
2024-02-21 13:25:08 +0000

    Fix merge error

--- src/gameobjects/nineslice/NineSliceRender.js
+++ src/gameobjects/nineslice/NineSliceRender.js
@@ -13,14 +13,6 @@ if (typeof WEBGL_RENDERER)
     renderWebGL = require('./NineSliceWebGLRenderer');
 }
 
-<<<<<<< HEAD
-=======
-if (typeof CANVAS_RENDERER)
-{
-    // renderCanvas = require('./MeshCanvasRenderer');
-}
-
->>>>>>> parent of 7aaa976a4 (Updated webpack config DefinePlugin use and corresponding defs to allow those crazy souls who import source directly on node to have a better life #6644)
 module.exports = {
 
     renderWebGL: renderWebGL,
