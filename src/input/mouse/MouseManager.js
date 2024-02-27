229f9232f49c32a1a1fcdb9e3b95595799861607
Richard Davey <rich@photonstorm.com>
2024-02-27 15:36:23 +0000

    Modified `onMouseUpWindow` and `onMouseDownWindow` in the `MouseManager` so they now check for `sourceCapabilities.firesTouchEvents` and if found, abort registering the event. This new browser event property is designed to prevent you accidentally registering a Mouse Event when a Touch Event has just occurred (see https://developer.mozilla.org/en-US/docs/Web/API/InputDeviceCapabilities/firesTouchEvents) #6747

--- src/input/mouse/MouseManager.js
+++ src/input/mouse/MouseManager.js
@@ -397,6 +397,11 @@ var MouseManager = new Class({
 
         this.onMouseDownWindow = function (event)
         {
+            if (event.sourceCapabilities && event.sourceCapabilities.firesTouchEvents)
+            {
+                return;
+            }
+
             if (!event.defaultPrevented && _this.enabled && manager && manager.enabled && event.target !== canvas)
             {
                 //  Only process the event if the target isn't the canvas
@@ -419,6 +424,11 @@ var MouseManager = new Class({
 
         this.onMouseUpWindow = function (event)
         {
+            if (event.sourceCapabilities && event.sourceCapabilities.firesTouchEvents)
+            {
+                return;
+            }
+
             if (!event.defaultPrevented && _this.enabled && manager && manager.enabled && event.target !== canvas)
             {
                 //  Only process the event if the target isn't the canvas
