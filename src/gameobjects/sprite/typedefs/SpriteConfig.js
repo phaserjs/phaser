2f3a92b2d9100c8d3a14c3a86b2a2e58aca42511
Richard Davey <rich@photonstorm.com>
2024-06-07 16:29:33 +0100

    Added useSpriteSheet config property. Fix #6752

--- src/gameobjects/sprite/typedefs/SpriteConfig.js
+++ src/gameobjects/sprite/typedefs/SpriteConfig.js
@@ -6,4 +6,5 @@
  * @property {(string|Phaser.Textures.Texture)} [key] - The key, or instance of the Texture this Game Object will use to render with, as stored in the Texture Manager.
  * @property {(string|number)} [frame] - An optional frame from the Texture this Game Object is rendering with.
  * @property {(string|Phaser.Animations.Animation|Phaser.Types.Animations.PlayAnimationConfig)} [anims] - The string-based key of the animation to play, or an Animation instance, or a `PlayAnimationConfig` object.
+ * @property {boolean} [useSpriteSheet] - This property is used exclusively by `Tilemap.createFromTiles`. Set to `true` if this Sprite is being created by a Tilemap and is part of a spritesheet.
  */
