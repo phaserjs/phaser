a98f1f5713150198143aa32a14a8f8e3d11b0b77
samme <samme@users.noreply.github.com>
2024-07-18 19:03:08 -0700

    Docs: correct args in some scene events

--- src/scene/events/UPDATE_EVENT.js
+++ src/scene/events/UPDATE_EVENT.js
@@ -26,7 +26,6 @@
  * @type {string}
  * @since 3.0.0
  *
- * @param {Phaser.Scenes.Systems} sys - A reference to the Scene Systems class of the Scene that emitted this event.
  * @param {number} time - The current time. Either a High Resolution Timer value if it comes from Request Animation Frame, or Date.now if using SetTimeout.
  * @param {number} delta - The delta time in ms since the last frame. This is a smoothed and capped value based on the FPS rate.
  */
