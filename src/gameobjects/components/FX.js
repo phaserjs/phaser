389e2af60f852b389d52ec80a2158f9a75d78be7
Zeke Chan <zeke.chan.tk@gmail.com>
2024-07-23 16:50:00 +0800

    #6816 Phaser.GameObjects.Components.FX#add fix null pipeline pointer

--- src/gameobjects/components/FX.js
+++ src/gameobjects/components/FX.js
@@ -409,7 +409,10 @@ var FX = new Class({
                     pipeline = pipeline.pop();
                 }
 
-                pipeline.controller = fx;
+                if (pipeline)
+                {
+                    pipeline.controller = fx;
+                }
 
                 return fx;
             }
