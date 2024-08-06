d4d6a4c30831e9695e4c222d9f41e6fb40103f18
Richard Davey <rich@photonstorm.com>
2024-08-06 18:41:11 +0100

    Beta 3 tag

--- src/const.js
+++ src/const.js
@@ -20,7 +20,7 @@ var CONST = {
      * @type {string}
      * @since 3.0.0
      */
-    VERSION: '3.85.0 Beta 2',
+    VERSION: '3.85.0 Beta 3',
 
     BlendModes: require('./renderer/BlendModes'),
 
