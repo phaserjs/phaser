8b24e73d3d778ab7918e4eda1f464b71ca109127
Andres <costandres8@gmail.com>
2024-08-22 21:49:51 -0400

    fix(typedefs): fixing some typo error

--- src/cameras/2d/typedefs/JSONCamera.js
+++ src/cameras/2d/typedefs/JSONCamera.js
@@ -9,7 +9,7 @@
  * @property {number} height - The height size of camera
  * @property {number} zoom - The zoom of camera
  * @property {number} rotation - The rotation of camera
- * @property {boolean} roundPixels - The round pixels st status of camera
+ * @property {boolean} roundPixels - The round pixels indicate the status of the camera
  * @property {number} scrollX - The horizontal scroll of camera
  * @property {number} scrollY - The vertical scroll of camera
  * @property {string} backgroundColor - The background color of camera
