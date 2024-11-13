0123081aa9833a52bf10baee770326490808b9d5
Heeryong Kang <drakang4@gmail.com>
2024-11-13 12:08:22 +0900

    Fix stream parameter type of video.loadMediaStream

--- src/gameobjects/video/Video.js
+++ src/gameobjects/video/Video.js
@@ -689,7 +689,7 @@ var Video = new Class({
      * @method Phaser.GameObjects.Video#loadMediaStream
      * @since 3.50.0
      *
-     * @param {string} stream - The MediaStream object.
+     * @param {MediaStream} stream - The MediaStream object.
      * @param {boolean} [noAudio=false] - Does the video have an audio track? If not you can enable auto-playing on it.
      * @param {string} [crossOrigin] - The value to use for the `crossOrigin` property in the video load request.  Either undefined, `anonymous` or `use-credentials`. If no value is given, `crossorigin` will not be set in the request.
      *
