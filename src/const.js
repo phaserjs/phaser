4216d8def577041eec600449c5bac69c56a3de59
Richard Davey <rich@photonstorm.com>
2024-11-14 23:16:39 +0000

    Preparing for beta2

--- src/const.js
+++ src/const.js
@@ -20,7 +20,7 @@ var CONST = {
      * @type {string}
      * @since 3.0.0
      */
-    VERSION: '4.0.0 Beta 1',
+    VERSION: '4.0.0 Beta 2',
 
     /**
      * Phaser Release Version as displayed in the console.log header URL.
