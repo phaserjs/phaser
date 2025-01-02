354b573e064972ae92afc140ce163a84dfd29459
Richard Davey <rich@photonstorm.com>
2025-01-02 14:43:45 +0000

    The Texture Manager will now fail gracefully when a texture isn't created as a result of calling the `addBase64` method. Rather than the error "TypeError: null is not an object (evaluating 'texture.source')" is will not return early

--- src/textures/TextureManager.js
+++ src/textures/TextureManager.js
@@ -366,6 +366,11 @@ var TextureManager = new Class({
             {
                 var texture = _this.create(key, image);
 
+                if (!texture)
+                {
+                    return;
+                }
+                
                 Parser.Image(texture, 0);
 
                 _this.emit(Events.ADD, key, texture);
