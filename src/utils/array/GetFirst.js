60bbb565e55f960b7b0672bab557b55d9d623b3b
Zeke Chan <zeke.chan.tk@gmail.com>
2025-01-20 12:28:56 +0800

    Update GetFirst.js

--- src/utils/array/GetFirst.js
+++ src/utils/array/GetFirst.js
@@ -15,6 +15,7 @@ var SafeRange = require('./SafeRange');
  *
  * Optionally you can specify a start and end index. For example if the array had 100 elements,
  * and you set `startIndex` to 0 and `endIndex` to 50, it would search only the first 50 elements.
+ * You can also specify a negative `startIndex`, such as `-1`, which would start the search at the end of the array
  *
  * @function Phaser.Utils.Array.GetFirst
  * @since 3.4.0
@@ -22,7 +23,7 @@ var SafeRange = require('./SafeRange');
  * @param {array} array - The array to search.
  * @param {string} [property] - The property to test on each array element.
  * @param {*} [value] - The value to test the property against. Must pass a strict (`===`) comparison check.
- * @param {number} [startIndex=0] - An optional start index to search from.
+ * @param {number} [startIndex=0] - An optional start index to search from. You cn also set `startIndex` to -1 to start the search from the end of the array.
  * @param {number} [endIndex=array.length] - An optional end index to search up to (but not included)
  *
  * @return {?object} The first matching element from the array, or `null` if no element could be found in the range given.
@@ -34,8 +35,11 @@ var GetFirst = function (array, property, value, startIndex, endIndex)
 
     var i, child;
 
-    if (startIndex > endIndex)
+    if (startIndex === - 1)
     {
+        startIndex = array.length - 1;
+        if (endIndex === undefined) { endIndex = 0; }
+
         if (SafeRange(array, endIndex, startIndex))
         {
             for (i = startIndex; i > endIndex; i--)
