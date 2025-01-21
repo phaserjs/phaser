c145f730413f0d33624fac291c52914b6724d880
Zeke Chan <zeke.chan.tk@gmail.com>
2025-01-21 15:32:59 +0800

    Includes image collection offsets

--- src/tilemaps/components/RenderDebug.js
+++ src/tilemaps/components/RenderDebug.js
@@ -42,10 +42,11 @@ var RenderDebug = function (graphics, styleConfig, layer)
     {
         var tile = tiles[i];
 
+        var offset = tile.tileset ? tile.tileset.tileOffset : { x: 0, y: 0 };
         var tw = tile.width;
         var th = tile.height;
-        var x = tile.pixelX;
-        var y = tile.pixelY;
+        var x = tile.pixelX - offset.x;
+        var y = tile.pixelY - offset.y;
 
         var color = tile.collides ? collidingTileColor : tileColor;
 
