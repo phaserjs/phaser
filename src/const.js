9c2462f244cb9cf44b1f1a35b05db9ead438677c
Richard Davey <rich@photonstorm.com>
2025-04-21 10:13:02 +0100

    v4.0.0-rc.2

--- src/const.js
+++ src/const.js
@@ -20,7 +20,7 @@ var CONST = {
      * @type {string}
      * @since 3.0.0
      */
-    VERSION: '4.0.0 RC1',
+    VERSION: '4.0.0 RC2',
 
     /**
      * Phaser Release Version as displayed in the console.log header URL.
